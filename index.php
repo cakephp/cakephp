bb71238ec1b8f14fc3c267972822244c17f5081c
phpnut <phpnut@cakephp.org>
2005-05-16 00:52:42 +0000

    change license to The MIT License
    git-svn-id: https://svn.cakephp.org/repo/trunk/cake@109 3807eeeb-6ff5-0310-8944-8be069107fe0

--- index.php
+++ index.php
@@ -2,13 +2,20 @@
 //////////////////////////////////////////////////////////////////////////
 // + $Id$
 // +------------------------------------------------------------------+ //
-// + Cake <http://sputnik.pl/cake>                                    + //
-// + Copyright: (c) 2005 Michal Tatarynowicz                          + //
+// + Cake <https://developers.nextco.com/cake/>                       + //
+// + Copyright: (c) 2005 Cake Authors/Developers                      + //
 // +                                                                  + //
-// + Author(s): (c) 2005 Michal Tatarynowicz <tatarynowicz@gmail.com> + //
+// + Author(s): Michal Tatarynowicz aka Pies <tatarynowicz@gmail.com> + //
+// +            Larry E. Masters aka PhpNut <nut@phpnut.com>          + //
+// +            Kamil Dzielinski aka Brego <brego.dk@gmail.com>       + //
 // +                                                                  + //
 // +------------------------------------------------------------------+ //
-// + Licensed under the Public Domain Licence                         + //
+// + Licensed under The MIT License                                   + //
+// + Redistributions of files must retain the above copyright notice. + //
+// + You may not use this file except in compliance with the License. + //
+// +                                                                  + //
+// + You may obtain a copy of the License at:                         + //
+// + License page: http://www.opensource.org/licenses/mit-license.php + //
 // +------------------------------------------------------------------+ //
 //////////////////////////////////////////////////////////////////////////
 
@@ -16,14 +23,17 @@
   * Enter description here...
   * 
   * @filesource 
-  * @modifiedby $LastChangedBy$  
-  * @lastmodified $Date$
   * @author Michal Tatarynowicz <tatarynowicz@gmail.com>
-  * @copyright Copyright (c) 2005, Michal Tatarynowicz <tatarynowicz@gmail.com>
+  * @author Larry E. Masters aka PhpNut <nut@phpnut.com>
+  * @author Kamil Dzielinski aka Brego <brego.dk@gmail.com>
+  * @copyright Copyright (c) 2005, Cake Authors/Developers
+  * @link https://developers.nextco.com/cake/wiki/Authors Authors/Developers
   * @package cake
   * @since Cake v 0.2.9
   * @version $Revision$
-  * @license Public_Domain
+  * @modifiedby $LastChangedBy$
+  * @lastmodified $Date$
+  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
   *
   */
 
