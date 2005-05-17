a04f96df6b78720d57b95f0f28f05264edbc65cf
phpnut <phpnut@cakephp.org>
2005-05-17 21:27:56 +0000

    modifying file headers
    git-svn-id: https://svn.cakephp.org/repo/trunk/cake@130 3807eeeb-6ff5-0310-8944-8be069107fe0

--- index.php
+++ index.php
@@ -3,29 +3,21 @@
 // + $Id$
 // +------------------------------------------------------------------+ //
 // + Cake <https://developers.nextco.com/cake/>                       + //
-// + Copyright: (c) 2005 Cake Authors/Developers                      + //
-// +                                                                  + //
+// + Copyright: (c) 2005, Cake Authors/Developers                     + //
 // + Author(s): Michal Tatarynowicz aka Pies <tatarynowicz@gmail.com> + //
 // +            Larry E. Masters aka PhpNut <nut@phpnut.com>          + //
 // +            Kamil Dzielinski aka Brego <brego.dk@gmail.com>       + //
-// +                                                                  + //
 // +------------------------------------------------------------------+ //
 // + Licensed under The MIT License                                   + //
 // + Redistributions of files must retain the above copyright notice. + //
-// + You may not use this file except in compliance with the License. + //
-// +                                                                  + //
-// + You may obtain a copy of the License at:                         + //
-// + License page: http://www.opensource.org/licenses/mit-license.php + //
-// +------------------------------------------------------------------+ //
+// + See: http://www.opensource.org/licenses/mit-license.php          + //
 //////////////////////////////////////////////////////////////////////////
 
 /**
   * Enter description here...
   * 
   * @filesource 
-  * @author Michal Tatarynowicz <tatarynowicz@gmail.com>
-  * @author Larry E. Masters aka PhpNut <nut@phpnut.com>
-  * @author Kamil Dzielinski aka Brego <brego.dk@gmail.com>
+  * @author Cake Authors/Developers
   * @copyright Copyright (c) 2005, Cake Authors/Developers
   * @link https://developers.nextco.com/cake/wiki/Authors Authors/Developers
   * @package cake
@@ -34,7 +26,6 @@
   * @modifiedby $LastChangedBy$
   * @lastmodified $Date$
   * @license http://www.opensource.org/licenses/mit-license.php The MIT License
-  *
   */
 
 /**
