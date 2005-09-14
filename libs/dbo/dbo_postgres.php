3c97ea84748f3c5f1b0f84e5ab210579f8299579
phpnut <phpnut@cakephp.org>
2005-09-14 04:11:56 +0000

    merging changes from [816]
    git-svn-id: https://svn.cakephp.org/repo/trunk/cake@819 3807eeeb-6ff5-0310-8944-8be069107fe0

--- libs/dbo/dbo_postgres.php
+++ libs/dbo/dbo_postgres.php
@@ -81,32 +81,32 @@ class DBO_Postgres extends DBO
       return pg_close($this->_conn);
    }
 
-	/**
-	  * Executes given SQL statement.
-	  *
-	  * @param string $sql SQL statement
-	  * @return resource Result resource identifier
-	  */
+/**
+  * Executes given SQL statement.
+  *
+  * @param string $sql SQL statement
+  * @return resource Result resource identifier
+  */
    function execute ($sql) 
    {
       return pg_query($this->_conn, $sql);
    }
 
-	/**
- 	 * Returns a row from given resultset as an array .
-	  *
-	  * @return array The fetched row as an array
-	  */
+/**
+  * Returns a row from given resultset as an array .
+  *
+  * @return array The fetched row as an array
+  */
    function fetchRow () 
    {
        return pg_fetch_array($this->_result);
    }
 
-	/**
-	  * Returns an array of tables in the database. If there are no tables, an error is raised and the application exits.
-	  *
-	  * @return array Array of tablenames in the database
-	  */
+/**
+  * Returns an array of tables in the database. If there are no tables, an error is raised and the application exits.
+  *
+  * @return array Array of tablenames in the database
+  */
    function tablesList () 
    {
       $sql = "SELECT a.relname AS name
@@ -130,12 +130,12 @@ class DBO_Postgres extends DBO
       }
    }
 
-	/**
-	  * Returns an array of the fields in given table name.
-	  *
-	  * @param string $table_name Name of database table to inspect
-	  * @return array Fields in table. Keys are name and type
-	  */
+/**
+  * Returns an array of the fields in given table name.
+  *
+  * @param string $table_name Name of database table to inspect
+  * @return array Fields in table. Keys are name and type
+  */
    function fields ($table_name)
    {
       $sql = "SELECT c.relname, a.attname, t.typname FROM pg_class c, pg_attribute a, pg_type t WHERE c.relname = '{$table_name}' AND a.attnum > 0 AND a.attrelid = c.oid AND a.atttypid = t.oid";
@@ -150,56 +150,56 @@ class DBO_Postgres extends DBO
       return $fields;
    }
 
-	/**
-	  * Returns a quoted and escaped string of $data for use in an SQL statement.
-	  *
-	  * @param string $data String to be prepared for use in an SQL statement
-	  * @return string Quoted and escaped
-	  */
+/**
+  * Returns a quoted and escaped string of $data for use in an SQL statement.
+  *
+  * @param string $data String to be prepared for use in an SQL statement
+  * @return string Quoted and escaped
+  */
    function prepareValue ($data)
    {
-      return "'" . pg_escape_string($data) . "'";
+      return "'".pg_escape_string($data)."'";
    }
 
-	/**
-	  * Returns a formatted error message from previous database operation.
-	  *
-	  * @return string Error message
-	  */
+/**
+  * Returns a formatted error message from previous database operation.
+  *
+  * @return string Error message
+  */
    function lastError () 
    {
       $last_error = pg_last_error($this->_conn);
       return $last_error? $last_error: null;
    }
 
-	/**
-	  * Returns number of affected rows in previous database operation. If no previous operation exists, this returns false.
-	  *
-	  * @return int Number of affected rows
-	  */
+/**
+  * Returns number of affected rows in previous database operation. If no previous operation exists, this returns false.
+  *
+  * @return int Number of affected rows
+  */
    function lastAffected ()
    {
       return $this->_result? pg_affected_rows($this->_result): false;
    }
 
-	/**
-	  * Returns number of rows in previous resultset. If no previous resultset exists, 
-	  * this returns false.
-	  *
-	  * @return int Number of rows in resultset
-	  */
+/**
+  * Returns number of rows in previous resultset. If no previous resultset exists, 
+  * this returns false.
+  *
+  * @return int Number of rows in resultset
+  */
    function lastNumRows () 
    {
       return $this->_result? pg_num_rows($this->_result): false;
    }
 
-	/**
-	  * Returns the ID generated from the previous INSERT operation.
-	  *
-	  * @param string $table Name of the database table
-	  * @param string $field Name of the ID database field. Defaults to "id"
-	  * @return int 
-	  */
+/**
+  * Returns the ID generated from the previous INSERT operation.
+  *
+  * @param string $table Name of the database table
+  * @param string $field Name of the ID database field. Defaults to "id"
+  * @return int 
+  */
    function lastInsertId ($table, $field='id') 
    {
       $sql = "SELECT CURRVAL('{$table}_{$field}_seq') AS max";
@@ -208,13 +208,13 @@ class DBO_Postgres extends DBO
       return $data['max'];
    }
 
-	/**
-	 * Returns a limit statement in the correct format for the particular database.
-	 *
-	 * @param int $limit Limit of results returned
-	 * @param int $offset Offset from which to start results
-	 * @return string SQL limit/offset statement
-	 */
+/**
+ * Returns a limit statement in the correct format for the particular database.
+ *
+ * @param int $limit Limit of results returned
+ * @param int $offset Offset from which to start results
+ * @return string SQL limit/offset statement
+ */
    function selectLimit ($limit, $offset=null)
    {
       return " LIMIT {$limit}".($offset? " OFFSET {$offset}": null);
