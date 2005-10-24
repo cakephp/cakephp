dd6024b1c46bcee531a1fe6ed113e07e116bd052
phpnut <phpnut@cakephp.org>
2005-10-24 03:57:58 +0000

    [1185] Author: phpnut Date: 10:55:53 PM, Sunday, October 23, 2005 Message: fixed text call issues with routes
    git-svn-id: https://svn.cakephp.org/repo/trunk/cake@1186 3807eeeb-6ff5-0310-8944-8be069107fe0

--- app/config/routes.php
+++ app/config/routes.php
@@ -38,17 +38,17 @@
  * its action called 'display', and we pass a param to select the view file 
  * to use (in this case, /app/views/pages/home.thtml)...
  */
-$Route->connect ('/', array('controller'=>'Pages', 'action'=>'display', 'home'));
+$Route->connect ('/', array('controller'=>'pages', 'action'=>'display', 'home'));
 
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
-$Route->connect ('/pages/*', array('controller'=>'Pages', 'action'=>'display'));
+$Route->connect ('/pages/*', array('controller'=>'pages', 'action'=>'display'));
 
 /**
  * Then we connect url '/test' to our test controller. This is helpfull in
  * developement.
  */
-$Route->connect ('/tests', array('controller'=>'Tests', 'action'=>'index'));
+$Route->connect ('/tests', array('controller'=>'tests', 'action'=>'index'));
 
 ?>
