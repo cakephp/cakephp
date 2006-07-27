c50275de246aa18729f343177a73df23bb5f9d49
nate <nate@cakephp.org>
2006-07-27 04:12:24 +0000

    Cleaning up TextHelper code
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@3303 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/libs/view/helpers/text.php
+++ cake/libs/view/helpers/text.php
@@ -32,12 +32,9 @@
  * Included libraries.
  *
  */
-if (!class_exists('Flay')) {
-	 uses('flay');
-}
 
 if (!class_exists('Html')) {
-	 uses(DS . 'view' . DS . 'helpers' . DS . 'html');
+	uses(DS . 'view' . DS . 'helpers' . DS . 'html');
 }
 
 /**
@@ -48,7 +45,8 @@ if (!class_exists('Html')) {
  * @package		cake
  * @subpackage	cake.cake.libs.view.helpers
  */
-class TextHelper extends Helper{
+class TextHelper extends Helper {
+
 /**
  * Highlights a given phrase in a text.
  *
@@ -57,40 +55,39 @@ class TextHelper extends Helper{
  * @param string $highlighter The piece of html with that the phrase will be highlighted
  * @return string The highlighted text
  */
-	 function highlight($text, $phrase, $highlighter = '<span class="highlight">\1</span>') {
-		  if (empty($phrase))
-				return $text;
+	function highlight($text, $phrase, $highlighter = '<span class="highlight">\1</span>') {
+		if (empty($phrase)) {
+			return $text;
+		}
 
-		  if (is_array($phrase)) {
-				$replace=array();
-				$with=array();
+		if (is_array($phrase)) {
+			$replace = array();
+			$with = array();
 
-				foreach($phrase as $key => $value) {
-					 if (empty($key)) {
-						  $key  =$value;
-						  $value=$highlighter;
-					 }
-
-					 $replace[]='|(' . $key . ')|';
-					 $with[]=empty($value) ? $highlighter : $value;
+			foreach($phrase as $key => $value) {
+				if (empty($key)) {
+					$key = $value;
+					$value = $highlighter;
 				}
 
-				return preg_replace($replace, $with, $text);
-		  } else {
-				return preg_replace("|({$phrase})|i", $highlighter, $text);
-		  }
-	 }
+				$replace[] = '|(' . $key . ')|';
+				$with[] = empty($value) ? $highlighter : $value;
+			}
 
+			return preg_replace($replace, $with, $text);
+		} else {
+			return preg_replace("|({$phrase})|i", $highlighter, $text);
+		}
+	}
 /**
  * Strips given text of all links (<a href=....)
  *
  * @param string $text Text
  * @return string The text without links
  */
-	 function stripLinks($text) {
-		  return preg_replace('|<a.*>(.*)<\/a>|im', '\1', $text);
-	 }
-
+	function stripLinks($text) {
+		return preg_replace('|<a.*>(.*)<\/a>|im', '\1', $text);
+	}
 /**
  * Adds links (<a href=....) to a given text, by finding text that begins with
  * strings like http:// and ftp://.
@@ -99,25 +96,20 @@ class TextHelper extends Helper{
  * @param array $htmlOptions Array of HTML options.
  * @return string The text with links
  */
-	 function autoLinkUrls($text, $htmlOptions = array()) {
-		  $options='array(';
+	function autoLinkUrls($text, $htmlOptions = array()) {
+		$options = 'array(';
 
-		  foreach($htmlOptions as $option => $value) {
+		foreach($htmlOptions as $option => $value) {
 				$options .= "'$option' => '$value', ";
-		  }
+		}
+		$options .= ')';
 
-		  $options .= ')';
+		$text = preg_replace_callback('#((?:http|https|ftp|nntp)://[^ ]+)#', create_function('$matches',
+			'$Html = new HtmlHelper(); $Html->tags = $Html->loadConfig(); return $Html->linkOut($matches[0], $matches[0],' . $options . ');'), $text);
 
-		  $text=preg_replace_callback('#((?:http|https|ftp|nntp)://[^ ]+)#',
-												create_function('$matches',
-																	 '$Html = new HtmlHelper(); $Html->tags = $Html->loadConfig(); return $Html->linkOut($matches[0], $matches[0],' . $options . ');'),
-												$text);
-		  return preg_replace_callback('#(?<!http://|https://|ftp://|nntp://)(www\.[^\n\%\ ]+[^\n\%\,\.\ ])#',
-												 create_function('$matches',
-																	  '$Html = new HtmlHelper(); $Html->tags = $Html->loadConfig(); return $Html->linkOut($matches[0], "http://" . $matches[0],' . $options . ');'),
-												 $text);
-	 }
-
+		return preg_replace_callback('#(?<!http://|https://|ftp://|nntp://)(www\.[^\n\%\ ]+[^\n\%\,\.\ ])#',
+			create_function('$matches', '$Html = new HtmlHelper(); $Html->tags = $Html->loadConfig(); return $Html->linkOut($matches[0], "http://" . $matches[0],' . $options . ');'), $text);
+	}
 /**
  * Adds email links (<a href="mailto:....) to a given text.
  *
@@ -125,22 +117,17 @@ class TextHelper extends Helper{
  * @param array $htmlOptions Array of HTML options.
  * @return string The text with links
  */
-	 function autoLinkEmails($text, $htmlOptions = array()) {
-		  $options='array(';
+	function autoLinkEmails($text, $htmlOptions = array()) {
+		$options = 'array(';
 
-		  foreach($htmlOptions as $option => $value) {
-				$options .= "'$option' => '$value', ";
-		  }
+		foreach($htmlOptions as $option => $value) {
+			$options .= "'$option' => '$value', ";
+		}
+		$options .= ')';
 
-		  $options .= ')';
-
-		  return preg_replace_callback(
-						'#([_A-Za-z0-9+-+]+(?:\.[_A-Za-z0-9+-]+)*@[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*)#',
-						create_function('$matches',
-											 '$Html = new HtmlHelper(); $Html->tags = $Html->loadConfig(); return $Html->linkEmail($matches[0], $matches[0],' . $options . ');'),
-						$text);
-	 }
-
+		return preg_replace_callback('#([_A-Za-z0-9+-+]+(?:\.[_A-Za-z0-9+-]+)*@[A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*)#',
+						create_function('$matches', '$Html = new HtmlHelper(); $Html->tags = $Html->loadConfig(); return $Html->linkEmail($matches[0], $matches[0],' . $options . ');'), $text);
+	}
 /**
  * Convert all links and email adresses to HTML links.
  *
@@ -148,10 +135,9 @@ class TextHelper extends Helper{
  * @param array $htmlOptions Array of HTML options.
  * @return string The text with links
  */
-	 function autoLink($text, $htmlOptions = array()) {
-		  return $this->autoLinkEmails($this->autoLinkUrls($text, $htmlOptions), $htmlOptions);
-	 }
-
+	function autoLink($text, $htmlOptions = array()) {
+		return $this->autoLinkEmails($this->autoLinkUrls($text, $htmlOptions), $htmlOptions);
+	}
 /**
  * Truncates text.
  *
@@ -164,35 +150,31 @@ class TextHelper extends Helper{
  * @param boolean $exact If false, $test will not be cut mid-word
  * @return string Trimmed string.
  */
-	 function truncate($text, $length, $ending = '...', $exact = true) {
-		  if (strlen($text) <= $length) {
-				return $text;
-		  } else {
-				$truncate=substr($text, 0, $length - strlen($ending));
+	function truncate($text, $length, $ending = '...', $exact = true) {
+		if (strlen($text) <= $length) {
+			return $text;
+		} else {
+			$truncate = substr($text, 0, $length - strlen($ending));
 
-				if (!$exact) {
-					 $spacepos=strrpos($truncate, ' ');
+			if (!$exact) {
+				$spacepos = strrpos($truncate, ' ');
 
-					 if (isset($spacepos)) {
-						  return substr($truncate, 0, $spacepos) . $ending;
-					 }
+				if (isset($spacepos)) {
+					return substr($truncate, 0, $spacepos) . $ending;
 				}
-
-				return $truncate . $ending;
-		  }
-	 }
-
+			}
+			return $truncate . $ending;
+		}
+	}
 /**
  * Alias for truncate().
  *
  * @see TextHelper::truncate()
  */
-	 function trim() {
-		  $args=func_get_args();
-		  return call_user_func_array(array(&$this,
-					  "truncate"),       $args);
-	 }
-
+	function trim() {
+		$args = func_get_args();
+		return call_user_func_array(array(&$this, 'truncate'), $args);
+	}
 /**
  * Extracts an excerpt from the text surrounding the phrase with a number of characters on each side determined by radius.
  *
@@ -202,29 +184,30 @@ class TextHelper extends Helper{
  * @param string $ending Ending that will be appended
  * @return string Enter description here...
  */
-	 function excerpt($text, $phrase, $radius = 100, $ending = "...") {
-		  if (empty($text) or empty($phrase))
-				return $this->truncate($text, $radius * 2, $ending);
+	function excerpt($text, $phrase, $radius = 100, $ending = "...") {
+		if (empty($text) or empty($phrase)) {
+			return $this->truncate($text, $radius * 2, $ending);
+		}
 
-		  if ($radius < strlen($phrase))
-				$radius=strlen($phrase);
+		if ($radius < strlen($phrase))
+			$radius = strlen($phrase);
+		}
 
-		  $pos     =strpos($text, $phrase);
-		  $startPos=$pos <= $radius ? 0 : $pos - $radius;
-		  $endPos  =$pos + strlen($phrase) + $radius >= strlen($text)
-			  ? strlen($text) : $pos + strlen($phrase) + $radius;
+		$pos = strpos($text, $phrase);
+		$startPos = $pos <= $radius ? 0 : $pos - $radius;
+		$endPos = $pos + strlen($phrase) + $radius >= strlen($text) ? strlen($text) : $pos + strlen($phrase) + $radius;
+		$excerpt = substr($text, $startPos, $endPos - $startPos);
 
-		  $excerpt =substr($text, $startPos, $endPos - $startPos);
+		if ($startPos != 0) {
+			$excerpt = substr_replace($excerpt, $ending, 0, strlen($phrase));
+		}
 
-		  if ($startPos != 0)
-				$excerpt=substr_replace($excerpt, $ending, 0, strlen($phrase));
+		if ($endPos != strlen($text)) {
+			$excerpt = substr_replace($excerpt, $ending, -strlen($phrase));
+		}
 
-		  if ($endPos != strlen($text))
-				$excerpt=substr_replace($excerpt, $ending, -strlen($phrase));
-
-		  return $excerpt;
-	 }
-
+		return $excerpt;
+	}
 /**
  * Text-to-html parser, similar to Textile or RedCloth, only with a little different syntax.
  *
@@ -233,8 +216,11 @@ class TextHelper extends Helper{
  * @return string "Flayed" text
  * @todo Change this. We need a real Textile parser.
  */
-	 function flay($text, $allowHtml = false) {
-		  return Flay::toHtml($text, false, $allowHtml);
-	 }
+	function flay($text, $allowHtml = false) {
+		if (!class_exists('Flay')) {
+			uses('flay');
+		}
+		return Flay::toHtml($text, false, $allowHtml);
+	}
 }
 ?>
