58f1dcd0ffe65c5b01c2d9f85c8f204672180711
nate <nate@cakephp.org>
2006-10-26 17:31:46 +0000

    Adding install task for bake
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@3745 3807eeeb-6ff5-0310-8944-8be069107fe0

---
+++ cake/scripts/tasks/install_task.php
@@ -0,0 +1,38 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * The InstallTask installs Cake packages from remote sources.
+ *
+ * Long description for file
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP :  Rapid Development Framework <http://www.cakephp.org/>
+ * Copyright (c)	2005, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright		Copyright (c) 2005, Cake Software Foundation, Inc.
+ * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP Project
+ * @package			cake
+ * @subpackage		cake.cake.scripts.bake
+ * @since			CakePHP v 1.2
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+class InstallTask extends BakeTask {
+
+	function execute($params) {
+	}
+	
+	function help() {
+	}
+}
+
+?>
