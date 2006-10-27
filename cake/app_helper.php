094ed0e0d5557e65dc3f02f84369757d4063748f
nate <nate@cakephp.org>
2006-10-27 17:10:13 +0000

    Adding AppHelper shell class
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@3747 3807eeeb-6ff5-0310-8944-8be069107fe0

---
+++ cake/app_helper.php
@@ -0,0 +1,42 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Short description for file.
+ *
+ * This file is application-wide helper file. You can put all
+ * application-wide helper-related methods here.
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP :  Rapid Development Framework <http://www.cakephp.org/>
+ * Copyright (c)	2006, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright		Copyright (c) 2006, Cake Software Foundation, Inc.
+ * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP Project
+ * @package			cake
+ * @subpackage		cake.cake
+ * @since			CakePHP v 0.2.9
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+/**
+ * This is a placeholder class.
+ * Create the same file in app/app_helper.php
+ *
+ * Add your application-wide methods in the class below, your helpers
+ * will inherit them.
+ *
+ * @package		cake
+ * @subpackage	cake.cake
+ */
+class AppHelper extends Helper {
+}
+?>
