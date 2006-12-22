238ddacded6bccc2a923fcfdf2b68dc3db9acf9e
phpnut <phpnut@cakephp.org>
2006-12-22 20:30:09 +0000

    Cleaning up translations model
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@4124 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/libs/model/i18n/i18n.sql
+++ cake/libs/model/i18n/i18n.sql
@@ -1,18 +1,18 @@
-CREATE TABLE `i18n` (
-  `id` int(11) NOT NULL auto_increment,
-  `locale` varchar(8) NOT NULL default '',
-  `i18n_content_id` int(11) NOT NULL default '0',
-  `model` varchar(255) NOT NULL default '',
-  `row_id` int(11) NOT NULL default '0',
-  `field` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`id`),
-  KEY `row_id` (`row_id`),
-  KEY `model` (`model`),
-  KEY `field` (`field`)
+CREATE TABLE i18n (
+  id int(10) NOT NULL auto_increment,
+  locale varchar(6) NOT NULL,
+  i18n_content_id int(10) NOT NULL,
+  model varchar(255) NOT NULL,
+  row_id int(10) NOT NULL,
+  field varchar(255) NOT NULL,
+  PRIMARY KEY  (id),
+  KEY row_id (row_id),
+  KEY model (model),
+  KEY field (field)
 );
 
-CREATE TABLE `i18n_content` (
-  `id` int(11) NOT NULL auto_increment,
-  `content` text,
-  PRIMARY KEY  (`id`)
+CREATE TABLE i18n_content (
+  id int(10) NOT NULL auto_increment,
+  content text,
+  PRIMARY KEY  (id)
 );
