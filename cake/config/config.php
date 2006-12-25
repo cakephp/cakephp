3fb74c2796faa2286868a95b73c77ed37a50971c
phpnut <phpnut@cakephp.org>
2006-12-25 12:44:31 +0000

    Updating version files for 1.2.x.x branch
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@4206 3807eeeb-6ff5-0310-8944-8be069107fe0

---
+++ cake/config/config.php
@@ -0,0 +1,28 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Core Configurations.
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP :  Rapid Development Framework <http://www.cakephp.org/>
+ * Copyright (c)	2006, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright		Copyright (c) 2006, Cake Software Foundation, Inc.
+ * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP Project
+ * @package			cake
+ * @subpackage		cake.app.config
+ * @since			CakePHP v 1.1.11.4062
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+$config['Cake.version'] = '1.2.0.4206';
+?>
