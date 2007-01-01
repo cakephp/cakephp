929539146e298eb6024e9a5e9ca0a70ec186655e
phpnut <phpnut@cakephp.org>
2007-01-01 22:03:15 +0000

    Adding skeleton of translation string extractor class
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@4243 3807eeeb-6ff5-0310-8944-8be069107fe0

---
+++ cake/scripts/extract.php
@@ -0,0 +1,68 @@
+#!/usr/bin/php -q
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Short description for file.
+ *
+ * Long description for file
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP :  Rapid Development Framework <http://www.cakephp.org/>
+ * Copyright (c)	2005, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright		Copyright (c) 2005, Cake Software Foundation, Inc.
+ * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP Project
+ * @package			cake
+ * @subpackage		cake.cake.scripts
+ * @since			CakePHP v 1.2.0.4243
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+/**
+ * Enter description here...
+ *
+ * @package		cake
+ * @subpackage	cake.cake.scripts
+ */
+class I18nExtractor {
+	var $stdin;
+	var $stdout;
+	var $stderr;
+
+	function __construct() {
+		$this->stdin = fopen('php://stdin', 'r');
+		$this->stdout = fopen('php://stdout', 'w');
+		$this->stderr = fopen('php://stderr', 'w');
+	}
+
+	function I18nExtractor() {
+		return $this->__construct();
+	}
+
+	function stdout($string, $newline = true) {
+		if ($newline) {
+			fwrite($this->stdout, $string . "\n");
+		} else {
+			fwrite($this->stdout, $string);
+		}
+	}
+
+	function stderr($string) {
+		fwrite($this->stderr, $string, true);
+	}
+
+	function hr() {
+		$this->stdout('---------------------------------------------------------------');
+	}
+}
+
+?>
