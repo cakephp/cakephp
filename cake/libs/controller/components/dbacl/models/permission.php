6ec10af951f260a420a5064b6fcea167326d0499
nate <nate@cakephp.org>
2007-02-09 08:29:07 +0000

    Adding Permission join model for ACL nodes
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@4488 3807eeeb-6ff5-0310-8944-8be069107fe0

---
+++ cake/libs/controller/components/dbacl/models/permission.php
@@ -0,0 +1,62 @@
+<?php
+/* SVN FILE: $Id$ */
+
+/**
+ * Short description for file.
+ *
+ * Long description for file
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) :  Rapid Development Framework <http://www.cakephp.org/>
+ * Copyright 2005-2007, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright		Copyright 2005-2007, Cake Software Foundation, Inc.
+ * @link				http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
+ * @package			cake
+ * @subpackage		cake.cake.libs.controller.components.dbacl.models
+ * @since			CakePHP(tm) v 0.10.0.1232
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+
+/**
+ * Short description for file.
+ *
+ * Long description for file
+ *
+ * @package		cake
+ * @subpackage	cake.cake.libs.controller.components.dbacl.models
+ */
+
+class Permission extends AppModel {
+
+	var $useDbConfig = ACL_DATABASE;
+/**
+ * Enter description here...
+ *
+ * @var unknown_type
+ */
+	var $cacheQueries = false;
+/**
+ * Enter description here...
+ *
+ * @var unknown_type
+ */
+	 var $name = 'Permission';
+/**
+ * Enter description here...
+ *
+ * @var unknown_type
+ */
+	 var $useTable = 'aros_acos';
+}
+?>
