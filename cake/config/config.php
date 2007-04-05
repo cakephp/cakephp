a677c882e7c09f3e7a22f589ffbbebf7892f4257
phpnut <phpnut@cakephp.org>
2007-04-05 19:23:46 +0000

    Merging fixes into the trunk:
    Revision: [4789]
    Fixing last commit, had removed the ! prior to committing
    
    Revision: [4788]
    Adding fix for duplicate conditions being merged into the final query on self associations
    
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@4791 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/config/config.php
+++ cake/config/config.php
@@ -24,5 +24,5 @@
  * @lastmodified	$Date$
  * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-$config['Cake.version'] = '1.2.0.4787alpha';
+$config['Cake.version'] = '1.2.0.4791alpha';
 ?>
