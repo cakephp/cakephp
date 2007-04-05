56b7050858f4354840eb1abaa3ea15fd1b669295
phpnut <phpnut@cakephp.org>
2007-04-05 04:22:42 +0000

    Removing old AclNodeTest methods this code is no longer valid on 1.2
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@4774 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/tests/cases/libs/controller/components/dbacl/models/aclnode.test.php
+++ cake/tests/cases/libs/controller/components/dbacl/models/aclnode.test.php
@@ -30,7 +30,7 @@
 		define('CAKEPHP_UNIT_TEST_EXECUTION', 1);
 	}
 	require_once LIBS.'controller'.DS.'components'.DS.'dbacl'.DS.'models'.DS.'aclnode.php';
-	
+
 	/**
 	 * Short description for class.
 	 *
@@ -40,7 +40,7 @@
 	class AclNodeTestBase extends AclNode {
 		var $useDbConfig = 'test_suite';
 	}
-	
+
 	/**
 	 * Short description for class.
 	 *
@@ -52,7 +52,7 @@
 		var $useTable = 'aros';
 		var $hasAndBelongsToMany = array('AcoTest' => array('with' => 'PermissionTest'));
 	}
-	
+
 	/**
 	 * Short description for class.
 	 *
@@ -64,7 +64,7 @@
 		var $useTable = 'acos';
 		var $hasAndBelongsToMany = array('AroTest' => array('with' => 'PermissionTest'));
 	}
-	
+
 	/**
 	 * Short description for class.
 	 *
@@ -78,7 +78,7 @@
 		var $belongsTo = 'AroTest,AcoTest';
 		var $actsAs = null;
 	}
-	
+
 	/**
 	 * Short description for class.
 	 *
@@ -90,7 +90,7 @@
 		var $useTable = 'aco_actions';
 		var $belongsTo = 'AcoTest';
 	}
-	
+
 /**
  * Short description for class.
  *
@@ -99,30 +99,8 @@
  */
 class AclNodeTest extends CakeTestCase {
 	var $fixtures = array( 'core.aro', 'core.aco', 'core.aros_aco', 'core.aco_action' );
-	
+
 	function testNodeNesting() {
-		$this->Aro =& new AroTest();
-		
-		$this->Aro->create(1, null, 'Food');
-		$this->Aro->create(2, null, 'Fruit');
-		$this->Aro->create(3, null, 'Red');
-		$this->Aro->create(4, null, 'Cherry');
-		$this->Aro->create(5, null, 'Yellow');
-		$this->Aro->create(6, null, 'Banana');
-		$this->Aro->create(7, null, 'Meat');
-		$this->Aro->create(8, null, 'Beef');
-		$this->Aro->create(9, null, 'Pork');
-
-		$this->Aro->setParent('Food', 'Meat');
-		$this->Aro->setParent('Food', 'Fruit');
-
-		$this->Aro->setParent('Fruit', 'Yellow');
-		$this->Aro->setParent('Yellow', 'Banana');
-		$this->Aro->setParent('Fruit', 'Red');
-		$this->Aro->setParent('Red', 'Cherry');
-
-		$this->Aro->setParent('Meat', 'Pork');
-		$this->Aro->setParent('Meat', 'Beef');
 	}
 }
 
