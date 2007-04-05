df78e1154875669fb09deb991b76c5d4148f5a67
mariano.iglesias <mariano.iglesias@3807eeeb-6ff5-0310-8944-8be069107fe0>
2007-04-05 06:08:00 +0000

    Adding headers
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@4780 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/tests/cases/libs/set.test.php
+++ cake/tests/cases/libs/set.test.php
@@ -1,11 +1,37 @@
 <?php
+/* SVN FILE: $Id$ */
+/**
+ * Short description for file.
+ *
+ * Long description for file
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) Tests <https://trac.cakephp.org/wiki/Developement/TestSuite>
+ * Copyright 2005-2007, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ *  Licensed under The Open Group Test Suite License
+ *  Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright		Copyright 2005-2007, Cake Software Foundation, Inc.
+ * @link				https://trac.cakephp.org/wiki/Developement/TestSuite CakePHP(tm) Tests
+ * @package			cake.tests
+ * @subpackage		cake.tests.cases.libs.model
+ * @since			CakePHP(tm) v 1.2.0.4206
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://www.opensource.org/licenses/opengroup.php The Open Group Test Suite License
+ */
 
 // Include the class to be tested
 uses('set');
 
 /**
  * UnitTestCase for the Set class
- *
  * 
  * @package		cake
  * @subpackage	cake.cake.libs
