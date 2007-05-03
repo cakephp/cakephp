383c8d36d33b3d17974aaef7b607472007fbd54d
mariano.iglesias <mariano.iglesias@3807eeeb-6ff5-0310-8944-8be069107fe0>
2007-05-03 23:25:21 +0000

    Adding empty test cases for File and Folder
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@5001 3807eeeb-6ff5-0310-8944-8be069107fe0

---
+++ cake/tests/cases/libs/file.test.php
@@ -0,0 +1,40 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Short description for file.
+ *
+ * Long description for file
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP Test Suite <https://trac.cakephp.org/wiki/Developement/TestSuite>
+ * Copyright (c) 2006, Larry E. Masters Shorewood, IL. 60431
+ * Author(s): Larry E. Masters aka PhpNut <phpnut@gmail.com>
+ *
+ *  Licensed under The Open Group Test Suite License
+ *  Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @author       Larry E. Masters aka PhpNut <phpnut@gmail.com>
+ * @copyright    Copyright (c) 2006, Larry E. Masters Shorewood, IL. 60431
+ * @link         http://www.phpnut.com/projects/
+ * @package      test_suite
+ * @subpackage   test_suite.cases.app
+ * @since        CakePHP Test Suite v 1.0.0.0
+ * @version      $Revision$
+ * @modifiedby   $LastChangedBy$
+ * @lastmodified $Date$
+ * @license      http://www.opensource.org/licenses/opengroup.php The Open Group Test Suite License
+ */
+	require_once LIBS.'file.php';
+/**
+ * Short description for class.
+ *
+ * @package    test_suite
+ * @subpackage test_suite.cases.libs
+ * @since      CakePHP Test Suite v 1.0.0.0
+ */
+class FileTest extends UnitTestCase {
+	
+}
+?>
