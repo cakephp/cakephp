20c0a87876ed30712716d42c3a9693c78e870371
chartjes <chartjes@3807eeeb-6ff5-0310-8944-8be069107fe0>
2007-05-24 03:03:15 +0000

    Added in initial functionality for for returning result sets and providing help information
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@5162 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/console/libs/console.php
+++ cake/console/libs/console.php
@@ -31,22 +31,8 @@
  * @subpackage	cake.cake.console.libs
  */
 class ConsoleShell extends Shell {
-	var $ignoreList = array(T_WHITESPACE, T_OPEN_TAG, T_CLOSE_TAG);
-	var $returnList = array(T_FOREACH, T_DO, T_WHILE, T_FOR, T_IF, T_RETURN,
-									T_CLASS, T_FUNCTION, T_INTERFACE, T_PRINT, T_ECHO,
-									T_COMMENT, T_UNSET, T_INCLUDE, T_REQUIRE, T_INCLUDE_ONCE,
-									T_REQUIRE_ONCE,T_TRY);
-	var $continueList = array(T_VARIABLE, T_STRING, T_NEW, T_EXTENDS, T_IMPLEMENTS,
-									T_OBJECT_OPERATOR, T_DOUBLE_COLON, T_INSTANCEOF, T_CATCH, T_ELSE,
-									T_AS, T_LNUMBER, T_DNUMBER, T_CONSTANT_ENCAPSED_STRING, T_ENCAPSED_AND_WHITESPACE,
-									T_CHARACTER, T_ARRAY, T_DOUBLE_ARROW, T_CONST, T_PUBLIC,
-									T_PROTECTED, T_PRIVATE, T_ABSTRACT, T_STATIC, T_VAR,
-									T_INC, T_DEC, T_SL, T_SL_EQUAL, T_SR,
-									T_SR_EQUAL, T_IS_EQUAL, T_IS_IDENTICAL, T_IS_GREATER_OR_EQUAL, T_IS_SMALLER_OR_EQUAL,
-									T_BOOLEAN_OR, T_LOGICAL_OR, T_BOOLEAN_AND, T_LOGICAL_AND, T_LOGICAL_XOR,
-									T_MINUS_EQUAL, T_PLUS_EQUAL, T_MUL_EQUAL, T_DIV_EQUAL, T_MOD_EQUAL,
-									T_XOR_EQUAL, T_AND_EQUAL, T_OR_EQUAL, T_FUNC_C, T_CLASS_C,
-									T_LINE, T_FILE);
+	var $associations = array('hasOne', 'hasMany', 'belongsTo', 'hasAndBelongsToMany');
+	
 	function main() {
 		$models = @loadModels();
 		foreach ($models as $model) {
@@ -59,6 +45,14 @@ class ConsoleShell extends Shell {
 			$command = trim($this->in(''));
 
 			switch($command) {
+				case 'help':
+					$this->out('Console help:');
+					$this->out('-------------');
+					$this->out('The interactive console is a tool for testing models before you commit code');
+					$this->out('');
+					$this->out('To test for results, use the name of your model without a leading $');
+					$this->out('e.g. Foo->findAll()');
+				break;
 				case 'quit':
 				case 'exit':
 					return true;
@@ -72,47 +66,44 @@ class ConsoleShell extends Shell {
 					}
 				break;
 				default:
-					$tokens = token_get_all($command);
-					$semicolon = FALSE;
-					$return = TRUE;
-					$ignore = FALSE;
-					$braces = array();
-					$methods = array();
-					$ws_t = array();
-					$command = '';
-
-					foreach ($tokens as $idx => $token) {
-						// Parse the tokens
-						if(is_array($token)) {
-							if(in_array($token[0], $this->ignoreList)) {
-								$ignore = TRUE;
-							} elseif(in_array($token[0], $this->returnList)) {
-								$return = FALSE;
-							} elseif(in_array($token[0], $this->continueList)) {
-								// everything is okay
-							} else {
-								$error = sprintf(">> Unknown tag: %d (%s): %s".PHP_EOL, $token[0], token_name($token[0]), $token[1]);
-							}
-							if($ignore == TRUE) {
-								$command .= $token[1] . " ";
-								$ws_t[] = array("token" => $token[0], "value" => $token[1]);
-							}
-						} else {
-							$ws_t[] = array("token" => $token, "value" => '');
-							$last_idx = count($ws_t) - 1;
-
-							switch ($token) {
-								case '(':
-								break;
-								case '{':
-								break;
-								case ')':
-								break;
-								case '}':
-								break;
-							}
+					// Look to see if we're dynamically binding something
+					$dynamicAssociation = false;
+					
+					foreach ($this->associations as $association) {
+						if (preg_match("/^(\w+) $association (\w+)/", $command, $models) == TRUE) {
+							$modelA = $models[1];
+							$modelB = $models[2];
+							loadModel($modelA);
+							$M = new $modelA();
+							$dynamicAssociation = true;
+							$M->bindModel(
+								array("$association" => array(
+									"$modelB" => array(
+										'className' => $modelB))));
+							print "Added association $command\n";
+							break;
 						}
 					}
+
+					if ($dynamicAssociation == false) {
+						// let's look for a find statment
+						if (strpos($command, "->find") > 0) {
+							$command = '$modelResults = $' . $command . ";";
+							eval($command);
+							
+							foreach ($modelResults as $results) {
+								foreach ($results as $modelName => $result) {
+									$this->out("$modelName");
+									foreach ($result as $field => $value) {
+										$this->out("\t$field: $value");
+									}
+								}
+								
+								$this->out('----------');
+							} 
+						}
+					}
+				break;
 			}
 		}
 	}
