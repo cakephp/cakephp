4f75a5e89e7ead3cbcf3c0aa8fc61e488bf60d1e
phpnut <phpnut@cakephp.org>
2007-07-09 19:52:31 +0000

    Adding tests for cake/libs/cache classes
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@5434 3807eeeb-6ff5-0310-8944-8be069107fe0

---
+++ cake/tests/cases/libs/cache/model.test.php
@@ -0,0 +1,42 @@
+<?php
+/* SVN FILE: $Id$ */
+/**
+ * Short description for file.
+ *
+ * Long description for file
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) Tests <https://trac.cakephp.org/wiki/Developement/TestSuite>
+ * Copyright 2005-2007, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ *  Licensed under The Open Group Test Suite License
+ *  Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright		Copyright 2005-2007, Cake Software Foundation, Inc.
+ * @link				https://trac.cakephp.org/wiki/Developement/TestSuite CakePHP(tm) Tests
+ * @package			cake.tests
+ * @subpackage		cake.tests.cases.libs.cache
+ * @since			CakePHP(tm) v 1.2.0.5434
+ * @version			$Revision$
+ * @modifiedby		$LastChangedBy$
+ * @lastmodified	$Date$
+ * @license			http://www.opensource.org/licenses/opengroup.php The Open Group Test Suite License
+ */
+uses('cache', 'cache' . DS . 'model');
+/**
+ * Short description for class.
+ *
+ * @package    cake.tests
+ * @subpackage cake.tests.cases.libs.cache
+ */
+class ModelEngineTest extends UnitTestCase {
+
+	function skip() {
+		$this->skipIf(true, 'ModelEngineTest not implemented');
+	}
+}
+?>
