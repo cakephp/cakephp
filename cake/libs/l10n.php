d58b906f5cd05d0f53c945d11fb1daf8375b440e
phpnut <phpnut@cakephp.org>
2007-12-23 08:05:13 +0000

    "Fixes #3738, Inconsistencies with default languages and mappings in L10n.php"
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6217 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/libs/l10n.php
+++ cake/libs/l10n.php
@@ -399,7 +399,7 @@ class L10n extends Object {
 				$this->charset = $this->__l10nCatalog[$langKey]['charset'];
 
 				if ($this->default) {
-					$this->languagePath[2] = $this->__l10nCatalog[$this->default]['localeFallback'];
+					$this->languagePath[2] = $this->__l10nCatalog[$this->__l10nMap[$this->default]]['localeFallback'];
 				}
 				$this->found = true;
 				Configure::write('Config.language', $this->lang);
