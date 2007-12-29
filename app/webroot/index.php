31138695ed56e0fbfe849a62e0c323a49c8a4a97
nate <nate@cakephp.org>
2007-12-29 03:30:46 +0000

    Whitespace fixes in webroot/index
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6269 3807eeeb-6ff5-0310-8944-8be069107fe0

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -30,7 +30,7 @@
  * Do not change
  */
 	if (!defined('DS')) {
-		 define('DS', DIRECTORY_SEPARATOR);
+		define('DS', DIRECTORY_SEPARATOR);
 	}
 /**
  * These defines should only be edited if you have cake installed in
@@ -38,41 +38,41 @@
  * Each define has a commented line of code that explains what you would change.
  */
 	if (!defined('ROOT')) {
-		 //define('ROOT', 'FULL PATH TO DIRECTORY WHERE APP DIRECTORY IS LOCATED. DO NOT ADD A TRAILING DIRECTORY SEPARATOR');
-		 //You should also use the DS define to separate your directories
-		 define('ROOT', dirname(dirname(dirname(__FILE__))));
+		//define('ROOT', 'FULL PATH TO DIRECTORY WHERE APP DIRECTORY IS LOCATED. DO NOT ADD A TRAILING DIRECTORY SEPARATOR');
+		//You should also use the DS define to separate your directories
+		define('ROOT', dirname(dirname(dirname(__FILE__))));
 	}
 	if (!defined('APP_DIR')) {
-		 //define('APP_DIR', 'DIRECTORY NAME OF APPLICATION');
-		 define('APP_DIR', basename(dirname(dirname(__FILE__))));
+		//define('APP_DIR', 'DIRECTORY NAME OF APPLICATION');
+		define('APP_DIR', basename(dirname(dirname(__FILE__))));
 	}
 /**
  * This only needs to be changed if the cake installed libs are located
  * outside of the distributed directory structure.
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		 //define ('CAKE_CORE_INCLUDE_PATH', 'FULL PATH TO DIRECTORY WHERE CAKE CORE IS INSTALLED. DO NOT ADD A TRAILING DIRECTORY SEPARATOR');
-		 //You should also use the DS define to separate your directories
-		 define('CAKE_CORE_INCLUDE_PATH', ROOT);
+		//define ('CAKE_CORE_INCLUDE_PATH', 'FULL PATH TO DIRECTORY WHERE CAKE CORE IS INSTALLED. DO NOT ADD A TRAILING DIRECTORY SEPARATOR');
+		//You should also use the DS define to separate your directories
+		define('CAKE_CORE_INCLUDE_PATH', ROOT);
 	}
 ///////////////////////////////
 //DO NOT EDIT BELOW THIS LINE//
 ///////////////////////////////
 	if (!defined('WEBROOT_DIR')) {
-		 define('WEBROOT_DIR', basename(dirname(__FILE__)));
+		define('WEBROOT_DIR', basename(dirname(__FILE__)));
 	}
 	if (!defined('WWW_ROOT')) {
-		 define('WWW_ROOT', dirname(__FILE__) . DS);
+		define('WWW_ROOT', dirname(__FILE__) . DS);
 	}
 	if (!defined('CORE_PATH')) {
-		 if (function_exists('ini_set')) {
-			  ini_set('include_path', CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'));
-			  define('APP_PATH', null);
-			  define('CORE_PATH', null);
-		 } else {
-			  define('APP_PATH', ROOT . DS . APP_DIR . DS);
-			  define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
-		 }
+		if (function_exists('ini_set')) {
+			ini_set('include_path', CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'));
+			define('APP_PATH', null);
+			define('CORE_PATH', null);
+		} else {
+			define('APP_PATH', ROOT . DS . APP_DIR . DS);
+			define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+		}
 	}
 	if (!include(CORE_PATH . 'cake' . DS . 'bootstrap.php')) {
 		trigger_error("Can't find CakePHP core.  Check the value of CAKE_CORE_INCLUDE_PATH in app/webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
@@ -80,10 +80,10 @@
 	if (isset($_GET['url']) && $_GET['url'] === 'favicon.ico') {
 		return;
 	} else {
-		 $Dispatcher = new Dispatcher();
-		 $Dispatcher->dispatch($url);
+		$Dispatcher = new Dispatcher();
+		$Dispatcher->dispatch($url);
 	}
 	if (Configure::read() > 0) {
-		 echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
+		echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
 	}
 ?>
