67c91aa009b72aeed013b58c980ba2bbe81ca1c1
phpnut <phpnut@cakephp.org>
2008-01-02 06:33:52 +0000

    Merging bug fixes and enhancements to the trunk: [5876] [5878] [5879] [5880] [5881] [5882] [5883] [5884] [5885] [5886] [5887] [5888] [5889] [5890] [5891] [5892] [5893] [5894] [5895] [5896] [5897] [5898] [5899] [5900] [5901] [5902] [5903] [5904] [5905] [5906] [5907] [5908] [5909] [5910] [5911] [5912] [5913] [5914] [5915] [5916] [5917] [5918] [5919] [5920] [5921] [5922] [5923] [5924] [5925] [5926] [5927] [5928] [5929] [5930] [5931] [5932] [5933] [5934] [5935] [5936] [5937] [5938] [5939] [5940] [5941] [5942] [5943] [5944] [5945] [5946] [5947] [5948] [5949] [5950] [5951] [5952] [5953] [5954] [5955] [5956] [5957] [5958] [5959] [5960] [5961] [5962] [5963] [5964] [5965] [5966] [5967] [5968] [5969] [5970] [5971] [5972] [5973] [5974] [5975] [5976] [5977] [5978] [5979] [5980] [5981] [5982] [5983] [5984] [5985] [5986] [5987] [5988] [5989] [5990] [5991] [5992] [5993] [5994] [5995] [5996] [5997] [5998] [5999] [6000] [6001] [6002] [6003] [6004] [6005] [6006] [6007] [6008] [6009] [6010] [6011] [6012] [6013] [6014] [6015] [6016] [6017] [6018] [6019] [6020] [6021] [6022] [6023] [6024] [6025] [6026] [6027] [6028] [6030] [6031] [6032] [6033] [6034] [6035] [6036] [6037] [6038] [6039] [6041] [6042] [6043] [6044] [6046] [6047] [6048] [6049] [6050] [6051] [6052] [6053] [6054] [6055] [6056] [6057] [6058] [6059] [6060] [6061] [6062] [6063] [6064] [6065] [6066] [6067] [6068] [6069] [6070] [6071] [6072] [6073] [6074] [6075] [6076] [6077] [6078] [6079] [6080] [6081] [6082] [6083] [6084] [6085] [6086] [6087] [6088] [6089] [6090] [6091] [6092] [6093] [6094] [6095] [6096] [6097] [6098] [6099] [6100] [6101] [6102] [6103] [6104] [6105] [6106] [6107] [6108] [6109] [6110] [6111] [6112] [6113] [6114] [6115] [6117] [6118] [6119] [6120] [6121] [6122] [6123] [6124] [6125] [6126] [6127] [6128] [6129] [6130] [6131] [6132] [6133] [6134] [6135] [6136] [6137] [6138] [6139] [6140] [6141] [6142] [6143] [6144] [6145] [6146] [6147] [6148] [6149] [6150] [6151] [6152] [6153] [6154] [6155] [6156] [6157] [6158] [6159] [6160] [6161] [6162] [6163] [6164] [6165] [6166] [6167] [6170] [6171] [6172] [6173] [6174] [6175] [6176] [6177] [6178] [6179] [6180] [6181] [6182] [6183] [6184] [6185] [6186] [6187] [6188] [6189] [6190] [6191] [6192] [6193] [6194] [6195] [6196] [6197] [6198] [6199] [6200] [6201] [6202] [6203] [6204] [6205] [6206] [6207] [6208] [6209] [6210] [6211] [6212] [6213] [6214] [6215] [6216] [6217] [6220] [6221] [6222] [6223] [6224] [6227] [6230] [6231] [6233] [6234] [6235] [6236] [6237] [6239] [6240] [6241] [6242] [6243] [6244] [6245] [6246] [6247] [6248] [6249] [6251] [6252] [6253] [6255] [6256] [6257] [6258] [6259] [6262] [6263] [6264] [6265] [6266] [6268] [6269] [6270] [6271] [6272] [6273] [6274] [6275] [6276] [6277] [6278] [6279] [6280] [6281] [6282] [6283] [6284] [6285] [6286] [6287] [6288] [6289] [6290] [6291] [6292] [6293] [6294] [6295] [6296] [6297] [6298] [6299] [6300] [6301] [6302] [6303] [6304] [6306] [6307] [6308] [6309] [6310]
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6311 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/config/config.php
+++ cake/config/config.php
@@ -24,5 +24,5 @@
  * @lastmodified	$Date$
  * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-return $config['Cake.version'] = '1.2.0.5875 pre-beta';
+return $config['Cake.version'] = '1.2.0.6311 beta';
 ?>
