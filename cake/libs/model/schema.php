06175843a1df95c3d16b2170587c3d301bbd404d
phpnut <phpnut@cakephp.org>
2008-01-06 01:50:43 +0000

    "Removing unneeded :"
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6328 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/libs/model/schema.php
+++ cake/libs/model/schema.php
@@ -338,7 +338,7 @@ class CakeSchema extends Object {
 
 
 		$File =& new File($path . DS . $file, true);
-		$header = '$Id' . ':';
+		$header = '$Id';
 		$content = "<?php \n/* SVN FILE: $header$ */\n/* ". $name ." schema generated on: " . date('Y-m-d H:m:s') . " : ". time() . "*/\n{$out}?>";
 		$content = $File->prepare($content);
 		if ($File->write($content)) {
