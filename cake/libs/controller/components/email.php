348df0fca23e5291469526e1b5d6bc129f7ceedf
phpnut <phpnut@cakephp.org>
2008-01-08 02:48:07 +0000

    "Fixes #3849, Deprecated loadView() call in email component when using theme"
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6333 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/libs/controller/components/email.php
+++ cake/libs/controller/components/email.php
@@ -363,7 +363,7 @@ class EmailComponent extends Object{
 				list($plugin, $viewClass) = explode('.', $viewClass);
 			}
 			$viewClass = $viewClass . 'View';
-			loadView($this->Controller->view);
+			App::import('View', $this->Controller->view);
 		}
 		$View = new $viewClass($this->Controller, false);
 		$View->layout = $this->layout;
