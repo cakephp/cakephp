4e573f48f0bc20008195d05d3b18701a90b12d65
nate <nate@cakephp.org>
2008-02-24 15:08:01 +0000

    Updating view test case
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6475 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/tests/cases/libs/view/view.test.php
+++ cake/tests/cases/libs/view/view.test.php
@@ -267,10 +267,10 @@ class ViewTest extends UnitTestCase {
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 			<title>
 				CakePHP: the rapid development php framework:		Posts	</title>
 
-			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 			<link rel="icon" href="favicon.ico" type="image/x-icon" />
 			<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 			<link rel="stylesheet" type="text/css" href="css/cake.generic.css" />	</head>
@@ -336,10 +336,10 @@ class ViewTest extends UnitTestCase {
 		$expected = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 			<title>
 				CakePHP: the rapid development php framework:		Posts	</title>
 
-			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 			<link rel="icon" href="favicon.ico" type="image/x-icon" />
 			<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 			<link rel="stylesheet" type="text/css" href="css/cake.generic.css" />	</head>
