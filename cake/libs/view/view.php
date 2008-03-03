017afa637d1d2c512d4351a7ac71cfbaca5c7825
nate <nate@cakephp.org>
2008-03-03 23:30:52 +0000

    Fixing View::render() for custom views on Windows, fixes #4265
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6506 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/libs/view/view.php
+++ cake/libs/view/view.php
@@ -773,10 +773,10 @@ class View extends Object {
 			$name = $this->action;
 		}
 
-		if (strpos($name, '/') === false && strpos($name, '..') === false) {
+		if (strpos($name, DS) === false && strpos($name, '..') === false) {
 			$name = $this->viewPath . DS . $subDir . Inflector::underscore($name);
-		} elseif (strpos($name, '/') !== false) {
-			if ($name{0} === '/') {
+		} elseif (strpos($name, DS) !== false) {
+			if ($name{0} === '/' || $name{1} === ':') {
 				if (is_file($name)) {
 					return $name;
 				}
