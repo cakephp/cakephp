a5822e69672b1c856eb788fe9c1b32c57b7be25c
nate <nate@cakephp.org>
2008-05-09 16:13:30 +0000

    Additional test coverage for SessionHelper
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6778 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/tests/cases/libs/view/helpers/session.test.php
+++ cake/tests/cases/libs/view/helpers/session.test.php
@@ -42,30 +42,30 @@ App::import('Helper', array('Session'));
 class SessionHelperTest extends CakeTestCase {
 	
 	function setUp() {
-		$this->Session = new SessionHelper();		
+		$this->Session = new SessionHelper();
 		$this->Session->__start();
-		
-		$_SESSION = array('test' => 'info',
-				  'Message' => array(
-						'flash' => array(
-							'layout' => 'default', 
-							'params' => array(),
-							'message' => 'This is a calling'
-							),
-							'notification' => array(
-							'layout' => 'sessionHelper',
-							'params' => array('title' => 'Notice!',
-									  'name' => 'Alert!'),
-									  'message' => 'This is a test of the emergency broadcasting system',									
-						),	
-						'bare' => array(
-							'layout' => null,
-							'message' => 'Bare message',
-							'params' => array(),
-						),
-					),
-					'Deeply' => array('nested' => array('key' => 'value')),
-				);		
+
+		$_SESSION = array(
+			'test' => 'info',
+			'Message' => array(
+				'flash' => array(
+					'layout' => 'default',
+					'params' => array(),
+					'message' => 'This is a calling'
+				),
+				'notification' => array(
+					'layout' => 'sessionHelper',
+					'params' => array('title' => 'Notice!', 'name' => 'Alert!'),
+					'message' => 'This is a test of the emergency broadcasting system',
+				),
+				'bare' => array(
+					'layout' => null,
+					'message' => 'Bare message',
+					'params' => array(),
+				),
+			),
+			'Deeply' => array('nested' => array('key' => 'value')),
+		);
 	}
 
 	function tearDown() {
@@ -76,19 +76,18 @@ class SessionHelperTest extends CakeTestCase {
 	function testRead() {
 		$result = $this->Session->read('Deeply.nested.key');
 		$this->assertEqual($result, 'value');
-		
+
 		$result = $this->Session->read('test');
 		$this->assertEqual($result, 'info');
-				
 	}
 	
 	function testCheck() {
 		$this->assertTrue($this->Session->check('test'));
-		
+
 		$this->assertTrue($this->Session->check('Message.flash.layout'));
-		
+
 		$this->assertFalse($this->Session->check('Does.not.exist'));
-		
+
 		$this->assertFalse($this->Session->check('Nope'));
 	}
 	
@@ -98,8 +97,8 @@ class SessionHelperTest extends CakeTestCase {
 	}
 	
 	function testFlash() {
-		ob_start();		
-		$this->Session->flash();		
+		ob_start();
+		$this->Session->flash();
 		$result = ob_get_contents();
 		ob_clean();
 			
@@ -107,7 +106,7 @@ class SessionHelperTest extends CakeTestCase {
 		$this->assertEqual($result, $expected);		
 		$this->assertFalse($this->Session->check('Message.flash'));
 		
-		Configure::write('viewPaths', array(TEST_CAKE_CORE_INCLUDE_PATH . 'tests' . DS . 'test_app' . DS . 'views'. DS));		
+		Configure::write('viewPaths', array(TEST_CAKE_CORE_INCLUDE_PATH . 'tests' . DS . 'test_app' . DS . 'views'. DS));
 		$controller = new Controller();
 		$this->Session->view = new View($controller);
 		
@@ -115,11 +114,11 @@ class SessionHelperTest extends CakeTestCase {
 		$this->Session->flash('notification');
 		$result = ob_get_contents();
 		ob_clean();
-			
+
 		$expected = "<div id=\"notificationLayout\">\n\t<h1>Alert!</h1>\n\t<h3>Notice!</h3>\n\t<p>This is a test of the emergency broadcasting system</p>\n</div>";
 		$this->assertEqual($result, $expected);
-		$this->assertFalse($this->Session->check('Message.notification'));	
-		
+		$this->assertFalse($this->Session->check('Message.notification'));
+
 		ob_start();
 		$this->Session->flash('bare');
 		$result = ob_get_contents();
@@ -129,23 +128,39 @@ class SessionHelperTest extends CakeTestCase {
 		$this->assertEqual($result, $expected);
 		$this->assertFalse($this->Session->check('Message.bare'));
 	}
-	
+
 	function testID() {
 		$id = session_id();
 		$result = $this->Session->id();
 		$this->assertEqual($id, $result);
 	}
-	
+
 	function testError() {
 		$result = $this->Session->error();
 		$this->assertFalse($result);
-		
+
 		$this->Session->read('CauseError');
 		$result = $this->Session->error();
 		$expected = "CauseError doesn't exist";
 		$this->assertEqual($result, $expected);
 	}
-	
+
+	function testDisabling() {
+		Configure::write('Session.start', false);
+		$this->Session = new SessionHelper();
+		$this->assertFalse($this->Session->check('test'));
+		$this->assertFalse($this->Session->read('test'));
+
+		$this->Session->read('CauseError');
+		$this->assertFalse($this->Session->error());
+
+		ob_start();
+		$this->assertFalse($this->Session->flash('bare'));
+		$result = ob_get_contents();
+		ob_clean();
+		$this->assertFalse($result);
+	}
+
 	function testValid() {
 		//wierd it always ends up false in the test suite
 		//$this->assertFalse($this->Session->valid());
