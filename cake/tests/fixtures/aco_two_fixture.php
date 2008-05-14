05b3193c5409a586c7dc9cadca5ca7758066d1e0
mark_story <mark@mark-story.com>
2008-05-14 12:18:21 +0000

    Added primary keys to Acl fixtures fixes #4634 where tests were failing on InnoDB tables
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@6866 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/tests/fixtures/aco_two_fixture.php
+++ cake/tests/fixtures/aco_two_fixture.php
@@ -45,16 +45,16 @@ class AcoTwoFixture extends CakeTestFixture {
 	);
 
 	var $records = array(
-		array('parent_id' => null, 	'model' => null, 'foreign_key' => null, 'alias' => 'ROOT',			'lft' => 1,  'rght' => 20),
-        array('parent_id' => 1,		'model' => null, 'foreign_key' => null, 'alias' => 'tpsReports',	'lft' => 2,  'rght' => 9),
-        array('parent_id' => 2,		'model' => null, 'foreign_key' => null, 'alias' => 'view',			'lft' => 3,  'rght' => 6),
-        array('parent_id' => 3,		'model' => null, 'foreign_key' => null, 'alias' => 'current',		'lft' => 4,  'rght' => 5),
-        array('parent_id' => 2,		'model' => null, 'foreign_key' => null, 'alias' => 'update',		'lft' => 7,  'rght' => 8),
-        array('parent_id' => 1,		'model' => null, 'foreign_key' => null, 'alias' => 'printers',		'lft' => 10, 'rght' => 19),
-        array('parent_id' => 6,		'model' => null, 'foreign_key' => null, 'alias' => 'print',			'lft' => 11, 'rght' => 14),
-        array('parent_id' => 7,		'model' => null, 'foreign_key' => null, 'alias' => 'lettersize',	'lft' => 12, 'rght' => 13),
-        array('parent_id' => 6,		'model' => null, 'foreign_key' => null, 'alias' => 'refill',		'lft' => 15, 'rght' => 16),
-		array('parent_id' => 6,		'model' => null, 'foreign_key' => null, 'alias' => 'smash',			'lft' => 17, 'rght' => 18),
+		array('id' => 1, 'parent_id' => null,	'model' => null, 'foreign_key' => null, 'alias' => 'ROOT',		'lft' => 1,  'rght' => 20),
+        array('id' => 2, 'parent_id' => 1,		'model' => null, 'foreign_key' => null, 'alias' => 'tpsReports', 'lft' => 2,  'rght' => 9),
+        array('id' => 3, 'parent_id' => 2,		'model' => null, 'foreign_key' => null, 'alias' => 'view',		'lft' => 3,  'rght' => 6),
+        array('id' => 4, 'parent_id' => 3,		'model' => null, 'foreign_key' => null, 'alias' => 'current',	'lft' => 4,  'rght' => 5),
+        array('id' => 5, 'parent_id' => 2,		'model' => null, 'foreign_key' => null, 'alias' => 'update',	'lft' => 7,  'rght' => 8),
+        array('id' => 6, 'parent_id' => 1,		'model' => null, 'foreign_key' => null, 'alias' => 'printers',	'lft' => 10, 'rght' => 19),
+        array('id' => 7, 'parent_id' => 6,		'model' => null, 'foreign_key' => null, 'alias' => 'print',		'lft' => 11, 'rght' => 14),
+        array('id' => 8, 'parent_id' => 7,		'model' => null, 'foreign_key' => null, 'alias' => 'lettersize','lft' => 12, 'rght' => 13),
+        array('id' => 9, 'parent_id' => 6,		'model' => null, 'foreign_key' => null, 'alias' => 'refill',	'lft' => 15, 'rght' => 16),
+		array('id' => 10, 'parent_id' => 6,		'model' => null, 'foreign_key' => null, 'alias' => 'smash',		'lft' => 17, 'rght' => 18),
 	);
 }
 
