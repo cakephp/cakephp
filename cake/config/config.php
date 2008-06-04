a161d04cf1533a3c1f90f43e46f327359cc15a0c
gwoo <gwoo@cakephp.org>
2008-06-04 21:01:51 +0000

    version number sync.
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@7119 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/config/config.php
+++ cake/config/config.php
@@ -24,5 +24,5 @@
  * @lastmodified	$Date$
  * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-return $config['Cake.version'] = '1.2.0.6311 beta';
+return $config['Cake.version'] = '1.2.0.7119 RC1';
 ?>
