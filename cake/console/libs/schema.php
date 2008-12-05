8a0fb1904a04bc2605386037c4141f6ac3e670a3
mark_story <mark@mark-story.com>
2008-12-05 17:01:20 +0000

    Applying patch from teemow, fixes schema file generation so files with trailing characters do not match.  Fixes #5854
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@7905 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/console/libs/schema.php
+++ cake/console/libs/schema.php
@@ -153,7 +153,7 @@ class SchemaShell extends Shell {
 			$count = 1;
 			if (!empty($result[1])) {
 				foreach ($result[1] as $file) {
-					if (preg_match('/schema(?:[_\d]*)?\.php/', $file)) {
+					if (preg_match('/schema(?:[_\d]*)?\.php$/', $file)) {
 						$count++;
 					}
 				}
