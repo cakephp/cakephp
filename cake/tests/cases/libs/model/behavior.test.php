48119947b8aff0371521b37efb1a47e44c59af4d
mark_story <mark@mark-story.com>
2008-12-08 00:49:08 +0000

    Adding App::import() to fix broken tests when running behavior test individually.
    git-svn-id: https://svn.cakephp.org/repo/branches/1.2.x.x@7907 3807eeeb-6ff5-0310-8944-8be069107fe0

--- cake/tests/cases/libs/model/behavior.test.php
+++ cake/tests/cases/libs/model/behavior.test.php
@@ -23,6 +23,7 @@
  * @lastmodified  $Date$
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
+App::import('Model', 'AppModel');
 
 require_once dirname(__FILE__) . DS . 'models.php';
 /**
