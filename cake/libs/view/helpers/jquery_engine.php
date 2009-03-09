cf0582773b653f4bba233c444f8c7638b27dd385
mark_story <mark@mark-story.com>
2009-03-09 09:25:39 -0400

    Adding jqueryEngineHelper stub.

---
+++ cake/libs/view/helpers/jquery_engine.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * jQuery Engine Helper for JsHelper
+ *
+ * Provides jQuery specific Javascript for JsHelper.
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP :  Rapid Development Framework <http://www.cakephp.org/>
+ * Copyright 2006-2008, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright       Copyright 2006-2008, Cake Software Foundation, Inc.
+ * @link            http://www.cakefoundation.org/projects/info/cakephp CakePHP Project
+ * @package         cake
+ * @subpackage      cake.
+ * @version         
+ * @modifiedby      
+ * @lastmodified    
+ * @license         http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+class jqueryEngineHelper extends AppHelper {
+	
+}
+?>
