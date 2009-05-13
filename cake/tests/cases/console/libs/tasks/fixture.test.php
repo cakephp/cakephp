3a36979a210e5325662d9088a179abf51969fb40
mark_story <mark@mark-story.com>
2009-05-12 21:35:06 -0400

    fixing doc block

--- cake/tests/cases/console/libs/tasks/fixture.test.php
+++ cake/tests/cases/console/libs/tasks/fixture.test.php
@@ -1,28 +1,23 @@
 <?php
-/* SVN FILE: $Id$ */
 /**
- * TestTaskTest file
+ * FixtureTask Test case
  *
- * Test Case for test generation shell task
+ * 
  *
  * PHP versions 4 and 5
  *
- * CakePHP :  Rapid Development Framework (http://www.cakephp.org)
- * Copyright 2006-2008, Cake Software Foundation, Inc.
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @filesource
- * @copyright     Copyright 2006-2008, Cake Software Foundation, Inc.
- * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP Project
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
  * @package       cake
- * @subpackage    cake.tests.cases.console.libs.tasks
- * @since         CakePHP v 1.2.0.7726
- * @version       $Revision$
- * @modifiedby    $LastChangedBy$
- * @lastmodified  $Date$
- * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @subpackage    cake.
+ * @since         CakePHP(tm) v 1.3
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 App::import('Core', 'Shell');
 
@@ -37,10 +32,8 @@ if (!class_exists('ShellDispatcher')) {
 	ob_end_clean();
 }
 
-if (!class_exists('FixtureTask')) {
-	require CAKE . 'console' .  DS . 'libs' . DS . 'tasks' . DS . 'template.php';
-	require CAKE . 'console' .  DS . 'libs' . DS . 'tasks' . DS . 'fixture.php';
-}
+require_once CAKE . 'console' .  DS . 'libs' . DS . 'tasks' . DS . 'template.php';
+require_once CAKE . 'console' .  DS . 'libs' . DS . 'tasks' . DS . 'fixture.php';
 
 Mock::generatePartial(
 	'ShellDispatcher', 'TestFixtureTaskMockShellDispatcher',
