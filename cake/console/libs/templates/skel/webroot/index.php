f84ed697803ebee5026ca18a06ba6c452cb562ae
gwoo <gwoo@cakephp.org>
2009-06-05 19:05:05 -0700

    fixing undefined variable when new project is baked from skel

--- cake/console/libs/templates/skel/webroot/index.php
+++ cake/console/libs/templates/skel/webroot/index.php
@@ -85,7 +85,7 @@
 		return;
 	} else {
 		$Dispatcher = new Dispatcher();
-		$Dispatcher->dispatch($url);
+		$Dispatcher->dispatch();
 	}
 	if (Configure::read() > 0) {
 		echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
