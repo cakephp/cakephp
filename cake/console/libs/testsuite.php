9e4cb97026571b2ba4ac6c1d5b955a3b51845152
gwoo <gwoo@cakephp.org>
2009-08-02 13:19:16 -0700

    Revert "allow running all core test cases via the console"
    This reverts commit 86e5cc5a89e7307ebf827c688311cdbe945feb85.

--- cake/console/libs/testsuite.php
+++ cake/console/libs/testsuite.php
@@ -94,7 +94,7 @@ class TestSuiteShell extends Shell {
 		if (isset($corePath[0])) {
 			define('TEST_CAKE_CORE_INCLUDE_PATH', rtrim($corePath[0], DS) . DS);
 		} else {
-			define('TEST_CAKE_CORE_INCLUDE_PATH', CAKE_CORE_INCLUDE_PATH . DS . 'cake' . DS);
+			define('TEST_CAKE_CORE_INCLUDE_PATH', CAKE_CORE_INCLUDE_PATH);
 		}
 
 		$this->__installSimpleTest();
@@ -236,13 +236,8 @@ class TestSuiteShell extends Shell {
 					return true;
 				}
 
-				if ($this->category == 'core') {
-					if (file_exists($folder.DS.'cases'.DS.$this->file.'.test.php')) {
-						return true;
-					}
-					if (file_exists($folder.DS.'cases'.DS.'libs'.DS.$this->file.'.test.php')) {
-						return true;
-					}
+				if ($this->category == 'core' && file_exists($folder.DS.'cases'.DS.'libs'.DS.$this->file.'.test.php')) {
+					return true;
 				}
 
 				if ($isPlugin && file_exists($folder.DS.'cases'.DS.$this->file.'.test.php')) {
@@ -297,11 +292,7 @@ class TestSuiteShell extends Shell {
 			return $result;
 		}
 
-		if (file_exists(CORE_TEST_CASES . DS . $this->file . '.test.php')) {
-			$case = $this->file . '.test.php';
-		} else {
-			$case = 'libs' . DS . $this->file . '.test.php';
-		}
+		$case = 'libs'.DS.$this->file.'.test.php';
 		if ($this->category == 'app') {
 			$case = $this->file.'.test.php';
 		} elseif ($this->isPluginTest) {
