2ff53343471f8e07db04c2282653e72b29acae45
mark_story <mark@mark-story.com>
2009-09-03 09:37:25 -0400

    Minor refactor to remove method calls to Inflector::camelize. Adding tests to ensure references are maintained.

--- cake/libs/view/view.php
+++ cake/libs/view/view.php
@@ -1,26 +1,21 @@
 <?php
-/* SVN FILE: $Id$ */
-
 /**
  * Methods for displaying presentation data in the view.
  *
  * PHP versions 4 and 5
  *
  * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
- * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * Copyright 2005-2009, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
  * @filesource
- * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
  * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
  * @package       cake
  * @subpackage    cake.cake.libs.view
  * @since         CakePHP(tm) v 0.10.0.1076
- * @version       $Revision$
- * @modifiedby    $LastChangedBy$
- * @lastmodified  $Date$
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 
@@ -699,10 +694,8 @@ class View extends Object {
 				$name = $helperNames[$i];
 
 				${$name} =& $loadedHelpers[$helpers[$i]];
-				$this->loaded[$name] =& ${$name};
-
-				$memberName = Inflector::camelize($name);
-				$this->{$memberName} =& ${$name};
+				$this->loaded[$helperNames[$i]] =& ${$name};
+				$this->{$helpers[$i]} =& ${$name};
 			}
 			$this->_triggerHelpers('beforeRender');
 		}
