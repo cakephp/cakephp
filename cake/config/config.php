115ae3f1df1ba94cfcfeca7c899cbea714d2a578
predominant <graham@grahamweldon.com>
2009-09-09 19:12:00 +1000

    Updating CakePHP version.

--- cake/config/config.php
+++ cake/config/config.php
@@ -22,5 +22,5 @@
  * @lastmodified  $Date$
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-return $config['Cake.version'] = '1.2.4.8284';
+return $config['Cake.version'] = '1.2.5';
 ?>
