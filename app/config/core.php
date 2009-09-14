a499c980cd239156bc60b5e6300d7d7ab666dae5
mark_story <mark@mark-story.com>
2009-09-13 23:32:27 -0400

    Removing webroot/js/vendors.php.  These files are no longer needed as Dispatcher can handle serving js files from vendors directories. Adding empty files to empty directories. Refs #50

--- app/config/core.php
+++ app/config/core.php
@@ -39,7 +39,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug', 0);
 
 /**
  * CakePHP Log Level:
