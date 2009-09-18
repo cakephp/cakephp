caf780fdfb4d60fe706f2b28d653d8cc276e9d30
mark_story <mark@mark-story.com>
2009-09-17 20:25:44 -0400

    Correcting mistake from earlier.

--- app/config/core.php
+++ app/config/core.php
@@ -39,7 +39,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 0);
+	Configure::write('debug', 2);
 
 /**
  * CakePHP Log Level:
