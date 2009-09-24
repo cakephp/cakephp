23954a4b5e59d0f3528d4a948db4dbe9d2029c9d
jperras <joel.perras@gmail.com>
2009-09-24 16:34:27 -0400

    Fixing incorrect path to test file in database group test.

--- cake/tests/groups/database.group.php
+++ cake/tests/groups/database.group.php
@@ -53,7 +53,7 @@ class DatabaseGroupTest extends TestSuite {
  */
 	function DatabaseGroupTest() {
 		TestManager::addTestFile($this, CORE_TEST_CASES . DS . 'libs' . DS . 'model' . DS . 'db_acl');
-		TestManager::addTestFile($this, CORE_TEST_CASES . DS . 'libs' . DS . 'model' . DS . 'schema');
+		TestManager::addTestFile($this, CORE_TEST_CASES . DS . 'libs' . DS . 'model' . DS . 'cake_schema');
 		TestManager::addTestFile($this, CORE_TEST_CASES . DS . 'libs' . DS . 'model' . DS . 'connection_manager');
 		TestManager::addTestFile($this, CORE_TEST_CASES . DS . 'libs' . DS . 'model' . DS . 'datasources' . DS . 'dbo_source');
 	}
