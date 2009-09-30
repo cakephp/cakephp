9409f2651e02b9019be5e0ff80d99b7a1459acd0
jperras <joel.perras@gmail.com>
2009-09-30 00:04:46 -0400

    Removing deprecated validation constants of VALID_NOT_EMPTY, VALID_NUMBER, VALID_EMAIL and VALID_YEAR.

--- cake/libs/validation.php
+++ cake/libs/validation.php
@@ -27,30 +27,6 @@
  */
 
 /**
- * Deprecated
- */
-
-/**
- * Not empty.
- */
-	define('VALID_NOT_EMPTY', '/.+/');
-
-/**
- * Numbers [0-9] only.
- */
-	define('VALID_NUMBER', '/^[-+]?\\b[0-9]*\\.?[0-9]+\\b$/');
-
-/**
- * A valid email address.
- */
-	define('VALID_EMAIL', "/^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[a-z]{2,4}|museum|travel)$/i");
-
-/**
- * A valid year (1000-2999).
- */
-	define('VALID_YEAR', '/^[12][0-9]{3}$/');
-
-/**
  * Offers different validation methods.
  *
  * Long description for file
