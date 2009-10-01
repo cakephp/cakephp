f12cbdba380479b96bb842c11e6e8f250e584728
mark_story <mark@mark-story.com>
2009-10-01 01:03:47 -0400

    Fixing CacheHelper and multiple cake:nocache tags in a view file, breaking cake:nocache following $content_for_layout. Fixes #136

--- cake/libs/view/helpers/cache.php
+++ cake/libs/view/helpers/cache.php
@@ -156,27 +156,30 @@ class CacheHelper extends AppHelper {
 		} elseif ($file = fileExistsInPath($file)) {
 			$file = file_get_contents($file);
 		}
-
-		preg_match_all('/(<cake:nocache>(?<=<cake:nocache>)[\\s\\S]*?(?=<\/cake:nocache>)<\/cake:nocache>)/i', $cache, $oresult, PREG_PATTERN_ORDER);
-		preg_match_all('/(?<=<cake:nocache>)([\\s\\S]*?)(?=<\/cake:nocache>)/i', $file, $result, PREG_PATTERN_ORDER);
+		preg_match_all('/(<cake:nocache>(?<=<cake:nocache>)[\\s\\S]*?(?=<\/cake:nocache>)<\/cake:nocache>)/i', $cache, $outputResult, PREG_PATTERN_ORDER);
+		preg_match_all('/(?<=<cake:nocache>)([\\s\\S]*?)(?=<\/cake:nocache>)/i', $file, $fileResult, PREG_PATTERN_ORDER);
+		$fileResult = $fileResult[0];
+		$outputResult = $outputResult[0];
 
 		if (!empty($this->__replace)) {
-			foreach ($oresult['0'] as $k => $element) {
+			foreach ($outputResult as $i => $element) {
 				$index = array_search($element, $this->__match);
 				if ($index !== false) {
-					array_splice($oresult[0], $k, 1);
+					unset($outputResult[$i]);
 				}
 			}
+			$outputResult = array_values($outputResult);
 		}
 
-		if (!empty($result['0'])) {
-			$count = 0;
-			foreach ($result['0'] as $block) {
-				if (isset($oresult['0'][$count])) {
-					$this->__replace[] = $block;
-					$this->__match[] = $oresult['0'][$count];
+
+		if (!empty($fileResult)) {
+			$i = 0;
+			foreach ($fileResult as $cacheBlock) {
+				if (isset($outputResult[$i])) {
+					$this->__replace[] = $cacheBlock;
+					$this->__match[] = $outputResult[$i];
 				}
-				$count++;
+				$i++;
 			}
 		}
 	}
