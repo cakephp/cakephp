e5a99b26852fce13efb33c07db1ea67bae5bf801
mark_story <mark@mark-story.com>
2009-10-12 23:55:40 -0400

    Updating RequestHandler::renderAs() to respect DS constant, fixing issues on PHP4 + Windows. Fixes #97

--- cake/tests/cases/libs/controller/components/request_handler.test.php
+++ cake/tests/cases/libs/controller/components/request_handler.test.php
@@ -267,13 +267,13 @@ class RequestHandlerComponentTest extends CakeTestCase {
  **/
 	function testRenderAsCalledTwice() {
 		$this->RequestHandler->renderAs($this->Controller, 'xml');
-		$this->assertEqual($this->Controller->viewPath, 'request_handler_test/xml');
+		$this->assertEqual($this->Controller->viewPath, 'request_handler_test' . DS . 'xml');
 		$this->assertEqual($this->Controller->layoutPath, 'xml');
 		
 		$this->assertTrue(in_array('Xml', $this->Controller->helpers));
 
 		$this->RequestHandler->renderAs($this->Controller, 'js');
-		$this->assertEqual($this->Controller->viewPath, 'request_handler_test/js');
+		$this->assertEqual($this->Controller->viewPath, 'request_handler_test' . DS . 'js');
 		$this->assertEqual($this->Controller->layoutPath, 'js');
 		$this->assertTrue(in_array('Js', $this->Controller->helpers));
 	}
