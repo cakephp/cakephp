62810fb4f424c735e1d6c4458e128e0948cec1cd
predominant <graham@grahamweldon.com>
2009-11-06 18:04:38 +1100

    Updating more Copyright dates.

--- cake/console/cake.bat
+++ cake/console/cake.bat
@@ -4,13 +4,13 @@
 :: PHP versions 4 and 5
 ::
 :: CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
-:: Copyright 2005-2007, Cake Software Foundation, Inc.
+:: Copyright 2005-2009, Cake Software Foundation, Inc.
 ::
 :: Licensed under The MIT License
 :: Redistributions of files must retain the above copyright notice.
 ::
 :: @filesource
-:: @copyright		Copyright 2005-2007, Cake Software Foundation, Inc.
+:: @copyright		Copyright 2005-2009, Cake Software Foundation, Inc.
 :: @link				http://cakephp.org CakePHP(tm) Project
 :: @package			cake
 :: @subpackage		cake.cake.console
