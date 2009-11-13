9376826aa4232a103494166de1a60a84671c2064
mark_story <mark@mark-story.com>
2009-11-13 14:42:40 -0500

    Fixing HttpSocket::buildUri when host key is empty. Tests added Fixes #271

--- cake/libs/http_socket.php
+++ cake/libs/http_socket.php
@@ -564,7 +564,8 @@ class HttpSocket extends CakeSocket {
 		$stripIfEmpty = array(
 			'query' => '?%query',
 			'fragment' => '#%fragment',
-			'user' => '%user:%pass@'
+			'user' => '%user:%pass@',
+			'host' => '%host:%port/'
 		);
 
 		foreach ($stripIfEmpty as $key => $strip) {
@@ -577,7 +578,6 @@ class HttpSocket extends CakeSocket {
 		if (array_key_exists($uri['scheme'], $defaultPorts) && $defaultPorts[$uri['scheme']] == $uri['port']) {
 			$uriTemplate = str_replace(':%port', null, $uriTemplate);
 		}
-
 		foreach ($uri as $property => $value) {
 			$uriTemplate = str_replace('%'.$property, $value, $uriTemplate);
 		}
