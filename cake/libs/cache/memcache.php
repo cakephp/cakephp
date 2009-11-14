06d4e5e5531b1fcee464495fa6e83a372e3cb904
mark_story <mark@mark-story.com>
2009-11-14 18:50:23 -0500

    Reformatting code in memcache. Adding unset() to test case to remove unnecessary keys that can cause test failure.

--- cake/libs/cache/memcache.php
+++ cake/libs/cache/memcache.php
@@ -61,7 +61,10 @@ class MemcacheEngine extends CacheEngine {
 			return false;
 		}
 		parent::init(array_merge(array(
-			'engine'=> 'Memcache', 'prefix' => Inflector::slug(APP_DIR) . '_', 'servers' => array('127.0.0.1'), 'compress'=> false
+			'engine'=> 'Memcache', 
+			'prefix' => Inflector::slug(APP_DIR) . '_', 
+			'servers' => array('127.0.0.1'),
+			'compress'=> false
 			), $settings)
 		);
 
@@ -100,7 +103,7 @@ class MemcacheEngine extends CacheEngine {
  */
 	function write($key, &$value, $duration) {
 		$expires = time() + $duration;
-		$this->__Memcache->set($key.'_expires', $expires, $this->settings['compress'], $expires);
+		$this->__Memcache->set($key . '_expires', $expires, $this->settings['compress'], $expires);
 		return $this->__Memcache->set($key, $value, $this->settings['compress'], $expires);
 	}
 /**
@@ -112,7 +115,7 @@ class MemcacheEngine extends CacheEngine {
  */
 	function read($key) {
 		$time = time();
-		$cachetime = intval($this->__Memcache->get($key.'_expires'));
+		$cachetime = intval($this->__Memcache->get($key . '_expires'));
 		if ($cachetime < $time || ($time + $this->settings['duration']) < $cachetime) {
 			return false;
 		}
