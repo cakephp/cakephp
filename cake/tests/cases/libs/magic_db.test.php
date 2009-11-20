c6c747450252c01cc18495c083df16cd1dea353c
Juan Basso <jrbasso@gmail.com>
2009-11-19 22:57:53 -0200

    Fix test of the size of lines in MagicDb (included test for it).
    Signed-off-by: Mark Story <mark@mark-story.com>

--- cake/tests/cases/libs/magic_db.test.php
+++ cake/tests/cases/libs/magic_db.test.php
@@ -116,6 +116,9 @@ class MagicDbTest extends UnitTestCase {
 		$r = $this->Db->toArray(array('yeah'));
 		$this->assertTrue($r === array('yeah'));
 
+		$r = $this->Db->toArray("# FILE_ID DB\r\n# Date:2009-10-10\r\n# Source:xxx.php");
+		$this->assertTrue($r === array());
+
 		$r = $this->Db->toArray('foo');
 		$this->assertTrue($r === array());
 
