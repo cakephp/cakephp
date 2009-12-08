8b9ee22c84030ad5b648e9c4fb20a3bce9c28d7e
predominant <graham@grahamweldon.com>
2009-12-08 20:18:19 +1100

    Removing "Long description for file" from /cake/console tree.

--- cake/console/templates/skel/webroot/test.php
+++ cake/console/templates/skel/webroot/test.php
@@ -2,8 +2,6 @@
 /**
  * Web Access Frontend for TestSuite
  *
- * Long description for file
- *
  * PHP versions 4 and 5
  *
  * CakePHP(tm) Tests <https://trac.cakephp.org/wiki/Developement/TestSuite>
