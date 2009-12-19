8586af33a2466e8812bc07080c213bec86ab76c1
Mark Story <mark@mark-story.com>
2009-12-19 13:31:50 -0500

    Removing debug = 3 docs.

--- app/config/core.php
+++ app/config/core.php
@@ -29,7 +29,6 @@
  * Development Mode:
  * 	1: Errors and warnings shown, model caches refreshed, flash messages halted.
  * 	2: As in 1, but also with full debug messages and SQL output.
- * 	3: As in 2, but also with full controller dump.
  *
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
