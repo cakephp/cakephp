cakephp/cakephp

commit efa36abdf049a57662bc3323828957de3742dfb1

Fixing double merging of url options in paginator helper. This was causing duplicate passed url parameters in next()/prev(). Test cases added. Fixes #391

cake/libs/view/helpers/paginator.php
@@ -411,12 +411,12 @@ class PaginatorHelper extends AppHelper {
 			${$key} = $options[$key];
 			unset($options[$key]);
 		}
-		$url = array_merge(array('page' => $paging['page'] + ($which == 'Prev' ? $step * -1 : $step)), $url);
+		$urlParams = array('page' => $paging['page'] + ($which == 'Prev' ? $step * -1 : $step));
 
 		if ($this->{$check}($model)) {
-			return $this->link($title, $url, array_merge($options, array('escape' => $escape, 'class' => $class)));
+			return $this->link($title, $urlParams, array_merge($options, compact('escape', 'class', 'url')));
 		} else {
-			return $this->Html->tag($tag, $title, array_merge($options, array('class' => $class, 'escape' => $escape)));
+			return $this->Html->tag($tag, $title, array_merge($options, compact('escape', 'class')));
 		}
 	}
 

cake/tests/cases/libs/view/helpers/paginator.test.php
@@ -745,6 +745,25 @@ class PaginatorHelperTest extends CakeTestCase {
 			'/a',
 		);
 		$this->assertTags($result, $expected);
+
+		$this->Paginator->params['paging'] = array(
+			'Client' => array(
+				'page' => 2, 'current' => 1, 'count' => 13, 'prevPage' => true, 
+				'nextPage' => false, 'pageCount' => 2,
+				'defaults' => array(),
+				'options' => array(
+					'page' => 2, 'limit' => 10, 'order' => array(), 'conditions' => array()
+				)
+			)
+		);
+		$this->Paginator->options(array('url' => array(12, 'page' => 3)));
+		$result = $this->Paginator->prev('Prev', array('url' => array(12, 'foo' => 'bar')));
+		$expected = array(
+			'a' => array('href' => '/index/12/page:1/limit:10/foo:bar', 'class' => 'prev'),
+			'Prev',
+			'/a',
+		);
+		$this->assertTags($result, $expected);
 	}
 
 /**

commit 1a387e6b8ac90c8ab2f96a887d4d9dad8b536a64

Fixing url parameters set with options() or beforeRender not being merged on next() / prev(). Tests added. Fixes #129 This reverses the change made in efa36abdf049a57662bc3323828957de3742dfb1 which broke this behavior.

cake/libs/view/helpers/paginator.php
@@ -411,10 +411,10 @@ class PaginatorHelper extends AppHelper {
 			${$key} = $options[$key];
 			unset($options[$key]);
 		}
-		$urlParams = array('page' => $paging['page'] + ($which == 'Prev' ? $step * -1 : $step));
+		$url = array_merge(array('page' => $paging['page'] + ($which == 'Prev' ? $step * -1 : $step)), $url);
 
 		if ($this->{$check}($model)) {
-			return $this->link($title, $urlParams, array_merge($options, compact('escape', 'class', 'url')));
+			return $this->link($title, $url, array_merge($options, compact('escape', 'class')));
 		} else {
 			return $this->Html->tag($tag, $title, array_merge($options, compact('escape', 'class')));
 		}

cake/tests/cases/libs/view/helpers/paginator.test.php
@@ -585,6 +585,18 @@ class PaginatorHelperTest extends CakeTestCase {
 			array('plugin' => null, 'controller' => 'articles', 'action' => 'index', 'pass' => array('2'), 'named' => array('foo' => 'bar'), 'form' => array(), 'url' => array('url' => 'articles/index/2/foo:bar'), 'bare' => 0),
 			array('plugin' => null, 'controller' => null, 'action' => null, 'base' => '/', 'here' => '/articles/', 'webroot' => '/', 'passedArgs' => array(0 => '2', 'foo' => 'bar'))
 		));
+		$this->Paginator->params['paging'] = array(
+			'Article' => array(
+				'page' => 1, 'current' => 3, 'count' => 13, 
+				'prevPage' => false, 'nextPage' => true, 'pageCount' => 8,
+				'defaults' => array(
+					'limit' => 3, 'step' => 1, 'order' => array(), 'conditions' => array()
+				),
+				'options' => array(
+					'page' => 1, 'limit' => 3, 'order' => array(), 'conditions' => array()
+				)
+			)
+		);
 
 		$this->Paginator->params['pass'] = array(2);
 		$this->Paginator->params['named'] = array('foo' => 'bar');
@@ -613,6 +625,14 @@ class PaginatorHelperTest extends CakeTestCase {
 			array('span' => array()), array('a' => array('href' => '/articles/index/2/page:6/foo:bar')), '6', '/a', '/span',
 			' | ',
 			array('span' => array()), array('a' => array('href' => '/articles/index/2/page:7/foo:bar')), '7', '/a', '/span',
+		);
+		$this->assertTags($result, $expected);
+
+		$result = $this->Paginator->next('Next');
+		$expected = array(
+			'a' => array('href' => '/articles/index/2/page:2/foo:bar', 'class' => 'next'),
+			'Next',
+			'/a'
 		);
 		$this->assertTags($result, $expected);
 	}
@@ -757,7 +777,7 @@ class PaginatorHelperTest extends CakeTestCase {
 			)
 		);
 		$this->Paginator->options(array('url' => array(12, 'page' => 3)));
-		$result = $this->Paginator->prev('Prev', array('url' => array(12, 'foo' => 'bar')));
+		$result = $this->Paginator->prev('Prev', array('url' => array('foo' => 'bar')));
 		$expected = array(
 			'a' => array('href' => '/index/12/page:1/limit:10/foo:bar', 'class' => 'prev'),
 			'Prev',