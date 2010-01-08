11a5403aaf4049b4a3e605b14057ba17ff093990
Mark Story <mark@mark-story.com>
2010-01-08 18:35:01 -0500

    Removing unused variables from inflections.php.  These variables are artifacts from previous refactorings and have been non-functional for quite sometime.  Fixes #163

--- app/config/inflections.php
+++ app/config/inflections.php
@@ -53,18 +53,5 @@
  *  $singularRules = array('/(s)tatuses$/i' => '\1\2tatus', '/(matr)ices$/i' =>'\1ix','/(vert|ind)ices$/i')
  */
 	$singularRules = array();
-/**
- * This is a key only array of singular words that should not be inflected.
- * You should not have to change this value below if you do change it use same format
- * as the $uninflectedPlural above.
- */
-	$uninflectedSingular = $uninflectedPlural;
-/**
- * This is a key => value array of singular irregular words.
- * Most of the time this will be a reverse of the above $irregularPlural array
- * You should not have to change this value below if you do change it use same format
- *
- * $irregularSingular = array('atlases' => 'atlas', 'beefs' => 'beef', 'brothers' => 'brother')
- */
-	$irregularSingular = array_flip($irregularPlural);
+
 ?>
