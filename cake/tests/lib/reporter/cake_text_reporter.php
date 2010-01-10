489958ea48f43e1832130d527ceb76ca4b22ee8f
Mark Story <mark@mark-story.com>
2010-01-10 15:56:24 -0500

    CakeTextReporter now outputs code coverage.

--- cake/tests/lib/reporter/cake_text_reporter.php
+++ cake/tests/lib/reporter/cake_text_reporter.php
@@ -62,6 +62,13 @@ class CakeTextReporter extends CakeBaseReporter {
 		if (function_exists('memory_get_peak_usage')) {
 			echo 'Peak memory use: (in bytes): ' . number_format(memory_get_peak_usage()) . "\n";
 		}
+		if (
+			isset($this->params['codeCoverage']) && 
+			$this->params['codeCoverage'] && 
+			class_exists('CodeCoverageManager')
+		) {
+			CodeCoverageManager::report();
+		}
 	}
 
 /**
