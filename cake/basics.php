6e22261786a4355adcaec1def633de0753f48274
Robust Solution <hussein_elharake@yahoo.com>
2010-01-20 12:39:26 +0000

    small optimization in time units

--- cake/basics.php
+++ cake/basics.php
@@ -24,12 +24,12 @@
  * Basic defines for timing functions.
  */
 	define('SECOND', 1);
-	define('MINUTE', 60 * SECOND);
-	define('HOUR', 60 * MINUTE);
-	define('DAY', 24 * HOUR);
-	define('WEEK', 7 * DAY);
-	define('MONTH', 30 * DAY);
-	define('YEAR', 365 * DAY);
+	define('MINUTE', 60);
+	define('HOUR', 3600);
+	define('DAY', 86400);
+	define('WEEK', 604800);
+	define('MONTH', 2592000);
+	define('YEAR', 31536000);
 
 /**
  * Patch for PHP < 5.0
