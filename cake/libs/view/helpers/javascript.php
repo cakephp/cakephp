ab688b88f408132a272b71b1c93dbb8ec42fcbad
Mark Story <mark@mark-story.com>
2010-02-17 22:21:48 -0500

    Adding test to check that postfix and prefix options don't go into inner objects.  Tests added, fixes #348

--- cake/libs/view/helpers/javascript.php
+++ cake/libs/view/helpers/javascript.php
@@ -632,7 +632,10 @@ class JavascriptHelper extends AppHelper {
 
 			foreach ($data as $key => $val) {
 				if (is_array($val) || is_object($val)) {
-					$val = $this->object($val, array_merge($options, array('block' => false)));
+					$val = $this->object(
+						$val,
+						array_merge($options, array('block' => false, 'prefix' => '', 'postfix' => ''))
+					);
 				} else {
 					$quoteStrings = (
 						!count($options['stringKeys']) ||
