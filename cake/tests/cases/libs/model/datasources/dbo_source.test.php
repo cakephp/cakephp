416abed211cb4af4d0729b1400977e719c079a66
Juan Basso <juan.basso@digitro.com.br>
2010-02-19 13:41:52 -0200

    Test of ticket #323.
    Signed-off-by: Mark Story <mark@mark-story.com>

--- cake/tests/cases/libs/model/datasources/dbo_source.test.php
+++ cake/tests/cases/libs/model/datasources/dbo_source.test.php
@@ -4033,6 +4033,10 @@ class DboSourceTest extends CakeTestCase {
 		$result = $this->testDb->name('Function(Something.foo) AS x');
 		$expected = 'Function(`Something`.`foo`) AS `x`';
 		$this->assertEqual($result, $expected);
+
+		$result = $this->testDb->name('name-with-minus');
+		$expected = '`name-with-minus`';
+		$this->assertEqual($result, $expected);
 	}
 
 /**
