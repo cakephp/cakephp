781af4a80cd7713750fabc53c69ff1faf628bd2f
Mark Story <mark@mark-story.com>
2010-04-04 00:13:16 -0400

    Updating doc block for Router::__connectDefaultRoutes()

--- cake/libs/router.php
+++ cake/libs/router.php
@@ -540,17 +540,29 @@ class Router {
  * Connects the default, built-in routes, including prefix and plugin routes. The following routes are created
  * in the order below:
  *
+ * For each of the Routing.prefixes the following routes are created. Routes containing `:plugin` are only
+ * created when your application has one or more plugins.
+ *
+ * - `/:prefix/:plugin` a plugin shortcut route.
+ * - `/:prefix/:plugin/:action/*` a plugin shortcut route.
  * - `/:prefix/:plugin/:controller`
  * - `/:prefix/:plugin/:controller/:action/*`
  * - `/:prefix/:controller`
  * - `/:prefix/:controller/:action/*`
+ *
+ * If plugins are found in your application the following routes are created:
+ *
+ * - `/:plugin` a plugin shortcut route.
+ * - `/:plugin/:action/*` a plugin shortcut route.
  * - `/:plugin/:controller`
  * - `/:plugin/:controller/:action/*`
+ *
+ * And lastly the following catch-all routes are connected.
+ *
  * - `/:controller'
  * - `/:controller/:action/*'
  *
- * A prefix route is generated for each Routing.prefixes declared in core.php. You can disable the
- * connection of default routes with Router::defaults().
+ * You can disable the connection of default routes with Router::defaults().
  *
  * @return void
  * @access private
