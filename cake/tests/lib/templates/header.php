4ef899fa54315357d58b905b75fc216fec276666
Mark Story <mark@mark-story.com>
2010-04-14 22:41:12 -0400

    Updating test suite to say 2.0!

--- cake/tests/lib/templates/header.php
+++ cake/tests/lib/templates/header.php
@@ -22,7 +22,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>CakePHP Test Suite 1.3</title>
+		<title>CakePHP Test Suite 2.0</title>
 		<style type="text/css">
 			h3 {font-size: 170%; padding-top: 1em}
 			a {font-size: 120%}
@@ -122,4 +122,4 @@
 				<h1>CakePHP: the rapid development php framework</h1>
 			</div>
 			<div id="content">
-			<h2>CakePHP Test Suite 1.3</h2>
+			<h2>CakePHP Test Suite 2.0</h2>
