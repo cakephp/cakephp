70b3225ff03729778b1455b01f77105c195f61e0
Mark Story <mark@mark-story.com>
2010-04-23 00:46:13 -0400

    Fixing group path checking in test manager.
    Conflicts:
    
    	cake/tests/lib/test_manager.php

--- cake/tests/lib/test_manager.php
+++ cake/tests/lib/test_manager.php
@@ -154,7 +154,7 @@ class TestManager {
 	public function runGroupTest($groupTestName, &$reporter) {
 		$filePath = $this->_getTestsPath('groups') . DS . strtolower($groupTestName) . $this->_groupExtension;
 
-		if (!file_exists($filePath) || strpos($testCaseFileWithPath, '..')) {
+		if (!file_exists($filePath) || strpos($filePath, '..')) {
 			trigger_error(sprintf(
 					__("Group test %s cannot be found at %s", true), 
 					htmlentities($groupTestName), 
