f6b49cd865b1c5f428db646ea489ecb143bb09ee
Mark Story <mark@mark-story.com>
2010-05-08 18:12:20 -0400

    Updating file headers.

--- cake/tests/cases/libs/html_coverage_report.test.php
+++ cake/tests/cases/libs/html_coverage_report.test.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * Test case for HtmlCoverageReport
+ *
+ * PHP5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.cake
+ * @since         CakePHP(tm) v 2.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 require_once CAKE . 'tests' . DS . 'lib' . DS . 'coverage' . DS . 'html_coverage_report.php';
 
--- cake/tests/lib/coverage/html_coverage_report.php
+++ cake/tests/lib/coverage/html_coverage_report.php
@@ -1,10 +1,21 @@
 <?php
-
 /**
- * Generates an HTML coverage report from data provided by PHPUnit.
+ * Generates code coverage reports in HTML from data obtained from PHPUnit
  *
- * @package default
- * @author Mark Story
+ * PHP5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.cake
+ * @since         CakePHP(tm) v 2.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 class HtmlCoverageReport {
 /**
