17f338a9b22ccbc9b7d58fafd2b3dea2e794d900
Mark Story <mark@mark-story.com>
2010-05-13 00:18:22 -0400

    Moving methods up into CakeBaseReporter. Starting to make CakeTextReporter work well with PHPUnit so the Text code coverage reporting can be implemented.

--- cake/tests/lib/reporter/cake_html_reporter.php
+++ cake/tests/lib/reporter/cake_html_reporter.php
@@ -28,7 +28,7 @@ PHPUnit_Util_Filter::addFileToFilter(__FILE__, 'DEFAULT');
  * @package cake
  * @subpackage cake.tests.lib
  */
-class CakeHtmlReporter extends CakeBaseReporter implements PHPUnit_Framework_TestListener {
+class CakeHtmlReporter extends CakeBaseReporter {
 
 /**
  * Paints the top of the web page setting the
@@ -152,7 +152,7 @@ class CakeHtmlReporter extends CakeBaseReporter implements PHPUnit_Framework_Tes
  * Paints the end of the test with a summary of
  * the passes and failures.
  *
- * @param string $test_name Name class of test.
+ * @param PHPUnit_Framework_TestResult $result Result object
  * @return void
  */
 	public function paintFooter($result) {
@@ -161,7 +161,7 @@ class CakeHtmlReporter extends CakeBaseReporter implements PHPUnit_Framework_Tes
 		echo "<div style=\"";
 		echo "padding: 8px; margin: 1em 0; background-color: $colour; color: white;";
 		echo "\">";
-		echo $result->count() . "/" . $result->count() - $result->skippedCount();
+		echo $result->count() . "/" . ($result->count() - $result->skippedCount());
 		echo " test methods complete:\n";
 		echo "<strong>" . count($result->passed()) . "</strong> passes, ";
 		echo "<strong>" . $result->failureCount() . "</strong> fails, ";
@@ -272,9 +272,6 @@ class CakeHtmlReporter extends CakeBaseReporter implements PHPUnit_Framework_Tes
 		echo "<div class='msg'>" . sprintf(__('File: %s'), $context['file']) . "</div>\n";
 		echo "<div class='msg'>" . sprintf(__('Method: %s'), $realContext['function']) . "</div>\n";
 		echo "<div class='msg'>" . sprintf(__('Line: %s'), $context['line']) . "</div>\n";
-		//$breadcrumb = $this->getTestList();
-		//array_shift($breadcrumb);
-		//echo "<div>" . implode(" -&gt; ", $breadcrumb) . "</div>\n";
 		echo "</li>\n";
 	}
 
@@ -368,104 +365,13 @@ class CakeHtmlReporter extends CakeBaseReporter implements PHPUnit_Framework_Tes
 		return htmlentities($message, ENT_COMPAT, $this->_characterSet);
 	}
 
-	public function paintResult(PHPUnit_Framework_TestResult $result) {
-
-		/*if ($result->errorCount() > 0) {
-			$this->printErrors($result);
-		}
-
-		if ($result->failureCount() > 0) {
-			$this->printFailures($result);
-		}
-
-		if ($result->skippedCount() > 0) {
-			 $this->printIncompletes($result);
-		}
-
-		if ($result->skippedCount() > 0) {
-			$this->printSkipped($result);
-		}*/
-
-		$this->paintFooter($result);
-	}
-
-/**
-* An error occurred.
-*
-* @param  PHPUnit_Framework_Test $test
-* @param  Exception              $e
-* @param  float                  $time
-*/
-	public function addError(PHPUnit_Framework_Test $test, Exception $e, $time) {
-		$this->paintException($e);
-	}
-
-/**
-* A failure occurred.
-*
-* @param  PHPUnit_Framework_Test $test
-* @param  PHPUnit_Framework_AssertionFailedError $e
-* @param  float $time
-*/
-	public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time) {
-		$this->paintFail($e);
-	}
-
-/**
-* Incomplete test.
-*
-* @param  PHPUnit_Framework_Test $test
-* @param  Exception $e
-* @param  float $time
-*/
-	public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time) {
-
-	}
-
-/**
-* Skipped test.
-*
-* @param  PHPUnit_Framework_Test $test
-* @param  Exception $e
-* @param  float $time
-*/
-	public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time) {
-	}
-
 /**
  * A test suite started.
  *
  * @param  PHPUnit_Framework_TestSuite $suite
  */
 	public function startTestSuite(PHPUnit_Framework_TestSuite $suite) {
-		echo '<h2>' . sprintf(__('Running  %s'), $suite->getName()) . '</h2>';
-	}
-
-/**
- * A test suite ended.
- *
- * @param  PHPUnit_Framework_TestSuite $suite
- */
-	public function endTestSuite(PHPUnit_Framework_TestSuite $suite) {
-	}
-
-/**
- * A test started.
- *
- * @param  PHPUnit_Framework_Test $test
- */
-	public function startTest(PHPUnit_Framework_Test $test) {
-	}
-
-/**
- * A test ended.
- *
- * @param  PHPUnit_Framework_Test $test
- * @param  float $time
- */
-	public function endTest(PHPUnit_Framework_Test $test, $time) {
-		$this->numAssertions += $test->getNumAssertions();
-		$this->paintPass($test, $time);
+		echo '<h2>' . sprintf(__('Running  %s'), $suite->getName())  . '</h2>';
 	}
 }
 ?>
