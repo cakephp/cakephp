d974e65b1a1c6bf8c89e97e842c48d369393ec42
Mark Story <mark@mark-story.com>
2010-05-13 23:08:15 -0400

    Adding tests for welsh added in previous commit.

--- cake/tests/cases/libs/l10n.test.php
+++ cake/tests/cases/libs/l10n.test.php
@@ -428,6 +428,10 @@ class L10nTest extends CakeTestCase {
 		$result = $l10n->map(array('xho', 'xh'));
 		$expected = array('xho' => 'xh', 'xh' => 'xho');
 		$this->assertEqual($result, $expected);
+	
+		$result = $l10n->map(array('cy', 'cym'));
+		$expected = array('cym' => 'cy', 'cy' => 'cym');
+		$this->assertEqual($result, $expected);
 
 		$result = $l10n->map(array('yid', 'yi'));
 		$expected = array('yid' => 'yi', 'yi' => 'yid');
@@ -882,6 +886,12 @@ class L10nTest extends CakeTestCase {
 		$result = $l10n->catalog(array('vi'));
 		$expected = array(
 			'vi' => array('language' => 'Vietnamese', 'locale' => 'vie', 'localeFallback' => 'vie', 'charset' => 'utf-8')
+		);
+		$this->assertEqual($result, $expected);
+
+		$result = $l10n->catalog(array('cy'));
+		$expected = array(
+			'cy' => array('language' => 'Welsh', 'locale' => 'cym', 'localeFallback' => 'cym', 'charset' => 'utf-8')
 		);
 		$this->assertEqual($result, $expected);
 
