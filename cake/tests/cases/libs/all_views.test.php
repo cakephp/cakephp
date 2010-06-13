268b269eaa184e4cf212b81239e35af1d0be74cc
mark_story <mark@mark-story.com>
2010-06-13 10:14:28 -0400

    Updating file headers on test cases. Changing license on test cases to match the rest of the framework as the tests no longer interact with GPL code.

--- cake/tests/cases/libs/all_views.test.php
+++ cake/tests/cases/libs/all_views.test.php
@@ -1,25 +1,25 @@
 <?php
 /**
- * ViewsGroupTest file
+ * AllViewsTest file
  *
- * PHP versions 4 and 5
+ * PHP 5
  *
- * CakePHP(tm) Tests <http://book.cakephp.org/view/1196/Testing>
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
- *  Licensed under The Open Group Test Suite License
- *  Redistributions of files must retain the above copyright notice.
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://book.cakephp.org/view/1196/Testing CakePHP(tm) Tests
+ * @link          http://cakephp.org CakePHP(tm) Project
  * @package       cake
- * @subpackage    cake.tests.groups
+ * @subpackage    cake.tests.cases
  * @since         CakePHP(tm) v 1.2.0.4206
- * @license       http://www.opensource.org/licenses/opengroup.php The Open Group Test Suite License
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
 /**
- * ViewsGroupTest class
+ * AllViewsTest class
  *
  * This test group will run view class tests (view, theme)
  *
--- cake/tests/cases/libs/view/all_js_helpers.test.php
+++ cake/tests/cases/libs/view/all_js_helpers.test.php
@@ -1,25 +1,24 @@
 <?php
 /**
- * AllCoreJavascriptHelpersGroupTest file
+ * AllJavascriptHelpersTest file
  *
+ * PHP 5
  *
- * PHP versions 4 and 5
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
- * CakePHP(tm) Tests <http://book.cakephp.org/view/1196/Testing>
- * Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
  *
- *  Licensed under The Open Group Test Suite License
- *  Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://book.cakephp.org/view/1196/Testing CakePHP(tm) Tests
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
  * @package       cake
- * @subpackage    cake.tests.groups
+ * @subpackage    cake.tests.cases
  * @since         CakePHP(tm) v 1.3
- * @license       http://www.opensource.org/licenses/opengroup.php The Open Group Test Suite License
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 /**
- * AllCoreJavascriptHelpersGroupTest class
+ * AllJavascriptHelpersTest class
  *
  * This test group will run all test in the cases/libs/view/helpers directory related
  * to Js helper and its engines
