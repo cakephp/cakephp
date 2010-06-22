e9684fb1aa4a6c1741a4fae2aa3a3cd663aa5674
mark_story <mark@mark-story.com>
2010-06-21 22:55:12 -0400

    Porting Libs group to PHP Unit.

---
+++ cake/tests/cases/libs/all_libs.test.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * AllLibsTest file
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.tests.cases
+ * @since         CakePHP(tm) v 2.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * AllLibsTest class
+ *
+ * This test group will run all non mvc related lib class tests
+ *
+ * @package       cake
+ * @subpackage    cake.tests.cases
+ */
+class AllLibsTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * suite method, defines tests for this suite.
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new PHPUnit_Framework_TestSuite('All non-MVC lib class tests');
+		
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'basics.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'cake_session.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'debugger.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'error.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'file.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'folder.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'log' . DS . 'file_log.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'cake_log.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'class_registry.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'sanitize.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'set.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'string.test.php');
+		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'validation.test.php');
+		return $suite;
+	}
+}
