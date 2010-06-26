c084456a08249846b049dbe8ae6ea797a4078632
mark_story <mark@mark-story.com>
2010-06-25 23:36:08 -0400

    Moving console and database groups to PHPUnit. Both have some fails caused by the current fixture implementation. Removing groups folder as its no longer used.

---
+++ cake/tests/cases/libs/all_database.test.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * AllDatabaseTest file
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.tests.cases
+ * @since         CakePHP(tm) v 2.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * AllDatabaseTest class
+ *
+ * This test group will run database tests not in model or behavior group.
+ *
+ * @package       cake
+ * @subpackage    cake.tests.groups
+ */
+class AllDatabaseTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * suite method, defines tests for this suite.
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new PHPUnit_Framework_TestSuite('Datasources, Schema and DbAcl tests');
+
+		$path = CORE_TEST_CASES . DS . 'libs' . DS . 'model' . DS;
+
+		$tasks = array('db_acl', 'cake_schema', 'connection_manager', 'datasources' . DS . 'dbo_source');
+		foreach ($tasks as $task) {
+			$suite->addTestFile($path . $task . '.test.php');
+		}
+		return $suite;
+	}
+}
