35d232f08fdfd764f7b105e0e4f0fda22ced4376
mark_story <mark@mark-story.com>
2010-06-27 12:50:03 -0400

    Updating documentation in app/core.php to match historical values. Updating skel/core.php to reflect current core.php.

--- app/config/core.php
+++ app/config/core.php
@@ -188,9 +188,9 @@
  * in 'Session.timeout' is multiplied according to the settings here.
  * Valid values:
  *
- * 'high'	Session timeout in 'Session.timeout' x 10
- * 'medium'	Session timeout in 'Session.timeout' x 5040
- * 'low'		Session timeout in 'Session.timeout' x 2628000
+ * 'high'   Session timeout in 'Session.timeout' x 10
+ * 'medium' Session timeout in 'Session.timeout' x 100
+ * 'low'    Session timeout in 'Session.timeout' x 300
  *
  * CakePHP session IDs are also regenerated between requests if
  * 'Security.level' is set to 'high'.
--- cake/console/templates/skel/config/core.php
+++ cake/console/templates/skel/config/core.php
@@ -29,7 +29,6 @@
  * Development Mode:
  * 	1: Errors and warnings shown, model caches refreshed, flash messages halted.
  * 	2: As in 1, but also with full debug messages and SQL output.
- * 	3: As in 2, but also with full controller dump.
  *
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
@@ -70,20 +69,10 @@
 	//Configure::write('App.baseUrl', env('SCRIPT_NAME'));
 
 /**
- * Uncomment the define below to use CakePHP admin routes.
- *
- * The value of the define determines the name of the route
- * and its associated controller actions:
- *
- * 'admin' -> admin_index() and /admin/controller/index
- * 'superuser' -> superuser_index() and /superuser/controller/index
- *
- * [Note Routing.admin is deprecated in 1.3.  Use Routing.prefixes instead]
- */
-	//Configure::write('Routing.admin', 'admin');
-
-/**
  * Uncomment the define below to use CakePHP prefix routes.
+ *
+ * The value of the define determines the names of the routes
+ * and their associated controller actions:
  *
  * Set to an array of prefixes you want to use in your application. Use for
  * admin or other prefixed routes.
@@ -93,6 +82,8 @@
  * Enables:
  *	`admin_index()` and `/admin/controller/index`
  *	`manager_index()` and `/manager/controller/index`
+ *
+ * [Note Routing.admin is deprecated in 1.3.  Use Routing.prefixes instead]
  */
 	//Configure::write('Routing.prefixes', array('admin'));
 
@@ -167,11 +158,16 @@
 
 /**
  * The name of CakePHP's session cookie.
+ *
+ * Note the guidelines for Session names states: "The session name references
+ * the session id in cookies and URLs. It should contain only alphanumeric
+ * characters."
+ * @link http://php.net/session_name
  */
 	Configure::write('Session.cookie', 'CAKEPHP');
 
 /**
- * Session time out time (in seconds).
+ * Session time out time (in minutes).
  * Actual value depends on 'Security.level' setting.
  */
 	Configure::write('Session.timeout', '120');
@@ -192,9 +188,9 @@
  * in 'Session.timeout' is multiplied according to the settings here.
  * Valid values:
  *
- * 'high'	Session timeout in 'Session.timeout' x 10
- * 'medium'	Session timeout in 'Session.timeout' x 100
- * 'low'		Session timeout in 'Session.timeout' x 300
+ * 'high'   Session timeout in 'Session.timeout' x 10
+ * 'medium' Session timeout in 'Session.timeout' x 100
+ * 'low'    Session timeout in 'Session.timeout' x 300
  *
  * CakePHP session IDs are also regenerated between requests if
  * 'Security.level' is set to 'high'.
@@ -211,6 +207,15 @@
  */
 	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
 
+/**
+ * Apply timestamps with the last modified time to static assets (js, css, images).
+ * Will append a querystring parameter containing the time the file was modified. This is
+ * useful for invalidating browser caches.
+ *
+ * Set to `true` to apply timestamps, when debug = 0, or set to 'force' to always enable
+ * timestamping.
+ */
+	//Configure::write('Asset.timestamp', true);
 /**
  * Compress CSS output by removing comments, whitespace, repeating tags, etc.
  * This requires a/var/cache directory to be writable by the web server for caching.
@@ -241,11 +246,6 @@
  */
 	//date_default_timezone_set('UTC');
 
-/**
- * If you are on PHP 5.3 uncomment this line and correct your server timezone
- * to fix the date & time related errors.
- */
-	//date_default_timezone_set('UTC');
 /**
  *
  * Cache Engine Configuration
