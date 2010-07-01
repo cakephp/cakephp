8b9d9b1b12df64b7d228d0a222844cfe6306de06
mark_story <mark@mark-story.com>
2010-06-30 20:37:09 -0400

    Removing literal '/' for DS which works better under windows.

--- cake/dispatcher.php
+++ cake/dispatcher.php
@@ -583,7 +583,7 @@ class Dispatcher extends Object {
 		if ($parts[0] === 'theme') {
 			$themeName = $parts[1];
 			unset($parts[0], $parts[1]);
-			$fileFragment = implode('/', $parts);
+			$fileFragment = implode(DS, $parts);
 			
 			$path = App::themePath($themeName) . 'webroot' . DS;
 			if (file_exists($path . $fileFragment)) {
@@ -592,7 +592,7 @@ class Dispatcher extends Object {
 		} else {
 			$plugin = $parts[0];
 			unset($parts[0]);
-			$fileFragment = implode('/', $parts);
+			$fileFragment = implode(DS, $parts);
 			$pluginWebroot = App::pluginPath($plugin) . 'webroot' . DS;
 			if (file_exists($pluginWebroot . $fileFragment)) {
 				$assetFile = $pluginWebroot . $fileFragment;
