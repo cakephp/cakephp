037a7628c402177351480329d5282116140c33bd
Scott Reeves <cottser@mailworks.org>
2010-07-19 15:25:42 -0700

    Fixing typo in parameter description.
    Signed-off-by: mark_story <mark@mark-story.com>

--- cake/libs/cake_session.php
+++ cake/libs/cake_session.php
@@ -743,7 +743,7 @@ class CakeSession extends Object {
  * Helper function called on write for database sessions.
  *
  * @param integer $id ID that uniquely identifies session in database
- * @param mixed $data The value of the the data to be saved.
+ * @param mixed $data The value of the data to be saved.
  * @return boolean True for successful write, false otherwise.
  * @access private
  */
