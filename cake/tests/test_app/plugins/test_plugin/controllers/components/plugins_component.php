6c87a026b605c112990d93a49f4c451b3bcee828
mark_story <mark@mark-story.com>
2010-08-26 23:24:32 -0400

    Updating base class.

--- cake/tests/test_app/plugins/test_plugin/controllers/components/plugins_component.php
+++ cake/tests/test_app/plugins/test_plugin/controllers/components/plugins_component.php
@@ -17,6 +17,6 @@
  * @since         CakePHP(tm) v 1.2.0.4206
  * @license       http://www.opensource.org/licenses/opengroup.php The Open Group Test Suite License
  */
-class PluginsComponentComponent extends Object {
+class PluginsComponentComponent extends Component {
 	public $components = array('TestPlugin.OtherComponent');
 }
