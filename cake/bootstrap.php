d6b43c0e2935acd67e0ba125a4c7c2982892d41c
Mark Story <mark@mark-story.com>
2010-09-02 16:55:42 -0400

    Removing TIME_START global, use $_SERVER['REQUEST_TIME'] instead.

--- cake/bootstrap.php
+++ cake/bootstrap.php
@@ -25,7 +25,6 @@ if (!defined('E_DEPRECATED')) {
 error_reporting(E_ALL & ~E_DEPRECATED);
 
 require CORE_PATH . 'cake' . DS . 'basics.php';
-$TIME_START = microtime(true);
 require CORE_PATH . 'cake' . DS . 'config' . DS . 'paths.php';
 require LIBS . 'exceptions.php';
 require LIBS . 'object.php';
