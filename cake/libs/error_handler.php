fa7549dc4cd8e99cfcc4f7ad4c0abb4b7fc7fe34
Mark Story <mark@mark-story.com>
2010-09-04 01:34:45 -0400

    Fixing comments and removing cakeError().  Instead you should be throwing exceptions.

--- cake/libs/error_handler.php
+++ cake/libs/error_handler.php
@@ -24,8 +24,8 @@
  * Error Handler.
  *
  * Captures and handles all unhandled exceptions. Displays helpful framework errors when debug > 1.
- * When debug < 1 cakeError() will render 404 or  500 errors.  If an uncaught exception is thrown
- * and it is a type that ErrorHandler does not know about it will be treated as a 500 error. 
+ * When debug < 1 a CakeException will render 404 or  500 errors.  If an uncaught exception is thrown
+ * and it is a type that ErrorHandler does not know about it will be treated as a 500 error.
  *
  * ### Implementing application specific exception handling
  *
