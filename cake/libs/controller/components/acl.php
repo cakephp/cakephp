12338a0c16751889c2f885877602682350ae3b7c
mark_story <mark@mark-story.com>
2010-09-08 22:48:21 -0400

    Updating instance method calls in AclComponent.

--- cake/libs/controller/components/acl.php
+++ cake/libs/controller/components/acl.php
@@ -163,7 +163,7 @@ class AclComponent extends Component {
  */
 	public function grant($aro, $aco, $action = "*") {
 		trigger_error(__('AclComponent::grant() is deprecated, use allow() instead'), E_USER_WARNING);
-		return $this->_Instance->grant($aro, $aco, $action);
+		return $this->_Instance->allow($aro, $aco, $action);
 	}
 
 /**
@@ -176,7 +176,7 @@ class AclComponent extends Component {
  */
 	public function revoke($aro, $aco, $action = "*") {
 		trigger_error(__('AclComponent::revoke() is deprecated, use deny() instead'), E_USER_WARNING);
-		return $this->_Instance->revoke($aro, $aco, $action);
+		return $this->_Instance->deny($aro, $aco, $action);
 	}
 }
 
