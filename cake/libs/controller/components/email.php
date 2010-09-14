mtdmn/runking

Adding more tests for SMTP address formatting.  Fixes #1100

cake/libs/controller/components/email.php
@@ -743,9 +743,14 @@ class EmailComponent extends Object{
  * @access private
  */
 	function _formatAddress($string, $smtp = false) {
-		$hasAlias = preg_match('/(.+)\s<(.+)>/', $string, $matches);
-		if ($hasAlias) {
-			return $this->_strip($matches[1] . ' <' . $matches[2] . '>');
+		$hasAlias = preg_match('/((.*)\s)?<(.+)>/', $string, $matches);
+		if ($smtp && $hasAlias) {
+			return $this->_strip(' <' .  $matches[3] . '>');
+		} elseif ($smtp) {
+			return $this->_strip(' <' . $string . '>');
+		}
+		if ($hasAlias && !empty($matches[2])) {
+			return $this->_strip($matches[2] . ' <' . $matches[3] . '>');
 		}
 		return $this->_strip($string);
 	}

cakephp/cakephp

cake/libs/controller/components/email.php
@@ -617,9 +617,14 @@ class EmailComponent extends Object{
  * @access private
  */
 	function __formatAddress($string, $smtp = false) {
-		$hasAlias = preg_match('/(.+)\s<(.+)>/', $string, $matches);
-		if ($hasAlias) {
-			return $this->__strip($matches[1] . ' <' . $matches[2] . '>');
+		$hasAlias = preg_match('/((.*)\s)?<(.+)>/', $string, $matches);
+		if ($smtp && $hasAlias) {
+			return $this->__strip(' <' .  $matches[3] . '>');
+		} elseif ($smtp) {
+			return $this->__strip(' <' . $string . '>');
+		}
+		if ($hasAlias && !empty($matches[2])) {
+			return $this->__strip($matches[2] . ' <' . $matches[3] . '>');
 		}
 		return $this->__strip($string);
 	}