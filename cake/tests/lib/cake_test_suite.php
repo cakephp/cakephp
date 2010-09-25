d7b958f55d993311b79a4a1769f77fd834b4f096
mark_story <mark@mark-story.com>
2010-09-25 00:00:08 -0400

    Starting to move test suite manipulation methods into CakeTestSuite.

--- cake/tests/lib/cake_test_suite.php
+++ cake/tests/lib/cake_test_suite.php
@@ -26,13 +26,32 @@ class CakeTestSuite extends PHPUnit_Framework_TestSuite {
 	protected $_fixtureManager = null;
 
 /**
- * Sets the intances for the fixture manager that will be used by this class
+ * Sets the intances for the fixture manager that will be used by this class.
+ *
  * @param CakeFixtureManager $manager the instance of the manager class
  * @return void
  * @access public
  */
 	public function setFixtureManager(CakeFixtureManager $manager) {
 		$this->_fixtureManager = $manager;
+	}
+
+/**
+ * Adds all the files in a directory to the test suite. Does not recurse through directories.
+ *
+ * @param string $directory The directory to add tests from.
+ * @return void
+ */
+	public function addTestDirectory($directory = '.') {
+		$files = new DirectoryIterator($directory);
+
+		foreach ($files as $file) {
+			if (!$file->isFile()) {
+				continue;
+			}
+			$file = $file->getRealPath();
+			$this->addTestFile($file);
+		}
 	}
 
 /**
