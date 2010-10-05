d64078db2dd699324eb8f78baed0ed7642626e00
mark_story <mark@mark-story.com>
2010-10-04 23:18:59 -0400

    Adding ConsoleInput for reading stdin.

---
+++ cake/tests/cases/console/console_input.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * ConsoleInput file.
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.console
+ * @since         CakePHP(tm) v 2.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+/**
+ * Object wrapper for interacting with stdin 
+ *
+ * @package cake.console
+ */
+class ConsoleInput {
+/**
+ * Input value.
+ *
+ * @var resource
+ */
+	protected $_input;
+
+/**
+ * Constructor
+ *
+ * @return void
+ */
+	public function __construct($handle = 'php://stdin') {
+		$this->_input = fopen('php://stdin', 'r');
+	}
+
+/**
+ * Read a value from the stream
+ *
+ * @return mixed The value of the stream
+ */
+	public function read() {
+		return fgets($this->_input);
+	}
+}
