561067e797281e1b02885ff8f218fb1f88140df6
mark_story <mark@mark-story.com>
2010-10-17 22:36:59 -0400

    Making the skel tmp dirs world writable.

--- cake/console/templates/skel/console/cake.php
+++ cake/console/templates/skel/console/cake.php
@@ -20,6 +20,6 @@
  * @since         CakePHP(tm) v 1.2.0.5012
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-require_once(__APP_PATH__ . 'shell_dispatcher.php');
+require_once(__CAKE_PATH__ . 'shell_dispatcher.php');
 
 ShellDispatcher::run($argv);
