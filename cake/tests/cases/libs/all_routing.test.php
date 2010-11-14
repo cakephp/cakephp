dad7963ea43b1d474903c83ada79620bc81fd2e4
mark_story <mark@mark-story.com>
2010-11-14 00:00:23 -0500

    Moving Dispatcher inside libs directory, as it is part of CakePHP's libs.

--- cake/tests/cases/libs/all_routing.test.php
+++ cake/tests/cases/libs/all_routing.test.php
@@ -36,12 +36,14 @@ class AllRoutingTest extends PHPUnit_Framework_TestSuite {
 	public static function suite() {
 		$suite = new PHPUnit_Framework_TestSuite('All Router and Dispatcher class tests');
 
-		$suite->addTestFile(CORE_TEST_CASES . DS . 'dispatcher.test.php');
-		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'router.test.php');
-		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'route' . DS . 'cake_route.test.php');
-		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'route' . DS . 'plugin_short_route.test.php');
-		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'cake_response.test.php');
-		$suite->addTestFile(CORE_TEST_CASES . DS . 'libs' . DS . 'cake_request.test.php');
+		$libs = CORE_TEST_CASES . DS . 'libs' . DS;
+
+		$suite->addTestFile($libs . 'dispatcher.test.php');
+		$suite->addTestFile($libs . 'router.test.php');
+		$suite->addTestFile($libs . 'route' . DS . 'cake_route.test.php');
+		$suite->addTestFile($libs . 'route' . DS . 'plugin_short_route.test.php');
+		$suite->addTestFile($libs . 'cake_response.test.php');
+		$suite->addTestFile($libs . 'cake_request.test.php');
 		return $suite;
 	}
 }
--- cake/tests/cases/libs/dispatcher.test.php
+++ cake/tests/cases/libs/dispatcher.test.php
@@ -17,7 +17,7 @@
  * @since         CakePHP(tm) v 1.2.0.4206
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-require_once CAKE . 'dispatcher.php';
+App::import('Core', 'Dispatcher', false);
 App::import('Core', 'CakeResponse', false);
 
 if (!class_exists('AppController')) {
