fac222e2cdfa9a70aca91993caeb57207605859e
mark_story <mark@mark-story.com>
2010-11-14 22:24:29 -0500

    Removing classes that were not used in the ErrorHandler test case.  Adding a new test suite for error classes.

---
+++ cake/tests/cases/libs/all_error.test.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * AllErrorTest file
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.tests.cases
+ * @since         CakePHP(tm) v 2.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * AllErrorTest class
+ *
+ * This test group will run error handling related tests.
+ *
+ * @package       cake
+ * @subpackage    cake.tests.groups
+ */
+class AllErrorTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * suite method, defines tests for this suite.
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new PHPUnit_Framework_TestSuite('All Error handling tests');
+
+		$libs = CORE_TEST_CASES . DS . 'libs' . DS;
+
+		$suite->addTestFile($libs . 'error_handler.test.php');
+		$suite->addTestFile($libs . 'exception_renderer.test.php');
+		return $suite;
+	}
+}
