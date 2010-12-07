513eb5342629e9a8767192ed143a0266c6888b35
José Lorenzo Rodríguez <jose.zap@gmail.com>
2010-12-06 22:48:30 -0430

    Fixing package location in DboSource

--- lib/Cake/Model/Datasource/DboSource.php
+++ lib/Cake/Model/Datasource/DboSource.php
@@ -17,7 +17,7 @@
  * @since         CakePHP(tm) v 0.10.0.1076
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-App::uses('String', 'Core');
+App::uses('String', 'Utility');
 
 /**
  * DboSource
