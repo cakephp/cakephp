f50a97bff1e5948d041f4cb4b752705e1f142c3f
mark_story <mark@mark-story.com>
2010-12-16 22:53:00 -0500

    Making PagesController test unskipped in all_tests.

--- cake/tests/cases/libs/controller/pages_controller.test.php
+++ cake/tests/cases/libs/controller/pages_controller.test.php
@@ -17,11 +17,6 @@
  * @since         CakePHP(tm) v 1.2.0.5436
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-if (!class_exists('AppController')) {
-	require_once LIBS . 'controller' . DS . 'app_controller.php';
-} elseif (!defined('APP_CONTROLLER_EXISTS')) {
-	define('APP_CONTROLLER_EXISTS', true);
-}
 App::import('Controller', 'Pages');
 
 /**
@@ -49,10 +44,6 @@ class PagesControllerTest extends CakeTestCase {
  * @return void
  */
 	function testDisplay() {
-		if ($this->skipIf(defined('APP_CONTROLLER_EXISTS'), '%s Need a non-existent AppController')) {
-			return;
-		}
-
 		App::build(array(
 			'views' => array(TEST_CAKE_CORE_INCLUDE_PATH . 'tests' . DS . 'test_app' . DS . 'views'. DS, TEST_CAKE_CORE_INCLUDE_PATH . 'libs' . DS . 'view' . DS)
 		));
