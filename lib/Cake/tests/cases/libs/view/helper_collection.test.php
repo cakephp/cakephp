5605bf2d9ee9d746cf11b2a3ad21d8ea00e14c86
José Lorenzo Rodríguez <jose.zap@gmail.com>
2010-12-18 22:32:20 -0430

    Fixing failing test for HelperCollection

--- lib/Cake/tests/cases/libs/view/helper_collection.test.php
+++ lib/Cake/tests/cases/libs/view/helper_collection.test.php
@@ -73,7 +73,7 @@ class HelperCollectionTest extends CakeTestCase {
 /**
  * test missinghelper exception
  *
- * @expectedException MissingHelperFileException
+ * @expectedException MissingHelperClassException
  * @return void
  */
 	function testLoadMissingHelperFile() {
