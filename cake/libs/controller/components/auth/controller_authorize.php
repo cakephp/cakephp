67c9acbc94c5399f27cdf801d2492a7fe71af6b0
mark_story <mark@mark-story.com>
2011-01-02 13:39:48 -0500

    Adding a doc block.

--- cake/libs/controller/components/auth/controller_authorize.php
+++ cake/libs/controller/components/auth/controller_authorize.php
@@ -1,6 +1,38 @@
 <?php
+/**
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
-
+/**
+ * An authorization adapter for AuthComponent.  Provides the ability to authorize using a controller callback.
+ * Your controller's isAuthorized() method should return a boolean to indicate whether or not the user is authorized.
+ *
+ * {{{
+ *	function isAuthorized($user) {
+ *		if (!empty($this->request->params['admin'])) {
+ *			return $user['role'] == 'admin';
+ *		}
+ *		return !empty($user);
+ *	}
+ * }}}
+ *
+ * the above is simple implementation that would only authorize users of the 'admin' role to access
+ * admin routing.
+ *
+ * @package cake.libs.controller.components.auth
+ * @since 2.0
+ * @see AuthComponent::$authenticate
+ */
 class ControllerAuthorize {
 /**
  * Controller for the request.
