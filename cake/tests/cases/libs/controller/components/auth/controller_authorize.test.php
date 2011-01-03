ca9aabdfec3da8c0cd2b10ffa31f3e7f1d5a0636
mark_story <mark@mark-story.com>
2011-01-03 13:00:06 -0500

    Adding file headers.

--- cake/tests/cases/libs/controller/components/auth/controller_authorize.test.php
+++ cake/tests/cases/libs/controller/components/auth/controller_authorize.test.php
@@ -1,5 +1,18 @@
 <?php
-
+/**
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       cake.tests.cases.libs.controller.components.auth
+ * @since         CakePHP(tm) v 2.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 App::import('Component', 'auth/controller_authorize');
 App::import('Core', 'CakeRequest');
 App::import('Core', 'Controller');
