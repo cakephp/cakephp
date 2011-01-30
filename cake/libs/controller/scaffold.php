6db8515e60ae0031af4ea0e9d50099c6f1bb2f1e
mark_story <mark@mark-story.com>
2011-01-30 12:25:40 -0500

    Fixing incorrectly nested sprintf + __ call. Refs #1305

--- cake/libs/controller/scaffold.php
+++ cake/libs/controller/scaffold.php
@@ -223,7 +223,7 @@ class Scaffold extends Object {
 	function __scaffoldView($params) {
 		if ($this->controller->_beforeScaffold('view')) {
 
-			$message = __(sprintf("No id set for %s::view()", Inflector::humanize($this->modelKey)), true);
+			$message = sprintf(__("No id set for %s::view()", true), Inflector::humanize($this->modelKey));
 			if (isset($params['pass'][0])) {
 				$this->ScaffoldModel->id = $params['pass'][0];
 			} elseif ($this->_validSession) {
