ff5a809b2989fa21f8e42c94ec7102d8c7dc1a56
José Lorenzo Rodríguez <jose.zap@gmail.com>
2011-01-31 00:14:20 -0430

    Fixing log engines loading

--- lib/Cake/tests/test_app/libs/Log/Engine/TestAppLog.php
+++ lib/Cake/tests/test_app/libs/Log/Engine/TestAppLog.php
@@ -16,6 +16,9 @@
  * @since         CakePHP(tm) v 1.3
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+
+App::uses('CakeLogInterface', 'Log');
+
 class TestAppLog implements CakeLogInterface {
 
 	function write($type, $message) {
