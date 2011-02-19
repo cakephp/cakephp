be9613da77951e10187af648d70016f60fc9c791
Jason Dinh <jasondinh@Jason-Dinhs-MacBook.local>
2011-02-19 11:59:38 +0700

    modified core.php

--- app/config/core.php
+++ app/config/core.php
@@ -201,12 +201,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhG9is422777773b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '749238748927948732894789324');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
