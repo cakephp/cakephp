b1e007f231bb46062e2f033905afd511fe24e806
José Lorenzo Rodríguez <jose.zap@gmail.com>
2011-02-22 00:06:11 -0430

    Correctly importing classes in BasicAuthentication test

--- lib/Cake/tests/cases/libs/http/basic_authentication.test.php
+++ lib/Cake/tests/cases/libs/http/basic_authentication.test.php
@@ -17,8 +17,8 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-App::import('Core', 'HttpSocket');
-App::import('Lib', 'http/BasicAuthentication');
+App::uses('HttpSocket', 'Network/Http');
+App::uses('BasicAuthentication', 'Network/Http');
 
 /**
  * BasicMethodTest class
