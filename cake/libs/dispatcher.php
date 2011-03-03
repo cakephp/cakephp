a6abd61f3f419116b93eb13b3c9b339c36dd1f62
mark_story <mark@mark-story.com>
2011-03-02 21:52:25 -0500

    Removing Dispatcher::$here, it wasn't really used outside of a cached() which had a param that it just ignored.  Making Dispatcher::cached() pay attention to its parameter. Updating tests.

--- cake/libs/dispatcher.php
+++ cake/libs/dispatcher.php
@@ -38,14 +38,6 @@ App::import('Controller', 'Controller', false);
 class Dispatcher {
 
 /**
- * Current URL
- *
- * @var string
- * @access public
- */
-	public $here = false;
-
-/**
  * The request object
  *
  * @var CakeRequest
@@ -91,9 +83,7 @@ class Dispatcher {
  *    are encountered.
  */
 	public function dispatch(CakeRequest $request, $additionalParams = array()) {
-		$this->here = $request->here;
-
-		if ($this->asset($request->url) || $this->cached($request->url)) {
+		if ($this->asset($request->url) || $this->cached($request->here)) {
 			return;
 		}
 
@@ -260,12 +250,11 @@ class Dispatcher {
 /**
  * Outputs cached dispatch view cache
  *
- * @param string $url Requested URL
+ * @param string $path Requested URL path
  */
-	public function cached($url) {
+	public function cached($path) {
 		if (Configure::read('Cache.check') === true) {
-			$path = $this->here;
-			if ($this->here == '/') {
+			if ($path == '/') {
 				$path = 'home';
 			}
 			$path = strtolower(Inflector::slug($path));
