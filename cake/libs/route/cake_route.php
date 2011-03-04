b8aa78ce9f8f0534bb414d8ee1ff623da8a6b6e1
mark_story <mark@mark-story.com>
2011-03-03 21:16:43 -0500

    Making a method protected, it doesn't need to public really.

--- cake/libs/route/cake_route.php
+++ cake/libs/route/cake_route.php
@@ -228,7 +228,7 @@ class CakeRoute {
 		}
 		
 		if (isset($route['_args_'])) {
-			list($pass, $named) = $this->parseArgs($route['_args_'], $route['controller'], $route['action']);
+			list($pass, $named) = $this->_parseArgs($route['_args_'], $route['controller'], $route['action']);
 			$route['pass'] = array_merge($route['pass'], $pass);
 			$route['named'] = $named;
 			unset($route['_args_']);
@@ -255,7 +255,7 @@ class CakeRoute {
  * @param string $action The current action name, used to match named parameter rules.
  * @return array Array of ($pass, $named)
  */
-	public function parseArgs($args, $controller = null, $action = null) {
+	protected function _parseArgs($args, $controller = null, $action = null) {
 		$pass = $named = array();
 		$args = explode('/', $args);
 
