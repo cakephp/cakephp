e46bfff6a0ff7d57bc3d661a1e6fb30ab3787751
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2011-03-08 00:51:58 -0430

    Fixing FixtureTask tests

--- lib/Cake/tests/cases/console/shells/tasks/fixture.test.php
+++ lib/Cake/tests/cases/console/shells/tasks/fixture.test.php
@@ -19,6 +19,8 @@
 
 App::uses('ShellDispatcher', 'Console');
 App::uses('Shell', 'Console');
+App::uses('ConsoleOutput', 'Console');
+App::uses('ConsoleInput', 'Console');
 App::uses('FixtureTask', 'Console/Command/Task');
 App::uses('TemplateTask', 'Console/Command/Task');
 App::uses('DbConfigTask', 'Console/Command/Task');
