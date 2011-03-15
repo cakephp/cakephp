6fd5ff34a0efc2862008ae3c3cb33b7e1ccbda96
mark_story <mark@mark-story.com>
2011-03-14 21:13:24 -0400

    Fixing incorrect test case.  Thanks to hiromi2424 for pointing out the mistake.

--- cake/tests/cases/libs/cache.test.php
+++ cake/tests/cases/libs/cache.test.php
@@ -315,7 +315,7 @@ class CacheTest extends CakeTestCase {
 			'plugins' => array(TEST_CAKE_CORE_INCLUDE_PATH . 'tests' . DS . 'test_app' . DS . 'plugins' . DS)
 		), true);
 	
-		Cache::config('test_trigger', array('engine' => 'TestAppCache'));
+		Cache::config('test_trigger', array('engine' => 'TestAppCache', 'prefix' => ''));
 		try {
 			Cache::write('fail', 'value', 'test_trigger');
 			$this->fail('No exception thrown');
--- cake/tests/test_app/libs/cache/test_app_cache.php
+++ cake/tests/test_app/libs/cache/test_app_cache.php
@@ -19,7 +19,7 @@
 class TestAppCacheEngine extends CacheEngine {
 
 	public function write($key, $value, $duration) { 
-		if ($key = 'fail') {
+		if ($key == 'fail') {
 			return false;
 		}
 	}
