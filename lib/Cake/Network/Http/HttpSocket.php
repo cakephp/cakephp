c83a396eb45e65e4e26ba11b28a095cc6b8b6791
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2011-04-10 16:57:44 -0430

    Fixing class loading for HttpResponse classes, and making possible to pass plugin HttpResponse classes too

--- lib/Cake/Network/Http/HttpSocket.php
+++ lib/Cake/Network/Http/HttpSocket.php
@@ -363,7 +363,9 @@ class HttpSocket extends CakeSocket {
 			$this->disconnect();
 		}
 
-		if (!App::import('Lib', $this->responseClass)) {
+		list($plugin, $responseClass) = pluginSplit($this->responseClass, true);
+		App::uses($this->responseClass, $plugin . 'Network/Http');
+		if (!class_exists($responseClass)) {
 			throw new SocketException(__d('cake_dev', 'Class %s not found.', $this->responseClass));
 		}
 		$responseClass = $this->responseClass;
