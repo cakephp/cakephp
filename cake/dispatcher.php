d9c7f4df664553ef84c98ad7d7d346c9d0086c9c
mark_story <mark@mark-story.com>
2011-04-30 12:42:08 -0400

    Fixing import of MediaView in dispatcher so Media models used in Routing don't interfere with asset delivery. Fixes #1670

--- cake/dispatcher.php
+++ cake/dispatcher.php
@@ -627,7 +627,7 @@ class Dispatcher extends Object {
 			ob_start('ob_gzhandler');
 		}
 
-		App::import('View', 'Media', false);
+		App::import('View', 'Media');
 		$controller = null;
 		$Media = new MediaView($controller);
 		if (isset($Media->mimeType[$ext])) {
