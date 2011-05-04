f2c1f9d6b24cf193adacf6000a2735c892eb2ef0
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2011-05-04 01:17:28 -0430

    Removing unneeded lines in Debugger

--- lib/Cake/Utility/Debugger.php
+++ lib/Cake/Utility/Debugger.php
@@ -176,7 +176,6 @@ class Debugger {
 			if (!$instance || strtolower($class) != strtolower(get_class($instance[0]))) {
 				$instance[0] = new $class();
 				if (Configure::read('debug') > 0) {
-					Configure::version(); // Make sure the core config is loaded
 					$instance[0]->helpPath = Configure::read('Cake.Debugger.HelpPath');
 				}
 			}
@@ -185,7 +184,6 @@ class Debugger {
 		if (!$instance) {
 			$instance[0] = new Debugger();
 			if (Configure::read('debug') > 0) {
-				Configure::version(); // Make sure the core config is loaded
 				$instance[0]->helpPath = Configure::read('Cake.Debugger.HelpPath');
 			}
 		}
