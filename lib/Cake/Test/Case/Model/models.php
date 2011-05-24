69c0127604774faecd011086ee29423bce4ac5d3
Rachman Chavik <rchavik@xintesa.com>
2011-05-24 20:02:45 +0700

    adding tests for #275 (2.0)
    ported to 2.0 branch from cakephp/cakephp#93

--- lib/Cake/Test/Case/Model/models.php
+++ lib/Cake/Test/Case/Model/models.php
@@ -3217,6 +3217,24 @@ class TransactionManyTestModel extends CakeTestModel {
 	}
 }
 
+class Site extends CakeTestModel {
+	var $name = 'Site';
+	var $useTable = 'sites';
+
+	var $hasAndBelongsToMany = array(
+		'Domain' => array('unique' => true),
+		);
+}
+
+class Domain extends CakeTestModel {
+	var $name = 'Domain';
+	var $useTable = 'domains';
+
+	var $hasAndBelongsToMany = array(
+		'Site' => array('unique' => true),
+		);
+}
+
 /**
  * TestModel class
  *
