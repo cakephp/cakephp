5fd19266ba22ad0b171ab95da415baaa5eb4edd8
Cauan Cabral <cauan@radig.com.br>
2011-05-27 15:02:09 -0400

    Updating MediaView test
    UPPERCASE extesion test included
    
    Signed-off-by: mark_story <mark@mark-story.com>

--- cake/tests/cases/libs/view/media.test.php
+++ cake/tests/cases/libs/view/media.test.php
@@ -54,6 +54,13 @@ class MediaController extends Controller {
 		$extension = 'css';
 		$this->set(compact('path', 'id', 'extension'));
 	}
+	
+	function downloadUpper() {
+		$path = TEST_CAKE_CORE_INCLUDE_PATH . 'tests' . DS . 'test_app' . DS . 'vendors' . DS .'img' . DS;
+		$id = 'test_2.JPG';
+		$extension = 'JPG';
+		$this->set(compact('path', 'id', 'extension'));
+	}
 }
 
 /**
@@ -134,8 +141,6 @@ class MediaViewTest extends CakeTestCase {
 		$this->Controller =& new Controller();
 		$this->MediaController =& new MediaController();
 		$this->MediaController->viewPath = 'posts';
-		$this->MediaController->download();
-		$this->MediaView =& new TestMediaView($this->MediaController);
 	}
 
 /**
@@ -159,11 +164,28 @@ class MediaViewTest extends CakeTestCase {
  */
 	function testRender() {
 		ob_start();
+		$this->MediaController->download();
+		$this->MediaView =& new TestMediaView($this->MediaController);
 		$result = $this->MediaView->render();
 		$output = ob_get_clean();
 
 		$this->assertTrue($result !== false);
 		$this->assertEqual($output, 'this is the test asset css file');
+	}
+	
+	function testRenderUpperExtesnion() {
+		ob_start();
+		$this->MediaController->downloadUpper();
+		$this->MediaView =& new TestMediaView($this->MediaController);
+		$result = $this->MediaView->render();
+		$output = ob_get_clean();
+
+		$this->assertTrue($result !== false);
+		
+		$fileName = TEST_CAKE_CORE_INCLUDE_PATH . 'tests' . DS . 'test_app' . DS . 'vendors' . DS .'img' . DS . 'test_2.JPG';
+		$file = file_get_contents($fileName, 'r');
+		
+		$this->assertEqual(base64_encode($output), base64_encode($file));		
 	}
 
 /**
@@ -173,6 +195,8 @@ class MediaViewTest extends CakeTestCase {
  * @return void
  */
 	function testConnectionAborted() {
+		$this->MediaController->download();
+		$this->MediaView =& new TestMediaView($this->MediaController);
 		$this->MediaView->active = false;
 		$result = $this->MediaView->render();
 		$this->assertFalse($result);
