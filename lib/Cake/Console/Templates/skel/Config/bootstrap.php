feb567d4eebec384e9ca2ecdb3c57f620e8ff564
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2011-07-07 02:34:22 -0430

    Adding better help and documentation for loading plugins. The exception renderer will serve a helpful page if a plugin is missing

--- lib/Cake/Console/Templates/skel/Config/bootstrap.php
+++ lib/Cake/Console/Templates/skel/Config/bootstrap.php
@@ -46,9 +46,20 @@ Cache::config('default', array('engine' => 'File'));
  */
 
 /**
- * As of 1.3, additional rules for the inflector are added below
+ * Custom Inflector rules, can be set to correctly pluralize or singularize table, model, controller names or whatever other
+ * string is passed to the inflection functions
  *
  * Inflector::rules('singular', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
  * Inflector::rules('plural', array('rules' => array(), 'irregular' => array(), 'uninflected' => array()));
+ *
+ */
+
+/**
+ * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
+ * Uncomment one of the lines below, as you need. make sure you read the documentation on CakePlugin to use more
+ * advanced ways of loading plugins
+ *
+ * CakePlugin::loadAll(); // Loads all plugins at once
+ * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
