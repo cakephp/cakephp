ff0119ba6f4fb95249ab00616c7d9d67477fb550
Mark Story mark@mark-story.com <mark@freshbooks.com>
2011-08-28 22:19:53 -0400

    Fixing file deletion issue in windows where unlink() cannot delete files that have open file handles. Fixes #376

--- cake/tests/cases/libs/file.test.php
+++ cake/tests/cases/libs/file.test.php
@@ -395,7 +395,7 @@ class FileTest extends CakeTestCase {
 	function testDelete() {
 		if (!$tmpFile = $this->_getTmpFile()) {
 			return false;
-		};
+		}
 
 		if (!file_exists($tmpFile)) {
 			touch($tmpFile);
@@ -409,6 +409,24 @@ class FileTest extends CakeTestCase {
 		$TmpFile =& new File('/this/does/not/exist');
 		$result = $TmpFile->delete();
 		$this->assertFalse($result);
+	}
+
+/**
+ * Windows has issues unlinking files if there are
+ * active filehandles open.
+ *
+ * @return void
+ */
+	function testDeleteAfterRead() {
+		if (!$tmpFile = $this->_getTmpFile()) {
+			return false;
+		}
+		if (!file_exists($tmpFile)) {
+			touch($tmpFile);
+		}
+		$file =& new File($tmpFile);
+		$file->read();
+		$this->assertTrue($file->delete());
 	}
 
 /**
