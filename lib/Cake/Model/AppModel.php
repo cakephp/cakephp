705593908b193719482ab446aaf87f6d0d800b0b
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2011-09-30 13:17:12 -0430

    Fixing some @package declarations

--- lib/Cake/Model/AppModel.php
+++ lib/Cake/Model/AppModel.php
@@ -16,7 +16,6 @@
  * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
  * @package       Cake.Model
- * @subpackage    cake.cake.libs.model
  * @since         CakePHP(tm) v 0.2.9
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
@@ -31,7 +30,6 @@ App::uses('Model', 'Model');
  * Add your application-wide methods to the class, your models will inherit them.
  *
  * @package       Cake.Model
- * @subpackage    cake.cake.libs.model
  */
 class AppModel extends Model {
 }
