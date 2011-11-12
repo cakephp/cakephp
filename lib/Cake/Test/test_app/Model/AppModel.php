f2a58117ca2da75e5792c4fc56bce7d37a7f9441
ADmad <admad.coder@gmail.com>
2011-11-12 22:30:42 +0530

    Moving AppModel, AppController, PagesController out of core to app

---
+++ lib/Cake/Test/test_app/Model/AppModel.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Application model for Cake.
+ *
+ * This file is application-wide model file. You can put all
+ * application-wide model-related methods here.
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @package       Cake.Model
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+App::uses('Model', 'Model');
+
+/**
+ * Application model for Cake.
+ *
+ * This is a placeholder class.
+ * Create the same file in app/Model/AppModel.php
+ * Add your application-wide methods to the class, your models will inherit them.
+ *
+ * @package       Cake.Model
+ */
+class AppModel extends Model {
+}
