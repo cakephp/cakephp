73de68f5f484dc32e3667491d96dff7a78ae8acf
m <euromark@web.de>
2011-11-29 16:04:50 +0100

    Test suite should display current PHPUnit version.
    Fixes #2316

--- lib/Cake/TestSuite/templates/phpunit.php
+++ lib/Cake/TestSuite/templates/phpunit.php
@@ -28,7 +28,7 @@
 		<li><code>pear channel-discover pear.phpunit.de</code></li>
 		<li><code>pear channel-discover components.ez.no</code></li>
 		<li><code>pear channel-discover pear.symfony-project.com</code></li>
-		<li><code>pear install phpunit/PHPUnit-3.5.15</code></li>
+		<li><code>pear install phpunit/PHPUnit-3.6.4</code></li>
 	</ul>
 	<p>For full instructions on how to <a href="http://www.phpunit.de/manual/current/en/installation.html">install PHPUnit, see the PHPUnit installation guide</a>.</p>
 	<p><a href="http://github.com/sebastianbergmann/phpunit" target="_blank">Download PHPUnit</a></p>
