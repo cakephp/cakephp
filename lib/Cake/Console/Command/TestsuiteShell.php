545f4d281141951565834c02745a4994c087e749
mark_story <mark@mark-story.com>
2011-12-03 13:56:55 -0500

    Fix bad merge.

--- lib/Cake/Console/Command/TestsuiteShell.php
+++ lib/Cake/Console/Command/TestsuiteShell.php
@@ -30,18 +30,7 @@ App::uses('CakeTestLoader', 'TestSuite');
  *
  * @package       Cake.Console.Command
  */
-<<<<<<< HEAD
 class TestsuiteShell extends TestShell {
-=======
-class TestsuiteShell extends AppShell {
-
-/**
- * Dispatcher object for the run.
- *
- * @var CakeTestDispatcher
- */
-	protected $_dispatcher = null;
->>>>>>> 2.0
 
 /**
  * get the option parser for the test suite.
