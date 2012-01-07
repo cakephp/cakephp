399c293b776e8c6299cadcdf91eeebf343acb57d
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2012-01-07 18:20:16 -0430

    Extracting the CakeSessionHandlerInterface into its own file

--- lib/Cake/Model/Datasource/Session/DatabaseSession.php
+++ lib/Cake/Model/Datasource/Session/DatabaseSession.php
@@ -16,6 +16,9 @@
  * @since         CakePHP(tm) v 2.0
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+
+App::uses('CakeSessionHandlerInterface', 'Model/Datasource/Session');
+
 /**
  * DatabaseSession provides methods to be used with CakeSession.
  *
