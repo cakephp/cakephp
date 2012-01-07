abb370fe3f3a03f4552cbef225623b7b68f773c7
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2012-01-07 01:39:40 -0430

    Fixing windows failing test case on the debug function

--- lib/Cake/basics.php
+++ lib/Cake/basics.php
@@ -76,7 +76,7 @@ function debug($var = false, $showHtml = null, $showFrom = true) {
 		$lineInfo = '';
 		if ($showFrom) {
 			$calledFrom = debug_backtrace();
-			$file = substr(str_replace(ROOT, '', $calledFrom[0]['file']), 1);
+			$file = substr(str_ireplace(ROOT, '', $calledFrom[0]['file']), 1);
 			$line = $calledFrom[0]['line'];
 		}
 		$html = <<<HTML
