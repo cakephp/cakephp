54869fc965cd47668ce6a8a177a5f1c94289d456
0x20h <kohj@informatik.uni-marburg.de>
2012-01-17 19:39:46 +0100

    moved note about 'default' cache configuration to the APC config docblock

--- app/Config/core.php
+++ app/Config/core.php
@@ -235,6 +235,7 @@
  * Pick the caching engine to use.  If APC is enabled use it.
  * If running via cli - apc is disabled by default. ensure it's available and enabled in this case
  *
+ * Note: 'default' and other application caches should be configured in app/Config/bootstrap.php
  */
 $engine = 'File';
 if (extension_loaded('apc') && function_exists('apc_dec') && (php_sapi_name() !== 'cli' || ini_get('apc.enable_cli'))) {
@@ -270,7 +271,3 @@ Cache::config('_cake_model_', array(
 	'serialize' => ($engine === 'File'),
 	'duration' => $duration
 ));
-
-/**
- * Note: 'default' and other application caches should be configured in app/Config/bootstrap.php
- */
