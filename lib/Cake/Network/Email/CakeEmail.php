f02a3b05318ab1725a1b58ab2eea1677c4c2a7f6
mark_story <mark@mark-story.com>
2012-01-25 21:03:03 -0500

    Wrap aliases containing , in ""
    Fixes #2502

--- lib/Cake/Network/Email/CakeEmail.php
+++ lib/Cake/Network/Email/CakeEmail.php
@@ -1109,6 +1109,9 @@ class CakeEmail {
 			$restore = mb_internal_encoding();
 			mb_internal_encoding($this->_appCharset);
 		}
+		if (strpos($text, ',') !== false) {
+			$text = '"' . $text . '"';
+		}
 		$return = mb_encode_mimeheader($text, $this->headerCharset, 'B');
 		if ($internalEncoding) {
 			mb_internal_encoding($restore);
