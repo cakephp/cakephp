059a5f21ed52a779d12d9a55fc4707a60faf19fc
Thomas Ploch <t.ploch@reizwerk.com>
2012-02-09 14:24:10 +0100

    Fixed TODO: Refactored setting of Cookies into CakeResponse.

--- lib/Cake/Network/CakeResponse.php
+++ lib/Cake/Network/CakeResponse.php
@@ -326,6 +326,13 @@ class CakeResponse {
 	protected $_cacheDirectives = array();
 
 /**
+ * Holds cookies to be sent to the client
+ * 
+ * @var array
+ */
+	protected $_cookies = array();
+
+/**
  * Class constructor
  *
  * @param array $options list of parameters to setup the response. Possible values are:
@@ -361,6 +368,7 @@ class CakeResponse {
 		}
 
 		$codeMessage = $this->_statusCodes[$this->_status];
+		$this->_setCookies();
 		$this->_sendHeader("{$this->_protocol} {$this->_status} {$codeMessage}");
 		$this->_setContent();
 		$this->_setContentLength();
@@ -369,6 +377,22 @@ class CakeResponse {
 			$this->_sendHeader($header, $value);
 		}
 		$this->_sendContent($this->_body);
+	}
+
+/**
+ * Sets the cookies that have been added via static method CakeResponse::addCookie()
+ * before any other output is sent to the client.
+ * Will set the cookies in the order they have been set.
+ * 
+ * @return void
+ */
+	protected function _setCookies() {
+		foreach ($this->_cookies as $name => $c) {
+			setcookie(
+				$name, $c['value'], $c['expire'], $c['path'],
+				$c['domain'], $c['secure'], $c['httpOnly']
+			);
+		}
 	}
 
 /**
@@ -1059,5 +1083,68 @@ class CakeResponse {
  */
 	public function __toString() {
 		return (string)$this->_body;
+	}
+
+/**
+ * Getter/Setter for cookie configs
+ * 
+ * This method acts as a setter/getter depending on the type of the argument.
+ * If the method is called with no arguments, it returns all configurations.
+ * 
+ * If the method is called with a string as argument, it returns either the
+ * given configuration if it is set, or null, if it's not set.
+ * 
+ * If the method is called with an array as argument, it will set the cookie
+ * configuration to the cookie container.
+ * 
+ * ### Options (when setting a configuration)
+ *  - name: The Cookie name
+ *  - value: Value of the cookie
+ *  - expire: Time the cookie expires in
+ *  - path: Path the cookie applies to
+ *  - domain: Domain the cookie is for.
+ *  - secure: Is the cookie https?
+ *  - httpOnly: Is the cookie available in the client?
+ * 
+ * ## Examples
+ * 
+ * ### Getting all cookies
+ * 
+ * `$this->cookie()`
+ * 
+ * ### Getting a certain cookie configuration
+ * 
+ * `$this->cookie('MyCookie')`
+ * 
+ * ### Setting a cookie configuration
+ * 
+ * `$this->cookie((array) $config)`
+ * 
+ * @return mixed
+ */
+	public function cookie($config = null) {
+		if ($config === null) {
+			return $this->_cookies;
+		}
+
+		if (is_string($config)) {
+			if (!isset($this->_cookies[$config])) {
+				return null;
+			}
+			return $this->_cookies[$config];
+		}
+
+		$defaults = array(
+			'name' => 'CakeCookie[default]',
+			'value' => '',
+			'expire' => 0,
+			'path' => '/',
+			'domain' => '',
+			'secure' => false,
+			'httpOnly' => false
+		);
+		$config += $defaults;
+
+		$this->_cookies[$config['name']] = $config;
 	}
 }
