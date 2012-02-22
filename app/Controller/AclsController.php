0bdd6303298538cf9c00ec79ddd081901a049fc4
Richard Joo <richardjoo@gmail.com>
2012-02-21 22:24:17 -0800

    working on array index issue

--- app/Controller/AclsController.php
+++ app/Controller/AclsController.php
@@ -248,10 +248,14 @@ class AclsController extends AppController {
 				echo ('line 248'.' $controllerIndex='.$controllerIndex.' $roleIndex='.$roleIndex.' and '.' $compareName='.$compareName.' $functionIndex='.$functionIndex.'<br />');
 				
 				//if (isset($richardResult[$controllerIndex][$functionIndex])) {
-					if ($richardResult[$controllerIndex][$functionIndex] != "") {
+					if(!isset($richardResult[$controllerIndex][$functionIndex])) {
+						
+					} else if ($richardResult[$controllerIndex][$functionIndex] != "") {
 						$richardResult[$controllerIndex][$functionIndex] = $richardResult[$controllerIndex][$functionIndex].",".$roleIndex;
 						echo('line 253 after update: $richardResult[$controllerIndex][$functionIndex] ='.$richardResult[$controllerIndex][$functionIndex].' $functionIndex='.$functionIndex.'<br />');
-					}
+					}	
+						
+					
 				//} else {
 				//	$richardResult[$controllerIndex][$functionIndex] = $roleIndex;
 				//	echo('line 257 after update: $richardResult[$controllerIndex][$functionIndex] ='.$richardResult[$controllerIndex][$functionIndex].' $functionIndex='.$functionIndex.'<br /><br />');
