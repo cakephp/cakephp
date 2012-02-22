c35c3d23a514d8f4c4f65479eff09728ba7f38f4
Richard Joo <richardjoo@gmail.com>
2012-02-21 22:05:13 -0800

    working on array index issue

--- app/Controller/AclsController.php
+++ app/Controller/AclsController.php
@@ -243,16 +243,18 @@ class AclsController extends AppController {
 				
 				$compareName = $roleIndex;
 				$richardResult[$controllerIndex][$functionIndex] = $roleIndex;
-				
+				echo('line 246 after update: $richardResult[$controllerIndex][$functionIndex] ='.$richardResult[$controllerIndex][$functionIndex]);
 			} else {
 				echo ('line 248'.' $controllerIndex='.$controllerIndex.' $roleIndex='.$roleIndex.' and '.' $compareName='.$compareName.' $functionIndex='.$functionIndex.'<br />');
 				
 				if (isset($richardResult[$controllerIndex][$functionIndex])) {
 					if ($richardResult[$controllerIndex][$functionIndex] != "") {
 						$richardResult[$controllerIndex][$functionIndex] = $richardResult[$controllerIndex][$functionIndex].",".$roleIndex;
+						echo('line 253 after update: $richardResult[$controllerIndex][$functionIndex] ='.$richardResult[$controllerIndex][$functionIndex]);
 					}
-				//} else {
-				//	$richardResult[$controllerIndex][$functionIndex] = $roleIndex;
+				} else {
+					$richardResult[$controllerIndex][$functionIndex] = $roleIndex;
+					echo('line 257 after update: $richardResult[$controllerIndex][$functionIndex] ='.$richardResult[$controllerIndex][$functionIndex].'<br /><br />');
 				}
 			}
 		}
