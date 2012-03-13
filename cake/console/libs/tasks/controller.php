3806310a5efb1ca462cc9cb6eb6743a01abd964c
Juan Basso <jrbasso@gmail.com>
2012-03-12 22:43:25 -0400

    Updated copyright to 2012.

--- cake/console/libs/tasks/controller.php
+++ cake/console/libs/tasks/controller.php
@@ -13,7 +13,7 @@
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright 2005-2011, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
  * @package       cake
  * @subpackage    cake.cake.console.libs.tasks
