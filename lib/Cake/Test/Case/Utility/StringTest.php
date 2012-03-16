d3a44811ce70e6d9777d193e6ae5e001c7146237
dogmatic69 <dogmatic69@gmail.com>
2012-03-16 21:20:00 +0000

    adding test and fix for hightlighting tags, old bug 2111 for 1.3 but its the same issue. This should make more options possible

--- lib/Cake/Test/Case/Utility/StringTest.php
+++ lib/Cake/Test/Case/Utility/StringTest.php
@@ -454,6 +454,11 @@ podeís adquirirla.</span></p>
 		$result = $this->Text->highlight($text, $phrases, array('format' => '<b>\1</b>'));
 		$expected = '<b>This</b> is a test <b>text</b>';
 		$this->assertEquals($expected, $result);
+		
+		$phrases = array('is', 'text');
+		$result = $this->Text->highlight($text, $phrases, array('format' => '<b>\1</b>', 'regex' => "|\b%s\b|iu"));
+		$expected = 'This <b>is</b> a test <b>text</b>';
+		$this->assertEquals($expected, $result);
 
 		$text = 'This is a test text';
 		$phrases = null;
