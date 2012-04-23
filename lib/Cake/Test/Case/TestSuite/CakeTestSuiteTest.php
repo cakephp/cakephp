15e024b9a31d0f90d2d3b2ccbef501d156eb53c9
Till Claassen <code@pixelistik.de>
2012-04-23 12:49:28 +0200

    CakeTestSuite::addTestDirectory() ignore non-php
    CakeTestSuite::addTestDirectory() and addTestDirectoryRecursive()
    now ignore any files that do not end in .php
    
    This avoids any stray non-php files being parsed, especially
    tilde-style backup files that end in .php~
    
    Improves #2031

--- lib/Cake/Test/Case/TestSuite/CakeTestSuiteTest.php
+++ lib/Cake/Test/Case/TestSuite/CakeTestSuiteTest.php
@@ -78,4 +78,27 @@ class CakeTestSuiteTest extends CakeTestCase {
 
 		$Folder->delete();
 	}
+	
+/**
+ * testAddTestDirectoryRecursiveWithNonPhp
+ * 
+ * @return void
+ */
+	public function testAddTestDirectoryRecursiveWithNonPhp() {
+		$this->skipIf(!is_writeable(TMP), 'Cant addTestDirectoryRecursiveWithNonPhp unless the tmp folder is writable.');
+
+		$Folder = new Folder(TMP . 'MyTestFolder', true, 0777);
+		touch($Folder->path . DS . 'BackupTest.php~');
+		touch($Folder->path . DS . 'SomeNotesTest.txt');
+		touch($Folder->path . DS . 'NotHiddenTest.php');
+
+		$suite = $this->getMock('CakeTestSuite', array('addTestFile'));
+		$suite
+			->expects($this->exactly(1))
+			->method('addTestFile');
+
+		$suite->addTestDirectoryRecursive($Folder->pwd());
+
+		$Folder->delete();
+	}
 }
