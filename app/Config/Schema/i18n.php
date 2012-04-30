d40a8a90a8c239fe0af487273b7dc2e2112196b9
Graham Weldon <graham@grahamweldon.com>
2012-05-01 09:30:22 +1000

    Fixing coding standards in i18n schema in app/

--- app/Config/Schema/i18n.php
+++ app/Config/Schema/i18n.php
@@ -1,6 +1,4 @@
 <?php
-/*i18n schema generated on: 2007-11-25 07:11:25 : 1196004805*/
-
 /**
  * This is i18n Schema file
  *
@@ -39,13 +37,13 @@ class i18nSchema extends CakeSchema {
 	}
 
 	public $i18n = array(
-			'id' => array('type'=>'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-			'locale' => array('type'=>'string', 'null' => false, 'length' => 6, 'key' => 'index'),
-			'model' => array('type'=>'string', 'null' => false, 'key' => 'index'),
-			'foreign_key' => array('type'=>'integer', 'null' => false, 'length' => 10, 'key' => 'index'),
-			'field' => array('type'=>'string', 'null' => false, 'key' => 'index'),
-			'content' => array('type'=>'text', 'null' => true, 'default' => NULL),
-			'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'locale' => array('column' => 'locale', 'unique' => 0), 'model' => array('column' => 'model', 'unique' => 0), 'row_id' => array('column' => 'foreign_key', 'unique' => 0), 'field' => array('column' => 'field', 'unique' => 0))
-		);
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'locale' => array('type' => 'string', 'null' => false, 'length' => 6, 'key' => 'index'),
+		'model' => array('type' => 'string', 'null' => false, 'key' => 'index'),
+		'foreign_key' => array('type' => 'integer', 'null' => false, 'length' => 10, 'key' => 'index'),
+		'field' => array('type' => 'string', 'null' => false, 'key' => 'index'),
+		'content' => array('type' => 'text', 'null' => true, 'default' => null),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'locale' => array('column' => 'locale', 'unique' => 0), 'model' => array('column' => 'model', 'unique' => 0), 'row_id' => array('column' => 'foreign_key', 'unique' => 0), 'field' => array('column' => 'field', 'unique' => 0))
+	);
 
 }
