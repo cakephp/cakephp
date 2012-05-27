ba700a6c5ab38f6491d85fc0ac12116cb008a3d4
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2012-05-27 18:11:47 -0430

    Adding Statement class skeleton

---
+++ lib/Cake/Model/Datasource/Database/Statement.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Cake\Model\Datasource\Database;
+
+/**
+ * Represents a database statement
+ *
+ **/
+class Statement {
+
+	protected $_queryString;
+
+	public function bindValue($column, $value, $type = null) {
+
+	}
+
+	public function closeCursor() {
+
+	}
+
+	public function columnCount() {
+
+	}
+
+	public function errorCode() {
+
+	}
+
+	public function errorInfo() {
+
+	}
+
+	public function execute() {
+
+	}
+
+	public function fetch($type) {
+
+	}
+
+	public function fetchAll() {
+
+	}
+
+	public function rowCount() {
+
+	}
+
+}
