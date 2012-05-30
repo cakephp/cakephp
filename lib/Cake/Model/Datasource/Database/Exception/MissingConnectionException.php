4c4a7646addf4268702e5215aa62dd95b6331e71
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2012-05-29 21:13:55 -0430

    throwing exception when suplying invalid credentials

---
+++ lib/Cake/Model/Datasource/Database/Exception/MissingConnectionException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 
+ * PHP Version 5.x
+ *
+ * CakePHP(tm) Tests <http://book.cakephp.org/2.0/en/development/testing.html>
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The Open Group Test Suite License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://book.cakephp.org/2.0/en/development/testing.html CakePHP(tm) Tests
+ * @package       Cake.Model.Datasource.Database.Exception
+ * @since         CakePHP(tm) v 3.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+namespace Cake\Model\Datasource\Database\Exception;
+
+
+class MissingConnectionException extends \Cake\Error\Exception {
+
+	protected $_messageTemplate = 'Connection to database could not be established: %s';
+
+}
+
