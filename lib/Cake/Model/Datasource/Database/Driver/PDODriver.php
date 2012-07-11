3097c1a2750b3b02cc4d0efbe53101fc493f738e
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2012-07-10 22:17:00 -0430

    Extracting PDO driver functions into a trait, starting Sqlite driver

---
+++ lib/Cake/Model/Datasource/Database/Driver/PDODriver.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Cake\Model\Datasource\Database\Driver;
+
+use Cake\Model\Datasource\Database\Statement;
+use PDO;
+
+trait PDODriver {
+
+/**
+ * Establishes a connection to the databse server
+ *
+ * @param array $config configuration to be used for creating connection
+ * @return boolean true on success
+ **/
+	public function connect(array $config) {
+		$connection = new PDO(
+			$config['dsn'],
+			$config['login'],
+			$config['password'],
+			$config['flags']
+		);
+		$this->connection($connection);
+		return true;
+	}
+
+/**
+ * Returns correct connection resource or object that is internally used
+ * If first argument is passed, it will set internal conenction object or
+ * result to the value passed
+ *
+ * @return mixed connection object used internally
+ **/
+	public  function connection($connection = null) {
+		if ($connection !== null) {
+			$this->_connection = $connection;
+		}
+		return $this->_connection;
+	}
+
+/**
+ * Disconnects from database server
+ *
+ * @return void
+ **/
+	public function disconnect() {
+		$this->_connection = null;
+	}
+
+/**
+ * Prepares a sql statement to be executed
+ *
+ * @param string $sql
+ * @return Cake\Model\Datasource\Database\Statement
+ **/
+	public  function prepare($sql) {
+		$statement = $this->connection()->prepare($sql);
+		return new Statement($statement, $this);
+	}
+
+/**
+ * Starts a transaction
+ *
+ * @return boolean true on success, false otherwise
+ **/
+	public function beginTransaction() {
+		return $this->connection()->beginTransaction();
+	}
+
+/**
+ * Commits a transaction
+ *
+ * @return boolean true on success, false otherwise
+ **/
+	public function commitTransaction() {
+		return $this->connection()->commit();
+	}
+
+/**
+ * Rollsback a transaction
+ *
+ * @return boolean true on success, false otherwise
+ **/
+	public function rollbackTransaction() {
+		return $this->connection()->rollback();
+	}
+
+/**
+ * Returns a value in a safe representation to be used in a query string
+ *
+ * @return string
+ **/
+	public function quote($value, $type) {
+		return $this->connection()->quote($value, $type);
+	}
+
+/**
+ * Returns last id generated for a table or sequence in database
+ *
+ * @param string $table table name or sequence to get last insert value from
+ * @return string|integer
+ **/
+	public function lastInsertId($table = null) {
+		return $this->connection()->lastInsertId();
+	}
+
+}
