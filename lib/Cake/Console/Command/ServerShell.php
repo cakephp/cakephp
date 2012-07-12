08ff64d7405dd9aebc705ac78565933371b24f51
shin1x1 <shin1x1@gmail.com>
2012-07-13 00:06:14 +0900

    fixed indent, tab, typo.

--- lib/Cake/Console/Command/ServerShell.php
+++ lib/Cake/Console/Command/ServerShell.php
@@ -2,8 +2,6 @@
 /**
  * built-in Server Shell
  *
- * Implementation of a Cake Shell to show CakePHP core method signatures.
- *
  * PHP 5
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
@@ -14,7 +12,7 @@
  *
  * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 1.2.0.5012
+ * @since         CakePHP(tm) v 2.3.0
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
@@ -26,123 +24,129 @@ App::uses('AppShell', 'Console/Command');
  * @package       Cake.Console.Command
  */
 class ServerShell extends AppShell {
-    const DEFAULT_HOST = 'localhost';
-    const DEFAULT_PORT = 80;
+	/**
+	 * Default ServerHost
+	 */
+	const DEFAULT_HOST = 'localhost';
+	/**
+	 * Default ListenPort
+	 */
+	const DEFAULT_PORT = 80;
 
-    /**
-     * server host
-     *
-     * @var string
-     */
-    protected $_host = null;
+	/**
+	 * server host
+	 *
+	 * @var string
+	 */
+	protected $_host = null;
 
-    /**
-     * listern port
-     *
-     * @var string
-     */
-    protected $_port = null;
+	/**
+	 * listen port
+	 *
+	 * @var string
+	 */
+	protected $_port = null;
 
-    /**
-     * document root
-     *
-     * @var string
-     */
-    protected $_documentRoot = null;
+	/**
+	 * document root
+	 *
+	 * @var string
+	 */
+	protected $_documentRoot = null;
 
-    /**
-     * Override initialize of the Shell
-     *
-     * @return void
-     */
-    public function initialize() {
-        $this->_host = self::DEFAULT_HOST;
-        $this->_port = self::DEFAULT_PORT;
-        $this->_documentRoot = WWW_ROOT;
-    }
+	/**
+	 * Override initialize of the Shell
+	 *
+	 * @return void
+	 */
+	public function initialize() {
+		$this->_host = self::DEFAULT_HOST;
+		$this->_port = self::DEFAULT_PORT;
+		$this->_documentRoot = WWW_ROOT;
+	}
 
-    /**
-     * Starts up the Shell and displays the welcome message.
-     * Allows for checking and configuring prior to command or main execution
-     *
-     * Override this method if you want to remove the welcome information,
-     * or otherwise modify the pre-command flow.
-     *
-     * @return void
-     * @link http://book.cakephp.org/2.0/en/console-and-shells.html#Shell::startup
-     */
-    public function startup() {
-        parent::startup();
+	/**
+	 * Starts up the Shell and displays the welcome message.
+	 * Allows for checking and configuring prior to command or main execution
+	 *
+	 * Override this method if you want to remove the welcome information,
+	 * or otherwise modify the pre-command flow.
+	 *
+	 * @return void
+	 * @link http://book.cakephp.org/2.0/en/console-and-shells.html#Shell::startup
+	 */
+	public function startup() {
+		parent::startup();
 
-        if (!empty($this->params['host'])) {
-            $this->_host = $this->params['host'];
-        }
-        if (!empty($this->params['port'])) {
-            $this->_port = $this->params['port'];
-        }
-        if (!empty($this->params['document_root'])) {
-            $this->_documentRoot = $this->params['document_root'];
-        }
+		if (!empty($this->params['host'])) {
+			$this->_host = $this->params['host'];
+		}
+		if (!empty($this->params['port'])) {
+			$this->_port = $this->params['port'];
+		}
+		if (!empty($this->params['document_root'])) {
+			$this->_documentRoot = $this->params['document_root'];
+		}
 
-        // for windows
-        if (substr($this->_documentRoot, -1, 1) == DIRECTORY_SEPARATOR) {
-          $this->_documentRoot = substr($this->_documentRoot, 0, strlen($this->_documentRoot) -1);
-        }
-        if (preg_match("/^([a-z]:)[\\\]+(.+)$/i", $this->_documentRoot, $m)) {
-          $this->_documentRoot = $m[1].'\\'.$m[2];
-        }
-    }
+		// for windows
+		if (substr($this->_documentRoot, -1, 1) == DIRECTORY_SEPARATOR) {
+			$this->_documentRoot = substr($this->_documentRoot, 0, strlen($this->_documentRoot) -1);
+		}
+		if (preg_match("/^([a-z]:)[\\\]+(.+)$/i", $this->_documentRoot, $m)) {
+			$this->_documentRoot = $m[1].'\\'.$m[2];
+		}
+	}
 
-    /**
-     * Override main() to handle action
-     *
-     * @return void
-     */
-    public function main() {
-        if (version_compare(PHP_VERSION, '5.4.0') < 0) {
-            $this->out(__d('cake_console', '<warning>This command is available on PHP5.4 or above</warning>'));
-            return;
-        }
+	/**
+	 * Override main() to handle action
+	 *
+	 * @return void
+	 */
+	public function main() {
+		if (version_compare(PHP_VERSION, '5.4.0') < 0) {
+			$this->out(__d('cake_console', '<warning>This command is available on PHP5.4 or above</warning>'));
+			return;
+		}
 
-        $this->out(__d('cake_console', 'ServerHost  : %s', $this->_host));
-        $this->out(__d('cake_console', 'ListenPort  : %d', $this->_port));
-        $this->out(__d('cake_console', 'DocumentRoot: %s', $this->_documentRoot));
-        $this->hr();
+		$this->out(__d('cake_console', 'ServerHost  : %s', $this->_host));
+		$this->out(__d('cake_console', 'ListenPort  : %d', $this->_port));
+		$this->out(__d('cake_console', 'DocumentRoot: %s', $this->_documentRoot));
+		$this->hr();
 
-        $command = sprintf("php -S %s:%d -t %s",
-                      $this->_host,
-                      $this->_port,
-                      $this->_documentRoot
-                    );
+		$command = sprintf("php -S %s:%d -t %s",
+			$this->_host,
+			$this->_port,
+			$this->_documentRoot
+		);
 
-        $this->out(__d('cake_console', '<warning>[WARNING] Don\'t use this at the production enviroment</warning>'));
-        $this->out(__d('cake_console', 'built-in server is running...'));
-        $ret = system($command);
-    }
+		$this->out(__d('cake_console', '<warning>[WARNING] Don\'t use this at the production environment</warning>'));
+		$this->out(__d('cake_console', 'built-in server is running...'));
+		$ret = system($command);
+	}
 
-      /**
-       * Get and configure the optionparser.
-       *
-       * @return ConsoleOptionParser
-       */
-      public function getOptionParser() {
-          $parser = parent::getOptionParser();
+	/**
+	 * Get and configure the optionparser.
+	 *
+	 * @return ConsoleOptionParser
+	 */
+	public function getOptionParser() {
+		$parser = parent::getOptionParser();
 
-          $parser->addOption('host', array(
-              'short' => 'H',
-              'help' => __d('cake_console', 'ServerHost')
-          ));
-          $parser->addOption('port', array(
-              'short' => 'p',
-              'help' => __d('cake_console', 'ListenPort')
-          ));
-          $parser->addOption('document_root', array(
-              'short' => 'd',
-              'help' => __d('cake_console', 'DocumentRoot')
-          ));
+		$parser->addOption('host', array(
+			'short' => 'H',
+			'help' => __d('cake_console', 'ServerHost')
+		));
+		$parser->addOption('port', array(
+			'short' => 'p',
+			'help' => __d('cake_console', 'ListenPort')
+		));
+		$parser->addOption('document_root', array(
+			'short' => 'd',
+			'help' => __d('cake_console', 'DocumentRoot')
+		));
 
-          $parser->description(__('PHP Built-in Server for CakePHP'));
+		$parser->description(__d('cake_console', 'PHP Built-in Server for CakePHP'));
 
-          return $parser;
-      }
+		return $parser;
+	}
 }
