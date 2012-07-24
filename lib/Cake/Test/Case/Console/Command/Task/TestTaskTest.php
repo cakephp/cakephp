35bf3097cd3c22eedb9c9588e5caa3bee0866da8
mark_story <mark@mark-story.com>
2012-07-23 21:25:55 -0400

    Fixing missing imports in generated tests.
    Fixes #3059

--- lib/Cake/Test/Case/Console/Command/Task/TestTaskTest.php
+++ lib/Cake/Test/Case/Console/Command/Task/TestTaskTest.php
@@ -491,6 +491,8 @@ class TestTaskTest extends CakeTestCase {
 
 		$result = $this->Task->bake('Component', 'Example');
 
+		$this->assertContains("App::uses('Component', 'Controller')", $result);
+		$this->assertContains("App::uses('ComponentCollection', 'Controller')", $result);
 		$this->assertContains("App::uses('ExampleComponent', 'Controller/Component')", $result);
 		$this->assertContains('class ExampleComponentTest extends CakeTestCase', $result);
 
