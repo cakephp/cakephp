9b94133f0d1cba534e78aba96f5546adc33e5889
mark_story <mark@mark-story.com>
2012-07-30 21:27:21 -0400

    Fix uncaught exceptions when generating view cache files
    Fixes #3016

--- lib/Cake/View/Helper/CacheHelper.php
+++ lib/Cake/View/Helper/CacheHelper.php
@@ -150,7 +150,17 @@ class CacheHelper extends AppHelper {
 
 		if ($cacheTime != '' && $cacheTime > 0) {
 			$cached = $this->_parseOutput($out);
-			$this->_writeFile($cached, $cacheTime, $useCallbacks);
+			try {
+				$this->_writeFile($cached, $cacheTime, $useCallbacks);
+			} catch (Exception $e) {
+				$message = __d(
+					'cake_dev',
+					'Unable to write view cache file: "%s" for "%s"',
+					$e->getMessage(),
+					$this->request->here
+				);
+				$this->log($message, 'error');
+			}
 			$out = $this->_stripTags($out);
 		}
 		return $out;
