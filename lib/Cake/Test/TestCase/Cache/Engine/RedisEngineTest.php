2ed9ff925a5dda66063d335a20eac3262d688d4e
mark_story <mark@mark-story.com>
2012-09-05 22:04:25 -0400

    Update redis tests.

--- lib/Cake/Test/TestCase/Cache/Engine/RedisEngineTest.php
+++ lib/Cake/Test/TestCase/Cache/Engine/RedisEngineTest.php
@@ -1,9 +1,5 @@
 <?php
 /**
- * RedisEngineTest file
- *
- * PHP 5
- *
  * CakePHP(tm) Tests <http://book.cakephp.org/view/1196/Testing>
  * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
@@ -12,7 +8,6 @@
  *
  * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://book.cakephp.org/view/1196/Testing CakePHP(tm) Tests
- * @package       Cake.Test.Case.Cache.Engine
  * @since         CakePHP(tm) v 2.2
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
@@ -36,12 +31,12 @@ class RegisEngineTest extends TestCase {
  * @return void
  */
 	public function setUp() {
+		parent::setUp();
 		$this->skipIf(!class_exists('Redis'), 'Redis is not installed or configured properly.');
 
-		$this->_cacheDisable = Configure::read('Cache.disable');
 		Configure::write('Cache.disable', false);
-		Cache::config('redis', array(
-			'engine' => 'Cake\Cache\Redis',
+		Configure::write('Cache.redis', array(
+			'engine' => 'Cake\Cache\Engine\RedisEngine',
 			'prefix' => 'cake_',
 			'duration' => 3600
 		));
@@ -53,11 +48,10 @@ class RegisEngineTest extends TestCase {
  * @return void
  */
 	public function tearDown() {
-		Configure::write('Cache.disable', $this->_cacheDisable);
-		Cache::drop('');
+		parent::tearDown();
+		Cache::drop('redis');
 		Cache::drop('redis_groups');
 		Cache::drop('redis_helper');
-		Cache::config('default');
 	}
 
 /**
@@ -72,7 +66,7 @@ class RegisEngineTest extends TestCase {
 			'duration' => 3600,
 			'probability' => 100,
 			'groups' => array(),
-			'engine' => 'Redis',
+			'engine' => 'Cake\Cache\Engine\RedisEngine',
 			'server' => '127.0.0.1',
 			'port' => 6379,
 			'timeout' => 0,
@@ -97,7 +91,7 @@ class RegisEngineTest extends TestCase {
  * @return void
  */
 	public function testReadAndWriteCache() {
-		Cache::set(array('duration' => 1), null, 'redis');
+		Cache::set(['duration' => 1], 'redis');
 
 		$result = Cache::read('test', 'redis');
 		$expecting = '';
@@ -147,13 +141,13 @@ class RegisEngineTest extends TestCase {
 		$result = Cache::read('other_test', 'redis');
 		$this->assertFalse($result);
 
-		Cache::config('redis', array('duration' => '+1 second'));
+		Cache::set(['duration' => '+1 second'], 'redis');
 		sleep(2);
 
 		$result = Cache::read('other_test', 'redis');
 		$this->assertFalse($result);
 
-		Cache::config('redis', array('duration' => '+29 days'));
+		Cache::set(['duration' => '+29 days'], 'redis');
 		$data = 'this is a test of the emergency broadcasting system';
 		$result = Cache::write('long_expiry_test', $data, 'redis');
 		$this->assertTrue($result);
@@ -162,8 +156,6 @@ class RegisEngineTest extends TestCase {
 		$result = Cache::read('long_expiry_test', 'redis');
 		$expecting = $data;
 		$this->assertEquals($expecting, $result);
-
-		Cache::config('redis', array('duration' => 3600));
 	}
 
 /**
@@ -229,7 +221,7 @@ class RegisEngineTest extends TestCase {
  * @return void
  */
 	public function testClear() {
-		Cache::config('redis2', array(
+		Configure::write('Cache.redis2', array(
 			'engine' => 'Redis',
 			'prefix' => 'cake2_',
 			'duration' => 3600
@@ -255,7 +247,7 @@ class RegisEngineTest extends TestCase {
  * @return void
  */
 	public function testZeroDuration() {
-		Cache::config('redis', array('duration' => 0));
+		Cache::set(['duration' => 0], 'redis');
 		$result = Cache::write('test_key', 'written!', 'redis');
 
 		$this->assertTrue($result);
@@ -271,17 +263,17 @@ class RegisEngineTest extends TestCase {
  * @return void
  */
 	public function testGroupReadWrite() {
-		Cache::config('redis_groups', array(
+		Configure::write('Cache.redis_groups', [
 			'engine' => 'Redis',
 			'duration' => 3600,
-			'groups' => array('group_a', 'group_b'),
+			'groups' => ['group_a', 'group_b'],
 			'prefix' => 'test_'
-		));
-		Cache::config('redis_helper', array(
+		]);
+		Configure::write('Cache.redis_helper', [
 			'engine' => 'Redis',
 			'duration' => 3600,
 			'prefix' => 'test_'
-		));
+		]);
 		$this->assertTrue(Cache::write('test_groups', 'value', 'redis_groups'));
 		$this->assertEquals('value', Cache::read('test_groups', 'redis_groups'));
 
@@ -302,11 +294,11 @@ class RegisEngineTest extends TestCase {
  * @return void
  */
 	public function testGroupDelete() {
-		Cache::config('redis_groups', array(
+		Configure::write('Cache.redis_groups', [
 			'engine' => 'Redis',
 			'duration' => 3600,
-			'groups' => array('group_a', 'group_b')
-		));
+			'groups' => ['group_a', 'group_b']
+		]);
 		$this->assertTrue(Cache::write('test_groups', 'value', 'redis_groups'));
 		$this->assertEquals('value', Cache::read('test_groups', 'redis_groups'));
 		$this->assertTrue(Cache::delete('test_groups', 'redis_groups'));
@@ -320,11 +312,11 @@ class RegisEngineTest extends TestCase {
  * @return void
  **/
 	public function testGroupClear() {
-		Cache::config('redis_groups', array(
+		Configure::write('Cache.redis_groups', [
 			'engine' => 'Redis',
 			'duration' => 3600,
-			'groups' => array('group_a', 'group_b')
-		));
+			'groups' => ['group_a', 'group_b']
+		]);
 
 		$this->assertTrue(Cache::write('test_groups', 'value', 'redis_groups'));
 		$this->assertTrue(Cache::clearGroup('group_a', 'redis_groups'));
