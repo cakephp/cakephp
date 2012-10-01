c94886a9882e69629a07ba892f0d32891005f3f9
mark_story <mark@mark-story.com>
2012-09-30 22:51:19 -0400

    Add missing import that causes tests to fail in isolation.

--- lib/Cake/Test/Case/View/ViewTest.php
+++ lib/Cake/Test/Case/View/ViewTest.php
@@ -21,6 +21,7 @@ App::uses('View', 'View');
 App::uses('Helper', 'View');
 App::uses('Controller', 'Controller');
 App::uses('CacheHelper', 'View/Helper');
+App::uses('HtmlHelper', 'View/Helper');
 App::uses('ErrorHandler', 'Error');
 
 
