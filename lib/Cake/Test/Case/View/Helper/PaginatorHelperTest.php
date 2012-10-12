61dd1098d381f097c33f30c2bdfca8d252639547
ADmad <admad.coder@gmail.com>
2012-10-12 20:13:45 +0530

    Allow specifying tag for current page number. Closes #2892

--- lib/Cake/Test/Case/View/Helper/PaginatorHelperTest.php
+++ lib/Cake/Test/Case/View/Helper/PaginatorHelperTest.php
@@ -1436,6 +1436,28 @@ class PaginatorHelperTest extends CakeTestCase {
 		);
 		$this->assertTags($result, $expected);
 
+		$result = $this->Paginator->numbers(array('first' => 1, 'tag' => 'li', 'currentClass' => 'active', 'currentTag' => 'a'));
+		$expected = array(
+			array('li' => array()), array('a' => array('href' => '/index/page:1')), '1', '/a', '/li',
+			' | ',
+			array('li' => array('class' => 'active')), array('a' => array()), '2', '/a', '/li',
+			' | ',
+			array('li' => array()), array('a' => array('href' => '/index/page:3')), '3', '/a', '/li',
+			' | ',
+			array('li' => array()), array('a' => array('href' => '/index/page:4')), '4', '/a', '/li',
+			' | ',
+			array('li' => array()), array('a' => array('href' => '/index/page:5')), '5', '/a', '/li',
+			' | ',
+			array('li' => array()), array('a' => array('href' => '/index/page:6')), '6', '/a', '/li',
+			' | ',
+			array('li' => array()), array('a' => array('href' => '/index/page:7')), '7', '/a', '/li',
+			' | ',
+			array('li' => array()), array('a' => array('href' => '/index/page:8')), '8', '/a', '/li',
+			' | ',
+			array('li' => array()), array('a' => array('href' => '/index/page:9')), '9', '/a', '/li',
+		);
+		$this->assertTags($result, $expected);
+
 		$result = $this->Paginator->numbers(array('first' => 1, 'class' => 'page-link', 'currentClass' => 'active'));
 		$expected = array(
 			array('span' => array('class' => 'page-link')), array('a' => array('href' => '/index/page:1')), '1', '/a', '/span',
