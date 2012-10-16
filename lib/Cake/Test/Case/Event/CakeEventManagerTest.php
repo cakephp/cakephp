6d3ae3b83cc0ad7f2551ad4d35819cb56129cb30
mark_story <mark@mark-story.com>
2012-10-15 21:48:36 -0400

    Fix missed use of VERSION.

--- lib/Cake/Test/Case/Event/CakeEventManagerTest.php
+++ lib/Cake/Test/Case/Event/CakeEventManagerTest.php
@@ -235,7 +235,7 @@ class CakeEventManagerTest extends CakeTestCase {
  */
 	public function testDispatchReturnValue() {
 		$this->skipIf(
-			version_compare(PHPUnit_Runner_Version::VERSION, '3.7', '<'),
+			version_compare(PHPUnit_Runner_Version::id(), '3.7', '<'),
 			'These tests fail in PHPUnit 3.6'
 		);
 		$manager = new CakeEventManager;
