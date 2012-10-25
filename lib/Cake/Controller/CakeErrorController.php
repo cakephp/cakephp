d269588e78ce0b20e986136a598e548bdfabe3aa
Ber Clausen <crashcookie@gmail.com>
2012-10-25 12:19:53 -0300

    Avoid duplicating RequestHandler component.

--- lib/Cake/Controller/CakeErrorController.php
+++ lib/Cake/Controller/CakeErrorController.php
@@ -50,7 +50,11 @@ class CakeErrorController extends AppController {
  */
 	public function __construct($request = null, $response = null) {
 		parent::__construct($request, $response);
-		if (count(Router::extensions())) {
+		if (
+			count(Router::extensions()) &&
+			!array_key_exists('RequestHandler', $this->components) &&
+			!in_array('RequestHandler', $this->components, true)
+		) {
 			$this->components[] = 'RequestHandler';
 		}
 		$this->constructClasses();
