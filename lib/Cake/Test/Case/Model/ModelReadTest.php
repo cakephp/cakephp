ab2ce29bd690a6111354edbbe8ac88d34c254986
mark_story <mark@mark-story.com>
2012-10-28 16:11:07 -0400

    Fix a few more tests that sometimes fail on postgres.

--- lib/Cake/Test/Case/Model/ModelReadTest.php
+++ lib/Cake/Test/Case/Model/ModelReadTest.php
@@ -4959,7 +4959,9 @@ class ModelReadTest extends BaseModelTest {
 	public function testAssociationAfterFind() {
 		$this->loadFixtures('Post', 'Author', 'Comment');
 		$TestModel = new Post();
-		$result = $TestModel->find('all');
+		$result = $TestModel->find('all', array(
+			'order' => array('Post.id' => 'ASC')
+		));
 		$expected = array(
 			array(
 				'Post' => array(
@@ -5028,6 +5030,7 @@ class ModelReadTest extends BaseModelTest {
 		)));
 		$result = $Author->find('all', array(
 			'conditions' => array('Author.id' => 1),
+			'order' => array('Author.id' => 'ASC'),
 			'recursive' => 2
 		));
 		$expected = array(
