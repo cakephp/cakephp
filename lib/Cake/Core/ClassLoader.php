957d331815fbca286a1b92c79527d1cdada94055
mark_story <mark@mark-story.com>
2012-11-01 21:43:18 -0400

    Remove code that has no reason to exist.
    Fix code styles.

--- lib/Cake/Core/ClassLoader.php
+++ lib/Cake/Core/ClassLoader.php
@@ -25,136 +25,110 @@ namespace Cake\Core;
  */
 class ClassLoader {
 
-	/**
-	 * File extension
-	 *
-	 * @var string
-	 */
+/**
+ * File extension
+ *
+ * @var string
+ */
 	protected $fileExtension = '.php';
 
-	/**
-	 * Registered namespace
-	 *
-	 * @var string
-	 */
+/**
+ * Registered namespace
+ *
+ * @var string
+ */
 	protected $namespace;
 
-	/**
-	 * Store the namespace length for performance
-	 *
-	 * @var integer
-	 */
+/**
+ * Store the namespace length for performance
+ *
+ * @var integer
+ */
 	protected $namespaceLength;
 
-	/**
-	 * Path with the classes
-	 *
-	 * @var string
-	 */
+/**
+ * Path with the classes
+ *
+ * @var string
+ */
 	protected $includePath;
 
-	/**
-	 * Namespace separator
-	 *
-	 * @var string
-	 */
-	protected $namespaceSeparator = '\\';
-
-	/**
-	 * Constructor
-	 * 
-	 * @param string $ns The namespace to use.
-	 */
+/**
+ * Constructor
+ *
+ * @param string $ns The namespace to use.
+ */
 	public function __construct($ns = null, $includePath = null) {
-		$this->namespace = rtrim($ns, $this->namespaceSeparator) . $this->namespaceSeparator;
+		$this->namespace = rtrim($ns, '\\') . '\\';
 		$this->namespaceLength = strlen($this->namespace);
 		$this->includePath = $includePath;
 	}
 
-	/**
-	 * Sets the namespace separator used by classes in the namespace of this class loader.
-	 * 
-	 * @param string $sep The separator to use.
-	 * @return void
-	 */
-	public function setNamespaceSeparator($sep) {
-		$this->namespaceSeparator = $sep;
-	}
-
-	/**
-	 * Gets the namespace seperator used by classes in the namespace of this class loader.
-	 *
-	 * @return string
-	 */
-	public function getNamespaceSeparator() {
-		return $this->namespaceSeparator;
-	}
-
-	/**
-	 * Sets the base include path for all class files in the namespace of this class loader.
-	 * 
-	 * @param string $includePath
-	 * @return void
-	 */
+/**
+ * Sets the base include path for all class files in the namespace of this class loader.
+ *
+ * @param string $includePath
+ * @return void
+ */
 	public function setIncludePath($includePath) {
 		$this->includePath = $includePath;
 	}
 
-	/**
-	 * Gets the base include path for all class files in the namespace of this class loader.
-	 *
-	 * @return string
-	 */
+/**
+ * Gets the base include path for all class files in the namespace of this class loader.
+ *
+ * @return string
+ */
 	public function getIncludePath() {
 		return $this->includePath;
 	}
 
-	/**
-	 * Sets the file extension of class files in the namespace of this class loader.
-	 * 
-	 * @param string $fileExtension
-	 * @return void
-	 */
+/**
+ * Sets the file extension of class files in the namespace of this class loader.
+ *
+ * @param string $fileExtension
+ * @return void
+ */
 	public function setFileExtension($fileExtension) {
 		$this->fileExtension = $fileExtension;
 	}
 
-	/**
-	 * Gets the file extension of class files in the namespace of this class loader.
-	 *
-	 * @return string
-	 */
+/**
+ * Gets the file extension of class files in the namespace of this class loader.
+ *
+ * @return string
+ */
 	public function getFileExtension() {
 		return $this->fileExtension;
 	}
 
-	/**
-	 * Installs this class loader on the SPL autoload stack.
-	 *
-	 * @return void
-	 */
+/**
+ * Installs this class loader on the SPL autoload stack.
+ *
+ * @return void
+ */
 	public function register() {
 		spl_autoload_register(array($this, 'loadClass'));
 	}
 
-	/**
-	 * Uninstalls this class loader from the SPL autoloader stack.
-	 *
-	 * @return void
-	 */
+/**
+ * Uninstalls this class loader from the SPL autoloader stack.
+ *
+ * @return void
+ */
 	public function unregister() {
 		spl_autoload_unregister(array($this, 'loadClass'));
 	}
 
-	/**
-	 * Loads the given class or interface.
-	 *
-	 * @param string $className The name of the class to load.
-	 * @return boolean
-	 */
+/**
+ * Loads the given class or interface.
+ *
+ * @param string $className The name of the class to load.
+ * @return boolean
+ */
 	public function loadClass($className) {
 		if (substr($className, 0, $this->namespaceLength) === $this->namespace) {
-			$path = $this->includePath . DS . str_replace($this->namespaceSeparator, DS, $className) . $this->fileExtension;
+			$path = $this->includePath . DS . str_replace('\\', DS, $className) . $this->fileExtension;
 			if (!file_exists($path)) {
 				return false;
 			}
