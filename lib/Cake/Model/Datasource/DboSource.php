9c7f357029bd7526b30ddfe993f1c3afe4e7373d
Nicolas <nicolas@srwebs.it>
2012-11-17 12:39:08 +0100

    Check if bool operators in find conditions are empty.

--- lib/Cake/Model/Datasource/DboSource.php
+++ lib/Cake/Model/Datasource/DboSource.php
@@ -2464,6 +2464,10 @@ class DboSource extends DataSource {
 					$not = 'NOT ';
 				}
 
+				if (empty($value)) {
+					continue;
+				}
+
 				if (empty($value[1])) {
 					if ($not) {
 						$out[] = $not . '(' . $value[0] . ')';
