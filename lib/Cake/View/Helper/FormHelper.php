6d003d62ca68d7fdfc72fea1ce4beb1c85d9d9f4
Rachman Chavik <rchavik@xintesa.com>
2012-11-23 11:09:48 +0700

    FormHelper: refactor option detection as a method

--- lib/Cake/View/Helper/FormHelper.php
+++ lib/Cake/View/Helper/FormHelper.php
@@ -910,37 +910,14 @@ class FormHelper extends AppHelper {
 	}
 
 /**
- * Generates a form input element complete with label and wrapper div
+ * Detects input options for field
  *
- * ### Options
- *
- * See each field type method for more information. Any options that are part of
- * $attributes or $options for the different **type** methods can be included in `$options` for input().i
- * Additionally, any unknown keys that are not in the list below, or part of the selected type's options
- * will be treated as a regular html attribute for the generated input.
- *
- * - `type` - Force the type of widget you want. e.g. `type => 'select'`
- * - `label` - Either a string label, or an array of options for the label. See FormHelper::label()
- * - `div` - Either `false` to disable the div, or an array of options for the div.
- *	See HtmlHelper::div() for more options.
- * - `options` - for widgets that take options e.g. radio, select
- * - `error` - control the error message that is produced
- * - `empty` - String or boolean to enable empty select box options.
- * - `before` - Content to place before the label + input.
- * - `after` - Content to place after the label + input.
- * - `between` - Content to place between the label + input.
- * - `format` - format template for element order. Any element that is not in the array, will not be in the output.
- *	- Default input format order: array('before', 'label', 'between', 'input', 'after', 'error')
- *	- Default checkbox format order: array('before', 'input', 'between', 'label', 'after', 'error')
- *	- Hidden input will not be formatted
- *	- Radio buttons cannot have the order of input and label elements controlled with these settings.
- *
+ * @see FormHelper::input()
  * @param string $fieldName This should be "Modelname.fieldname"
  * @param array $options Each type of input takes different options.
- * @return string Completed form widget.
- * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/form.html#creating-form-elements
+ * @return array array of options
  */
-	public function input($fieldName, $options = array()) {
+	protected function _inputOptions($fieldName, $options = array()) {
 		$this->setEntity($fieldName);
 
 		$options = array_merge(
@@ -1092,6 +1069,48 @@ class FormHelper extends AppHelper {
 			$dateFormat = $this->_extractOption('dateFormat', $options, 'MDY');
 			$timeFormat = $this->_extractOption('timeFormat', $options, 12);
 			unset($options['dateFormat'], $options['timeFormat']);
+		}
+
+		return compact('options', 'label', 'modelKey', 'fieldKey', 'fieldDef',
+			'divOptions', 'radioOptions', 'error', 'selected', 'dateFormat',
+			'timeFormat');
+	}
+
+/**
+ * Generates a form input element complete with label and wrapper div
+ *
+ * ### Options
+ *
+ * See each field type method for more information. Any options that are part of
+ * $attributes or $options for the different **type** methods can be included in `$options` for input().i
+ * Additionally, any unknown keys that are not in the list below, or part of the selected type's options
+ * will be treated as a regular html attribute for the generated input.
+ *
+ * - `type` - Force the type of widget you want. e.g. `type => 'select'`
+ * - `label` - Either a string label, or an array of options for the label. See FormHelper::label()
+ * - `div` - Either `false` to disable the div, or an array of options for the div.
+ *	See HtmlHelper::div() for more options.
+ * - `options` - for widgets that take options e.g. radio, select
+ * - `error` - control the error message that is produced
+ * - `empty` - String or boolean to enable empty select box options.
+ * - `before` - Content to place before the label + input.
+ * - `after` - Content to place after the label + input.
+ * - `between` - Content to place between the label + input.
+ * - `format` - format template for element order. Any element that is not in the array, will not be in the output.
+ *	- Default input format order: array('before', 'label', 'between', 'input', 'after', 'error')
+ *	- Default checkbox format order: array('before', 'input', 'between', 'label', 'after', 'error')
+ *	- Hidden input will not be formatted
+ *	- Radio buttons cannot have the order of input and label elements controlled with these settings.
+ *
+ * @param string $fieldName This should be "Modelname.fieldname"
+ * @param array $options Each type of input takes different options.
+ * @return string Completed form widget.
+ * @link http://book.cakephp.org/2.0/en/core-libraries/helpers/form.html#creating-form-elements
+ */
+	public function input($fieldName, $options = array()) {
+		if (!isset($options['modelKey'])) {
+			$inputOptions = $this->_inputOptions($fieldName, $options);
+			extract($inputOptions);
 		}
 
 		$type = $options['type'];
