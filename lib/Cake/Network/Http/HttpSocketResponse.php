ec3e7afd75d9a4bd487211289009f6a2fffe9088
Ceeram <c33ram@gmail.com>
2012-12-10 17:06:06 +0100

    Remove todo's whihc have been removed in e02eab05d5e598bc2af3489a4e4cba61a2419d36 and accidentally been re-added in 3e3af1f99831b71abe41f7da7f54d6b9b9378d2e

--- lib/Cake/Network/Http/HttpSocketResponse.php
+++ lib/Cake/Network/Http/HttpSocketResponse.php
@@ -226,16 +226,12 @@ class HttpSocketResponse implements ArrayAccess {
 
 			$chunkSize = 0;
 			$hexLength = 0;
-			$chunkExtensionName = '';
 			$chunkExtensionValue = '';
 			if (isset($match[0])) {
 				$chunkSize = $match[0];
 			}
 			if (isset($match[1])) {
 				$hexLength = $match[1];
-			}
-			if (isset($match[2])) {
-				$chunkExtensionName = $match[2];
 			}
 			if (isset($match[3])) {
 				$chunkExtensionValue = $match[3];
@@ -244,9 +240,6 @@ class HttpSocketResponse implements ArrayAccess {
 			$body = substr($body, strlen($chunkSize));
 			$chunkLength = hexdec($hexLength);
 			$chunk = substr($body, 0, $chunkLength);
-			if (!empty($chunkExtensionName)) {
-				// @todo See if there are popular chunk extensions we should implement
-			}
 			$decodedBody .= $chunk;
 			if ($chunkLength !== 0) {
 				$body = substr($body, $chunkLength + strlen("\r\n"));
@@ -298,7 +291,6 @@ class HttpSocketResponse implements ArrayAccess {
  *
  * @param array $header Header array containing one ore more 'Set-Cookie' headers.
  * @return mixed Either false on no cookies, or an array of cookies received.
- * @todo Make this 100% RFC 2965 confirm
  */
 	public function parseCookies($header) {
 		$cookieHeader = $this->getHeader('Set-Cookie', $header);
@@ -341,7 +333,6 @@ class HttpSocketResponse implements ArrayAccess {
  * @param string $token Token to unescape
  * @param array $chars
  * @return string Unescaped token
- * @todo Test $chars parameter
  */
 	protected function _unescapeToken($token, $chars = null) {
 		$regex = '/"([' . implode('', $this->_tokenEscapeChars(true, $chars)) . '])"/';
@@ -355,7 +346,6 @@ class HttpSocketResponse implements ArrayAccess {
  * @param boolean $hex true to get them as HEX values, false otherwise
  * @param array $chars
  * @return array Escape chars
- * @todo Test $chars parameter
  */
 	protected function _tokenEscapeChars($hex = true, $chars = null) {
 		if (!empty($chars)) {
