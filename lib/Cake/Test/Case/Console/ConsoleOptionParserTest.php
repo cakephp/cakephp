729e97ce285ab8012d13445144dfca5bc4182653
euromark <euromark@web.de>
2012-12-13 22:55:49 +0100

    help text from the subcommands is missing on help

--- lib/Cake/Test/Case/Console/ConsoleOptionParserTest.php
+++ lib/Cake/Test/Case/Console/ConsoleOptionParserTest.php
@@ -506,6 +506,8 @@ class ConsoleOptionParserTest extends CakeTestCase {
 
 		$result = $parser->help('method');
 		$expected = <<<TEXT
+This is another command
+
 <info>Usage:</info>
 cake mycommand method [-h] [--connection]
 
