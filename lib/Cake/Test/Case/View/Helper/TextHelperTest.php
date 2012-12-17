1ea900a5aab3389fcbd2471029be73992e5721ef
Loki <lokitek@gmail.com>
2012-12-17 10:10:25 -0500

    Updated testAutoParagraph coding standard
    Updated the spaces in testAutoParagraph to match the coding standards

--- lib/Cake/Test/Case/View/Helper/TextHelperTest.php
+++ lib/Cake/Test/Case/View/Helper/TextHelperTest.php
@@ -338,29 +338,28 @@ class TextHelperTest extends CakeTestCase {
  *
  * @return void
  */
-  public function testAutoParagraph() {
-    $text = 'This is a test text';
-    $expected = '<p>This is a test text</p>
+	public function testAutoParagraph() {
+		$text = 'This is a test text';
+		$expected = '<p>This is a test text</p>
 ';
-    $result = $this->Text->autoParagraph($text);
-    $this->assertEquals($expected, $result);
-    
-    $text = 'This is a test text.
+		$result = $this->Text->autoParagraph($text);
+		$this->assertEquals($expected, $result);
+		$text = 'This is a test text.
 This is a line return.';
-    $expected = '<p>This is a test text.<br />
+		$expected = '<p>This is a test text.<br />
 This is a line return.</p>
 ';
-    $result = $this->Text->autoParagraph($text);
-    $this->assertEquals($expected, $result);
+		$result = $this->Text->autoParagraph($text);
+		$this->assertEquals($expected, $result);
     
-    $text = 'This is a test text.
+		$text = 'This is a test text.
 
 This is a new line.';
-    $expected = '<p>This is a test text.</p>
+		$expected = '<p>This is a test text.</p>
 <p>This is a new line.</p>
 ';
-    $result = $this->Text->autoParagraph($text);
-    $this->assertEquals($expected, $result);
-  }
+		$result = $this->Text->autoParagraph($text);
+		$this->assertEquals($expected, $result);
+	}
 
 }
