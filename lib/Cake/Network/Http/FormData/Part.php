2da4b7f61c08b915383aad258d352edb20d00d00
mark_story <mark@mark-story.com>
2012-12-17 22:11:20 -0500

    Add first batch of skeleton classes for new Http client.

---
+++ lib/Cake/Network/Http/FormData/Part.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         CakePHP(tm) v 3.0.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+namespace Cake\Network\Http\FormData;
+
+/**
+ * Contains the data and behavior for a single
+ * part in a Multipart FormData request body.
+ *
+ * Added to Cake\Network\Http\FormData when sending
+ * data to a remote server.
+ */
+class Part {
+
+}
