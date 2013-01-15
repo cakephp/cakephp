c07d3aa8bd60c5969f411cfe5d7c7b69560a0726
jroberts0001 <jroberts@v2cigs.com>
2013-01-15 03:13:54 -0500

    Added App::uses to the PluginAppController and PluginAppModel bake templates
    Changed \r\n to \n\n in PluginTask.php

--- lib/Cake/Console/Command/Task/PluginTask.php
+++ lib/Cake/Console/Command/Task/PluginTask.php
@@ -145,6 +145,7 @@ class PluginTask extends AppShell {
 			$controllerFileName = $plugin . 'AppController.php';
 
 			$out = "<?php\n\n";
+			$out .= "App::uses('AppController', 'Controller');\n\n";
 			$out .= "class {$plugin}AppController extends AppController {\n\n";
 			$out .= "}\n\n";
 			$this->createFile($this->path . $plugin . DS . 'Controller' . DS . $controllerFileName, $out);
@@ -152,6 +153,7 @@ class PluginTask extends AppShell {
 			$modelFileName = $plugin . 'AppModel.php';
 
 			$out = "<?php\n\n";
+			$out .= "App::uses('AppModel', 'Model');\n\n";
 			$out .= "class {$plugin}AppModel extends AppModel {\n\n";
 			$out .= "}\n\n";
 			$this->createFile($this->path . $plugin . DS . 'Model' . DS . $modelFileName, $out);
