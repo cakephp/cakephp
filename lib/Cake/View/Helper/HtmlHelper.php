db8127626a912fd3a2167d766d6bd107c485af59
ADmad <admad.coder@gmail.com>
2013-01-19 13:15:33 +0530

    Update helper usage to 2.x style in code examples

--- lib/Cake/View/Helper/HtmlHelper.php
+++ lib/Cake/View/Helper/HtmlHelper.php
@@ -557,7 +557,7 @@ class HtmlHelper extends AppHelper {
 	public function scriptBlock($script, $options = array()) {
 		$options += array('safe' => true, 'inline' => true);
 		if ($options['safe']) {
-			$script  = "\n" . '//<![CDATA[' . "\n" . $script . "\n" . '//]]>' . "\n";
+			$script = "\n" . '//<![CDATA[' . "\n" . $script . "\n" . '//]]>' . "\n";
 		}
 		if (!$options['inline'] && empty($options['block'])) {
 			$options['block'] = 'script';
@@ -616,7 +616,7 @@ class HtmlHelper extends AppHelper {
  * ### Usage:
  *
  * {{{
- * echo $html->style(array('margin' => '10px', 'padding' => '10px'), true);
+ * echo $this->Html->style(array('margin' => '10px', 'padding' => '10px'), true);
  *
  * // creates
  * 'margin:10px;padding:10px;'
@@ -745,11 +745,11 @@ class HtmlHelper extends AppHelper {
  *
  * Create a regular image:
  *
- * `echo $html->image('cake_icon.png', array('alt' => 'CakePHP'));`
+ * `echo $this->Html->image('cake_icon.png', array('alt' => 'CakePHP'));`
  *
  * Create an image link:
  *
- * `echo $html->image('cake_icon.png', array('alt' => 'CakePHP', 'url' => 'http://cakephp.org'));`
+ * `echo $this->Html->image('cake_icon.png', array('alt' => 'CakePHP', 'url' => 'http://cakephp.org'));`
  *
  * ### Options:
  *
