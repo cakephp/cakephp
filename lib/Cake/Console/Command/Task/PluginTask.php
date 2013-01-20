622d2f04f9b3cb10c72fb37aeba51c269d0616b2
Rachman Chavik <rchavik@xintesa.com>
2013-01-20 09:07:31 +0700

    removing extra newline in baked files

--- lib/Cake/Console/Command/Task/PluginTask.php
+++ lib/Cake/Console/Command/Task/PluginTask.php
@@ -147,7 +147,7 @@ class PluginTask extends AppShell {
 			$out = "<?php\n\n";
 			$out .= "App::uses('AppController', 'Controller');\n\n";
 			$out .= "class {$plugin}AppController extends AppController {\n\n";
-			$out .= "}\n\n";
+			$out .= "}\n";
 			$this->createFile($this->path . $plugin . DS . 'Controller' . DS . $controllerFileName, $out);
 
 			$modelFileName = $plugin . 'AppModel.php';
@@ -155,7 +155,7 @@ class PluginTask extends AppShell {
 			$out = "<?php\n\n";
 			$out .= "App::uses('AppModel', 'Model');\n\n";
 			$out .= "class {$plugin}AppModel extends AppModel {\n\n";
-			$out .= "}\n\n";
+			$out .= "}\n";
 			$this->createFile($this->path . $plugin . DS . 'Model' . DS . $modelFileName, $out);
 
 			$this->_modifyBootstrap($plugin);
