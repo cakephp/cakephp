51068c5dafa4788b11970f1745d6c92ffe7b63b6
Khaled Attia <github@khal3d.com>
2013-01-23 16:34:45 +0200

    Update lib/Cake/View/Helper/FormHelper.php

--- lib/Cake/View/Helper/FormHelper.php
+++ lib/Cake/View/Helper/FormHelper.php
@@ -936,7 +936,7 @@ class FormHelper extends AppHelper {
 	public function input($fieldName, $options = array()) {
 		$this->setEntity($fieldName);
 
-		$options = array_merge(
+		$options = Set::merge(
 			array('before' => null, 'between' => null, 'after' => null, 'format' => null),
 			$this->_inputDefaults,
 			$options
