72765a2afc586341250cb109dd9ea19292dce2b8
Marc Würth <ravage@bluewin.ch>
2013-02-03 20:00:32 +0100

    Brought \app\Config and \lib\Cake\Console\Templates\skel\Config in sync

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -32,7 +32,7 @@
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
- * Load all plugin routes.  See the CakePlugin documentation on
+ * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
 	CakePlugin::routes();
