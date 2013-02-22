4c531edfb9efa81e86d2e69fe3943e1e10be5e30
mark_story <mark@mark-story.com>
2013-02-21 22:50:27 -0500

    Start extracting set() into a trait.

---
+++ lib/Cake/Utility/SetContextTrait.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         CakePHP(tm) v 3.0.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+namespace Cake\Utility;
+
+/**
+ * Provides the set() method for collecting context.
+ *
+ * Once collected context data can be passed to another object.
+ * This is done in Controller, TemplateTask and View for example.
+ *
+ * @package Cake.Utility
+ */
+trait SetContextTrait {
+
+/**
+ * Saves a variable for use inside a template.
+ *
+ * @param string|array $one A string or an array of data.
+ * @param string|array $two Value in case $one is a string (which then works as the key).
+ *   Unused if $one is an associative array, otherwise serves as the values to $one's keys.
+ * @return void
+ * @link http://book.cakephp.org/2.0/en/controllers.html#interacting-with-views
+ */
+	public function set($one, $two = null) {
+		if (is_array($one)) {
+			if (is_array($two)) {
+				$data = array_combine($one, $two);
+			} else {
+				$data = $one;
+			}
+		} else {
+			$data = array($one => $two);
+		}
+		$this->viewVars = $data + $this->viewVars;
+	}
+
+}
