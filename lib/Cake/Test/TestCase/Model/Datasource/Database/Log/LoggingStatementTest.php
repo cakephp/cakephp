6cdfa75538843fadf0824048fb172ffa6a274d6c
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2013-03-31 11:19:35 +0200

    Adding test for LoggingStatement

---
+++ lib/Cake/Test/TestCase/Model/Datasource/Database/Log/LoggingStatementTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * PHP Version 5.4
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         CakePHP(tm) v 3.0.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+namespace Cake\Test\TestCase\Model\Datasource\Database\Log;
+
+use Cake\Model\Datasource\Database\Log\LoggingStatement;
+use PDOStatement;
+
+/**
+ * Tests LoggingStatement class
+ *
+ **/
+class LoggingStatementTest extends \Cake\TestSuite\TestCase {
+
+/**
+ * Tests that queries are logged when executed without params
+ *
+ * @return void
+ */
+	public function testExecuteNoParams() {
+		$inner = $this->getMock('PDOStatement');
+		$inner->expects($this->once())->method('rowCount')->will($this->returnValue(3));
+		$logger = $this->getMock('\Cake\Model\Datasource\Database\Log\QueryLogger');
+		$logger->expects($this->once())
+			->method('write')
+			->with($this->logicalAnd(
+				$this->isInstanceOf('\Cake\Model\Datasource\Database\Log\LoggedQuery'),
+				$this->attributeEqualTo('query', 'SELECT bar FROM foo'),
+				$this->attributeEqualTo('took', 5, 5),
+				$this->attributeEqualTo('numRows', 3),
+				$this->attributeEqualTo('params', [])
+			));
+		$st = new LoggingStatement($inner);
+		$st->queryString = 'SELECT bar FROM foo';
+		$st->logger($logger);
+		$st->execute();
+	}
+
+/**
+ * Tests that queries are logged when executed with params
+ *
+ * @return void
+ */
+	public function testExecuteWithParams() {
+		$inner = $this->getMock('PDOStatement');
+		$inner->expects($this->once())->method('rowCount')->will($this->returnValue(4));
+		$logger = $this->getMock('\Cake\Model\Datasource\Database\Log\QueryLogger');
+		$logger->expects($this->once())
+			->method('write')
+			->with($this->logicalAnd(
+				$this->isInstanceOf('\Cake\Model\Datasource\Database\Log\LoggedQuery'),
+				$this->attributeEqualTo('query', 'SELECT bar FROM foo'),
+				$this->attributeEqualTo('took', 5, 5),
+				$this->attributeEqualTo('numRows', 4),
+				$this->attributeEqualTo('params', ['a' => 1, 'b' => 2])
+			));
+		$st = new LoggingStatement($inner);
+		$st->queryString = 'SELECT bar FROM foo';
+		$st->logger($logger);
+		$st->execute(['a' => 1, 'b' => 2]);
+	}
+
+/**
+ * Tests that queries are logged when executed with bound params
+ *
+ * @return void
+ */
+	public function testExecuteWithBinding() {
+		$inner = $this->getMock('PDOStatement');
+		$inner->expects($this->any())->method('rowCount')->will($this->returnValue(4));
+		$logger = $this->getMock('\Cake\Model\Datasource\Database\Log\QueryLogger');
+		$logger->expects($this->at(0))
+			->method('write')
+			->with($this->logicalAnd(
+				$this->isInstanceOf('\Cake\Model\Datasource\Database\Log\LoggedQuery'),
+				$this->attributeEqualTo('query', 'SELECT bar FROM foo'),
+				$this->attributeEqualTo('took', 5, 5),
+				$this->attributeEqualTo('numRows', 4),
+				$this->attributeEqualTo('params', ['a' => 1, 'b' => '2013-01-01'])
+			));
+		$logger->expects($this->at(1))
+			->method('write')
+			->with($this->logicalAnd(
+				$this->isInstanceOf('\Cake\Model\Datasource\Database\Log\LoggedQuery'),
+				$this->attributeEqualTo('query', 'SELECT bar FROM foo'),
+				$this->attributeEqualTo('took', 5, 5),
+				$this->attributeEqualTo('numRows', 4),
+				$this->attributeEqualTo('params', ['a' => 1, 'b' => '2014-01-01'])
+			));
+		$inner->expects($this->at(0))->method('bindValue')->with('a', 1);
+		$inner->expects($this->at(1))->method('bindValue')->with('b', '2013-01-01');
+		$driver = $this->getMock('\Cake\Model\Datasource\Database\Driver');
+		$st = new LoggingStatement($inner, $driver);
+		$st->queryString = 'SELECT bar FROM foo';
+		$st->logger($logger);
+		$st->bindValue('a', 1);
+		$st->bindValue('b', new \DateTime('2013-01-01'), 'date');
+		$st->execute();
+		$st->bindValue('b', new \DateTime('2014-01-01'), 'date');
+		$st->execute();
+	}
+
+}
