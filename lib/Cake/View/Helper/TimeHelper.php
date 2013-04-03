8bc7bc9bb12cce8c6bccc258eec93e207bbfe584
Andreas Kristiansen <mail@ankr.dk>
2013-04-03 12:54:42 +0200

    Fixing comment for  in TimeHelper __construct

--- lib/Cake/View/Helper/TimeHelper.php
+++ lib/Cake/View/Helper/TimeHelper.php
@@ -49,7 +49,7 @@ class TimeHelper extends AppHelper {
  *            The class needs to be placed in the `Utility` directory.
  *
  * @param View $View the view object the helper is attached to.
- * @param array $settings Settings array Settings array
+ * @param array $settings Settings array
  * @throws CakeException When the engine class could not be found.
  */
 	public function __construct(View $View, $settings = array()) {
