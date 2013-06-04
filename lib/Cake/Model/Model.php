1f29f3a456933038374bda63fd7d987670d2a9c0
Pafu Inlu <paul@furiani.net>
2013-06-04 03:53:50 -0700

    Added a clear() function to Model.php
    Many times ``Model::create()`` is used with a single parameter of ``false``.  When this happens, what is actually happening is that the Model is being ***cleared*** rather than ***created***.
    
    ``Model::clear()`` is lightweight (since its a wrapper), provides a convenient way to clear a model, and also more intuitively describes a developer's true intentions within application code than ``Model::create(false)`` does.

--- lib/Cake/Model/Model.php
+++ lib/Cake/Model/Model.php
@@ -1497,6 +1497,14 @@ class Model extends Object implements CakeEventListener {
 		}
 		return $this->data;
 	}
+	
+/**
+ * This function is a convenient wrapper class to create(false) and, as the name suggests, clears the id, data, and validation errors.
+ * 
+ * @return array The current Model::data; after clearing via create(false)
+ * @see Model::create()
+ */
+	public function clear() { return $this->create(false); }
 
 /**
  * Returns a list of fields from the database, and sets the current model
