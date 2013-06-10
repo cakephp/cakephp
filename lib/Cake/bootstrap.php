9f9c103c6f8d54a37d3e5912f5cd1fcff739a850
euromark <euromark@web.de>
2013-06-11 00:06:27 +0200

    fix absolute url generation

--- lib/Cake/bootstrap.php
+++ lib/Cake/bootstrap.php
@@ -150,6 +150,10 @@ App::uses('Cache', 'Cache');
 App::uses('Object', 'Core');
 App::uses('Multibyte', 'I18n');
 
+App::$bootstrapping = true;
+
+Configure::bootstrap(isset($boot) ? $boot : true);
+
 /**
  * Full URL prefix
  */
@@ -167,10 +171,6 @@ if (!defined('FULL_BASE_URL')) {
 	}
 	unset($httpHost, $s);
 }
-
-App::$bootstrapping = true;
-
-Configure::bootstrap(isset($boot) ? $boot : true);
 
 if (function_exists('mb_internal_encoding')) {
 	$encoding = Configure::read('App.encoding');
