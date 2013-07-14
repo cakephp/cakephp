5a6f1259e34273eae8a28cb2aa4e8a5a58547e23
Adam Taylor <ataylor32@gmail.com>
2013-07-13 23:38:55 -0600

    Fix the "testAssetUrlNoRewrite" test
    When running the test in the browser from somewhere other than
    http://localhost (e.g. http://foo.dev), the test was failing.

--- lib/Cake/Test/Case/View/HelperTest.php
+++ lib/Cake/Test/Case/View/HelperTest.php
@@ -674,7 +674,8 @@ class HelperTest extends CakeTestCase {
 			'here' => '/cake_dev/index.php/tasks',
 		));
 		$result = $this->Helper->assetUrl('img/cake.icon.png', array('fullBase' => true));
-		$this->assertEquals('http://localhost/cake_dev/app/webroot/img/cake.icon.png', $result);
+		$expected = FULL_BASE_URL . '/cake_dev/app/webroot/img/cake.icon.png';
+		$this->assertEquals($expected, $result);
 	}
 
 /**
