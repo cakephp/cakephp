8209a298f7f1a827ced670f2374b180042b0cb6e
mark_story <mark@mark-story.com>
2013-08-02 13:28:58 -0400

    Make CakeRoute::persistParams() more tolerant
    Subclasses and instances may not always define persist options.
    CakeRoute should be accepting of these differences.
    
    Fixes #3957

--- lib/Cake/Routing/Route/CakeRoute.php
+++ lib/Cake/Routing/Route/CakeRoute.php
@@ -378,6 +378,9 @@ class CakeRoute {
  * @return array An array with persistent parameters applied.
  */
 	public function persistParams($url, $params) {
+		if (empty($this->options['persist']) || !is_array($this->options['persist'])) {
+			return $url;
+		}
 		foreach ($this->options['persist'] as $persistKey) {
 			if (array_key_exists($persistKey, $params) && !isset($url[$persistKey])) {
 				$url[$persistKey] = $params[$persistKey];
--- lib/Cake/Test/Case/Routing/Route/CakeRouteTest.php
+++ lib/Cake/Test/Case/Routing/Route/CakeRouteTest.php
@@ -485,6 +485,40 @@ class CakeRouteTest extends CakeTestCase {
 	}
 
 /**
+ * test persist with a non array value
+ *
+ * @return void
+ */
+	public function testPersistParamsNonArray() {
+		$url = array('controller' => 'posts', 'action' => 'index');
+		$params = array('lang' => 'en', 'color' => 'blue');
+
+		$route = new CakeRoute(
+			'/:lang/:color/blog/:action',
+			array('controller' => 'posts')
+			// No persist options
+		);
+		$result = $route->persistParams($url, $params);
+		$this->assertEquals($url, $result);
+
+		$route = new CakeRoute(
+			'/:lang/:color/blog/:action',
+			array('controller' => 'posts'),
+			array('persist' => false)
+		);
+		$result = $route->persistParams($url, $params);
+		$this->assertEquals($url, $result);
+
+		$route = new CakeRoute(
+			'/:lang/:color/blog/:action',
+			array('controller' => 'posts'),
+			array('persist' => 'derp')
+		);
+		$result = $route->persistParams($url, $params);
+		$this->assertEquals($url, $result);
+	}
+
+/**
  * test the parse method of CakeRoute.
  *
  * @return void
