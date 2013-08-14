b8b29585d0c2335005dd4cd291684d7abb109909
Mark van Driel <mvdriel@mvdriel-UL30>
2013-08-14 20:10:26 +0200

    Switched locale to nl_NL and fixed test

--- lib/Cake/Test/Case/Utility/CakeNumberTest.php
+++ lib/Cake/Test/Case/Utility/CakeNumberTest.php
@@ -603,9 +603,9 @@ class CakeNumberTest extends CakeTestCase {
  */
 	public function testPrecisionLocalized() {
 		$restore = setlocale(LC_NUMERIC, 0);
-		setlocale(LC_NUMERIC, 'de_DE');
+		setlocale(LC_NUMERIC, 'nl_NL');
 		$result = $this->Number->precision(1.234);
-		$this->assertRegExp('/1[,.]234/', $result);
+		$this->assertEquals('1,234', $result);
 		setlocale(LC_NUMERIC, $restore);
 	}
 
