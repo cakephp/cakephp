180ecb7929e3dde8286162418dd1834cb244135a
k-halaburda <k.halaburda@gmail.com>
2013-08-16 10:02:11 +0200

    Fixed some formatting

--- lib/Cake/Test/Case/Model/models.php
+++ lib/Cake/Test/Case/Model/models.php
@@ -1550,7 +1550,7 @@ class JoinThing extends CakeTestModel {
  */
 	public function afterFind($results, $primary = false) {
 		foreach ($results as $key => $result) {
-			if( !empty($result[$this->alias]) && is_array($result[$this->alias])) {
+			if (!empty($result[$this->alias]) && is_array($result[$this->alias])) {
 				$results[$key][$this->alias]['afterFind'] = 'Successfuly added by AfterFind';
 			}
 		}
