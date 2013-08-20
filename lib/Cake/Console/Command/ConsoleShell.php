6d80d6a3cbc89cddaf30fd051d4d74a4615cde83
mark_story <mark@mark-story.com>
2013-08-20 18:03:39 -0400

    Add missing return.

--- lib/Cake/Console/Command/ConsoleShell.php
+++ lib/Cake/Console/Command/ConsoleShell.php
@@ -439,7 +439,7 @@ class ConsoleShell extends AppShell {
  */
 	protected function _routesReload() {
 		if (!$this->_loadRoutes()) {
-			$this->err(__d('cake_console', "There was an error loading the routes config. Please check that the file exists and is free of parse errors."));
+			return $this->err(__d('cake_console', "There was an error loading the routes config. Please check that the file exists and is free of parse errors."));
 		}
 		$this->out(__d('cake_console', "Routes configuration reloaded, %d routes connected", count(Router::$routes)));
 	}
