67855896efb5d8e943dcf4e7dfa5991411a20c9a
mark_story <mark@mark-story.com>
2013-08-20 11:34:20 -0400

    Remove invalid code.
    Fixes #4002

--- lib/Cake/Console/Command/ConsoleShell.php
+++ lib/Cake/Console/Command/ConsoleShell.php
@@ -440,7 +440,6 @@ class ConsoleShell extends AppShell {
 	protected function _routesReload() {
 		if (!$this->_loadRoutes()) {
 			$this->err(__d('cake_console', "There was an error loading the routes config. Please check that the file exists and is free of parse errors."));
-			break;
 		}
 		$this->out(__d('cake_console', "Routes configuration reloaded, %d routes connected", count(Router::$routes)));
 	}
