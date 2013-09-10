867d4b312dcefe92dea5bcb4f392970c3daef280
Ber Clausen <crashcookie@gmail.com>
2013-09-09 21:20:22 -0300

    Update afterSave() callback signature to be PHP5.4+ compliant.

--- lib/Cake/Model/Behavior/AclBehavior.php
+++ lib/Cake/Model/Behavior/AclBehavior.php
@@ -97,9 +97,10 @@ class AclBehavior extends ModelBehavior {
  *
  * @param Model $model
  * @param boolean $created True if this is a new record
+ * @param array $options Options passed from Model::save().
  * @return void
  */
-	public function afterSave(Model $model, $created) {
+	public function afterSave(Model $model, $created, $options = array()) {
 		$types = $this->_typeMaps[$this->settings[$model->name]['type']];
 		if (!is_array($types)) {
 			$types = array($types);
