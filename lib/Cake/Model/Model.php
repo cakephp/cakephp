4ea6c158d1519677d4af6f2cf2d7f766d8efe2f8
mark_story <mark@mark-story.com>
2013-09-16 12:04:29 -0400

    Fix wording and remove trailing spaces.

--- lib/Cake/Model/Model.php
+++ lib/Cake/Model/Model.php
@@ -3143,12 +3143,11 @@ class Model extends Object implements CakeEventListener {
 
 /**
  * Returns an array of fields that have failed the validation of the current model.
- * 
+ *
  * Additionally it populates the validationErrors property of the model with the same array.
  *
  * @param string $options An optional array of custom options to be made available in the beforeValidate callback
- * @return array Array of invalid fields and its error messages
- * 
+ * @return array Array of invalid fields and their error messages
  * @see Model::validates()
  */
 	public function invalidFields($options = array()) {
