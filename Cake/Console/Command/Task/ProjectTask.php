0391c244a2da068fbcc80ccb592ff12c63b549e0
Renan Gonçalves <renan.saddam@gmail.com>
2013-09-18 11:22:37 +0200

    Fixing app repository/package name.

--- Cake/Console/Command/Task/ProjectTask.php
+++ Cake/Console/Command/Task/ProjectTask.php
@@ -55,7 +55,7 @@ class ProjectTask extends Shell {
 		}
 
 		while (!$project) {
-			$prompt = __d('cake_console', "What is the path to the project you want to bake?");
+			$prompt = __d('cake_console', 'What is the path to the project you want to bake?');
 			$project = $this->in($prompt, null, $suggestedPath);
 		}
 
@@ -91,7 +91,7 @@ class ProjectTask extends Shell {
 	}
 
 /**
- * Uses composer to generate a new package using the cakephp/cakephp-app project.
+ * Uses composer to generate a new package using the cakephp/app project.
  *
  * @param string $path Project path
  * @return mixed
@@ -102,9 +102,9 @@ class ProjectTask extends Shell {
 			$this->error(__d('cake_console', 'Cannot bake project. Could not find composer at "%s".', $composer));
 			return false;
 		}
-		$this->out('<info>Downloading a new cakephp-app from packagist.org</info>');
+		$this->out(__d('cake_console', '<info>Downloading a new cakephp app from packagist.org</info>'));
 
-		$command = 'php ' . escapeshellarg($composer) . ' create-project --dev cakephp/cakephp-app ' . escapeshellarg($path);
+		$command = 'php ' . escapeshellarg($composer) . ' create-project --dev cakephp/app ' . escapeshellarg($path);
 
 		$descriptorSpec = array(
 			0 => array('pipe', 'r'),
@@ -117,7 +117,7 @@ class ProjectTask extends Shell {
 			$pipes
 		);
 		if (!is_resource($process)) {
-			$this->err(__d('cake_console', 'Could not start subprocess.'));
+			$this->error(__d('cake_console', 'Could not start subprocess.'));
 			return false;
 		}
 		$output = $error = '';
@@ -131,7 +131,7 @@ class ProjectTask extends Shell {
 		proc_close($process);
 
 		if ($error) {
-			$this->error('Installation from packagist.org failed with: ' . $error);
+			$this->error(__d('cake_console', 'Installation from packagist.org failed with: %s', $error));
 			return false;
 		}
 		$this->out($output);
