c998888fe71248512b40c3d6213c019619a4a33e
Simon Males <sime@sime.net.au>
2013-09-20 18:20:47 +0800

    Do not assume CONTENT_TYPE is available.
    In some server environments notably the CLI server, _SERVER['CONTENT_TYPE'] is not available.
    In these cases, fall back to the HTTP_CONTENT_TYPE header.
    
    Refs #GH-1661

--- lib/Cake/Controller/Component/RequestHandlerComponent.php
+++ lib/Cake/Controller/Component/RequestHandlerComponent.php
@@ -518,6 +518,9 @@ class RequestHandlerComponent extends Component {
 		}
 
 		list($contentType) = explode(';', env('CONTENT_TYPE'));
+		if ($contentType === '') {
+			list($contentType) = explode(';', CakeRequest::header('CONTENT_TYPE'));
+		}
 		if (!$type) {
 			return $this->mapType($contentType);
 		}
