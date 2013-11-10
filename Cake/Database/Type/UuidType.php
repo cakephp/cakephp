4c86eb4c9e1c0be3b7cb038523edb5a6cc39672c
mark_story <mark@mark-story.com>
2013-11-10 16:40:46 -0500

    Add new UUID data type converter.
    Having a UUID data type converter allows the Type system to be extended
    and have new primary key provider methods added. This makes generating
    new ID's for UUID's extensible by userland code and should allow for
    sequence support to be added in the future.

---
+++ Cake/Database/Type/UuidType.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * PHP Version 5.4
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         CakePHP(tm) v 3.0.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+namespace Cake\Database\Type;
+
+use Cake\Database\Driver;
+use Cake\Utility\String;
+use Exception;
+use PDO;
+
+/**
+ * Provides behavior for the uuid type
+ */
+class UuidType extends \Cake\Database\Type {
+
+/**
+ * Casts give value to Statement equivalent
+ *
+ * @param mixed $value value to be converted to PHP equivalent
+ * @param Driver $driver object from which database preferences and configuration will be extracted
+ * @return mixed
+ */
+	public function toStatement($value, Driver $driver) {
+		if (is_null($value)) {
+			return PDO::PARAM_NULL;
+		}
+		return PDO::PARAM_STR;
+	}
+
+/**
+ * Casts given value from a PHP type to one acceptable by database
+ *
+ * @param mixed $value value to be converted to database equivalent
+ * @param Driver $driver object from which database preferences and configuration will be extracted
+ * @return mixed
+ */
+	public function toDatabase($value, Driver $driver) {
+		if ($value === null) {
+			return null;
+		}
+		return strval($value);
+	}
+
+/**
+ * Casts given value from a database type to PHP equivalent
+ *
+ * @param mixed $value value to be converted to PHP equivalent
+ * @param Driver $driver object from which database preferences and configuration will be extracted
+ * @return mixed
+ */
+	public function toPHP($value, Driver $driver) {
+		if ($value === null) {
+			return null;
+		}
+		return strval($value);
+	}
+
+/**
+ * Generate a new UUID
+ *
+ * @return string A new primary key value.
+ */
+	public function newId() {
+		return String::uuid();
+	}
+
+}
