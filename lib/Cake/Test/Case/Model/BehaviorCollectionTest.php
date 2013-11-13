70189979797d1ecc2547b911902a0f2bfe6fa1a7
Marc Würth <ravage@bluewin.ch>
2013-11-14 00:07:03 +0100

    Fixed some copyright and license statements in the file header DocBlocks

--- lib/Cake/Test/Case/Model/BehaviorCollectionTest.php
+++ lib/Cake/Test/Case/Model/BehaviorCollectionTest.php
@@ -13,7 +13,7 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
  * @package       Cake.Test.Case.Model
  * @since         1.2
