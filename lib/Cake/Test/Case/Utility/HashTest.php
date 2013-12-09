edcfe2d3a8905f5a2972a48a6d8427e85aab2632
mark_story <mark@mark-story.com>
2013-12-09 12:17:25 -0500

    Add tests for #2433

--- lib/Cake/Test/Case/Utility/HashTest.php
+++ lib/Cake/Test/Case/Utility/HashTest.php
@@ -829,6 +829,26 @@ class HashTest extends CakeTestCase {
 	}
 
 /**
+ * Test that attribute matchers don't cause errors on scalar data.
+ *
+ * @return void
+ */
+	public function testExtractAttributeEqualityOnScalarValue() {
+		$data = array(
+			'Entity' => array(
+				'id' => 1 ,
+				'data1' => 'value',
+			)
+		);
+		$result = Hash::extract($data, 'Entity[id=1].data1');
+		$this->assertEquals(array('value'), $result);
+
+		$data = array('Entity' => false );
+		$result = Hash::extract($data, 'Entity[id=1].data1');
+		$this->assertEquals(array(), $result);
+	}
+
+/**
  * Test comparison operators.
  *
  * @return void
