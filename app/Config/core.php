20faace7757b9cd75e491a8a62c2cddea78d8ac3
Bryan Crowe <bryan@bryan-crowe.com>
2013-12-16 21:59:29 -0500

    Clean up grammar in Configure timezone in bootstrap

--- app/Config/core.php
+++ app/Config/core.php
@@ -270,9 +270,9 @@
 	//date_default_timezone_set('UTC');
 
 /**
- * `Config.timezone` is available which you can set to user's timezone string.
+ * `Config.timezone` is available in which you can set users' timezone string.
  * If a method of CakeTime class is called with $timezone parameter as null and `Config.timezone` is set,
- * then the value of `Config.timezone` will be used. This feature allows you to set user's timezone just
+ * then the value of `Config.timezone` will be used. This feature allows you to set users' timezone just
  * once instead of passing it each time in function calls.
  */
 	//Configure::write('Config.timezone', 'Europe/Paris')
