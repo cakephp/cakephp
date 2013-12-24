ad180e909b5577fc8d78a747f4e759d78a1fda63
Derek Rosenzweig <derek.rosenzweig@gmail.com>
2013-12-24 12:21:10 -0500

    Made better test cases for pull #2543
    Better test cases for commit https://github.com/ozzyogkush/cakephp/commit/21c75062cb849f088d4c5b1f1f3c41ee74645621 - shows how to use quotes on model/field in condition key when casting to ensure proper quotes in returned condition string.

--- cake/tests/cases/libs/model/datasources/dbo_source.test.php
+++ cake/tests/cases/libs/model/datasources/dbo_source.test.php
@@ -2765,16 +2765,36 @@ class DboSourceTest extends CakeTestCase {
 		$expected = " WHERE '2009-03-04' BETWEEN Model.field1 AND Model.field2";
 		$this->assertEqual($result, $expected);
 		
+		$result = $this->testDb->conditions(array('Model.field::integer' => 5));
+		$expected = " WHERE `Model`.`field::integer` = '5'"; // known bug
+		$this->assertEqual($result, $expected);
+		
+		$result = $this->testDb->conditions(array('"Model"."field"::integer' => 5));
+		$expected = " WHERE \"Model\".\"field\"::integer = '5'";
+		$this->assertEqual($result, $expected);
+		
 		$result = $this->testDb->conditions(array('Model.field::integer' => array(5, 50, 500)));
 		$expected = " WHERE `Model`.`field`::integer IN ('5', '50', '500')";
+		$this->assertEqual($result, $expected);
+		
+		$result = $this->testDb->conditions(array('"Model"."field"::integer' => array(5, 50, 500)));
+		$expected = " WHERE \"Model\".\"field\"::integer IN ('5', '50', '500')";
 		$this->assertEqual($result, $expected);
 		
 		$result = $this->testDb->conditions(array('Model.field::integer' => array(5, 50)));
 		$expected = " WHERE `Model`.`field`::integer IN ('5', '50')";
 		$this->assertEqual($result, $expected);
 		
+		$result = $this->testDb->conditions(array('"Model"."field"::integer' => array(5, 50)));
+		$expected = " WHERE \"Model\".\"field\"::integer IN ('5', '50')";
+		$this->assertEqual($result, $expected);
+		
 		$result = $this->testDb->conditions(array('Model.field::integer BETWEEN ? AND ?' => array(5, 50)));
-		$expected = " WHERE `Model`.`field`::integer BETWEEN '5' AND '50'";
+		$expected = " WHERE `Model`.`field::integer` BETWEEN '5' AND '50'"; // known bug
+		$this->assertEqual($result, $expected);
+		
+		$result = $this->testDb->conditions(array('"Model"."field"::integer BETWEEN ? AND ?' => array(5, 50)));
+		$expected = " WHERE \"Model\".\"field\"::integer BETWEEN '5' AND '50'";
 		$this->assertEqual($result, $expected);
 	}
 
