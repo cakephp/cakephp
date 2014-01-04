3e48d2e082ae6207e0a64841defb870f5f1dba87
euromark <euromark@web.de>
2014-01-04 13:18:14 +0100

    Add return early statements for redirecting in controllers.

--- lib/Cake/Console/Command/UpgradeShell.php
+++ lib/Cake/Console/Command/UpgradeShell.php
@@ -501,6 +501,29 @@ class UpgradeShell extends AppShell {
 	}
 
 /**
+ * Update controllers.
+ *
+ * - Make redirect statements return early.
+ *
+ * @return void
+ */
+	public function controllers() {
+		$this->_paths = App::Path('Controller');
+		if (!empty($this->params['plugin'])) {
+			$this->_paths = App::Path('Controller', $this->params['plugin']);
+		}
+		$patterns = array(
+			array(
+				'$this->redirect() to return $this->redirect()',
+				'/\t\$this-\>redirect\(/',
+				"\t" . 'return $this->redirect('
+			),
+		);
+
+		$this->_filesRegexpUpdate($patterns);
+	}
+
+/**
  * Update components.
  *
  * - Make components that extend Object to extend Component.
@@ -839,6 +862,9 @@ class UpgradeShell extends AppShell {
 			'parser' => $subcommandParser
 		))->addSubcommand('constants', array(
 			'help' => __d('cake_console', "Replace Obsolete constants"),
+			'parser' => $subcommandParser
+		))->addSubcommand('controllers', array(
+			'help' => __d('cake_console', 'Update controllers.'),
 			'parser' => $subcommandParser
 		))->addSubcommand('components', array(
 			'help' => __d('cake_console', 'Update components to extend Component class.'),
