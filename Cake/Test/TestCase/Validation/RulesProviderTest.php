3e0a00598d0369a0b84570742f936408226c11dd
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2014-01-05 14:08:14 +0100

    Fixed a ouple file headers

--- Cake/Test/TestCase/Validation/RulesProviderTest.php
+++ Cake/Test/TestCase/Validation/RulesProviderTest.php
@@ -1,12 +1,10 @@
 <?php
 /**
- * PHP Version 5.4
- *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
  * Licensed under The MIT License
- * For full copyright and license infValidationation, please see the LICENSE.txt
+ * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
