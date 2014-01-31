40b7694891a597af6dd094c922b1c169a07fe470
Saleh Souzanchi <saleh.souzanchi@gmail.com>
2014-01-31 08:23:02 +0330

    adding options for react to response when asset not exists

--- lib/Cake/Routing/Filter/AssetDispatcher.php
+++ lib/Cake/Routing/Filter/AssetDispatcher.php
@@ -38,7 +38,8 @@ class AssetDispatcher extends DispatcherFilter {
  * Checks if a requested asset exists and sends it to the browser
  *
  * @param CakeEvent $event containing the request and response object
- * @return CakeResponse if the client is requesting a recognized asset, null otherwise
+ * @return mixed The resulting response.
+ * @throws NotFoundException When asset not found
  */
 	public function beforeDispatch(CakeEvent $event) {
 		$url = urldecode($event->data['request']->url);
@@ -52,12 +53,18 @@ class AssetDispatcher extends DispatcherFilter {
 		}
 
 		$assetFile = $this->_getAssetFile($url);
-		if ($assetFile === null || !file_exists($assetFile)) {
+		if ($assetFile === null) {
 			return null;
 		}
 
 		$response = $event->data['response'];
 		$event->stopPropagation();
+
+		if (!file_exists($assetFile)) {
+			$response->statusCode(404);
+			$response->send();
+			return $response;
+		}
 
 		$response->modified(filemtime($assetFile));
 		if ($response->checkNotModified($event->data['request'])) {
