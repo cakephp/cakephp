3fb3a903e64e39709f8f28d14b39822de7b28bdc
Anachron <Anachron14@gmx.de>
2014-02-05 22:11:30 +0100

    Remove time constants, since they are not useful

--- src/basics.php
+++ src/basics.php
@@ -22,17 +22,6 @@ use Cake\I18n\I18n;
 use Cake\Log\Log;
 use Cake\Utility\Debugger;
 
-/**
- * Basic defines for timing functions.
- */
-	define('SECOND', 1);
-	define('MINUTE', 60);
-	define('HOUR', 3600);
-	define('DAY', 86400);
-	define('WEEK', 604800);
-	define('MONTH', 2592000);
-	define('YEAR', 31536000);
-
 if (!function_exists('config')) {
 
 /**
