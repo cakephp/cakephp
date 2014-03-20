e912c9cc0f7749f1cceba60e5ef8ad0ab3550fd2
AD7six <andydawson76@gmail.com>
2014-03-20 10:22:31 +0000

    settings are not used when creating a task

--- src/Console/TaskRegistry.php
+++ src/Console/TaskRegistry.php
@@ -75,10 +75,9 @@ class TaskRegistry extends ObjectRegistry {
  *
  * @param string $class The classname to create.
  * @param string $alias The alias of the task.
- * @param array $settings An array of settings to use for the task.
  * @return Component The constructed task class.
  */
-	protected function _create($class, $alias, $settings) {
+	protected function _create($class, $alias) {
 		return new $class(
 			$this->_Shell->stdout,
 			$this->_Shell->stderr,
