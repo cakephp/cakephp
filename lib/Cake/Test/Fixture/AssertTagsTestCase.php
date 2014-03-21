c1b2b560bba2ad83752053d3160ce577b6f9ef89
mark_story <mark@mark-story.com>
2014-03-20 23:18:21 -0400

    Fix typo.

--- lib/Cake/Test/Fixture/AssertTagsTestCase.php
+++ lib/Cake/Test/Fixture/AssertTagsTestCase.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This class helpes in indirectly testing the functionalities of CakeTestCase::assertTags
+ * This class helps in indirectly testing the functionalities of CakeTestCase::assertTags
  *
  * @package       Cake.Test.Fixture
  */
@@ -115,4 +115,5 @@ class AssertTagsTestCase extends CakeTestCase {
 		);
 		$this->assertTags($input, $pattern);
 	}
+
 }
