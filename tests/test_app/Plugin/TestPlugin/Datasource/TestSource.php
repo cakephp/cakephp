10c175d2c344d5b206445ff0d78bd8b526c6b847
AD7six <andydawson76@gmail.com>
2014-03-29 22:13:11 +0000

    testfile mis-represents what's expected

--- tests/test_app/Plugin/TestPlugin/Datasource/TestSource.php
+++ tests/test_app/Plugin/TestPlugin/Datasource/TestSource.php
@@ -4,18 +4,4 @@ namespace TestPlugin\Datasource;
 
 class TestSource {
 
-/**
- * Settings
- *
- * @var array
- */
-	public $settings;
-
-/**
- * Constructor
- */
-	public function __construct(array $settings) {
-		$this->settings = $settings;
-	}
-
 }
