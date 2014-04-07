b86a1f4c485a1dc17ca1fe33d55b80db405f4544
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2014-04-07 20:41:22 +0200

    Started to implement a tree printer method

--- src/Collection/Iterator/TreeIterator.php
+++ src/Collection/Iterator/TreeIterator.php
@@ -15,6 +15,7 @@
 namespace Cake\Collection\Iterator;
 
 use Cake\Collection\CollectionTrait;
+use Cake\Collection\Iterator\TreePrinter;
 use RecursiveIteratorIterator;
 
 /**
@@ -25,5 +26,28 @@ use RecursiveIteratorIterator;
 class TreeIterator extends RecursiveIteratorIterator {
 
 	use CollectionTrait;
+
+	protected $_mode;
+
+	public function __construct($items, $mode = RecursiveIteratorIterator::LEAVES_ONLY, $flags = 0) {
+		parent::__construct($items, $mode, $flags);
+		$this->_mode = $mode;
+	}
+
+	public function printer($valuePath, $keyPath = null, $spacer = '__') {
+		if (!$keyPath) {
+			$counter = 0;
+			$keyPath = function() use ($counter) {
+				return $counter++;
+			};
+		}
+		return new TreePrinter(
+			$this->getInnerIterator(),
+			$valuePath,
+			$keyPath,
+			$spacer,
+			$this->_mode
+		);
+	}
 
 }
