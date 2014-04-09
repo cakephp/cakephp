9bef7b35fd783fe583270aeff9c57cb3ebb6365e
Ali Bakir <ali.m.bakir@gmail.com>
2014-04-09 12:38:48 +0100

    Cache: Implement writeMany, readMany, deleteMany
    Coding standards
    
    Coding standards
    
    Tests for Cache readMany writeMany deleteMany
    
    Fix abstraction problem
    
    Remove static calls
    
    adhere to 120 char line length for docblock
    
    Fix a typo
    
    Split long line
    
    Better tests
    
    Coding standards

--- tests/TestCase/Cache/CacheTest.php
+++ tests/TestCase/Cache/CacheTest.php
@@ -344,6 +344,58 @@ class CacheTest extends TestCase {
 	}
 
 /**
+ * testReadWriteMany method
+ *
+ * @return void
+ */
+	public function testReadWriteMany() {
+		$this->_configCache();
+		$data = array(
+			'App.falseTest' => false,
+			'App.trueTest' => true,
+			'App.nullTest' => null,
+			'App.zeroTest' => 0,
+			'App.zeroTest2' => '0'
+		);
+		Cache::writeMany($data, 'tests');
+
+		$read = Cache::readMany(array_keys($data), 'tests');
+
+		$this->assertSame($read['App.falseTest'], false);
+		$this->assertSame($read['App.trueTest'], true);
+		$this->assertSame($read['App.nullTest'], null);
+		$this->assertSame($read['App.zeroTest'], 0);
+		$this->assertSame($read['App.zeroTest2'], '0');
+	}
+
+/**
+ * testDeleteMany method
+ *
+ * @return void
+ */
+	public function testDeleteMany() {
+		$this->_configCache();
+		$data = array(
+			'App.falseTest' => false,
+			'App.trueTest' => true,
+			'App.nullTest' => null,
+			'App.zeroTest' => 0,
+			'App.zeroTest2' => '0'
+		);
+		Cache::writeMany(array_merge($data, array('App.keepTest' => 'keepMe')), 'tests');
+
+		Cache::deleteMany(array_keys($data), 'tests');
+		$read = Cache::readMany(array_merge(array_keys($data), array('App.keepTest')), 'tests');
+
+		$this->assertSame($read['App.falseTest'], false);
+		$this->assertSame($read['App.trueTest'], false);
+		$this->assertSame($read['App.nullTest'], false);
+		$this->assertSame($read['App.zeroTest'], false);
+		$this->assertSame($read['App.zeroTest2'], false);
+		$this->assertSame($read['App.keepTest'], 'keepMe');
+	}
+
+/**
  * Test that failed writes cause errors to be triggered.
  *
  * @return void
