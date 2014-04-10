1a004f912fbfb4931632b0063c3bdefcc09bba28
Renan Gonçalves <renan.saddam@gmail.com>
2014-04-11 00:30:12 +0200

    Removing Object::dispatchMethod, previously used by collections.

--- src/Core/Object.php
+++ src/Core/Object.php
@@ -46,35 +46,6 @@ class Object {
 	}
 
 /**
- * Calls a method on this object with the given parameters. Provides an OO wrapper
- * for `call_user_func_array`
- *
- * @param string $method Name of the method to call
- * @param array $params Parameter list to use when calling $method
- * @return mixed Returns the result of the method call
- */
-	public function dispatchMethod($method, array $params = []) {
-		switch (count($params)) {
-			case 0:
-				return $this->{$method}();
-			case 1:
-				return $this->{$method}($params[0]);
-			case 2:
-				return $this->{$method}($params[0], $params[1]);
-			case 3:
-				return $this->{$method}($params[0], $params[1], $params[2]);
-			case 4:
-				return $this->{$method}($params[0], $params[1], $params[2], $params[3]);
-			case 5:
-				return $this->{$method}($params[0], $params[1], $params[2], $params[3], $params[4]);
-			default:
-				// @codingStandardsIgnoreStart
-				return call_user_func_array([&$this, $method], $params);
-				// @codingStandardsIgnoreEnd
-		}
-	}
-
-/**
  * Stop execution of the current script. Wraps exit() making
  * testing easier.
  *
