9d19801cfa9687185991758b5c1c75d5d88c5a34
mark_story <mark@mark-story.com>
2014-04-24 09:19:00 -0400

    Clear data and validationErrors *after* calling clearCache().
    Having both properties cleaned after clearCache() means that you can use
    the model data in specialized clearCache() implementations.
    
    Fixes #3386

--- lib/Cake/Model/Model.php
+++ lib/Cake/Model/Model.php
@@ -1865,9 +1865,9 @@ class Model extends Object implements CakeEventListener {
 				$success = $this->data;
 			}
 
-			$this->data = false;
 			$this->_clearCache();
 			$this->validationErrors = array();
+			$this->data = false;
 		}
 
 		$this->whitelist = $_whitelist;
