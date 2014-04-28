8679c5cd180419e272833c4d1fa2f41844fc89de
euromark <euromark@web.de>
2014-04-28 17:33:56 +0200

    Fix test

--- lib/Cake/Test/Case/Controller/Component/AuthComponentTest.php
+++ lib/Cake/Test/Case/Controller/Component/AuthComponentTest.php
@@ -938,7 +938,7 @@ class AuthComponentTest extends CakeTestCase {
 			array($CakeRequest, $CakeResponse)
 		);
 
-		$expected = Router::url($this->Auth->loginRedirect, true);
+		$expected = Router::url($this->Auth->loginRedirect);
 		$Controller->expects($this->once())
 			->method('redirect')
 			->with($this->equalTo($expected));
