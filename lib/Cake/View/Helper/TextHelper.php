57cb7ea260399fbb2ef0a1e056ce4160b0d018ec
euromark <euromark@web.de>
2014-05-02 13:48:39 +0200

    Correct documentation.

--- lib/Cake/View/Helper/TextHelper.php
+++ lib/Cake/View/Helper/TextHelper.php
@@ -301,7 +301,7 @@ class TextHelper extends AppHelper {
  * - `ellipsis` Will be used as Beginning and prepended to the trimmed string
  * - `exact` If false, $text will not be cut mid-word
  *
- * @see String::truncate()
+ * @see String::tail()
  *
  * @param string $text String to truncate.
  * @param integer $length Length of returned string, including ellipsis.
