da99f2b74184e3b658b12ff7dabbdd15d57f81d9
mark_story <mark@mark-story.com>
2014-05-08 12:20:45 -0400

    Correct documentation.
    configTransport() does not accespt closures anymore. None of the
    registry based config() methods do either. This was left over by
    accident when closure factories were removed.
    
    Refs #3454

--- src/Network/Email/Email.php
+++ src/Network/Email/Email.php
@@ -1124,8 +1124,8 @@ class Email {
  *
  * @param string|array $key The configuration name to read/write. Or
  *   an array of multiple transports to set.
- * @param array|\Closure|AbstractTransport Either an array of configuration
- *   data, a Closure factory function, or a transport instance.
+ * @param array|AbstractTransport Either an array of configuration
+ *   data, or a transport instance.
  * @return mixed Either null when setting or an array of data when reading.
  * @throws \Cake\Error\Exception When modifying an existing configuration.
  */
