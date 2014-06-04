d7fa7d7031451a5a1c11a08b9af8f5ee7c441a1c
Bryan Crowe <bryan@bryan-crowe.com>
2014-06-03 23:04:25 -0400

    Add FlashComponent tests

---
+++ tests/TestCase/Controller/Component/FlashComponentTest.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * CakePHP(tm) Tests <http://book.cakephp.org/2.0/en/development/testing.html>
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://book.cakephp.org/2.0/en/development/testing.html CakePHP(tm) Tests
+ * @since         3.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Test\TestCase\Controller\Component;
+
+use Cake\Controller\ComponentRegistry;
+use Cake\Controller\Component\FlashComponent;
+use Cake\Controller\Controller;
+use Cake\Core\Configure;
+use Cake\Network\Request;
+use Cake\Event\Event;
+use Cake\Network\Session;
+use Cake\TestSuite\TestCase;
+
+/**
+ * FlashComponentTest class
+ *
+ */
+class FlashComponentTest extends TestCase {
+
+	protected static $_sessionBackup;
+
+/**
+ * fixtures
+ *
+ * @var string
+ */
+	public $fixtures = array('core.session');
+
+/**
+ * test case startup
+ *
+ * @return void
+ */
+	public static function setupBeforeClass() {
+		static::$_sessionBackup = Configure::read('Session');
+		Configure::write('Session', array(
+			'defaults' => 'php',
+			'timeout' => 100,
+			'cookie' => 'test'
+		));
+	}
+
+/**
+ * cleanup after test case.
+ *
+ * @return void
+ */
+	public static function teardownAfterClass() {
+		Configure::write('Session', static::$_sessionBackup);
+	}
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$_SESSION = [];
+		Configure::write('App.namespace', 'TestApp');
+		$controller = new Controller(new Request(['session' => new Session()]));
+		$this->ComponentRegistry = new ComponentRegistry($controller);
+		$this->Flash = new FlashComponent($this->ComponentRegistry);
+		$this->Session = new Session();
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+		$this->Session->destroy();
+	}
+
+/**
+ * testSet method
+ *
+ * @return void
+ * @covers \Cake\Controller\Component\FlashComponent::set
+ */
+	public function testSet() {
+		$this->assertNull($this->Session->read('Message.flash'));
+
+		$this->Flash->set('This is a test message');
+		$expected = ['message' => 'This is a test message', 'params' => ['element' => null], 'type' => 'info'];
+		$result = $this->Session->read('Message.flash');
+		$this->assertEquals($expected, $result);
+
+		$this->Flash->set('This is a test message', 'test', ['foo' => 'bar']);
+		$expected = ['message' => 'This is a test message','params' => ['foo' => 'bar', 'element' => 'test'], 'type' => 'info'];
+		$result = $this->Session->read('Message.flash');
+		$this->assertEquals($expected, $result);
+
+		$this->Flash->set('This is a test message', 'MyPlugin.alert');
+		$expected = ['message' => 'This is a test message', 'params' => ['element' => 'MyPlugin.alert'], 'type' => 'info'];
+		$result = $this->Session->read('Message.flash');
+		$this->assertEquals($expected, $result);
+
+		$this->Flash->set('This is a test message', null, [], 'foobar');
+		$expected = ['message' => 'This is a test message', 'params' => ['element' => null], 'type' => 'info'];
+		$result = $this->Session->read('Message.foobar');
+		$this->assertEquals($expected, $result);
+	}
+}
