2abaf3dd8ed0e5ba40358a021578f5832ea93b3b
Bryan Crowe <bryan@bryan-crowe.com>
2014-06-03 23:22:03 -0400

    Add FlashHelper tests

---
+++ tests/TestCase/View/Helper/FlashHelperTest.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * CakePHP(tm) Tests <http://book.cakephp.org/2.0/en/development/testing.html>
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://book.cakephp.org/2.0/en/development/testing.html CakePHP(tm) Tests
+ * @since         1.2.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Test\TestCase\View\Helper;
+
+use Cake\Controller\Controller;
+use Cake\Core\App;
+use Cake\Core\Plugin;
+use Cake\Network\Request;
+use Cake\Network\Session;
+use Cake\TestSuite\TestCase;
+use Cake\View\Helper\FlashHelper;
+use Cake\View\View;
+
+/**
+ * FlashHelperTest class
+ *
+ */
+class FlashHelperTest extends TestCase {
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->View = new View();
+		$session = new Session();
+		$this->View->request = new Request(['session' => $session]);
+		$this->Flash = new FlashHelper($this->View);
+
+		$session->write(array(
+			'Message' => array(
+				'flash' => array(
+					'type' => 'info',
+					'params' => array(),
+					'message' => 'This is a calling'
+				),
+				'notification' => array(
+					'type' => 'info',
+					'params' => array(
+						'title' => 'Notice!',
+						'name' => 'Alert!',
+						'element' => 'session_helper'
+					),
+					'message' => 'This is a test of the emergency broadcasting system',
+				),
+				'classy' => array(
+					'type' => 'success',
+					'params' => array('class' => 'positive'),
+					'message' => 'Recorded'
+				)
+			)
+		));
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		$_SESSION = [];
+		unset($this->View, $this->Session);
+		Plugin::unload();
+		parent::tearDown();
+	}
+
+/**
+ * testFlash method
+ *
+ * @return void
+ */
+	public function testFlash() {
+		$result = $this->Flash->out();
+		$expected = '<div id="flash-message" class="message-info">This is a calling</div>';
+		$this->assertEquals($expected, $result);
+
+		$expected = '<div id="classy-message" class="message-success">Recorded</div>';
+		$result = $this->Flash->out('classy');
+		$this->assertEquals($expected, $result);
+
+		$result = $this->Flash->out('notification');
+		$result = str_replace("\r\n", "\n", $result);
+		$expected = "<div id=\"notificationLayout\">\n\t<h1>Alert!</h1>\n\t<h3>Notice!</h3>\n\t<p>This is a test of the emergency broadcasting system</p>\n</div>";
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * test flash() with the attributes.
+ *
+ * @return void
+ */
+	public function testFlashAttributes() {
+		$result = $this->Flash->out('flash', array('class' => 'crazy'));
+		$expected = '<div id="flash-message" class="message-crazy">This is a calling</div>';
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * test setting the element from the attrs.
+ *
+ * @return void
+ */
+	public function testFlashElementInAttrs() {
+		$result = $this->Flash->out('flash', array(
+			'element' => 'session_helper',
+			'params' => array('title' => 'Notice!', 'name' => 'Alert!')
+		));
+		$expected = "<div id=\"notificationLayout\">\n\t<h1>Alert!</h1>\n\t<h3>Notice!</h3>\n\t<p>This is a calling</p>\n</div>";
+		$this->assertTextEquals($expected, $result);
+	}
+
+/**
+ * test using elements in plugins.
+ *
+ * @return void
+ */
+	public function testFlashWithPluginElement() {
+		Plugin::load('TestPlugin');
+
+		$result = $this->Flash->out('flash', array('element' => 'TestPlugin.plugin_element'));
+		$expected = 'this is the plugin element using params[plugin]';
+		$this->assertEquals($expected, $result);
+	}
+}
