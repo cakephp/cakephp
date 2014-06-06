07b43403fb7d89b62b3916ca602602f83b345356
mark_story <mark@mark-story.com>
2014-06-06 13:57:15 -0400

    Fix doc errors in Component/Acl.

--- lib/Cake/Controller/Component/Acl/PhpAcl.php
+++ lib/Cake/Controller/Component/Acl/PhpAcl.php
@@ -407,9 +407,9 @@ class PhpAro {
 /**
  * Constructor
  *
- * @param array $aro
- * @param array $map
- * @param array $aliases
+ * @param array $aro The aro data
+ * @param array $map The identifier mappings
+ * @param array $aliases The aliases to map.
  */
 	public function __construct(array $aro = array(), array $map = array(), array $aliases = array()) {
 		if (!empty($map)) {
