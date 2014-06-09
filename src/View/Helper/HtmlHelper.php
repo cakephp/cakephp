7fbae757f82a6d27135f4a942c94445ff8b1579c
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2014-06-09 20:39:25 +0200

    Reverting change that broke a few tests and was not actually needed

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -18,6 +18,7 @@ use Cake\Core\App;
 use Cake\Core\Configure;
 use Cake\Error;
 use Cake\Network\Response;
+use Cake\Utility\Inflector;
 use Cake\View\Helper;
 use Cake\View\Helper\StringTemplateTrait;
 use Cake\View\View;
