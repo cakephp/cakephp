0202f4c4b1670c50643ecbfa5a8df9d9be349ffc
Yves <havokinspiration@gmail.com>
2014-07-12 01:09:49 +0200

    Add tests for TableNameExpression

---
+++ tests/TestCase/Database/Expression/TableNameExpressionTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Test\TestCase\Database\Expression;
+
+use Cake\Database\Connection;
+use Cake\Database\Expression\TableNameExpression;
+use Cake\Database\ValueBinder;
+use Cake\Datasource\ConnectionManager;
+use Cake\TestSuite\TestCase;
+
+/**
+ * Tests TableNameExpression class
+ *
+ */
+class TableNameExpressionTest extends TestCase {
+
+/**
+ * Tests getting and setting the name
+ *
+ * @return void
+ */
+    public function testGetAndSetName() {
+        $expression = new TableNameExpression('foo', '');
+        $this->assertEquals('foo', $expression->getName());
+        $expression->setName('bar');
+        $this->assertEquals('bar', $expression->getName());
+    }
+
+/**
+ * Tests converting to sql
+ *
+ * @return void
+ */
+    public function testSQL() {
+        $expression = new TableNameExpression('foo', '');
+        $this->assertEquals('foo', $expression->sql(new ValueBinder));
+
+        $expression = new TableNameExpression('foo', 'prefix_');
+        $this->assertEquals('prefix_foo', $expression->sql(new ValueBinder));
+
+        $driver = $this->getMock('Cake\Database\Driver\Sqlite', ['enabled']);
+        $driver->expects($this->once())
+            ->method('enabled')
+            ->will($this->returnValue(true));
+        $connection = new Connection(['driver' => $driver]);
+
+        $name = "foo";
+        $expression = new TableNameExpression($name, 'prefix_');
+        $quoted = $connection->quoteIdentifier($expression->getName());
+        $expression->setName($quoted);
+        $expression->setQuoted();
+
+        $this->assertEquals('"prefix_foo"', $expression->sql(new ValueBinder));
+
+        $name = "bar";
+        $expression = new TableNameExpression($name, '');
+        $quoted = $connection->quoteIdentifier($expression->getName());
+        $expression->setName($quoted);
+
+        $this->assertEquals('"bar"', $expression->sql(new ValueBinder));
+    }
+
+}
