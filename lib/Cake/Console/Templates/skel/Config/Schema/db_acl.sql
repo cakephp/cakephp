08de917b3c8dabdf5cb88381ce655ddeb3d37005
mark_story <mark@mark-story.com>
2014-07-22 09:38:30 -0400

    Fix accidental typo in SQL file.
    Fixes #4061

--- lib/Cake/Console/Templates/skel/Config/Schema/db_acl.sql
+++ lib/Cake/Console/Templates/skel/Config/Schema/db_acl.sql
@@ -45,7 +45,6 @@ CREATE INDEX idx_acos_lft_rght ON `acos` (`lft`, `rght`);
 
 CREATE INDEX idx_acos_alias ON `acos` (`alias`);
 
-h
 CREATE INDEX idx_aros_lft_rght ON `aros` (`lft`, `rght`);
 
 CREATE INDEX idx_aros_alias ON `aros` (`alias`);
