cfbb08732411a180ab112ca651c47f8fccc5eb48
ADmad <admad.coder@gmail.com>
2014-08-01 12:00:26 +0530

    Update global config() function to use new path.

--- src/basics.php
+++ src/basics.php
@@ -45,8 +45,8 @@ if (!function_exists('config')) {
 		$count = count($args);
 		$included = 0;
 		foreach ($args as $arg) {
-			if (file_exists(APP . 'Config/' . $arg . '.php')) {
-				include_once APP . 'Config/' . $arg . '.php';
+			if (file_exists(CONFIG . $arg . '.php')) {
+				include_once CONFIG . $arg . '.php';
 				$included++;
 			}
 		}
