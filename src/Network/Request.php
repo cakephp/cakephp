451ab1d6e1b0f3c682f967344922fde6f61228ea
euromark <euromark@web.de>
2014-08-01 21:22:08 +0200

    Provide session defaults.

--- src/Network/Request.php
+++ src/Network/Request.php
@@ -156,6 +156,7 @@ class Request implements \ArrayAccess {
  */
 	public static function createFromGlobals() {
 		list($base, $webroot) = static::_base();
+		$sessionConfig = (array)Configure::read('Session') + array('defaults' => 'php');
 		$config = array(
 			'query' => $_GET,
 			'post' => $_POST,
@@ -164,7 +165,7 @@ class Request implements \ArrayAccess {
 			'environment' => $_SERVER + $_ENV,
 			'base' => $base,
 			'webroot' => $webroot,
-			'session' => Session::create(Configure::read('Session'))
+			'session' => Session::create($sessionConfig)
 		);
 		$config['url'] = static::_url($config);
 		return new static($config);
