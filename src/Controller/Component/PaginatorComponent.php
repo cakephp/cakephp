4d5d3e1fb253100270ac2278b8300a6e33f1f773
Patrick Conroy <patrick.conroy@rockstargames.com>
2014-08-25 14:36:21 -0400

    Add finder options to Paginator->paginate method
    The paginator right now doesn't send $options to any finder, custom or otherwise. This change allows setting 'finderOptions' directly from the controller's paginate array.

--- src/Controller/Component/PaginatorComponent.php
+++ src/Controller/Component/PaginatorComponent.php
@@ -164,7 +164,7 @@ class PaginatorComponent extends Component {
 		unset($options['finder'], $options['maxLimit']);
 
 		if (empty($query)) {
-			$query = $object->find($type);
+			$query = $object->find($type, isset($options['finderOptions']) ? $options['finderOptions'] : []);
 		}
 
 		$query->applyOptions($options);
