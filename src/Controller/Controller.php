a763ec9ec9d7a0ea65c03621c4375d0942cd83aa
euromark <euromark@web.de>
2014-08-25 17:44:13 +0200

    Adjust wording.

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -179,7 +179,7 @@ class Controller implements EventListener {
 	public $View;
 
 /**
- * These properties will be passed from the Controller to the View as options.
+ * These Controller properties will be passed from the Controller to the View as options.
  *
  * @var array
  * @see \Cake\View\View
