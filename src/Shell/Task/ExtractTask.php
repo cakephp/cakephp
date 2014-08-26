adca7b718aaff8b194398e698eeee186c5dffacc
luke83 <luctre@gmail.com>
2014-08-26 21:12:23 +0200

    Update ExtractTask.php
    Correct plugin Locale path: inside src folder (plugins/_NAME_/src/Locale, not plugins/_NAME_/Locale)

--- src/Shell/Task/ExtractTask.php
+++ src/Shell/Task/ExtractTask.php
@@ -187,7 +187,7 @@ class ExtractTask extends Shell {
 		if (isset($this->params['output'])) {
 			$this->_output = $this->params['output'];
 		} elseif (isset($this->params['plugin'])) {
-			$this->_output = $this->_paths[0] . DS . 'Locale';
+			$this->_output = rtrim($this->_paths[0], DS) . DS . 'src' . DS . 'Locale';
 		} else {
 			$message = "What is the path you would like to output?\n[Q]uit";
 			while (true) {
