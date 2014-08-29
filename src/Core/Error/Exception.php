ed9494e05fa6176ecabcecdb6ef751881769a4db
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2014-08-29 23:53:44 +0200

    Improving docblock

--- src/Core/Error/Exception.php
+++ src/Core/Error/Exception.php
@@ -50,6 +50,7 @@ class Exception extends \RuntimeException {
  * @param string|array $message Either the string of the error message, or an array of attributes
  *   that are made available in the view, and sprintf()'d into Exception::$_messageTemplate
  * @param int $code The code of the error, is also the HTTP status code for the error.
+ * @param \Exception $previous the previous exception.
  */
 	public function __construct($message, $code = 500, $previous = null) {
 		if (is_array($message)) {
