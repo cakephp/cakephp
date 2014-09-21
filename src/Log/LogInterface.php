86b0ab991d2e383a73da03369f46db3cfc7d0d78
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2014-09-21 12:14:28 +0200

    Deprecating LogIterface, it will be removed before the stable version

--- src/Log/LogInterface.php
+++ src/Log/LogInterface.php
@@ -21,18 +21,9 @@ use Psr\Log\LoggerInterface;
 /**
  * LogInterface is the interface that should be implemented
  * by all classes that are going to be used as Log streams.
+ *
+ * @deprecated
  */
 interface LogInterface extends LoggerInterface {
 
-/**
- * Write method to handle writes being made to the Logger
- *
- * @param string $level The severity level of the message being written.
- *    See Cake\Log\Log::$_levels for list of possible levels.
- * @param string $message Message content to log
- * @param array $scope The scope(s) a log message is being created in.
- *    See Cake\Log\Log::config() for more information on logging scopes.
- * @return void
- */
-	public function write($level, $message, $scope = []);
 }
