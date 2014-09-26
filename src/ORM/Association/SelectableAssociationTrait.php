100e1a4749b731be5802035e7e7aa1c655a297c2
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2014-09-26 20:59:03 +0200

    Removing magic left join elimation for subquery strategy.
    This was extremely error prone as some in the core team figured out,
    and was only solving a cosmetic issue.

--- src/ORM/Association/SelectableAssociationTrait.php
+++ src/ORM/Association/SelectableAssociationTrait.php
@@ -224,20 +224,12 @@ trait SelectableAssociationTrait {
 			$filterQuery->offset(null);
 		}
 
-		$joins = $filterQuery->join();
-		foreach ($joins as $i => $join) {
-			if (strtolower($join['type']) !== 'inner') {
-				unset($joins[$i]);
-			}
-		}
-
 		$keys = (array)$query->repository()->primaryKey();
 
 		if ($this->type() === $this::MANY_TO_ONE) {
 			$keys = (array)$this->foreignKey();
 		}
 
-		$filterQuery->join($joins, [], true);
 		$fields = $query->aliasFields($keys);
 		return $filterQuery->select($fields, true);
 	}
