be8c591dd868a6f923507e261605bfc2249be281
Hunter Perrin <hperrin@gmail.com>
2014-10-01 12:40:22 -0700

    Fixed coding standard violations.

--- lib/Cake/Utility/Hash.php
+++ lib/Cake/Utility/Hash.php
@@ -643,11 +643,11 @@ class Hash {
 			while (!empty($stack)) {
 				foreach ($stack as $curKey => &$curMerge) {
 					foreach ($curMerge[0] as $key => &$val) {
-						if (!empty($curMerge[1][$key]) && (array)$curMerge[1][$key]===$curMerge[1][$key] && (array)$val===$val) {
+						if (!empty($curMerge[1][$key]) && (array)$curMerge[1][$key] === $curMerge[1][$key] && (array)$val === $val) {
 							$stack[] = array(&$val, &$curMerge[1][$key]);
-						} elseif ((int)$key===$key && isset($curMerge[1][$key])) {
+						} elseif ((int)$key === $key && isset($curMerge[1][$key])) {
 							$curMerge[1][] = $val;
-						}  else {
+						} else {
 							$curMerge[1][$key] = $val;
 						}
 					}
@@ -685,11 +685,11 @@ class Hash {
 		while (!empty($stack)) {
 			foreach ($stack as $curKey => &$curMerge) {
 				foreach ($curMerge[0] as $key => &$val) {
-					if (!empty($curMerge[1][$key]) && (array)$curMerge[1][$key]===$curMerge[1][$key] && (array)$val===$val) {
+					if (!empty($curMerge[1][$key]) && (array)$curMerge[1][$key] === $curMerge[1][$key] && (array)$val === $val) {
 						$stack[] = array(&$val, &$curMerge[1][$key]);
-					} elseif ((int)$key===$key && isset($curMerge[1][$key])) {
+					} elseif ((int)$key === $key && isset($curMerge[1][$key])) {
 						$curMerge[1][] = $val;
-					}  else {
+					} else {
 						$curMerge[1][$key] = $val;
 					}
 				}
