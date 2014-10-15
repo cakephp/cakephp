1d97a7ff1cde8d9bdf50caef93555a6e65aa5bb1
Mark Story <mark@mark-story.com>
2014-10-14 23:12:46 -0400

    Replace simple cases with a simple regex.
    Hopefully this regex runs a wee bit faster than the last one and saves
    some time in the optimistic cases.

--- src/Database/SqlDialectTrait.php
+++ src/Database/SqlDialectTrait.php
@@ -35,6 +35,15 @@ trait SqlDialectTrait {
 			return '*';
 		}
 
+		if ($identifier === '') {
+			return '';
+		}
+
+		// string
+		if (preg_match('/^[\w-]+$/', $identifier)) {
+			return $this->_startQuote . $identifier . $this->_endQuote;
+		}
+
 		if (preg_match('/^[\w-]+(?:\.[^ \*]*)*$/', $identifier)) { // string.string
 			$items = explode('.', $identifier);
 			return $this->_startQuote . implode($this->_endQuote . '.' . $this->_startQuote, $items) . $this->_endQuote;
