bb67df4305c87850120ff122476d77591ed92b42
euromark <euromark@web.de>
2014-10-31 01:18:08 +0100

    Simplify test

--- lib/Cake/Test/Case/Model/Datasource/Database/MysqlTest.php
+++ lib/Cake/Test/Case/Model/Datasource/Database/MysqlTest.php
@@ -3442,14 +3442,17 @@ SQL;
 		);
 		$conditions = array('Article.id' => array(1, 2));
 		$contain = array('Comment.extra');
+
+		$test = ConnectionManager::getDatasource('test');
+		$test->getLog();
 		$result = $Article->find('all', compact('conditions', 'contain'));
 
 		$expected = 'SELECT `Comment`.`id`, `Comment`.`article_id`, `Comment`.`user_id`, `Comment`.`comment`, `Comment`.`published`, `Comment`.`created`,' .
 			' `Comment`.`updated`, (SELECT id FROM comments WHERE id = (SELECT 1)) AS  `Comment__extra`' .
 			' FROM `cake_test`.`comments` AS `Comment`   WHERE `Comment`.`article_id` IN (1, 2)';
-		$test = ConnectionManager::getDatasource('test');
+
 		$log = $test->getLog();
-		$this->assertTextEquals($expected, $log['log'][116]['query']);
+		$this->assertTextEquals($expected, $log['log'][count($log['log']) - 2]['query']);
 	}
 
 /**
