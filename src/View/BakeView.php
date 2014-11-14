e48ab63b44c25ba7ae181c4df806282a71082904
AD7six <andydawson76@gmail.com>
2014-11-14 10:07:52 +0000

    don't delete intermediary templates
    Users may find them useful and they don't hurt. Also the use of elements
    can mean that all tempaltes aren't deleted (because the reference to
    __viewFile changes when an element is rendered).

--- src/View/BakeView.php
+++ src/View/BakeView.php
@@ -142,9 +142,6 @@ class BakeView extends View {
 		ob_start();
 
 		include $this->__viewFile;
-		if (file_exists($this->__viewFile)) {
-			unlink($this->__viewFile);
-		}
 
 		$content = ob_get_clean();
 
