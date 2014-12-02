a5843dbe875a5b5b6b63a6e569a274327566d3ee
shin1x1 <shin1x1@gmail.com>
2014-12-02 19:56:30 +0900

    add binary protocol option in MemcachedEngine

--- lib/Cake/Cache/Engine/MemcachedEngine.php
+++ lib/Cake/Cache/Engine/MemcachedEngine.php
@@ -130,6 +130,7 @@ class MemcachedEngine extends CacheEngine {
 					__d('cake_dev', 'Memcached extension is not build with SASL support')
 				);
 			}
+			$this->_Memcached->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
 			$this->_Memcached->setSaslAuthData($this->settings['login'], $this->settings['password']);
 		}
 
