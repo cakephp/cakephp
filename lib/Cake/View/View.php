9be725613e818ed9cc1b8775dbbe569397904f70
Walther Lalk <walther@octoplus.co.za>
2014-12-11 16:44:11 +0200

    Backport helper method for view blocks from 3.0
    Related PR is #5385

--- lib/Cake/View/View.php
+++ lib/Cake/View/View.php
@@ -699,6 +699,16 @@ class View extends Object {
 	}
 
 /**
+ * Check if a block exists
+ *
+ * @param string $name Name of the block
+ * @return bool
+ */
+	public function exists($name) {
+		return $this->Blocks->exists($name);
+	}
+
+/**
  * End a capturing block. The compliment to View::start()
  *
  * @return void
