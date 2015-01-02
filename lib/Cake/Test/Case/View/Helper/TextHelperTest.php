2bf41f3d8525e2cc684c358b15a624e89578ceb1
Rob McVey <robmcvey@gmail.com>
2015-01-02 15:09:40 +0000

    Add missing double quote to link in test

--- lib/Cake/Test/Case/View/Helper/TextHelperTest.php
+++ lib/Cake/Test/Case/View/Helper/TextHelperTest.php
@@ -148,7 +148,7 @@ class TextHelperTest extends CakeTestCase {
 		$this->assertEquals($expected, $result);
 
 		$text = 'This is a test text with URL (http://www.cakephp.org/page/4) in brackets';
-		$expected = 'This is a test text with URL (<a href="http://www.cakephp.org/page/4>http://www.cakephp.org/page/4</a>) in brackets';
+		$expected = 'This is a test text with URL (<a href="http://www.cakephp.org/page/4">http://www.cakephp.org/page/4</a>) in brackets';
 		$result = $this->Text->autoLink($text);
 		$this->assertEquals($expected, $result);
 
