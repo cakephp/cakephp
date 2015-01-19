e9faa3701bb08a7486220561b44414ffa36a037a
David Yell <dyell@ukwebmedia.com>
2015-01-19 16:54:21 +0000

    Created a new test for four related tables using matching and contain

--- tests/Fixture/CommentsFixture.php
+++ tests/Fixture/CommentsFixture.php
@@ -32,6 +32,7 @@ class CommentsFixture extends TestFixture
         'id' => ['type' => 'integer'],
         'article_id' => ['type' => 'integer', 'null' => false],
         'user_id' => ['type' => 'integer', 'null' => false],
+        'category_author_id' => ['type' => 'integer', 'null' => false],
         'comment' => ['type' => 'text'],
         'published' => ['type' => 'string', 'length' => 1, 'default' => 'N'],
         'created' => ['type' => 'datetime'],
@@ -45,11 +46,11 @@ class CommentsFixture extends TestFixture
      * @var array
      */
     public $records = [
-        ['article_id' => 1, 'user_id' => 2, 'comment' => 'First Comment for First Article', 'published' => 'Y', 'created' => '2007-03-18 10:45:23', 'updated' => '2007-03-18 10:47:31'],
-        ['article_id' => 1, 'user_id' => 4, 'comment' => 'Second Comment for First Article', 'published' => 'Y', 'created' => '2007-03-18 10:47:23', 'updated' => '2007-03-18 10:49:31'],
-        ['article_id' => 1, 'user_id' => 1, 'comment' => 'Third Comment for First Article', 'published' => 'Y', 'created' => '2007-03-18 10:49:23', 'updated' => '2007-03-18 10:51:31'],
-        ['article_id' => 1, 'user_id' => 1, 'comment' => 'Fourth Comment for First Article', 'published' => 'N', 'created' => '2007-03-18 10:51:23', 'updated' => '2007-03-18 10:53:31'],
-        ['article_id' => 2, 'user_id' => 1, 'comment' => 'First Comment for Second Article', 'published' => 'Y', 'created' => '2007-03-18 10:53:23', 'updated' => '2007-03-18 10:55:31'],
-        ['article_id' => 2, 'user_id' => 2, 'comment' => 'Second Comment for Second Article', 'published' => 'Y', 'created' => '2007-03-18 10:55:23', 'updated' => '2007-03-18 10:57:31']
+        ['article_id' => 1, 'user_id' => 2, 'category_author_id' => 1, 'comment' => 'First Comment for First Article', 'published' => 'Y', 'created' => '2007-03-18 10:45:23', 'updated' => '2007-03-18 10:47:31'],
+        ['article_id' => 1, 'user_id' => 4, 'category_author_id' => 2, 'comment' => 'Second Comment for First Article', 'published' => 'Y', 'created' => '2007-03-18 10:47:23', 'updated' => '2007-03-18 10:49:31'],
+        ['article_id' => 1, 'user_id' => 1, 'category_author_id' => 3, 'comment' => 'Third Comment for First Article', 'published' => 'Y', 'created' => '2007-03-18 10:49:23', 'updated' => '2007-03-18 10:51:31'],
+        ['article_id' => 1, 'user_id' => 1, 'category_author_id' => 1, 'comment' => 'Fourth Comment for First Article', 'published' => 'N', 'created' => '2007-03-18 10:51:23', 'updated' => '2007-03-18 10:53:31'],
+        ['article_id' => 2, 'user_id' => 1, 'category_author_id' => 2, 'comment' => 'First Comment for Second Article', 'published' => 'Y', 'created' => '2007-03-18 10:53:23', 'updated' => '2007-03-18 10:55:31'],
+        ['article_id' => 2, 'user_id' => 2, 'category_author_id' => 3, 'comment' => 'Second Comment for Second Article', 'published' => 'Y', 'created' => '2007-03-18 10:55:23', 'updated' => '2007-03-18 10:57:31']
     ];
 }
