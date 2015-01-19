c755d9b5e5e220558c3d020578be02d9ace92e68
Robert <robert.pustulka@gmail.com>
2015-01-19 08:55:40 +0100

    Updated comments

--- lib/Cake/Test/test_app/Plugin/TestPlugin/Console/Command/TestPluginShell.php
+++ lib/Cake/Test/test_app/Plugin/TestPlugin/Console/Command/TestPluginShell.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Short description for file.
- *
  * CakePHP(tm) Tests <http://book.cakephp.org/2.0/en/development/testing.html>
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  *
@@ -12,7 +10,7 @@
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://book.cakephp.org/2.0/en/development/testing.html CakePHP(tm) Tests
  * @package       Cake.Test.TestApp.Plugin.TestPlugin.Console.Command
- * @since         CakePHP(tm) v 1.2.0.7871
+ * @since         CakePHP(tm) v 2.7.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
