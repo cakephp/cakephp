fd47d26f6bce090621d717f8a9e866b39d211837
Richan Fongdasen <richan.fongdasen@yahoo.com>
2015-01-21 22:50:46 +0700

    Keep the user agent list in alphabetical order

--- lib/Cake/Network/CakeRequest.php
+++ lib/Cake/Network/CakeRequest.php
@@ -106,10 +106,10 @@ class CakeRequest implements ArrayAccess {
 		'ajax' => array('env' => 'HTTP_X_REQUESTED_WITH', 'value' => 'XMLHttpRequest'),
 		'flash' => array('env' => 'HTTP_USER_AGENT', 'pattern' => '/^(Shockwave|Adobe) Flash/'),
 		'mobile' => array('env' => 'HTTP_USER_AGENT', 'options' => array(
-			'Android', 'AvantGo', 'BlackBerry', 'DoCoMo', 'Fennec', 'iPod', 'iPhone', 'iPad',
+			'Android', 'AvantGo', 'BB10', 'BlackBerry', 'DoCoMo', 'Fennec', 'iPod', 'iPhone', 'iPad',
 			'J2ME', 'MIDP', 'NetFront', 'Nokia', 'Opera Mini', 'Opera Mobi', 'PalmOS', 'PalmSource',
 			'portalmmm', 'Plucker', 'ReqwirelessWeb', 'SonyEricsson', 'Symbian', 'UP\\.Browser',
-			'webOS', 'Windows CE', 'Windows Phone OS', 'Xiino', 'BB10'
+			'webOS', 'Windows CE', 'Windows Phone OS', 'Xiino'
 		)),
 		'requested' => array('param' => 'requested', 'value' => 1)
 	);
