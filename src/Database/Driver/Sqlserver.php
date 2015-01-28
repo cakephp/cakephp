e236933544d52e7a16d5c77ecf9834ae38e4c6fa
ADmad <admad.coder@gmail.com>
2015-01-28 20:24:52 +0530

    Fix CS error.

--- src/Database/Driver/Sqlserver.php
+++ src/Database/Driver/Sqlserver.php
@@ -15,8 +15,8 @@
 namespace Cake\Database\Driver;
 
 use Cake\Database\Dialect\SqlserverDialectTrait;
-use Cake\Database\Statement\SqlserverStatement;
 use Cake\Database\Query;
+use Cake\Database\Statement\SqlserverStatement;
 use PDO;
 
 /**
