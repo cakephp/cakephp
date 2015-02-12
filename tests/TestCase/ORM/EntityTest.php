35a7038bd862430fb00c9de73f0342a5d74f119b
Mark Story <mark@mark-story.com>
2015-02-11 22:59:07 -0500

    Remove dead code in ORM.
    These classes were undocumented, and aren't part of the current ORM
    implementation.
    
    Refs #5755

--- tests/TestCase/ORM/EntityTest.php
+++ tests/TestCase/ORM/EntityTest.php
@@ -19,7 +19,6 @@ use Cake\TestSuite\TestCase;
 use Cake\Validation\Validator;
 use TestApp\Model\Entity\Extending;
 use TestApp\Model\Entity\NonExtending;
-use TestApp\Model\Entity\ValidatableEntity;
 
 /**
  * Entity test case.
@@ -774,58 +773,6 @@ class EntityTest extends TestCase
         $expected = ['email' => 'mark@example.com'];
         $this->assertEquals($expected, $entity->toArray());
         $this->assertEquals(['name'], $entity->hiddenProperties());
-    }
-
-    /**
-     * Tests that missing fields will not be passed as null to the validator
-     *
-     * @return void
-     */
-    public function testValidateMissingFields()
-    {
-        $entity = $this->getMockBuilder('TestApp\Model\Entity\ValidatableEntity')
-            ->setMethods(['getSomething'])
-            ->disableOriginalConstructor()
-            ->getMock();
-        $entity->accessible('*', true);
-
-        $validator = $this->getMock('\Cake\Validation\Validator');
-        $entity->set('a', 'b');
-
-        $validator->expects($this->once())
-            ->method('provider')
-            ->with('entity', $entity);
-        $validator->expects($this->once())->method('errors')
-            ->with(['a' => 'b'], true)
-            ->will($this->returnValue(['a' => ['not valid']]));
-        $this->assertNotEmpty($entity->validate($validator));
-        $this->assertEquals(['a' => ['not valid']], $entity->errors());
-    }
-
-    /**
-     * Tests validate when the validator returns no errors
-     *
-     * @return void
-     */
-    public function testValidateSuccess()
-    {
-        $validator = $this->getMock('\Cake\Validation\Validator');
-        $data = [
-            'a' => 'b',
-            'cool' => false,
-            'something' => true
-        ];
-        $entity = new ValidatableEntity($data);
-        $entity->isNew(true);
-
-        $validator->expects($this->once())
-            ->method('provider')
-            ->with('entity', $entity);
-        $validator->expects($this->once())->method('errors')
-            ->with($data, true)
-            ->will($this->returnValue([]));
-        $this->assertEmpty($entity->validate($validator));
-        $this->assertEquals([], $entity->errors());
     }
 
     /**
