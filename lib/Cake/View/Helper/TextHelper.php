b3c9c1615e36a200a308f7bd434eaad5ff4b89b8
mark_story <mark@mark-story.com>
2015-02-13 21:40:50 -0500

    Fix doubly linking URLs starting with //
    Fixes #5889

--- lib/Cake/View/Helper/TextHelper.php
+++ lib/Cake/View/Helper/TextHelper.php
@@ -114,7 +114,7 @@ class TextHelper extends AppHelper {
 			$text
 		);
 		$text = preg_replace_callback(
-			'#(?<!href="|">)(?<!\b[[:punct:]])(?<!http://|https://|ftp://|nntp://)www.[^\n\%\ <]+[^<\n\%\,\.\ <](?<!\))#i',
+			'#(?<!href="|">)(?<!\b[[:punct:]])(?<!http://|https://|ftp://|nntp://|//)www.[^\n\%\ <]+[^<\n\%\,\.\ <](?<!\))#i',
 			array(&$this, '_insertPlaceHolder'),
 			$text
 		);
