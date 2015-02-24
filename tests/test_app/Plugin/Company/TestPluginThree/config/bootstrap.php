7f23941e11773ac1728e59d8b8f4465d8f282979
Walther Lalk <walther@uafrica.com>
2015-02-24 14:16:54 +0200

    Correct code styling error

--- tests/test_app/Plugin/Company/TestPluginThree/config/bootstrap.php
+++ tests/test_app/Plugin/Company/TestPluginThree/config/bootstrap.php
@@ -3,3 +3,4 @@ use Cake\Core\Configure;
 
 Configure::write('PluginTest.test_plugin_three.bootstrap', 'loaded plugin three bootstrap');
 Configure::write('PluginTest.test_plugin_three.autoload', class_exists('Company\TestPluginThree\Utility\Hello'));
+
