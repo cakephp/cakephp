c0764568b008430eb6cd67ddbda59b89a7dc2749
Luan Hospodarsky <luan.handrade@gmail.com>
2015-03-16 23:38:21 -0300

    Fixing some more PHPCS issues in last commit

--- src/ORM/Marshaller.php
+++ src/ORM/Marshaller.php
@@ -108,7 +108,7 @@ class Marshaller
         $primaryKey = $schema->primaryKey();
 
         if (array_intersect($primaryKey, array_keys($data)) == $primaryKey) {
-            $tableName=$this->_table->table();
+            $tableName = $this->_table->table();
             $record = $this->_table->find('all');
             foreach ($primaryKey as $pkey) {
                 $record->where(["$tableName.$pkey" => $data[$pkey] ]);
