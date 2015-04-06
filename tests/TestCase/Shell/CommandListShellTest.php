c954e758ad085c86169363401128fc56537a771d
Mark Story <mark@mark-story.com>
2015-04-05 20:39:32 -0400

    Fix failing tests.

--- tests/TestCase/Shell/CommandListShellTest.php
+++ tests/TestCase/Shell/CommandListShellTest.php
@@ -97,7 +97,7 @@ class CommandListShellTest extends TestCase
         $expected = "/\[.*TestPluginTwo.*\] example, welcome/";
         $this->assertRegExp($expected, $output);
 
-        $expected = "/\[.*CORE.*\] i18n, orm_cache, plugin, server/";
+        $expected = "/\[.*CORE.*\] i18n, orm_cache, plugin, routes, server/";
         $this->assertRegExp($expected, $output);
 
         $expected = "/\[.*app.*\] sample/";
