d94003c2dab0643a5e0539de6f7eae7f72f9074d
ADmad <admad.coder@gmail.com>
2015-04-21 01:49:45 +0530

    Reduce code duplication

--- src/View/Widget/TextareaWidget.php
+++ src/View/Widget/TextareaWidget.php
@@ -15,7 +15,7 @@
 namespace Cake\View\Widget;
 
 use Cake\View\Form\ContextInterface;
-use Cake\View\Widget\WidgetInterface;
+use Cake\View\Widget\BasicWidget;
 
 /**
  * Input widget class for generating a textarea control.
@@ -23,19 +23,8 @@ use Cake\View\Widget\WidgetInterface;
  * This class is intended as an internal implementation detail
  * of Cake\View\Helper\FormHelper and is not intended for direct use.
  */
-class TextareaWidget implements WidgetInterface
+class TextareaWidget extends BasicWidget
 {
-
-    /**
-     * Constructor
-     *
-     * @param \Cake\View\StringTemplate $templates Templates list.
-     */
-    public function __construct($templates)
-    {
-        $this->_templates = $templates;
-    }
-
     /**
      * Render a text area form widget.
      *
@@ -67,13 +56,5 @@ class TextareaWidget implements WidgetInterface
                 ['name', 'val']
             )
         ]);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function secureFields(array $data)
-    {
-        return [$data['name']];
     }
 }
