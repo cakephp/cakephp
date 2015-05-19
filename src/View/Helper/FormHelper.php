333aff46f51ca8efd5a5af329051c2dee8b2b252
Lukas Marks <info@lumax-web.de>
2015-05-19 20:53:15 +0200

    escape fix

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -1641,7 +1641,7 @@ class FormHelper extends Helper
      */
     public function postLink($title, $url = null, array $options = [])
     {
-        $options += ['block' => null, 'confirm' => null];
+        $options += ['escape' => true, 'block' => null, 'confirm' => null];
 
         $requestMethod = 'POST';
         if (!empty($options['method'])) {
