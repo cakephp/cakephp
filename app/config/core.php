5c34402e2e3b0568ff0d0a724c197c6eac6f6cf1
phuclvv89 <levinhvinhphuc@gmail.com>
2015-05-22 21:33:29 +0700

    Install composer include phpcs,phpcpd,phpmd

--- app/config/core.php
+++ app/config/core.php
@@ -201,12 +201,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'Dskdjfkjshjfskfhs87394hkjfslkjfs');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '646309287492785972424556743');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
