31e95ec0a52e6966c98c574f8c67245c4fc854b4
Mark Story <mark@mark-story.com>
2015-05-24 09:13:53 -0400

    Add missing doc blcoks.

--- src/TestSuite/Stub/ConsoleOutput.php
+++ src/TestSuite/Stub/ConsoleOutput.php
@@ -18,16 +18,44 @@ use Cake\Console\ConsoleOutput as ConsoleOutputBase;
 
 /**
  * StubOutput makes testing shell commands/shell helpers easier.
+ *
+ * You can use this class by injecting it into a ConsoleIo instance
+ * that your command/task/helper uses:
+ *
+ * ```
+ * use Cake\Console\ConsoleIo;
+ * use Cake\TestSuite\Stub\ConsoleOutput;
+ *
+ * $output = new ConsoleOutput();
+ * $io = new ConsoleIo($output);
+ * ```
  */
 class ConsoleOutput extends ConsoleOutputBase
 {
+    /**
+     * Buffered messages.
+     *
+     * @var array
+     */
     protected $_out = [];
 
+    /**
+     * Write output to the buffer.
+     *
+     * @param string $message The message to write.
+     * @param int $newlines Unused.
+     * @return void
+     */
     public function write($message, $newlines = 1)
     {
         $this->_out[] = $message;
     }
 
+    /**
+     * Get the buffered output.
+     *
+     * @return array
+     */
     public function messages()
     {
         return $this->_out;
