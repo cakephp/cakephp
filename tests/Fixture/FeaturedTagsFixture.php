3d2ae62f8f6ffa3d9f4ee4cb0b565d703a0be338
Patrick Conroy <patrick@rockstargames.com>
2015-06-12 13:03:47 -0400

    Adding relevant test for deep hasMany w/subquery

---
+++ tests/Fixture/FeaturedTagsFixture.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * CakePHP(tm) Tests <http://book.cakephp.org/2.0/en/development/testing.html>
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://book.cakephp.org/2.0/en/development/testing.html CakePHP(tm) Tests
+ * @since         1.2.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * Class TagFixture
+ *
+ */
+class FeaturedTagsFixture extends TestFixture
+{
+
+    /**
+     * fields property
+     *
+     * @var array
+     */
+    public $fields = [
+        'tag_id' => ['type' => 'integer', 'null' => false],
+        'priority' => ['type' => 'integer', 'null' => false],
+        '_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['tag_id']]]
+    ];
+
+    /**
+     * records property
+     *
+     * @var array
+     */
+    public $records = [
+        ['tag_id' => 1, 'priority' => 1],
+        ['tag_id' => 2, 'priority' => 2],
+        ['tag_id' => 3, 'priority' => 3]
+    ];
+}
