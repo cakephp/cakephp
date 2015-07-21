742f85de80f2cf4a8c165b33d5742a268d343dae
Mark Story <mark@mark-story.com>
2015-07-20 22:31:18 -0400

    Port Hash::maxDimension() fixes to 3.0
    Port fixes from #7040 and #7041 to 3.0.

--- src/Utility/Hash.php
+++ src/Utility/Hash.php
@@ -803,7 +803,7 @@ class Hash
         if (is_array($data) && reset($data) !== false) {
             foreach ($data as $value) {
                 if (is_array($value)) {
-                    $depth[] = static::dimensions($value) + 1;
+                    $depth[] = static::maxDimensions($value) + 1;
                 } else {
                     $depth[] = 1;
                 }
