4750b5739be6e8e4c1a632267544d402044b5253
Daniel Martuszewski <daniel.martuszewski@gmail.com>
2015-08-10 23:52:13 +0200

    Used spaces instead of tabs, added test

--- src/ORM/Table.php
+++ src/ORM/Table.php
@@ -1786,15 +1786,15 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
                     count($fields)
                 ));
             }
-			foreach ($fields as $field) {
-				$value = array_shift($args);
+            foreach ($fields as $field) {
+                $value = array_shift($args);
 
-				if(is_array($value)) {
-					$field .= ' IN';
-				}
+                if (is_array($value)) {
+                    $field .= ' IN';
+                }
 
-				$conditions[$this->aliasField($field)] = $value;
-			}
+                $conditions[$this->aliasField($field)] = $value;
+            }
             return $conditions;
         };
 
