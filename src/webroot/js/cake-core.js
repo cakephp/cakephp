1df8459fc45ab74a035ac0bdf2d807bff8f9224d
Frederik Bauer <thefredfox@googlemail.com>
2015-10-07 18:16:52 +0200

    Update cake-core.js
    reformats to 4 spaces as indention
    removes a redundant else

--- src/webroot/js/cake-core.js
+++ src/webroot/js/cake-core.js
@@ -1,25 +1,23 @@
 //$(document).ready(function () {
-  var elements = document.getElementsByClassName('cake-core-postLink');
+var elements = document.getElementsByClassName('cake-core-postLink');
 
-  var postLinkFunction = function (event) {
+var postLinkFunction = function (event) {
     var formName = this.getAttribute("data-cake-core-form");
     var confirmMessage = this.getAttribute('data-cake-core-confirm');
     if (confirmMessage !== null && confirmMessage !== 'undefined') {
-      if (confirm(confirmMessage)) {
-        document[formName].submit();
-        return false;
-      }
-      else {
+        if (confirm(confirmMessage)) {
+            document[formName].submit();
+            return false;
+        }
         return true;
-      }
     }
     document[formName].submit();
     event.returnValue = false;
     return false;
-  };
+};
 
-  for(var i = 0; i < elements.length; i++) {
+for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
     element.onclick = postLinkFunction;
-  }
+}
 //});
