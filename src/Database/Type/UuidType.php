4373f30be375c5a1c349d377275d21f002c266e1
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2015-10-16 22:23:37 +0200

    Adding code back in UuidType

--- src/Database/Type/UuidType.php
+++ src/Database/Type/UuidType.php
@@ -24,6 +24,23 @@ use PDO;
  */
 class UuidType extends StringType
 {
+
+    /**
+     * Casts given value from a PHP type to one acceptable by database
+     *
+     * @param mixed $value value to be converted to database equivalent
+     * @param Driver $driver object from which database preferences and configuration will be extracted
+     * @return mixed
+     */
+    public function toDatabase($value, Driver $driver)
+    {
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        return parent::toDatabase($value, $driver);
+    }
+
     /**
      * Generate a new UUID
      *
@@ -34,6 +51,12 @@ class UuidType extends StringType
         return Text::uuid();
     }
 
+    /**
+     * Marshalls request data into a PHP string
+     *
+     * @param mixed $value The value to convert.
+     * @return string|null Converted value.
+     */
     public function marshal($value)
     {
         if ($value === null || $value === '') {
