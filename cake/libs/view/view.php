f2ef634b173397ff0e59b267de7f7767d117c241
mark_story <mark@mark-story.com>
2015-10-31 14:04:02 -0400

    Remove ability to use arbitrary view templates paths.
    View templates should only be loadable from within the configured
    template paths. Allowing any file path causes issues when users can
    define the template to be loaded.

--- cake/libs/view/view.php
+++ cake/libs/view/view.php
@@ -857,9 +857,6 @@ class View extends Object {
 			$name = $this->viewPath . DS . $subDir . Inflector::underscore($name);
 		} elseif (strpos($name, DS) !== false) {
 			if ($name{0} === DS || $name{1} === ':') {
-				if (is_file($name)) {
-					return $name;
-				}
 				$name = trim($name, DS);
 			} else if ($name[0] === '.') {
 				$name = substr($name, 3);
