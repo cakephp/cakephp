c7e89d43f00c2114b6ee7f4997f2571fde4a2097
Mark Story <mark@mark-story.com>
2015-11-28 16:08:46 -0500

    Fix docblock issues.

--- src/I18n/RelativeTimeFormatter.php
+++ src/I18n/RelativeTimeFormatter.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.2.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 namespace Cake\I18n;
 
 use Cake\I18n\FrozenDate;
@@ -304,6 +317,7 @@ class RelativeTimeFormatter
      *
      * @param array $options The options provided by the user.
      * @param string $class The class name to use for defaults.
+     * @return array Options with defaults applied.
      */
     protected function _options($options, $class)
     {
