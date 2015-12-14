8c514a5eed4bc24c9ee3f31d04b8deeba0151a3c
Travis Gross <gross.t@choiceenergyservices.com>
2015-12-14 13:59:13 -0600

    Update With Formats and Wildcards

--- src/Database/Schema/SqlserverSchema.php
+++ src/Database/Schema/SqlserverSchema.php
@@ -45,18 +45,17 @@ class SqlserverSchema extends BaseSchema
     /**
      * {@inheritDoc}
      */
-<<<<<<< HEAD
-    public function listTablesSql($config) {
-      
-=======
     public function listTablesSql($config)
     {
->>>>>>> 3x-sqlserverschema
         $sql = "SELECT TABLE_NAME
                 FROM INFORMATION_SCHEMA.TABLES
-                WHERE TABLE_SCHEMA = ?
-                    AND (TABLE_TYPE = 'BASE TABLE' OR TABLE_TYPE = 'VIEW') 
-                    ORDER BY TABLE_NAME, TABLE_TYPE";
+                WHERE TABLE_SCHEMA = 'dbo'
+                    AND TABLE_TYPE = 'BASE TABLE'
+                UNION
+                SELECT TABLE_NAME
+                FROM INFORMATION_SCHEMA.VIEWS
+                WHERE TABLE_SCHEMA = 'dbo'
+                ORDER BY TABLE_NAME";
         $schema = empty($config['schema']) ? static::DEFAULT_SCHEMA_NAME : $config['schema'];
         return [$sql, [$schema]];
     }
@@ -64,14 +63,9 @@ class SqlserverSchema extends BaseSchema
     /**
      * {@inheritDoc}
      */
-<<<<<<< HEAD
-    public function describeColumnSql($tableName, $config) {
-          $sql = "SELECT DISTINCT
-=======
     public function describeColumnSql($tableName, $config)
     {
         $sql = "SELECT DISTINCT
->>>>>>> 3x-sqlserverschema
             AC.column_id AS [column_id],
             AC.name AS [name],
             TY.name AS [type],
@@ -85,7 +79,23 @@ class SqlserverSchema extends BaseSchema
             INNER JOIN sys.[schemas] S ON S.[schema_id] = T.[schema_id]
             INNER JOIN sys.[all_columns] AC ON T.[object_id] = AC.[object_id]
             INNER JOIN sys.[types] TY ON TY.[user_type_id] = AC.[user_type_id]
-            WHERE T.[name] = ? AND S.[name] = ?";
+            WHERE T.[name] = ? AND S.[name] = 'dbo'
+            UNION
+            SELECT DISTINCT
+            AC.column_id AS [column_id],
+            AC.name AS [name],
+            TY.name AS [type],
+            AC.max_length AS [char_length],
+            AC.precision AS [precision],
+            AC.scale AS [scale],
+            AC.is_identity AS [autoincrement],
+            AC.is_nullable AS [null],
+            OBJECT_DEFINITION(AC.default_object_id) AS [default]
+            FROM sys.[objects] T
+            INNER JOIN sys.[schemas] S ON S.[schema_id] = T.[schema_id]
+            INNER JOIN sys.[all_columns] AC ON T.[object_id] = AC.[object_id]
+            INNER JOIN sys.[types] TY ON TY.[user_type_id] = AC.[user_type_id]
+            WHERE T.[name] = ? AND S.[name] = 'dbo'";
 
         $schema = empty($config['schema']) ? static::DEFAULT_SCHEMA_NAME : $config['schema'];
         return [$sql, [$tableName, $schema]];
