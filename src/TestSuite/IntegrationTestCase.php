6d969d6b7a54fa088e04871d692ee62e99d906ff
Wataru Terada <w.terada@gmail.com>
2015-12-19 20:58:57 +0900

    Fix the version number in the comment.

--- src/TestSuite/IntegrationTestCase.php
+++ src/TestSuite/IntegrationTestCase.php
@@ -8,7 +8,7 @@
  * Redistributions of files must retain the above copyright notice
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @since         3.0.0
+ * @since         3.2.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Cake\TestSuite;
