1c593eea6327a9f542880d7909898cc7b9416215
Larry E. Masters <phpnut@cakephp.org>
2015-12-22 16:20:24 -0500

    Adding return types to Session test classes.

--- lib/Cake/Test/test_app/Plugin/TestPlugin/Model/Datasource/Session/TestPluginSession.php
+++ lib/Cake/Test/test_app/Plugin/TestPlugin/Model/Datasource/Session/TestPluginSession.php
@@ -12,18 +12,23 @@ class TestPluginSession implements CakeSessionHandlerInterface {
 	}
 
 	public function close() {
+		return true;
 	}
 
 	public function read($id) {
+		return '';
 	}
 
 	public function write($id, $data) {
+		return true;
 	}
 
 	public function destroy($id) {
+		return true;
 	}
 
 	public function gc($expires = null) {
+		return true;
 	}
 
 }
