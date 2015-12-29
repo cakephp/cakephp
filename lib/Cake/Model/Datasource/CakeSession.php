6ee621011bbae2a6615edb40801098a5d8c43931
Larry E. Masters <phpnut@cakephp.org>
2015-12-28 23:50:35 -0500

    Using function_exists instead of using constant

--- lib/Cake/Model/Datasource/CakeSession.php
+++ lib/Cake/Model/Datasource/CakeSession.php
@@ -217,7 +217,7 @@ class CakeSession {
  * @return bool True if session has been started.
  */
 	public static function started() {
-		if (PHP_VERSION >= 5.4) {
+		if (function_exists('session_status')) {
 			return isset($_SESSION) && (session_status() === PHP_SESSION_ACTIVE);
 		}
 		return isset($_SESSION) && session_id();
