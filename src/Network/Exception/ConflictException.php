2d55212d391ec6ce14ad4298b8a3ea6a99f7efaa
ionas <jh@ht-studios.de>
2015-12-31 13:05:23 +0000

    updated copyright file docblock to correct @since information

--- src/Network/Exception/ConflictException.php
+++ src/Network/Exception/ConflictException.php
@@ -7,7 +7,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @since         3.0.0
+ * @since         3.1.7
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
