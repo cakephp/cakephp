daca52e37c62697959a90ff844131dcfaaed9c07
Mark S <dereuromark@users.noreply.github.com>
2016-01-06 18:05:01 +0100

    Use is_array() check

--- lib/Cake/View/Helper/FormHelper.php
+++ lib/Cake/View/Helper/FormHelper.php
@@ -382,7 +382,7 @@ class FormHelper extends AppHelper {
 		if (isset($options['action'])) {
 			trigger_error('Using key `action` is deprecated, use `url` directly instead.', E_USER_DEPRECATED);
 		}
-		if (isset($options['url']) && isset($options['url']['action'])) {
+		if (is_array($options['url']) && isset($options['url']['action'])) {
 			$options['action'] = $options['url']['action'];
 		}
 
