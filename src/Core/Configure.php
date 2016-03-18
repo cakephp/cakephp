9984317aacd4cb32f9f4e809729b4af2b3828217
Richard Kersey <CodeBlastr@users.noreply.github.com>
2016-03-17 23:23:11 -0400

    renamed append to updateDump

--- src/Core/Configure.php
+++ src/Core/Configure.php
@@ -340,38 +340,39 @@ class Configure
         }
         return (bool)$engine->dump($key, $values);
     }
-    
 
     /**
-     * Write new or overwrite existing values into the specified configuration file.
+     * Write new and overwrite existing values into an existing configuration file.
      *
      * ### Usage
      *
      * ```
-     * Configure::append('my_config', 'default', ['Key.key' => 'value'])
+     * Configure::updateDump('my_config', 'default', ['Key.key' => 'value'])
      * ```
      *
      * The third parameter ($append) follows the same syntax as Configure::write()
      * but as an array of values to be created or updated.
      *
      *
-     * @param $key Identifier of the config adapter to be appended.
+     * @param string $key Identifier of the config file to be updated.
      * @param string $config  The name of the configured adapter to dump data with.
-     * @param array $append Values to add or delete (added by default, deleted if $append is false)
+     * @param array $append Values to add or overwrite
      */
-    public static function append($key, $config = 'default', $append = [])
+    public static function updateDump($key, $config = 'default', array $append = [])
     {
+        $engine = static::_getEngine($config);
+        if (!$engine) {
+            throw new Exception(sprintf('There is no "%s" config engine.', $config));
+        }
+
         $current = static::$_values;
         static::clear();
-
         static::load($key, $config);
         foreach ($append as $str => $value) {
             static::write($str, $value);
         }
-
         $newConfig = static::read();
         static::dump($key, $config, array_keys($newConfig));
-
         return (bool)static::write($current);
     }
 
