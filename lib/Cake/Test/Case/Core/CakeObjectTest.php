5006cc396a41475a1c65e4ddb8f263df568c6d62
chinpei215 <chinpei215@hotmail.com>
2016-04-11 00:43:35 +0900

    Add a test case to make sure of backward compatibility

--- lib/Cake/Test/Case/Core/CakeObjectTest.php
+++ lib/Cake/Test/Case/Core/CakeObjectTest.php
@@ -17,6 +17,7 @@
  */
 
 App::uses('CakeObject', 'Core');
+App::uses('Object', 'Core');
 App::uses('Router', 'Routing');
 App::uses('Controller', 'Controller');
 App::uses('Model', 'Model');
@@ -676,5 +677,16 @@ class ObjectTest extends CakeTestCase {
 			array('data' => $data)
 		);
 		$this->assertEquals($data, $result);
+	}
+
+/**
+ * Test backward compatibility
+ *
+ * @return voind
+ */
+	public function testBackwardCompatibility() {
+		$this->skipIf(version_compare(PHP_VERSION, '7.0.0', '>='));
+
+		$this->assertInstanceOf('Object', new ObjectTestModel);
 	}
 }
