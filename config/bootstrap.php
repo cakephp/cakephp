6cda83e02e973e569f2a7424ec0a0dda493753da
Mark Story <mark@mark-story.com>
2016-04-28 22:03:09 -0400

    Revert "Don't autoload aliased classes."
    This reverts commit 65c36e533813ee75a8567c5a7fdb1da748c3fb91.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -19,28 +19,28 @@ define('TIME_START', microtime(true));
 
 // @deprecated Backward compatibility with 2.x series
 if (PHP_VERSION_ID < 70000) {
-    class_alias('Cake\Utility\Text', 'Cake\Utility\String', false);
+    class_alias('Cake\Utility\Text', 'Cake\Utility\String');
 }
 
 // @deprecated Backward compatibility with 2.x, 3.0.x
-class_alias('Cake\Mailer\AbstractTransport', 'Cake\Network\Email\AbstractTransport', false);
-class_alias('Cake\Mailer\Transport\DebugTransport', 'Cake\Network\Email\DebugTransport', false);
-class_alias('Cake\Mailer\Email', 'Cake\Network\Email\Email', false);
-class_alias('Cake\Mailer\Transport\MailTransport', 'Cake\Network\Email\MailTransport', false);
-class_alias('Cake\Mailer\Transport\SmtpTransport', 'Cake\Network\Email\SmtpTransport', false);
+class_alias('Cake\Mailer\AbstractTransport', 'Cake\Network\Email\AbstractTransport');
+class_alias('Cake\Mailer\Transport\DebugTransport', 'Cake\Network\Email\DebugTransport');
+class_alias('Cake\Mailer\Email', 'Cake\Network\Email\Email');
+class_alias('Cake\Mailer\Transport\MailTransport', 'Cake\Network\Email\MailTransport');
+class_alias('Cake\Mailer\Transport\SmtpTransport', 'Cake\Network\Email\SmtpTransport');
 
 // @deprecated Backwards compatibility with earler 3.x versions.
-class_alias('Cake\Http\Client', 'Cake\Network\Http\Client', false);
-class_alias('Cake\Http\Client\CookieCollection', 'Cake\Network\Http\CookieCollection', false);
-class_alias('Cake\Http\Client\FormData', 'Cake\Network\Http\FormData', false);
-class_alias('Cake\Http\Client\Message', 'Cake\Network\Http\Message', false);
-class_alias('Cake\Http\Client\Request', 'Cake\Network\Http\Request', false);
-class_alias('Cake\Http\Client\Response', 'Cake\Network\Http\Response', false);
-class_alias('Cake\Http\Client\Adapter\Stream', 'Cake\Network\Http\Adapter\Stream', false);
-class_alias('Cake\Http\Client\Auth\Basic', 'Cake\Network\Http\Auth\Basic', false);
-class_alias('Cake\Http\Client\Auth\Digest', 'Cake\Network\Http\Auth\Digest', false);
-class_alias('Cake\Http\Client\Auth\Oauth', 'Cake\Network\Http\Auth\Oauth', false);
-class_alias('Cake\Http\Client\FormDataPart', 'Cake\Network\Http\FormData\Part', false);
+class_alias('Cake\Http\Client', 'Cake\Network\Http\Client');
+class_alias('Cake\Http\Client\CookieCollection', 'Cake\Network\Http\CookieCollection');
+class_alias('Cake\Http\Client\FormData', 'Cake\Network\Http\FormData');
+class_alias('Cake\Http\Client\Message', 'Cake\Network\Http\Message');
+class_alias('Cake\Http\Client\Request', 'Cake\Network\Http\Request');
+class_alias('Cake\Http\Client\Response', 'Cake\Network\Http\Response');
+class_alias('Cake\Http\Client\Adapter\Stream', 'Cake\Network\Http\Adapter\Stream');
+class_alias('Cake\Http\Client\Auth\Basic', 'Cake\Network\Http\Auth\Basic');
+class_alias('Cake\Http\Client\Auth\Digest', 'Cake\Network\Http\Auth\Digest');
+class_alias('Cake\Http\Client\Auth\Oauth', 'Cake\Network\Http\Auth\Oauth');
+class_alias('Cake\Http\Client\FormDataPart', 'Cake\Network\Http\FormData\Part');
 
 require CAKE . 'basics.php';
 
