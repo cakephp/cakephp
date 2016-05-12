86e64baabb6724dc01bd1098505ec518be79979c
mscherer <mark.scherer@spryker.com>
2016-05-12 12:59:25 +0200

    Fix err() to highlight error as red similar to warn() which is yellow.

--- src/Console/Shell.php
+++ src/Console/Shell.php
@@ -640,11 +640,11 @@ class Shell
      *
      * @param string|array|null $message A string or an array of strings to output
      * @param int $newlines Number of newlines to append
-     * @return void
+     * @return int|bool Returns the number of bytes returned from writing to stderr.
      */
     public function err($message = null, $newlines = 1)
     {
-        $this->_io->err($message, $newlines);
+        return $this->_io->err('<error>' . $message . '</error>', $newlines);
     }
 
     /**
