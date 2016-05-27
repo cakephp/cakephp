602e0f923c94ff44cf0916c4a49ee8495c0ce3d9
chris48s <c.mshaw@yahoo.co.uk>
2016-05-27 22:29:20 +0100

    fix typo in description

--- src/Shell/CacheShell.php
+++ src/Shell/CacheShell.php
@@ -48,7 +48,7 @@ class CacheShell extends Shell
                 'description' => [
                     'Clear the cache for a particular prefix.',
                     'For example, `cake cache clear _cake_model_` will clear the model cache',
-                    'Use `cake cache clear list_prefixes` to list available prefixes'
+                    'Use `cake cache list_prefixes` to list available prefixes'
                 ],
                 'arguments' => [
                     'prefix' => [
