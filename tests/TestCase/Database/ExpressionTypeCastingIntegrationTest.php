05fa655d29d4ae5b531e180a734ccd459db4da74
Mark Story <mark@mark-story.com>
2016-05-27 11:49:07 +0200

    Use Query methods instead of digging into internals.

--- tests/TestCase/Database/ExpressionTypeCastingIntegrationTest.php
+++ tests/TestCase/Database/ExpressionTypeCastingIntegrationTest.php
@@ -86,16 +86,12 @@ class ExpressionTypeCastingIntegrationTest extends TestCase
     protected function _insert()
     {
         $query = $this->connection->newQuery();
-        $values = $query
+        $query
             ->insert(['id', 'published', 'name'], ['id' => 'ordered_uuid'])
             ->into('ordered_uuid_items')
-            ->clause('values');
-        $values
-            ->values([
-                ['id' => '481fc6d0-b920-43e0-a40d-6d1740cf8569', 'published' => 0, 'name' => 'Item 1'],
-                ['id' => '48298a29-81c0-4c26-a7fb-413140cf8569', 'published' => 0, 'name' => 'Item 2'],
-                ['id' => '482b7756-8da0-419a-b21f-27da40cf8569', 'published' => 0, 'name' => 'Item 3'],
-            ]);
+            ->values(['id' => '481fc6d0-b920-43e0-a40d-6d1740cf8569', 'published' => 0, 'name' => 'Item 1'])
+            ->values(['id' => '48298a29-81c0-4c26-a7fb-413140cf8569', 'published' => 0, 'name' => 'Item 2'])
+            ->values(['id' => '482b7756-8da0-419a-b21f-27da40cf8569', 'published' => 0, 'name' => 'Item 3']);
 
         $query->execute();
     }
