2cb3803a9f3129c39b7c4ab72d8108926300bed5
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2016-05-29 14:50:23 +0200

    Making test more resilient for SQL Server

--- tests/TestCase/ORM/QueryRegressionTest.php
+++ tests/TestCase/ORM/QueryRegressionTest.php
@@ -572,7 +572,7 @@ class QueryRegressionTest extends TestCase
         $findViaSubquery = $featuredTags
             ->find()
             ->where(['FeaturedTags.tag_id' => 2])
-            ->contain('Tags.TagsTranslations');
+            ->contain(['Tags.TagsTranslations' => ['sort' => 'TagsTranslations.id']]);
 
         $expected = [2 => 'tag 2 translated into en_us'];
 
