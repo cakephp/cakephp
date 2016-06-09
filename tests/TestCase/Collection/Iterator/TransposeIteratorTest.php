c49518028f72a7f6e1b33c8b62cb7c79f516524d
xu ding <thedilab@gmail.com>
2016-06-09 18:29:30 +0800

    Use standard CakePHP file header

--- tests/TestCase/Collection/Iterator/TransposeIteratorTest.php
+++ tests/TestCase/Collection/Iterator/TransposeIteratorTest.php
@@ -1,9 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: xu
- * Date: 8/6/16
- * Time: 7:31 PM
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.3.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 namespace Cake\Test\TestCase\Collection\Iterator;
