74e396809512538780acd4cc819c6af8386a183c
Florian Krämer <florian.kraemer@cakedc.com>
2016-06-13 12:10:52 +0200

    Improving the error message for associations with no PK.
    This makes it more clear what needs to be done to fix the issue.

--- src/ORM/Association.php
+++ src/ORM/Association.php
@@ -841,6 +841,11 @@ abstract class Association
         $bindingKey = (array)$this->bindingKey();
 
         if (count($foreignKey) !== count($bindingKey)) {
+            if (empty($bindingKey)) {
+                $msg = 'The "%s" table does not define a primary key. Please set one.';
+                throw new RuntimeException(sprintf($msg, $this->target()->table()));
+            }
+
             $msg = 'Cannot match provided foreignKey for "%s", got "(%s)" but expected foreign key for "(%s)"';
             throw new RuntimeException(sprintf(
                 $msg,
