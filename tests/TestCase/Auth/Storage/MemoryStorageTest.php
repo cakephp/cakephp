9a4632e073116a95537efd18bdd6547372d59952
Thomas Lefebvre <thomas.lefebvre@watma.be>
2016-06-27 23:43:52 -0400

    Added version to MemoryStorageTest

--- tests/TestCase/Auth/Storage/MemoryStorageTest.php
+++ tests/TestCase/Auth/Storage/MemoryStorageTest.php
@@ -9,7 +9,7 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         ???
+ * @since         3.2.12
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Cake\Test\TestCase\Auth\Storage;
