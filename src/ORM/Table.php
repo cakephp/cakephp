104f79608a1abd5e001359a3eed072e267cb926a
Mark Story <mark@mark-story.com>
2016-07-14 22:16:48 -0400

    Fix validateUnique emitting errors when non-scalar data is received.
    While normally the IsUnique() rule can expect sane data as it will have
    already been validated, that is not true when the rule is used inside
    a validator function. This prevents non-scalar data from getting to the
    rule by failing early.
    
    Refs #9088

--- src/ORM/Table.php
+++ src/ORM/Table.php
@@ -2267,8 +2267,13 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
             [$context['field']],
             isset($options['scope']) ? (array)$options['scope'] : []
         );
+        $values = $entity->extract($fields);
+        foreach ($values as $field) {
+            if ($field !== null && !is_scalar($field)) {
+                return false;
+            }
+        }
         $rule = new IsUnique($fields);
-
         return $rule($entity, ['repository' => $this]);
     }
 
