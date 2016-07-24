1ade959c37f4ba66f102e50ae395c03b1b673ecb
Albert Cansado Solà <albert@iMac-de-Albert.Home>
2016-07-24 11:53:32 +0200

    update MarshallerTest
    - separate method (_hasTranslations)
    - delete all “pass by reference”
    - simplify tests and remove all mocks

--- src/ORM/Marshaller.php
+++ src/ORM/Marshaller.php
@@ -148,6 +148,9 @@ class Marshaller
         }
 
         $hasTranslations = $this->_hasTranslations($options);
+        if ($hasTranslations) {
+            $options = $this->_addTranslationsToFieldList($options);
+        }
 
         $errors = $this->_validate($data, $options, true);
         $properties = [];
@@ -169,7 +172,10 @@ class Marshaller
                 $converter = Type::build($columnType);
                 $value = $converter->marshal($value);
             } elseif ($key === '_translations' && $hasTranslations) {
-                $value = $this->_mergeTranslations(null, $value, $errors, $options);
+                list($value, $translationsErrors) = $this->_mergeTranslations(null, $value, $options);
+                if (!empty($translationsErrors)) {
+                    $errors += $translationsErrors;
+                }
             }
             $properties[$key] = $value;
         }
@@ -454,18 +460,18 @@ class Marshaller
     }
 
     /**
-     * Call translation merge. Validations errors during merge will be added to `$errors` param
+     * Call translation merge
      *
      * @param \Cake\Datasource\EntityInterface $original The original entity
      * @param array $data key value list of languages with fields to be merged into the translate entity
-     * @param array $errors array with entity errors
      * @param array $options list of options
-     * @return array|null
+     * @return array
      */
-    protected function _mergeTranslations($original, array $data, array &$errors, array $options = [])
+    protected function _mergeTranslations($original, array $data, array $options = [])
     {
         $result = $this->_table->mergeTranslations($original, $data, $this, $options);
 
+        $errors = [];
         if (is_array($result)) {
             if ((bool)$result[1]) {
                 $errors['_translations'] = $result[1];
@@ -473,13 +479,11 @@ class Marshaller
             $result = $result[0];
         }
 
-        return $result;
+        return [$result, $errors];
     }
 
     /**
      * Return if table contains translate behavior or we specificate to use via `translations` options.
-     *
-     * In case that $options has `fieldList` option and `_translations` field is not present inside it, it will include
      *
      * ### Options:
      *
@@ -488,14 +492,24 @@ class Marshaller
      * @param array $options List of options
      * @return bool
      */
-    protected function _hasTranslations(array &$options = [])
+    protected function _hasTranslations(array $options = [])
     {
-        $hasTranslations = ($this->_table->behaviors()->hasMethod('mergeTranslations') && (bool)$options['translations']);
-        if ($hasTranslations && !empty($options['fieldList']) && !in_array('_translations', $options['fieldList'])) {
+        return ($this->_table->behaviors()->hasMethod('mergeTranslations') && (bool)$options['translations']);
+    }
+
+    /**
+     * Add `_translations` field to `fieldList` $options if it's not present inside
+     *
+     * @param array $options List of options
+     * @return array
+     */
+    protected function _addTranslationsToFieldList(array $options = [])
+    {
+        if (!empty($options['fieldList']) && !in_array('_translations', $options['fieldList'])) {
             array_push($options['fieldList'], '_translations');
         }
 
-        return $hasTranslations;
+        return $options;
     }
 
     /**
@@ -553,6 +567,9 @@ class Marshaller
         }
 
         $hasTranslations = $this->_hasTranslations($options);
+        if ($hasTranslations) {
+            $options = $this->_addTranslationsToFieldList($options);
+        }
 
         $errors = $this->_validate($data + $keys, $options, $isNew);
         $schema = $this->_table->schema();
@@ -582,7 +599,10 @@ class Marshaller
                     continue;
                 }
             } elseif ($key === '_translations' && $hasTranslations) {
-                $value = $this->_mergeTranslations($original, $value, $errors, $options);
+                list($value, $translationsErrors) = $this->_mergeTranslations($original, $value, $options);
+                if (!empty($translationsErrors)) {
+                    $errors += $translationsErrors;
+                }
             }
 
             $properties[$key] = $value;
