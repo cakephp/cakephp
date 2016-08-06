11c14849c0f89a0fa68741fa1a4a60d3ea524652
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2016-08-06 19:57:04 +0200

    Aadding missing docblock

--- src/ORM/Association.php
+++ src/ORM/Association.php
@@ -641,6 +641,8 @@ abstract class Association
      *   should be found
      * @param bool $joined Whether or not the row is a result of a direct join
      *   with this association
+     * @param string $targetProperty The property name in the source results where the association
+     * data shuld be nested in. Will use the default one if not provided.
      * @return array
      */
     public function transformRow($row, $nestKey, $joined, $targetProperty = null)
