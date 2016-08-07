acc32f5c58fc0ad6daa44ad6774e7664a9cc0fd4
Sebastien Barre <sebastien.barre@gmail.com>
2016-08-07 19:50:23 -0400

    Work around Apache handling the Authorization: header differently

--- lib/Cake/Network/CakeRequest.php
+++ lib/Cake/Network/CakeRequest.php
@@ -397,7 +397,7 @@ class CakeRequest implements ArrayAccess {
 
 /**
  * Get the content type used in this request.
- * 
+ *
  * @return string
  */
 	public function contentType() {
@@ -748,7 +748,12 @@ class CakeRequest implements ArrayAccess {
  * @return mixed Either false on no header being set or the value of the header.
  */
 	public static function header($name) {
-		$name = 'HTTP_' . strtoupper(str_replace('-', '_', $name));
+		$http_name = 'HTTP_' . strtoupper(str_replace('-', '_', $name));
+		if (isset($_SERVER[$http_name])) {
+			return $_SERVER[$http_name];
+		}
+		// Work around Apache issue handling the "Authorization" header
+		// differently than other headers.
 		if (isset($_SERVER[$name])) {
 			return $_SERVER[$name];
 		}
