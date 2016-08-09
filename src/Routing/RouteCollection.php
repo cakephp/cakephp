94571c536ff744e08e9ad7b5de4d51c59e89b2b1
saeid <saeideng@yahoo.com>
2016-08-10 03:12:48 +0430

    add tests and DuplicateRouteException

--- src/Routing/RouteCollection.php
+++ src/Routing/RouteCollection.php
@@ -15,6 +15,7 @@
 namespace Cake\Routing;
 
 use Cake\Routing\Exception\MissingRouteException;
+use Cake\Routing\Exception\DuplicateRouteException;
 use Cake\Routing\Route\Route;
 
 /**
@@ -78,7 +79,7 @@ class RouteCollection
         // Explicit names
         if (isset($options['_name'])) {
             if (isset($this->_named[$options['_name']])) {
-                throw new MissingRouteException([
+                throw new DuplicateRouteException([
                     'url' => $options['_name'],
                     'message' => 'A named route was found for "%s" that is already used, route names must be unique across your entire application.'
                 ]);
--- tests/TestCase/Routing/RouteCollectionTest.php
+++ tests/TestCase/Routing/RouteCollectionTest.php
@@ -356,6 +356,22 @@ class RouteCollectionTest extends TestCase
     }
 
     /**
+     * Test the add() with some _name.
+	 * 
+     * @expectedException \Cake\Routing\Exception\DuplicateRouteException
+	 * 
+     * @return void
+     */
+    public function testAddingDuplicatedRoutesName()
+    {
+        $one = new Route('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+        $two = new Route('/', ['controller' => 'Dashboards', 'action' => 'display']);
+        $this->collection->add($one,['_name' => 'test']);
+        $this->collection->add($two,['_name' => 'test']);
+
+    }
+
+    /**
      * Test basic get/set of extensions.
      *
      * @return void
