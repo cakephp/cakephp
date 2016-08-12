bc73e928b6f0caca97155ce767a229a461080801
mark_story <mark@mark-story.com>
2016-08-11 21:53:46 -0400

    Restore header() behavior inadvertantely removed.
    In eaa2bbbcaebaa29dc4fb55817b261ccfa9135335 I changed the behavior to
    now use the TitleCased name as I didn't understand the intended behavior
    or how apache was working in the reporter's specific case.
    
    Refs #9229

--- lib/Cake/Test/Case/Network/CakeRequestTest.php
+++ lib/Cake/Test/Case/Network/CakeRequestTest.php
@@ -1147,13 +1147,14 @@ class CakeRequestTest extends CakeTestCase {
 		$_SERVER['HTTP_X_THING'] = '';
 		$_SERVER['HTTP_HOST'] = 'localhost';
 		$_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-ca) AppleWebKit/534.8+ (KHTML, like Gecko) Version/5.0 Safari/533.16';
-		$_SERVER['AUTHORIZATION'] = 'foobar';
+		$_SERVER['Authorization'] = 'foobar';
 		$request = new CakeRequest('/', false);
 
 		$this->assertEquals($_SERVER['HTTP_HOST'], $request->header('host'));
 		$this->assertEquals($_SERVER['HTTP_USER_AGENT'], $request->header('User-Agent'));
 		$this->assertSame('', $request->header('X-thing'));
-		$this->assertEquals($_SERVER['AUTHORIZATION'], $request->header('Authorization'));
+		$this->assertEquals($_SERVER['Authorization'], $request->header('Authorization'));
+		$this->assertFalse($request->header('authorization'));
 	}
 
 /**
