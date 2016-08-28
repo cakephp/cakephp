41851d60b466b7740647f73f317494b23ff7344c
Nicola Beghin <nicolabeghin@gmail.com>
2016-08-28 19:16:59 +0200

    fix HTTP Basic Auth on FastCGI PHP

--- lib/Cake/Controller/Component/Auth/BasicAuthenticate.php
+++ lib/Cake/Controller/Component/Auth/BasicAuthenticate.php
@@ -82,6 +82,15 @@ class BasicAuthenticate extends BaseAuthenticate {
  * @return mixed Either false or an array of user information
  */
 	public function getUser(CakeRequest $request) {
+		if(!isset($_SERVER['PHP_AUTH_USER'])) {
+		    if (isset($_SERVER['HTTP_AUTHORIZATION']) && (strlen($_SERVER['HTTP_AUTHORIZATION']) > 0)) {
+		        list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+		        if( strlen($_SERVER['PHP_AUTH_USER']) == 0 || strlen($_SERVER['PHP_AUTH_PW']) == 0 ) {
+		            unset($_SERVER['PHP_AUTH_USER']);
+		            unset($_SERVER['PHP_AUTH_PW']);
+		        }
+		    }
+		}
 		$username = env('PHP_AUTH_USER');
 		$pass = env('PHP_AUTH_PW');
 
