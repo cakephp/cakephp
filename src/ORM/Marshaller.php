36052a633b5e5463931715973e4d69099993e43f
Mark Story <mark@mark-story.com>
2016-08-31 22:33:08 -0400

    Don't mark null values as dirty when they are still null.
    `is_scalar()` does not include `null` so treat is specially.
    
    Refs #9381

--- src/ORM/Marshaller.php
+++ src/ORM/Marshaller.php
@@ -552,6 +552,7 @@ class Marshaller
                 // the original/updated list could contain references to the
                 // same objects, even though those objects may have changed internally.
                 if ((is_scalar($value) && $original === $value) ||
+                    ($value === null && $original === $value) ||
                     (is_object($value) && !($value instanceof EntityInterface) && $original == $value)
                 ) {
                     continue;
