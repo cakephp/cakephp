63017f4fcbaca7c0138feb59ba192595d02a4d55
mark_story <mark@mark-story.com>
2016-09-02 12:55:10 -0400

    Document option added in #9396

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -331,6 +331,7 @@ class FormHelper extends Helper
      * - `context` Additional options for the context class. For example the EntityContext accepts a 'table'
      *   option that allows you to set the specific Table class the form should be based on.
      * - `idPrefix` Prefix for generated ID attributes.
+     * - `templateVars` Provide template variables for the formStart template.
      *
      * @param mixed $model The context for which the form is being defined. Can
      *   be an ORM entity, ORM resultset, or an array of meta data. You can use false or null
