7fec65dc5c967ae87ad841adb99fa6be105d1a5d
ndm2 <github-46j8925q79hg@nowak-media.de>
2016-09-04 17:59:09 +0200

    Fix has many save replace doesn't respect association conditions.
    When removing associated records using the `SAVE_REPLACE` save
    strategy, the associations conditions need to be included for
    non-dependent/cascading configurations, otherwise the only condition
    used will be the primary key of the parent record, causing all linked
    records to be deleted, irrespectively of whether they are actually in
    the scope of the association.

--- src/ORM/Association/HasMany.php
+++ src/ORM/Association/HasMany.php
@@ -461,12 +461,14 @@ class HasMany extends Association
                 return $ok;
             }
 
+            $conditions = array_merge($conditions, $this->conditions());
             $target->deleteAll($conditions);
 
             return true;
         }
 
         $updateFields = array_fill_keys($foreignKey, null);
+        $conditions = array_merge($conditions, $this->conditions());
         $target->updateAll($updateFields, $conditions);
 
         return true;
