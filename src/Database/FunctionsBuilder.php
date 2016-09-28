4654b0bb75480e5ad3d364c1b6e52803c1fee15b
Niklas Rother <info@niklas-rother.de>
2016-09-29 00:30:42 +0200

    Fix doc-block

--- src/Database/FunctionsBuilder.php
+++ src/Database/FunctionsBuilder.php
@@ -142,8 +142,8 @@ class FunctionsBuilder
     /**
      * Returns a FunctionExpression representing a group concatenation.
      *
-     * @param string $expression the function argument
-     * @param string|null $separator Separator to use
+     * @param mixed $expression the function argument
+     * @param string $separator Separator to use
      * @param array $types list of types to bind to the arguments
      * @return \Cake\Database\Expression\FunctionExpression
      */
