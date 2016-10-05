1e300009a1ac961376f6c389691da55c2919d6f0
Jorge Alberto Cricelli <jacricelli@gmail.com>
2016-10-05 15:26:38 -0300

    Corrected parameter description.

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -302,7 +302,7 @@ class Validator implements ArrayAccess, IteratorAggregate, Countable
      *      ]);
      * ```
      *
-     * @param string $field The name of the field from which the rule will be removed
+     * @param string $field The name of the field from which the rule will be added
      * @param array|string $name The alias for a single rule or multiple rules array
      * @param array|\Cake\Validation\ValidationRule $rule the rule to add
      * @return $this
