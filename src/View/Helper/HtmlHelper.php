795c202056278063d0d64b8fe8b4b3d4f1ad5ceb
thinkingmedia <mathew@thinkingmedia.ca>
2016-10-13 18:35:17 -0400

    adds canonical to meta()

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -238,6 +238,7 @@ class HtmlHelper extends Helper
                 'description' => ['name' => 'description', 'content' => $content],
                 'robots' => ['name' => 'robots', 'content' => $content],
                 'viewport' => ['name' => 'viewport', 'content' => $content],
+                'canonical' => ['rel' => 'canonical', 'link' => $content]
             ];
 
             if ($type === 'icon' && $content === null) {
