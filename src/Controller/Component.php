46deb665f010082b7b6d8736b0c3b35ed3110504
mscherer <mark.scherer@spryker.com>
2016-10-27 17:36:22 +0200

    Fix components enabled config option when merging with defaults.

--- src/Controller/Component.php
+++ src/Controller/Component.php
@@ -151,7 +151,7 @@ class Component implements EventListenerInterface
     public function __get($name)
     {
         if (isset($this->_componentMap[$name]) && !isset($this->{$name})) {
-            $config = ['enabled' => false] + (array)$this->_componentMap[$name]['config'];
+            $config = (array)$this->_componentMap[$name]['config'] + ['enabled' => false];
             $this->{$name} = $this->_registry->load($this->_componentMap[$name]['class'], $config);
         }
         if (isset($this->{$name})) {
