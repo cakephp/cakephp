2417f0170c848aa45244671e178125d06477b666
dereuromark <euromark@web.de>
2016-11-03 21:15:08 +0100

    Fix typo.

--- src/Datasource/ConnectionManager.php
+++ src/Datasource/ConnectionManager.php
@@ -183,7 +183,7 @@ class ConnectionManager
      *
      * @param string $name The connection name.
      * @param bool $useAliases Set to false to not use aliased connections.
-     * @return \Cake\\ConnectionInterface A connection object.
+     * @return \Cake\Datasource\ConnectionInterface A connection object.
      * @throws \Cake\Datasource\Exception\MissingDatasourceConfigException When config
      * data is missing.
      */
