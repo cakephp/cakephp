8eb4ecffc5eacc2a8d97de0b98d8e10d8d8c25d6
dereuromark <euromark@web.de>
2016-11-12 15:02:00 +0100

    Split up more $this|other mixed methods into separate getter/setter methods.

--- src/Console/ConsoleOptionParser.php
+++ src/Console/ConsoleOptionParser.php
@@ -271,25 +271,75 @@ class ConsoleOptionParser
     }
 
     /**
+     * Sets the command name for shell/task.
+     *
+     * @param string $text The text to set.
+     * @return $this
+     */
+    public function setCommand($text)
+    {
+        $this->_command = Inflector::underscore($text);
+
+        return $this;
+    }
+
+    /**
+     * Gets the command name for shell/task.
+     *
+     * @return string The value of the command.
+     */
+    public function getCommand()
+    {
+        return $this->_command;
+    }
+
+    /**
      * Gets or sets the command name for shell/task.
      *
+     * @deprecated 3.4.0 Use setCommand()/getCommand() instead.
      * @param string|null $text The text to set, or null if you want to read
      * @return string|$this If reading, the value of the command. If setting $this will be returned.
      */
     public function command($text = null)
     {
         if ($text !== null) {
-            $this->_command = Inflector::underscore($text);
-
-            return $this;
+            return $this->setCommand($text);
         }
 
-        return $this->_command;
+        return $this->getCommand();
+    }
+
+    /**
+     * Sets the description text for shell/task.
+     *
+     * @param string|array $text The text to set. If an array the
+     *   text will be imploded with "\n".
+     * @return $this
+     */
+    public function setDescription($text)
+    {
+        if (is_array($text)) {
+            $text = implode("\n", $text);
+        }
+        $this->_description = $text;
+
+        return $this;
+    }
+
+    /**
+     * Gets the description text for shell/task.
+     *
+     * @return string The value of the description
+     */
+    public function getDescription()
+    {
+        return $this->_description;
     }
 
     /**
      * Get or set the description text for shell/task.
      *
+     * @deprecated 3.4.0 Use setDescription()/getDescription() instead.
      * @param string|array|null $text The text to set, or null if you want to read. If an array the
      *   text will be imploded with "\n".
      * @return string|$this If reading, the value of the description. If setting $this will be returned.
@@ -297,21 +347,45 @@ class ConsoleOptionParser
     public function description($text = null)
     {
         if ($text !== null) {
-            if (is_array($text)) {
-                $text = implode("\n", $text);
-            }
-            $this->_description = $text;
-
-            return $this;
+            return $this->setDescription($text);
         }
 
-        return $this->_description;
+        return $this->getDescription();
     }
 
     /**
-     * Get or set an epilog to the parser. The epilog is added to the end of
+     * Sets an epilog to the parser. The epilog is added to the end of
      * the options and arguments listing when help is generated.
      *
+     * @param string|array $text The text to set. If an array the text will
+     *   be imploded with "\n".
+     * @return $this
+     */
+    public function setEpilog($text)
+    {
+        if (is_array($text)) {
+            $text = implode("\n", $text);
+        }
+        $this->_epilog = $text;
+
+        return $this;
+    }
+
+    /**
+     * Gets the epilog.
+     *
+     * @return string The value of the epilog.
+     */
+    public function getEpilog()
+    {
+        return $this->_epilog;
+    }
+
+    /**
+     * Gets or sets an epilog to the parser. The epilog is added to the end of
+     * the options and arguments listing when help is generated.
+     *
+     * @deprecated 3.4.0 Use setEpilog()/getEpilog() instead.
      * @param string|array|null $text Text when setting or null when reading. If an array the text will
      *   be imploded with "\n".
      * @return string|$this If reading, the value of the epilog. If setting $this will be returned.
@@ -319,15 +393,10 @@ class ConsoleOptionParser
     public function epilog($text = null)
     {
         if ($text !== null) {
-            if (is_array($text)) {
-                $text = implode("\n", $text);
-            }
-            $this->_epilog = $text;
-
-            return $this;
+            return $this->setEpilog($text);
         }
 
-        return $this->_epilog;
+        return $this->getEpilog();
     }
 
     /**
