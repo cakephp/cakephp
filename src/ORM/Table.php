11e783c7fa2f79bb009575ade36e0c6df5e99a81
thinkingmedia <mathew@thinkingmedia.ca>
2016-11-17 21:36:41 -0500

    adds deleteEach method

--- src/ORM/Table.php
+++ src/ORM/Table.php
@@ -36,7 +36,6 @@ use Cake\ORM\Exception\MissingEntityException;
 use Cake\ORM\Exception\RolledbackTransactionException;
 use Cake\ORM\Rule\IsUnique;
 use Cake\Utility\Inflector;
-use Cake\Validation\Validation;
 use Cake\Validation\ValidatorAwareTrait;
 use InvalidArgumentException;
 use RuntimeException;
@@ -965,7 +964,7 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
         $options += ['sourceTable' => $this];
         $association = new BelongsTo($associated, $options);
 
-        return $this->_associations->add($association->name(), $association);
+        return $this->_associations->add($association->getName(), $association);
     }
 
     /**
@@ -1009,7 +1008,7 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
         $options += ['sourceTable' => $this];
         $association = new HasOne($associated, $options);
 
-        return $this->_associations->add($association->name(), $association);
+        return $this->_associations->add($association->getName(), $association);
     }
 
     /**
@@ -1059,7 +1058,7 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
         $options += ['sourceTable' => $this];
         $association = new HasMany($associated, $options);
 
-        return $this->_associations->add($association->name(), $association);
+        return $this->_associations->add($association->getName(), $association);
     }
 
     /**
@@ -1111,7 +1110,7 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
         $options += ['sourceTable' => $this];
         $association = new BelongsToMany($associated, $options);
 
-        return $this->_associations->add($association->name(), $association);
+        return $this->_associations->add($association->getName(), $association);
     }
 
     /**
@@ -1573,6 +1572,37 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
         $statement->closeCursor();
 
         return $statement->rowCount();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function deleteEach($conditions, $options = [])
+    {
+        $options = $options + [
+                'strict' => true,
+                'limit' => 0
+            ];
+
+        $event = $this->dispatchEvent('Model.beforeDeleteEach', compact('conditions', 'options'));
+        if ($event->isStopped()) {
+            return $event->result();
+        }
+        $conditions = $event->data('conditions');
+
+        $count = 0;
+        do {
+            $entity = $this->find()->where($conditions)->first();
+            if ($entity === null) {
+                break;
+            }
+            if ($this->delete($entity, $options) === false && $options['strict']) {
+                break;
+            }
+            $count++;
+        } while ($entity !== null && ($options['limit'] == 0 || $count < $options['limit']));
+
+        return $this->dispatchEvent('Model.afterDeleteEach', compact('count'))->data('count');
     }
 
     /**
@@ -2563,6 +2593,8 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
      * - Model.beforeDelete => beforeDelete
      * - Model.afterDelete => afterDelete
      * - Model.afterDeleteCommit => afterDeleteCommit
+     * - Model.beforeDeleteEach => beforeDeleteEach
+     * - Model.afterDeleteEach => afterDeleteEach
      * - Model.beforeRules => beforeRules
      * - Model.afterRules => afterRules
      *
@@ -2580,6 +2612,8 @@ class Table implements RepositoryInterface, EventListenerInterface, EventDispatc
             'Model.beforeDelete' => 'beforeDelete',
             'Model.afterDelete' => 'afterDelete',
             'Model.afterDeleteCommit' => 'afterDeleteCommit',
+            'Model.beforeDeleteEach' => 'beforeDeleteEach',
+            'Model.afterDeleteEach' => 'afterDeleteEach',
             'Model.beforeRules' => 'beforeRules',
             'Model.afterRules' => 'afterRules',
         ];
