98e3a7cd0bb7c9d9c952fc45eb828603108b5281
Mark Story <mark@mark-story.com>
2016-12-02 23:15:09 -0500

    Don't test sorting by modified time twice.
    Not only is this redundant it is causing test failures on travis.

--- tests/TestCase/Filesystem/FolderTest.php
+++ tests/TestCase/Filesystem/FolderTest.php
@@ -1372,33 +1372,6 @@ class FolderTest extends TestCase
     }
 
     /**
-     * testSortByTime2 method
-     *
-     * Verify that the order using modified time is correct.
-     *
-     * @return void
-     */
-    public function testSortByTime2()
-    {
-        $Folder = new Folder(TMP . 'tests', true);
-
-        $fileA = new File($Folder->pwd() . DS . 'a.txt');
-        $fileA->create();
-
-        $fileC = new File($Folder->pwd() . DS . 'c.txt');
-        $fileC->create();
-
-        sleep(1);
-
-        $fileB = new File($Folder->pwd() . DS . 'b.txt');
-        $fileB->create();
-
-        $results = $Folder->find('.*', Folder::SORT_TIME);
-
-        $this->assertSame(['a.txt', 'c.txt', 'b.txt'], $results);
-    }
-
-    /**
      * Verify that the order using name is correct.
      */
     public function testSortByName()
