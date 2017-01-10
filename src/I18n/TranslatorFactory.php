0c4ae66fe0401f502115da8c1b3f25ff9b4dbd91
Mark Story <mark@mark-story.com>
2017-01-09 23:58:51 -0500

    Fix context/plural related issues in i18n
    Because we were using the stock translator from Aura, we had painful
    issues when:
    
    * A plural message was used with `__()` an array would be returned which
      is never right.
    * When an unknown context is used the `__x()` method fails.
    
    Refs #8833
    Refs #9985

---
+++ src/I18n/TranslatorFactory.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.3.12
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\I18n;
+
+use Aura\Intl\TranslatorFactory as BaseTranslatorFactory;
+
+use Aura\Intl\TranslatorInterface;
+use Aura\Intl\FormatterInterface;
+
+/**
+ * Factory to create translators
+ *
+ * @internal
+ */
+class TranslatorFactory extends BaseTranslatorFactory
+{
+    /**
+     * The class to use for new instances.
+     *
+     * @var string
+     */
+    protected $class = 'Cake\I18n\Translator';
+}
