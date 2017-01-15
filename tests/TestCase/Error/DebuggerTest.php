323401cf68bb59e046aafbb499e1020c73cc025d
ADmad <admad.coder@gmail.com>
2017-01-15 19:44:03 +0530

    Fix CS error

--- tests/TestCase/Error/DebuggerTest.php
+++ tests/TestCase/Error/DebuggerTest.php
@@ -1,17 +1,17 @@
 <?php
 /**
-* CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
-* Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
-*
-* Licensed under The MIT License
-* For full copyright and license information, please see the LICENSE.txt
-* Redistributions of files must retain the above copyright notice.
-*
-* @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
-* @link          http://cakephp.org CakePHP Project
-* @since         1.2.0
-* @license       http://www.opensource.org/licenses/mit-license.php MIT License
-*/
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP Project
+ * @since         1.2.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 namespace Cake\Test\TestCase\Error;
 
 use Cake\Controller\Controller;
@@ -21,8 +21,8 @@ use Cake\Log\Log;
 use Cake\TestSuite\TestCase;
 
 /**
-* DebuggerTestCaseDebugger class
-*/
+ * DebuggerTestCaseDebugger class
+ */
 class DebuggerTestCaseDebugger extends Debugger
 {
 }
