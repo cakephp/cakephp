b42077527994db16f870da48e2a251c59f01b483
Cory Thompson <coryjthompson@gmail.com>
2017-01-26 10:00:07 +1100

    Unit test proving ValueBinder->placeholder is returning unexpected results

---
+++ tests/TestCase/Database/ValueBinderTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.3.12
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Test\TestCase\Database;
+
+use Cake\Database\ValueBinder;
+use Cake\TestSuite\TestCase;
+
+/**
+ * Tests ValueBinder class
+ */
+class ValueBinderTest extends TestCase
+{
+    /**
+     * test the placeholder method
+     */
+    public function testPlaceholder()
+    {
+        $valueBinder = new ValueBinder();
+        $result = $valueBinder->placeholder('?');
+        $this->assertEquals('?', $result);
+
+        $valueBinder = new ValueBinder();
+        $result = $valueBinder->placeholder(':param');
+        $this->assertEquals(':param', $result);
+
+        $valueBinder = new ValueBinder();
+        $result = $valueBinder->placeholder('p');
+        $this->assertEquals(':p0', $result);
+        $result = $valueBinder->placeholder('p');
+        $this->assertEquals(':p1', $result);
+        $result = $valueBinder->placeholder('c');
+        $this->assertEquals(':c2', $result);
+    }
+}
