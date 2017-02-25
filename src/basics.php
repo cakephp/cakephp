5ca71bce17950056490aa516b632da73d5708b92
Koji Tanaka <tenkoma@gmail.com>
2017-02-25 22:32:37 +0900

    Unified arguments of dd() and debug()

--- src/basics.php
+++ src/basics.php
@@ -121,22 +121,26 @@ if (!function_exists('dd')) {
      *
      * @param mixed $var Variable to show debug information for.
      * @param bool|null $showHtml If set to true, the method prints the debug data in a browser-friendly way.
+     * @param bool $showFrom If set to true, the method prints from where the function was called.
      * @return void
      * @link http://book.cakephp.org/3.0/en/development/debugging.html#basic-debugging
      */
-    function dd($var, $showHtml = null)
+    function dd($var, $showHtml = null, $showFrom = true)
     {
         if (!Configure::read('debug')) {
             return;
         }
 
-        $trace = Debugger::trace(['start' => 1, 'depth' => 2, 'format' => 'array']);
-        $location = [
-            'line' => $trace[0]['line'],
-            'file' => $trace[0]['file']
-        ];
+        $location = [];
+        if ($showFrom) {
+            $trace = Debugger::trace(['start' => 1, 'depth' => 2, 'format' => 'array']);
+            $location = [
+                'line' => $trace[0]['line'],
+                'file' => $trace[0]['file']
+            ];
+        }
 
-        Debugger::printVar($var, $location);
+        Debugger::printVar($var, $location, $showHtml);
         die(1);
     }
 }
