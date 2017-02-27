9df275c17ab5e9abcde3e05ed362a157b26e922a
Mark Story <mark@mark-story.com>
2017-02-27 00:26:11 -0500

    Fix since tag.

--- src/ORM/Association/DependentDeleteHelper.php
+++ src/ORM/Association/DependentDeleteHelper.php
@@ -9,7 +9,7 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         3.0.0
+ * @since         3.5.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Cake\ORM\Association;
