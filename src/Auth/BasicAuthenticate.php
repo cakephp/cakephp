6851896205706e9319918ba0fa94b9b84f9acd3f
Manuel1948 <manuel.himmler@gmail.com>
2017-03-06 14:08:16 +0100

    Update BasicAuthenticate.php

--- src/Auth/BasicAuthenticate.php
+++ src/Auth/BasicAuthenticate.php
@@ -75,10 +75,8 @@ class BasicAuthenticate extends BaseAuthenticate
     public function getUser(ServerRequest $request)
     {
         $username = $request->env('PHP_AUTH_USER');
-        $pass = $request->env('PHP_AUTH_PW');
-        
-        $encoding = Configure::read('App.encoding');
-        
+        $pass = $request->env('PHP_AUTH_PW');        
+        $encoding = Configure::read('App.encoding');        
         if ($encoding and in_array($encoding, mb_list_encodings()) and $encoding != 'ISO-8859-1')
         {
             $username = mb_convert_encoding($username, $encoding, 'ISO-8859-1');
