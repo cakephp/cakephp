78f3e0435864b38aaa295287162762ce28000b43
mscherer <mark.scherer@spryker.com>
2017-03-07 15:15:38 +0100

    Add test case for the limit option of Text::highlight().

--- src/Utility/Text.php
+++ src/Utility/Text.php
@@ -456,7 +456,8 @@ class Text
      *
      * - `format` The piece of HTML with that the phrase will be highlighted
      * - `html` If true, will ignore any HTML tags, ensuring that only the correct text is highlighted
-     * - `regex` a custom regex rule that is used to match words, default is '|$tag|iu'
+     * - `regex` A custom regex rule that is used to match words, default is '|$tag|iu'
+     * - `limit` A limit, optional, defaults to -1 (none)
      *
      * @param string $text Text to search the phrase in.
      * @param string|array $phrase The phrase or phrases that will be searched.
@@ -473,7 +474,7 @@ class Text
         $defaults = [
             'format' => '<span class="highlight">\1</span>',
             'html' => false,
-            'regex' => "|%s|iu",
+            'regex' => '|%s|iu',
             'limit' => -1,
         ];
         $options += $defaults;
