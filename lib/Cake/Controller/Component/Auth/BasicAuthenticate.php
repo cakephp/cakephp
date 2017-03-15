f5795f05a5409cd6e8ae5b1a8742cf17e346b425
Nicola Beghin <nicolabeghin@gmail.com>
2017-03-15 13:59:56 +0100

    BasicAuthenticate - code styling and strong type comparison

--- lib/Cake/Controller/Component/Auth/BasicAuthenticate.php
+++ lib/Cake/Controller/Component/Auth/BasicAuthenticate.php
@@ -85,9 +85,8 @@ class BasicAuthenticate extends BaseAuthenticate {
 		if(!isset($_SERVER['PHP_AUTH_USER'])) {
 		    if (isset($_SERVER['HTTP_AUTHORIZATION']) && (strlen($_SERVER['HTTP_AUTHORIZATION']) > 0)) {
 		        list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
-		        if( strlen($_SERVER['PHP_AUTH_USER']) == 0 || strlen($_SERVER['PHP_AUTH_PW']) == 0 ) {
-		            unset($_SERVER['PHP_AUTH_USER']);
-		            unset($_SERVER['PHP_AUTH_PW']);
+		        if(strlen($_SERVER['PHP_AUTH_USER']) === 0 || strlen($_SERVER['PHP_AUTH_PW']) === 0) {
+		            unset($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
 		        }
 		    }
 		}
