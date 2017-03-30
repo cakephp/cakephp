b05ffe4239033dedb174c9958362ecae03878034
Mark Story <mark@mark-story.com>
2017-03-30 14:05:52 -0400

    Add deprecation to Client\Request::cookie()
    This method isn't necessary anymore. The new CookieCollections are used
    by the Client making this method un-necessary.

--- src/Http/Client/Request.php
+++ src/Http/Client/Request.php
@@ -188,6 +188,9 @@ class Request extends Message implements RequestInterface
      * @param string $name The name of the cookie to get/set
      * @param string|null $value Either the value or null when getting values.
      * @return mixed Either $this or the cookie value.
+     * @deprected 3.5.0 No longer used. CookieCollections now add `Cookie` header to the request
+     *   before sending. Use Cake\Http\Cookie\CookieCollection::addToRequest() to make adding cookies
+     *   to a request easier.
      */
     public function cookie($name, $value = null)
     {
