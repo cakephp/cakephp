3880183e0e0bbe2e7f5069149d98662bbfacfd51
Travis Rowland <travis.rowland@gmail.com>
2017-04-17 14:31:13 -0700

    Changing --$years to $years-- in src/I18n/RelativeTimeFormatter.php

--- src/I18n/RelativeTimeFormatter.php
+++ src/I18n/RelativeTimeFormatter.php
@@ -231,7 +231,7 @@ class RelativeTimeFormatter
             }
 
             if ($months >= 12) {
-                ++$years;
+                $years++;
                 $months = $months - 12;
             }
 
