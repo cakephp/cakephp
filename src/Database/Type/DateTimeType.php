b69f5ba15ce138e782c02e9e513df5f8d526fe2d
Thomas von Hassel <thomas@partner-it.dk>
2017-05-18 11:40:59 +0200

    update allowed date format string to match `ATOM` format

--- src/Database/Type/DateTimeType.php
+++ src/Database/Type/DateTimeType.php
@@ -57,7 +57,7 @@ class DateTimeType extends Type implements TypeInterface
      */
     protected $_format = [
         'Y-m-d H:i:s',
-        'Y-m-d\TH:i:s.uO',
+        'Y-m-d\TH:i:sP',
     ];
 
     /**
