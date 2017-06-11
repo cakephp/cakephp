0bedd034490f8955c7db3a4dca84654b95161d43
ADmad <admad.coder@gmail.com>
2017-06-11 14:18:31 +0530

    Update docblock.

--- src/Datasource/PaginatorInterface.php
+++ src/Datasource/PaginatorInterface.php
@@ -22,7 +22,7 @@ interface PaginatorInterface
     /**
      * Handles pagination of datasource records.
      *
-     * @param \Cake\Datasource\RepositoryInterface|\Cake\Datasource\QueryInterface $object The repository or query to paginate.
+     * @param mixed $object The repository or query to paginate.
      * @param array $params Request params
      * @param array $settings The settings/configuration used for pagination.
      * @return mixed Query results
