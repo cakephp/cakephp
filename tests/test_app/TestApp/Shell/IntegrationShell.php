7bf1f0d8f378fc36123c383153334a803d8f4419
Jeremy Harris <jeremy@someguyjeremy.com>
2017-06-23 16:46:19 -0500

    Fixed copy/paste error

--- tests/test_app/TestApp/Shell/IntegrationShell.php
+++ tests/test_app/TestApp/Shell/IntegrationShell.php
@@ -11,7 +11,7 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         1.5.0
+ * @since         3.5.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
