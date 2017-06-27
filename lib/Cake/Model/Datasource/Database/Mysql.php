4ec195f9c87ab71d8b12ba6225d8d3e29568c981
Kurre Ståhlberg <kurre.stahlberg@readpeak.com>
2017-06-27 11:46:51 +0300

    Fix error when default value is reported as CURRENT_TIMESTAMP() with parenthesis

--- lib/Cake/Model/Datasource/Database/Mysql.php
+++ lib/Cake/Model/Datasource/Database/Mysql.php
@@ -352,7 +352,8 @@ class Mysql extends DboSource {
 			if (in_array($fields[$column->Field]['type'], $this->fieldParameters['unsigned']['types'], true)) {
 				$fields[$column->Field]['unsigned'] = $this->_unsigned($column->Type);
 			}
-			if (in_array($fields[$column->Field]['type'], array('timestamp', 'datetime')) && strtoupper($column->Default) === 'CURRENT_TIMESTAMP') {
+			if (in_array($fields[$column->Field]['type'], array('timestamp', 'datetime')) &&
+					in_array(strtoupper($column->Default), array('CURRENT_TIMESTAMP', 'CURRENT_TIMESTAMP()'))) {
 				$fields[$column->Field]['default'] = null;
 			}
 			if (!empty($column->Key) && isset($this->index[$column->Key])) {
