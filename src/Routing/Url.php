148dd540c6cb1025a9039122b700cc6b6c4c690f
Florian Krämer <florian.kraemer@kreative-design.net>
2017-07-08 15:32:29 +0200

    URL object to build URLs instead of using an array

---
+++ src/Routing/Url.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         3.5.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Routing;
+
+/**
+ * URL Object
+ */
+class Url {
+
+	/**
+	 * Array representation of the URL
+	 *
+	 * @var array
+	 */
+	protected $url = [];
+
+	/**
+	 * Absolute URL
+	 *
+	 * @var bool
+	 */
+	protected $absolute;
+
+	/**
+	 * Sets the action
+	 *
+	 * @param string
+	 * @return $this
+	 */
+	public function setAction($action)
+	{
+		$this->url['action'] = $action;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the controller
+	 *
+	 * @param string $controller
+	 * @return $this
+	 */
+	public function setController($controller)
+	{
+		$this->url['controller'] = $controller;
+
+		return $this;
+	}
+
+	/**
+	 * Set prefix
+	 *
+	 * @param $prefix
+	 * @return $this
+	 */
+	public function setPrefix($prefix)
+	{
+		$this->url['prefix'] = $prefix;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the plugin
+	 *
+	 * @param string|null
+	 * @return $this
+	 */
+	public function setPlugin($plugin)
+	{
+		$this->url['plugin'] = $plugin;
+
+		return $this;
+	}
+
+	/**
+	 * Sets values to be passed in the URL
+	 *
+	 * @param string $pass
+	 * @return $this
+	 */
+	public function setPass($pass)
+	{
+		$this->url[] = $pass;
+
+		return $this;
+	}
+
+	/**
+	 * Sets a query param
+	 *
+	 * @param string $key $value
+	 * @param string|int|float
+	 * @return $this
+	 */
+	public function setQuery($key, $value)
+	{
+		if (!isset($this->url['?'])) {
+			$this->url['?'] = [];
+		}
+		$this->url['?'][$key] = $value;
+	}
+
+	/**
+	 * Sets multiple query params
+	 *
+	 * @param array $params Query params as key value list
+	 * @return $this
+	 */
+	public function setQueryParams(array $params)
+	{
+		if (!isset($this->url['?'])) {
+			$this->url['?'] = [];
+		}
+
+		$this->url['?'] = array_merge($this->url['?'], $params);
+
+		return $this;
+	}
+
+	/**
+	 * Absolute URL
+	 *
+	 * @param bool $absolute
+	 * @return $this
+	 */
+	public function absolute($absolute = true)
+	{
+		$this->absolute = (bool)$absolute;
+
+		return $this;
+	}
+
+	/**
+	 * To string
+	 *
+	 * @return string;
+	 */
+	public function toString()
+	{
+		return Router::url($this->url, $this->absolute);
+	}
+
+	/**
+	 * To array
+	 *
+	 * @return array
+	 */
+	public function toArray()
+	{
+		return $this->url;
+	}
+
+	/**
+	 * To string
+	 *
+	 * @return string String URL
+	 */
+	public function __toString()
+	{
+		return $this->toString();
+	}
+}
