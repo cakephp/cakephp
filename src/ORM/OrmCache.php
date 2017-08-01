27497cf3d24c4fd75f0d0698c1545fbadc4f6af1
Florian Krämer <florian.kraemer@kreative-design.net>
2017-08-01 22:57:10 +0200

    Adding a test for the new OrmCache class

--- src/ORM/OrmCache.php
+++ src/ORM/OrmCache.php
@@ -9,7 +9,7 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @link          https://cakephp.org CakePHP(tm) Project
- * @since         3.0.0
+ * @since         3.5.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace Cake\Orm;
