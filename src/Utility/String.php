91bedb73c19e1dd863e2d3958b2b6f98fdc35c22
inoas <mail@inoas.com>
2017-08-21 22:30:52 +0200

    Update String.php

--- src/Utility/String.php
+++ src/Utility/String.php
@@ -1,5 +1,6 @@
 <?php
 // @deprecated Backward compatibility with 2.x series
-class_alias('Cake\Utility\Text', 'Cake\Utility\String');
-
-trigger_error('Using Cake\Utility\String will throw an error in CakePHP 3.6.0. Please use Cake\Utility\Text instead.', E_USER_NOTICE);
+if (PHP_VERSION_ID < 70000) {
+    trigger_error('Using Cake\Utility\String will throw an error in CakePHP 3.6.0. Please use Cake\Utility\Text instead.', E_USER_NOTICE);
+    class_alias('Cake\Utility\Text', 'Cake\Utility\String');
+}
