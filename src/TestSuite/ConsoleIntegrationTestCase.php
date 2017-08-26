07640f8108e2c3309744cb30c59035b116e6df32
Mark Story <mark@mark-story.com>
2017-08-25 21:27:02 -0400

    Add more assertion methods to ConsoleIntegrationTestCase.
    Add assertions for checking table rows, and empty output.

--- src/TestSuite/ConsoleIntegrationTestCase.php
+++ src/TestSuite/ConsoleIntegrationTestCase.php
@@ -139,6 +139,18 @@ class ConsoleIntegrationTestCase extends TestCase
     }
 
     /**
+     * Asserts that `stdout` is empty
+     *
+     * @param string $message The message to output when the assertion fails.
+     * @return void
+     */
+    public function assertOutputEmpty($message = 'stdout was not empty')
+    {
+        $output = implode(PHP_EOL, $this->_out->messages());
+        $this->assertSame('', $output, $message);
+    }
+
+    /**
      * Asserts `stdout` contains expected output
      *
      * @param string $expected Expected output
@@ -162,6 +174,23 @@ class ConsoleIntegrationTestCase extends TestCase
     {
         $output = implode(PHP_EOL, $this->_out->messages());
         $this->assertRegExp($pattern, $output, $message);
+    }
+
+    /**
+     * Check that a row of cells exists in the output.
+     *
+     * @param array $row Row of cells to ensure exist in the output.
+     * @param string $message Failure message.
+     * @return void
+     */
+    protected function assertOutputContainsRow(array $row, $message = '')
+    {
+        $row = array_map(function ($cell) {
+            return preg_quote($cell, '/');
+        }, $row);
+        $cells = implode('\s+\|\s+', $row);
+        $pattern = '/' . $cells . '/';
+        $this->assertOutputRegexp($pattern);
     }
 
     /**
