36fdf90d0260402485a859907a86c0d57b68443d
Florian Krämer <florian.kraemer@kreative-design.net>
2017-08-29 00:20:48 +0200

    Adding a CSP Middleware

---
+++ src/Http/Middleware/CspMiddleWare.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.6.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Http\Middleware;
+
+use Cake\Core\Configure;
+use Cake\Http\Response;
+use Cake\Http\ServerRequest;
+use InvalidArgumentException;
+use ParagonIE\CSPBuilder\CSPBuilder;
+
+/**
+ * Content Security Policy Middleware
+ */
+class CspMiddleware
+{
+
+    /**
+     * CSP Builder
+     *
+     * @var \ParagonIE\CSPBuilder\
+     */
+    protected $csp;
+
+    /**
+     * Constructor
+     *
+     * @param null|\ParagonIE\CSPBuilder\
+     */
+    public function __construct($csp = null)
+    {
+        if (!is_array($csp)) {
+            $cspConfig = (array)Configure::read('App.CSP');
+        } else {
+            $cspConfig = $csp;
+        }
+
+        if (!empty($cspConfig)) {
+            $this->csp = CSPBuilder::fromData(json_encode($cspConfig));
+
+            return;
+        }
+
+        if (!$csp instanceof CSPBuilder) {
+            throw new InvalidArgumentException(sprintf(
+                'Expected `%s`, `%s` given.',
+                CSPBuilder::class,
+                gettype($csp)
+            ));
+        }
+
+        $this->csp = $csp;
+    }
+
+    /**
+     * Adds the CSP Headers
+     *
+     * @param \Cake\Http\ServerRequest $request The request.
+     * @param \Cake\Http\Response $response The response.
+     * @param callable $next Callback to invoke the next middleware.
+     * @return \Cake\Http\Response A response
+     */
+    public function __invoke(ServerRequest $request, Response $response, $next)
+    {
+        $response = $next($request, $response);
+
+        return $this->csp->injectCSPHeader($response);
+    }
+}
---
+++ tests/TestCase/Http/Middleware/CspMiddlewareTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.6.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Http\Middleware;
+
+use Cake\Http\Response;
+use Cake\Http\ServerRequest;
+use Cake\TestSuite\TestCase;
+
+/**
+ * Content Security Policy Middleware Test
+ */
+class CspMiddlewareTest extends TestCase
+{
+
+    /**
+     * testInvoke
+     *
+     * @return void
+     */
+    public function testInvoke() {
+        $request = new ServerRequest();
+        $response = new Response();
+        $callable = function($request, $response) {
+            return $response;
+        };
+
+        $middleware = new CspMiddleware([
+            'script-src' => [
+                'allow' => [
+                    'https://www.google-analytics.com'
+                ],
+                'self' => true,
+                'unsafe-inline' => false,
+                'unsafe-eval' => false
+            ]
+        ]);
+
+        $response = $middleware($request, $response, $callable);
+        $headers = $response->getHeaders();
+        $expected = [
+            'script-src \'self\' https://www.google-analytics.com; '
+        ];
+
+        $this->assertNotEmpty($headers['Content-Security-Policy']);
+        $this->assertEquals($expected, $headers['Content-Security-Policy']);
+    }
+}
