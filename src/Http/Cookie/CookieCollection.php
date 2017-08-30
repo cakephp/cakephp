11375f7d3a5f87da829407aee6a69bbb82e53bc2
supermanner <supermannermode@gmail.com>
2017-08-30 16:19:32 +0900

    Fix CookieCollection::parseSetCookieHeader when the cookie has properties that are not expected in Cookie Object

--- src/Http/Cookie/CookieCollection.php
+++ src/Http/Cookie/CookieCollection.php
@@ -357,7 +357,7 @@ class CookieCollection implements IteratorAggregate, Countable
                     continue;
                 }
                 $key = strtolower($key);
-                if (!strlen($cookie[$key])) {
+                if (array_key_exists($key, $cookie) && !strlen($cookie[$key])) {
                     $cookie[$key] = $value;
                 }
             }
