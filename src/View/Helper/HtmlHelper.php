a392600bada351b2d51f0d5e3985b755fbd7cf45
Huw Jones <huwcbjones@outlook.com>
2017-09-02 19:27:49 +0100

    Changed array_key_diff to unset

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -347,7 +347,7 @@ class HtmlHelper extends Helper
         $escapeTitle = true;
         if ($url !== null) {
             $url = $this->Url->build($url, $options);
-            $options = array_diff_key($options, ['fullBase' => null]);
+            unset($options['fullBase']);
         } else {
             $url = $this->Url->build($title);
             $title = htmlspecialchars_decode($url, ENT_QUOTES);
