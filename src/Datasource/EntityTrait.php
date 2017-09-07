b2cfce9f9f8b5592c2734c0855d3441e60d83b89
Markus Ramšak <markus@imdsolution.at>
2017-09-07 14:45:06 +0200

    Update EntityTrait.php
    Check virtual property too

--- src/Datasource/EntityTrait.php
+++ src/Datasource/EntityTrait.php
@@ -807,13 +807,22 @@ trait EntityTrait
      * Check if property exists in entity even if it is null
      *
      * @param string $property The field to check for existence.
+     * @param bool $alsoCheckForVirtual Check virtual property too?
      * @return bool
      */
-    public function propertyExists($property)
+    public function propertyExists($property, $alsoCheckForVirtual = false)
     {
         $hasProperty = ($this->has($property) || $this->isDirty($property));
-        if (!$hasProperty && array_key_exists($property, $this->_properties)) {
-            return true;
+        if (!$hasProperty) {
+            if (array_key_exists($property, $this->_properties)) {
+                return true;
+            }
+            if ($alsoCheckForVirtual) {
+                $method = static::_accessor($property, 'get');
+                if ($method) {
+                    return true;
+                }
+            }
         }
 
         return $hasProperty;
