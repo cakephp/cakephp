f94d8581c8d80e60098d112feee54a030520b1b4
Rachman Chavik <rchavik@xintesa.com>
2017-09-15 11:04:59 +0700

    Fix: Duplicate path in $themePaths
    Consider:
    
    $themePaths = [
        'Theme1',
        'Theme2',
    ];
    
    array_unshift($themePaths, $themePaths[$i] . 'Plugin' . DS . $plugin . DS);
    
    After first iteration, a new element is inserted as first element
    so at 2nd iteration, Theme1 is no longer at key 0, but pushed to key 1,
    Hence it will do another one with Theme1
    
    - Ceeram, 2017
    
    Now we can correctly override plugin layouts and elements from app and themes
    template files again.
    
    Fixed with a little help from admad, ceeram and robertpustulka

--- src/View/View.php
+++ src/View/View.php
@@ -1459,8 +1459,8 @@ class View implements EventDispatcherInterface
             array_unshift($themePaths, APP . 'Template' . DIRECTORY_SEPARATOR . 'Plugin' . DIRECTORY_SEPARATOR . $this->theme . DIRECTORY_SEPARATOR);
 
             if ($plugin) {
-                for ($i = 0, $count = count($themePaths); $i < $count; $i++) {
-                    array_unshift($themePaths, $themePaths[$i] . 'Plugin' . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR);
+                foreach (array_reverse($themePaths) as $path) {
+                    array_unshift($themePaths, $path . 'Plugin' . DIRECTORY_SEPARATOR . $plugin . DIRECTORY_SEPARATOR);
                 }
             }
         }
