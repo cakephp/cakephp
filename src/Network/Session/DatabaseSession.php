a078e01490dbfab3c4b6101286bac581ca64bf74
Robert Pustułka <robert.pustulka@gmail.com>
2017-09-21 10:16:35 +0200

    Use LocatorAwareTrait in DatabaseSession.

--- src/Network/Session/DatabaseSession.php
+++ src/Network/Session/DatabaseSession.php
@@ -18,7 +18,7 @@
 namespace Cake\Network\Session;
 
 use Cake\ORM\Entity;
-use Cake\ORM\Locator\TableLocator;
+use Cake\ORM\Locator\LocatorAwareTrait;
 use SessionHandlerInterface;
 
 /**
@@ -26,6 +26,7 @@ use SessionHandlerInterface;
  */
 class DatabaseSession implements SessionHandlerInterface
 {
+    use LocatorAwareTrait;
 
     /**
      * Reference to the table handling the session data
@@ -50,7 +51,10 @@ class DatabaseSession implements SessionHandlerInterface
      */
     public function __construct(array $config = [])
     {
-        $tableLocator = isset($config['tableLocator']) ? $config['tableLocator'] : TableLocator::getInstance();
+        if (isset($config['tableLocator'])) {
+            $this->setTableLocator($config['tableLocator']);
+        }
+        $tableLocator = $this->getTableLocator();
 
         if (empty($config['model'])) {
             $config = $tableLocator->exists('Sessions') ? [] : ['table' => 'sessions'];
