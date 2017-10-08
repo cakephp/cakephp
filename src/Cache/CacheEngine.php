fe1f7109d130219f4da4497bc4edaa324a591bcc
inoas <mail@inoas.com>
2017-10-09 00:04:33 +0200

    add forgotten warn method when doing git resets and force pushes

--- src/Cache/CacheEngine.php
+++ src/Cache/CacheEngine.php
@@ -276,4 +276,30 @@ abstract class CacheEngine
 
         return $this->_config['prefix'] . $key;
     }
+
+    /**
+     * Cache Engines may trigger warnings if they encounter failures during operation,
+     * if option warningOnFailures is set to true.
+     *
+     * @param string $message The warning message.
+     * @return void
+     */
+    protected function warn($message)
+    {
+        if ($this->config('disableWarningsOnFailures') !== true) {
+            $stackFrame = 2;
+            $trace = debug_backtrace();
+            if (isset($trace[$stackFrame])) {
+                $frame = $trace[$stackFrame];
+                $frame += ['file' => '[internal]', 'line' => '??'];
+                $message = sprintf(
+                    '%s - %s, line: %s',
+                    $message,
+                    $frame['file'],
+                    $frame['line']
+                );
+            }
+            trigger_error($message, E_USER_WARNING);
+        }
+    }
 }
