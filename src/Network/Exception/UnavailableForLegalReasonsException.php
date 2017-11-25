f95d9daec3b0a90a9e17e619883003294398b573
Mark Story <mark@mark-story.com>
2017-11-25 16:59:54 -0500

    Fix phpcs errors.

--- src/Network/Exception/UnavailableForLegalReasonsException.php
+++ src/Network/Exception/UnavailableForLegalReasonsException.php
@@ -2,4 +2,3 @@
 // @deprecated Backward compatibility alias
 class_alias('Cake\Http\Exception\UnavailableForLegalReasonsException', 'Cake\Network\Exception\UnavailableForLegalReasonsException');
 deprecationWarning('Use Cake\Http\Exception\UnavailableForLegalReasonsException instead of Cake\Network\Exception\UnavailableForLegalReasonsException.');
-
