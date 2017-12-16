35a24369e9b5d357a3d83ac0d57ffa9b3e7a0994
ADmad <admad.coder@gmail.com>
2017-12-16 18:53:48 +0530

    Fix CS error.

--- src/Event/EventManager.php
+++ src/Event/EventManager.php
@@ -576,7 +576,8 @@ class EventManager
             $properties['_listeners'][$key] = $listenerCount . ' listener(s)';
         }
         if ($this->_eventList) {
-            for ($i = 0; $i < count($this->_eventList); $i++) {
+            $count = count($this->_eventList);
+            for ($i = 0; $i < $count; $i++) {
                 $event = $this->_eventList[$i];
                 $properties['_dispatchedEvents'][] = $event->getName() . ' with subject ' . get_class($event->getSubject());
             }
