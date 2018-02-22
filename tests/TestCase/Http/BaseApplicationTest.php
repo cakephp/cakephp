afd1fd6e845eb189a30e1441fc0d1016e65df289
mark_story <mark@mark-story.com>
2018-02-22 11:22:59 -0500

    Add missing file header.

--- tests/TestCase/Http/BaseApplicationTest.php
+++ tests/TestCase/Http/BaseApplicationTest.php
@@ -1,5 +1,18 @@
 <?php
-namespace Cake\Test\TestCase;
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         3.5.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Test\TestCase\Http;
 
 use Cake\Core\Configure;
 use Cake\Core\Plugin;
