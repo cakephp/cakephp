c5f27f3e793186ad80f18b9f0651e8f2371e28ac
chinpei215 <chinpei215@hotmail.com>
2018-04-03 00:36:23 +0900

    Fix toDatabase() to work with non-DateTimeImmutable properly

--- tests/TestCase/Database/Type/DateTimeTypeTest.php
+++ tests/TestCase/Database/Type/DateTimeTypeTest.php
@@ -146,9 +146,11 @@ class DateTimeTypeTest extends TestCase
         $result = $this->type->toDatabase($date, $this->driver);
         $this->assertEquals('2013-08-12 15:16:17', $result);
 
+        $tz = $date->getTimezone();
         $this->type->setTimezone('Asia/Kolkata'); // UTC+5:30
         $result = $this->type->toDatabase($date, $this->driver);
         $this->assertEquals('2013-08-12 20:46:17', $result);
+        $this->assertEquals($tz, $date->getTimezone());
 
         $this->type->setTimezone(new DateTimeZone('Asia/Kolkata'));
         $result = $this->type->toDatabase($date, $this->driver);
