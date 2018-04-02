aae9471498fdca74b27e4f502a15a0c198becff3
ADmad <admad.coder@gmail.com>
2018-04-03 00:05:46 +0530

    Allow configuring grouped input types.
    Closes #11850.

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -254,6 +254,13 @@ class FormHelper extends Helper
     protected $_valueSources = ['context'];
 
     /**
+     * Grouped input types.
+     *
+     * @var array
+     */
+    protected $_groupedInputTypes = ['radio', 'multicheckbox', 'date', 'time', 'datetime'];
+
+    /**
      * Construct the widgets and binds the default context providers
      *
      * @param \Cake\View\View $View The View this helper is being attached to.
@@ -278,6 +285,11 @@ class FormHelper extends Helper
             }
             $widgets = $config['widgets'] + $widgets;
             unset($config['widgets']);
+        }
+
+        if (isset($config['groupedInputTypes'])) {
+            $this->_groupedInputTypes = $config['groupedInputTypes'];
+            unset($config['groupedInputTypes']);
         }
 
         parent::__construct($View, $config);
@@ -1550,8 +1562,7 @@ class FormHelper extends Helper
         }
 
         $labelAttributes['for'] = $options['id'];
-        $groupTypes = ['radio', 'multicheckbox', 'date', 'time', 'datetime'];
-        if (in_array($options['type'], $groupTypes, true)) {
+        if (in_array($options['type'], $this->_groupedInputTypes, true)) {
             $labelAttributes['for'] = false;
         }
         if ($options['nestedInput']) {
