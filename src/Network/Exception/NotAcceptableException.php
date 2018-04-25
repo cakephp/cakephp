958b92e19a57d0a02ce35b59f425b597b56d5b33
ADmad <admad.coder@gmail.com>
2018-04-25 23:03:40 +0530

    Removed deprecated classes in Network package.

--- src/Network/Exception/NotAcceptableException.php
+++
@@ -1,4 +0,0 @@
-<?php
-// @deprecated Backward compatibility alias
-class_alias('Cake\Http\Exception\NotAcceptableException', 'Cake\Network\Exception\NotAcceptableException');
-deprecationWarning('Use Cake\Http\Exception\NotAcceptableException instead of Cake\Network\Exception\NotAcceptableException.');
--- src/Network/Session.php
+++
@@ -1,4 +0,0 @@
-<?php
-// @deprecated Backwards compatibility alias
-class_alias('Cake\Http\Session', 'Cake\Network\Session');
-deprecationWarning('Use Cake\Http\Session instead of Cake\Network\Session.');
