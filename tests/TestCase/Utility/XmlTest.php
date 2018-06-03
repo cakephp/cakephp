cc2cef610294fd8cf2d72f99b014156cf6452ac2
Fernando Herrero <ininflamable@gmail.com>
2018-06-03 02:19:21 +0200

    Revert "Fixing style errors."
    This reverts commit 6572d67.

--- tests/TestCase/Utility/XmlTest.php
+++ tests/TestCase/Utility/XmlTest.php
@@ -270,14 +270,12 @@ close to 5 million globally.
         $this->assertEquals($paragraph, (string)$xml->body->p);
         $this->assertTrue(isset($xml->body->blockquote), 'Blockquote present');
         $this->assertEquals($blockquote, (string)$xml->body->blockquote);
-        $this->assertEquals($html, '<!DOCTYPE html>' . "\n" . $xml->asXML());
 
         $xml = Xml::loadHtml($html, ['parseHuge' => true]);
         $this->assertTrue(isset($xml->body->p), 'Paragraph present');
         $this->assertEquals($paragraph, (string)$xml->body->p);
         $this->assertTrue(isset($xml->body->blockquote), 'Blockquote present');
         $this->assertEquals($blockquote, (string)$xml->body->blockquote);
-        $this->assertEquals($html, '<!DOCTYPE html>' . "\n" . $xml->asXML());
     }
 
     /**
