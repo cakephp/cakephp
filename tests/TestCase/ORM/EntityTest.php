6e70be93a0179f3205837a2c2e4202e387ce574f
Joshua Lückers <joshualuckers@me.com>
2018-07-06 10:23:19 +0200

    Unit Test for the hasErrors() method.

--- tests/TestCase/ORM/EntityTest.php
+++ tests/TestCase/ORM/EntityTest.php
@@ -1282,6 +1282,42 @@ class EntityTest extends TestCase
     }
 
     /**
+     * Tests that check if hasErrors() works
+     *
+     * @return void
+     */
+    public function testHasErrors()
+    {
+        $entity = new Entity();
+        $hasErrors = $entity->hasErrors();
+        $this->assertFalse($hasErrors);
+
+        $nestedEntity = new Entity();
+        $entity->set([
+            'nested' => $nestedEntity,
+        ]);
+        $hasErrors = $entity->hasErrors();
+        $this->assertFalse($hasErrors);
+
+        $nestedEntity->setError('description', 'oops');
+        $hasErrors = $entity->hasErrors();
+        $this->assertTrue($hasErrors);
+
+        $hasErrors = $entity->hasErrors(false);
+        $this->assertFalse($hasErrors);
+
+        $entity->clean();
+        $hasErrors = $entity->hasErrors();
+        $this->assertTrue($hasErrors);
+        $hasErrors = $entity->hasErrors(false);
+        $this->assertFalse($hasErrors);
+
+        $nestedEntity->clean();
+        $hasErrors = $entity->hasErrors();
+        $this->assertFalse($hasErrors);
+    }
+
+    /**
      * Test that errors can be read with a path.
      *
      * @return void
