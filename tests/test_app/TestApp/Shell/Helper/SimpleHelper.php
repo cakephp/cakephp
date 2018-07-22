0e57510276cd66623ea794104b6365212d52a1d8
Florian Krämer <florian.kraemer@kreative-design.net>
2018-07-23 01:27:37 +0200

    Fixing tests

--- tests/test_app/TestApp/Shell/Helper/SimpleHelper.php
+++ tests/test_app/TestApp/Shell/Helper/SimpleHelper.php
@@ -5,7 +5,7 @@ use Cake\Console\Helper;
 
 class SimpleHelper extends Helper
 {
-    public function output(array $args)
+    public function output(array $args): void
     {
         $this->_io->out('It works!' . implode(' ', $args));
     }
