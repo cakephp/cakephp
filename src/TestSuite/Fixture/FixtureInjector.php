adac03b6ec0c37955b56c95e86b2e7acb3af5070
Mark Story <mark@mark-story.com>
2018-08-05 22:30:12 -0400

    Add typehints to TestSuite/Fixture & Stub.

--- src/TestSuite/Fixture/FixtureInjector.php
+++ src/TestSuite/Fixture/FixtureInjector.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -60,7 +61,7 @@ class FixtureInjector extends BaseTestListener
      * @param \PHPUnit\Framework\TestSuite $suite The test suite
      * @return void
      */
-    public function startTestSuite(TestSuite $suite)
+    public function startTestSuite(TestSuite $suite): void
     {
         if (empty($this->_first)) {
             $this->_first = $suite;
@@ -74,7 +75,7 @@ class FixtureInjector extends BaseTestListener
      * @param \PHPUnit\Framework\TestSuite $suite The test suite
      * @return void
      */
-    public function endTestSuite(TestSuite $suite)
+    public function endTestSuite(TestSuite $suite): void
     {
         if ($this->_first === $suite) {
             $this->_fixtureManager->shutDown();
@@ -87,7 +88,7 @@ class FixtureInjector extends BaseTestListener
      * @param \PHPUnit\Framework\Test $test The test case
      * @return void
      */
-    public function startTest(Test $test)
+    public function startTest(Test $test): void
     {
         $test->fixtureManager = $this->_fixtureManager;
         if ($test instanceof TestCase) {
@@ -103,7 +104,7 @@ class FixtureInjector extends BaseTestListener
      * @param float $time current time
      * @return void
      */
-    public function endTest(Test $test, $time)
+    public function endTest(Test $test, $time): void
     {
         if ($test instanceof TestCase) {
             $this->_fixtureManager->unload($test);
