98c5776251d342f04fa2042bc4b6b34bc7eda9af
ADmad <admad.coder@gmail.com>
2018-08-09 11:45:36 +0530

    Use strict typing for date/time classes.

--- src/I18n/FrozenTime.php
+++ src/I18n/FrozenTime.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
@@ -158,7 +159,7 @@ class FrozenTime extends Chronos implements JsonSerializable
      * @param array $options Array of options.
      * @return string Relative time string.
      */
-    public function timeAgoInWords(array $options = [])
+    public function timeAgoInWords(array $options = []): string
     {
         return static::diffFormatter()->timeAgoInWords($this, $options);
     }
@@ -178,7 +179,7 @@ class FrozenTime extends Chronos implements JsonSerializable
      * @return array List of timezone identifiers
      * @since 2.2
      */
-    public static function listTimezones($filter = null, $country = null, $options = [])
+    public static function listTimezones($filter = null, $country = null, $options = []): array
     {
         if (is_bool($options)) {
             $options = [
