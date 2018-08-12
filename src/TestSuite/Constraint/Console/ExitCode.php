c2c813889b4a5c11130760f377423f374c6744a3
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2018-08-12 22:18:08 +0200

    Fixed another method signature

--- src/TestSuite/Constraint/Console/ExitCode.php
+++ src/TestSuite/Constraint/Console/ExitCode.php
@@ -45,7 +45,7 @@ class ExitCode extends Constraint
      * @param mixed $other Constraint check
      * @return bool
      */
-    public function matches($other)
+    public function matches($other): bool
     {
         return $other === $this->exitCode;
     }
