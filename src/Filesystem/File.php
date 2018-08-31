a93e9272318f9a01e97a7912d931b3ed034a488b
ADmad <admad.coder@gmail.com>
2018-08-31 17:23:45 +0530

    Avoid string manipulation.

--- src/Filesystem/File.php
+++ src/Filesystem/File.php
@@ -455,7 +455,7 @@ class File
     public function perms()
     {
         if ($this->exists()) {
-            return substr(sprintf('%o', fileperms($this->path)), -3);
+            return decoct(fileperms($this->path) & 0777);
         }
 
         return false;
