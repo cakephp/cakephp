824d8c6f260418a16b0b1cc0d5fdd11ef3db634c
itosho <www.itosho@gmail.com>
2018-09-02 14:19:01 +0900

    Fixed "Comparision" is a misspelling of "Comparison"

--- src/Validation/Validation.php
+++ src/Validation/Validation.php
@@ -1350,7 +1350,7 @@ class Validation
      * Validates the image width.
      *
      * @param array $file The uploaded file data from PHP.
-     * @param string $operator Comparision operator.
+     * @param string $operator Comparison operator.
      * @param int $width Min or max width.
      * @return bool
      */
@@ -1368,7 +1368,7 @@ class Validation
      * Validates the image width.
      *
      * @param array $file The uploaded file data from PHP.
-     * @param string $operator Comparision operator.
+     * @param string $operator Comparison operator.
      * @param int $height Min or max width.
      * @return bool
      */
