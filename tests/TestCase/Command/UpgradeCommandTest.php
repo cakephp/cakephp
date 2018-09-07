62401f5654d7a4ccf811a5f83de19da17ef0038e
ADmad <admad.coder@gmail.com>
2018-09-08 01:53:39 +0530

    Add upgrade command.
    Currently moves templates to new location and changes file extension.

---
+++ tests/TestCase/Command/UpgradeCommandTest.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         4.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Test\TestCase\Command;
+
+use Cake\Core\Configure;
+use Cake\TestSuite\ConsoleIntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\visitor\vfsStreamPrintVisitor;
+
+/**
+ * UpgradeCommand test.
+ */
+class UpgradeCommandTest extends TestCase
+{
+    use ConsoleIntegrationTestTrait;
+
+    /**
+     * setup method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->setAppNamespace();
+        $this->useCommandRunner(true);
+
+        $ds = [
+            'src' => [
+                'Template' => [
+                    'Pages' => [
+                        'home.ctp' => '',
+                    ]
+                ]
+            ],
+            'plugins' => [
+                'TestPlugin' => [
+                    'src' => [
+                        'Template' => [
+                            'Element' => [
+                                'foo.ctp' => '',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $this->fs = vfsStream::setup('root', 444, $ds);
+
+        $this->pluginPaths = Configure::read('App.paths.plugins');
+        Configure::write('App.paths.plugins', [$this->fs->url() . '/plugins'], true);
+    }
+
+    /**
+     * tearDown
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        Configure::read('App.paths.plugins', $this->pluginPaths);
+    }
+
+    /**
+     * testExecute
+     *
+     * @return void
+     */
+    public function testExecute()
+    {
+        $this->exec('upgrade --path ' . $this->fs->url());
+
+        $this->assertTrue($this->fs->hasChild('templates/Pages/home.php'));
+        $this->assertTrue($this->fs->hasChild('plugins/TestPlugin/templates/Element/foo.php'));
+    }
+}
