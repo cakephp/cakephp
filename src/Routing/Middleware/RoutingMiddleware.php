c9249afd1b2da3c58aa691a45322b63568c1f588
Ian den Hartog <iandenhartog@hotmail.com>
2018-09-13 21:48:15 +0200

    Allow instance of HttpApplicationInterface in RoutingMiddleware

--- src/Routing/Middleware/RoutingMiddleware.php
+++ src/Routing/Middleware/RoutingMiddleware.php
@@ -15,8 +15,8 @@
 namespace Cake\Routing\Middleware;
 
 use Cake\Cache\Cache;
+use Cake\Core\HttpApplicationInterface;
 use Cake\Core\PluginApplicationInterface;
-use Cake\Http\BaseApplication;
 use Cake\Http\MiddlewareQueue;
 use Cake\Http\Runner;
 use Cake\Routing\Exception\RedirectException;
@@ -39,7 +39,7 @@ class RoutingMiddleware
     /**
      * The application that will have its routing hook invoked.
      *
-     * @var \Cake\Http\BaseApplication
+     * @var \Cake\Core\HttpApplicationInterface
      */
     protected $app;
 
@@ -54,10 +54,10 @@ class RoutingMiddleware
     /**
      * Constructor
      *
-     * @param \Cake\Http\BaseApplication $app The application instance that routes are defined on.
+     * @param \Cake\Core\HttpApplicationInterface $app The application instance that routes are defined on.
      * @param string|null $cacheConfig The cache config name to use or null to disable routes cache
      */
-    public function __construct(BaseApplication $app = null, $cacheConfig = null)
+    public function __construct(HttpApplicationInterface $app = null, $cacheConfig = null)
     {
         $this->app = $app;
         $this->cacheConfig = $cacheConfig;
