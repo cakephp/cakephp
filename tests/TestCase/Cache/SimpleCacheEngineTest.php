61f77ff1c89d3fba1de8959c00844432903d9d3d
ADmad <admad.coder@gmail.com>
2018-09-15 01:26:02 +0530

    Fix CS error.

--- tests/TestCase/Cache/SimpleCacheEngineTest.php
+++ tests/TestCase/Cache/SimpleCacheEngineTest.php
@@ -16,7 +16,6 @@ declare(strict_types=1);
 
 namespace Cake\Test\TestCase\Cache;
 
-use Cake\Cache\CacheEngine;
 use Cake\Cache\Engine\FileEngine;
 use Cake\Cache\SimpleCacheEngine;
 use Cake\TestSuite\TestCase;
