68d0bf95ef944c90c4e766e3f8fc2a1d80558253
ADmad <ADmad@users.noreply.github.com>
2018-11-02 10:48:08 +0530

    Fix error reported by psalm.

--- src/Cache/Engine/WincacheEngine.php
+++ src/Cache/Engine/WincacheEngine.php
@@ -79,6 +79,7 @@ class WincacheEngine extends CacheEngine
      */
     public function get($key, $default = null)
     {
+        $success = null;
         $value = wincache_ucache_get($this->_key($key), $success);
         if ($success === false) {
             return $default;
