3dd784559c04c23ebaa95bea5b5d18dfeb89df5e
Yaser Naderi <42739735+yasnaderi@users.noreply.github.com>
2018-11-21 08:24:58 -0500

    Correct typo error

--- lib/Cake/Model/Datasource/DboSource.php
+++ lib/Cake/Model/Datasource/DboSource.php
@@ -370,9 +370,9 @@ class DboSource extends DataSource {
 		$isStringEnum = false;
 		if (strpos($column, "enum") === 0) {
 			$firstValue = null;
-			if (preg_match("/(enum\()(.*)(\))/",$column, $acceptingValues)) {
+			if (preg_match("/(enum\()(.*)(\))/i",$column, $acceptingValues)) {
 				$values = explode(",", $acceptingValues[2]);
-				$firstValue = $value[0];
+				$firstValue = $values[0];
 			}
 			if (is_string($firstValue)) {
 				$isStringEnum = true;
