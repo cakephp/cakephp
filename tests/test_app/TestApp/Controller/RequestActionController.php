0ec1bc996cfa2060169ff87c97286862ed3e7d22
Robert Pustułka <robert.pustulka@gmail.com>
2018-12-21 12:00:09 +0100

    CS fix

--- tests/test_app/TestApp/Controller/RequestActionController.php
+++ tests/test_app/TestApp/Controller/RequestActionController.php
@@ -188,7 +188,7 @@ class RequestActionController extends AppController
     public function uploaded_files()
     {
         $files = Hash::flatten($this->request->getUploadedFiles());
-        $names = collection($files)->map(function(UploadedFileInterface $file) {
+        $names = collection($files)->map(function (UploadedFileInterface $file) {
             return $file->getClientFilename();
         });
 
