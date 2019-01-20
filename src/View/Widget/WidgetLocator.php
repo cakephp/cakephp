e7a46b72cc889e9b4bf1c8ff0701799ac0fbf934
ADmad <admad.coder@gmail.com>
2019-01-20 21:11:47 +0530

    Add typehint.

--- src/View/Widget/WidgetLocator.php
+++ src/View/Widget/WidgetLocator.php
@@ -60,7 +60,7 @@ class WidgetLocator
      * @param \Cake\View\View $view The view instance to set as a widget.
      * @param array $widgets See add() method for more information.
      */
-    public function __construct(StringTemplate $templates, View $view, $widgets = [])
+    public function __construct(StringTemplate $templates, View $view, array $widgets = [])
     {
         $this->_templates = $templates;
         if (!empty($widgets)) {
