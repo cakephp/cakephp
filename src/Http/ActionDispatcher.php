d54e0a87187662f41c9bfff2ec9619bd0a05fdd6
ADmad <ADmad@users.noreply.github.com>
2019-01-25 07:19:06 +0530

    Update docblock.

--- src/Http/ActionDispatcher.php
+++ src/Http/ActionDispatcher.php
@@ -23,11 +23,7 @@ use Cake\Routing\Router;
 use LogicException;
 
 /**
- * This class provides compatibility with dispatcher filters
- * and interacting with the controller layers.
- *
- * Long term this should just be the controller dispatcher, but
- * for now it will do a bit more than that.
+ * Dispatch the request to a controller for generating a response.
  */
 class ActionDispatcher implements EventDispatcherInterface
 {
