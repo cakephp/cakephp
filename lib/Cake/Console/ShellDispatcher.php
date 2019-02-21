3f909e9a00170ff4926036c009717af9b53fbab4
bancer <val@dinnerbooking.com>
2019-02-21 17:18:15 +0100

    Define CONFIG constant

--- lib/Cake/Console/ShellDispatcher.php
+++ lib/Cake/Console/ShellDispatcher.php
@@ -140,6 +140,9 @@ class ShellDispatcher {
 			define('TMP', CAKE_CORE_INCLUDE_PATH . DS . 'Cake' . DS . 'Console' . DS . 'Templates' . DS . 'skel' . DS . 'tmp' . DS);
 		}
 
+		if (!defined('CONFIG')) {
+			define('CONFIG', ROOT . DS . APP_DIR . DS . 'Config' . DS);
+		}
 		// $boot is used by Cake/bootstrap.php file
 		$boot = file_exists(CONFIG . 'bootstrap.php');
 		require CORE_PATH . 'Cake' . DS . 'bootstrap.php';
