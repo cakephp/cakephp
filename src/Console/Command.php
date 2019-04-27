ced58b479cff986604e72753ea66518ec1fbc86c
Mark Story <mark@mark-story.com>
2019-04-26 21:36:58 -0400

    Remove PHP7 specific syntax

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -258,7 +258,7 @@ class Command
      * @param \Cake\Console\ConsoleIo $io The ConsoleIo instance to use for the executed command.
      * @return int|null The exit code or null for success of the command.
      */
-    public function executeCommand($command, array $args = [], ?ConsoleIo $io = null)
+    public function executeCommand($command, array $args = [], ConsoleIo $io = null)
     {
         if (is_string($command)) {
             if (!class_exists($command)) {
