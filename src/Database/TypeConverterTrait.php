7e0d8b25925909a2f0d9f923ab60d8afa2cf6274
ADmad <admad.coder@gmail.com>
2019-05-19 23:56:16 +0530

    Typehint using TypeInterface instead of BaseType.

--- src/Database/TypeConverterTrait.php
+++ src/Database/TypeConverterTrait.php
@@ -26,7 +26,7 @@ trait TypeConverterTrait
      * and return relevant internal statement type
      *
      * @param mixed $value The value to cast
-     * @param \Cake\Database\Type\BaseType|string $type The type name or type instance to use.
+     * @param \Cake\Database\TypeInterface|string $type The type name or type instance to use.
      * @return array list containing converted value and internal type
      */
     public function cast($value, $type): array
