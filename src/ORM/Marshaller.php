d77f0c49febe182d5105b7df07456e541a02b1d4
ADmad <admad.coder@gmail.com>
2019-05-19 17:51:54 +0530

    Update param type in docblock

--- src/ORM/Marshaller.php
+++ src/ORM/Marshaller.php
@@ -622,7 +622,7 @@ class Marshaller
      *   the accessible fields list in the entity will be used.
      * - accessibleFields: A list of fields to allow or deny in entity accessible fields.
      *
-     * @param \Cake\Datasource\EntityInterface[] $entities the entities that will get the
+     * @param \Cake\Datasource\EntityInterface[]|\Traversable<\Cake\Datasource\EntityInterface> $entities the entities that will get the
      *   data merged in
      * @param array $data list of arrays to be merged into the entities
      * @param array $options List of options.
