bbf1af0c80ddf4e5b059fefde4682920fad11f5c
ADmad <admad.coder@gmail.com>
2019-08-06 01:27:27 +0530

    Fix namespace

--- tests/TestCase/TestSuite/AssertHtmlTest.php
+++ tests/TestCase/TestSuite/AssertHtmlTest.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
  * @since         3.0.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
-namespace Cake\Test\TestSuite;
+namespace Cake\Test\TestCase\TestSuite;
 
 use Cake\TestSuite\TestCase;
 use PHPUnit\Framework\ExpectationFailedException;
