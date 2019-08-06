91f681b20aa778748d266b43419891436fd527e5
ADmad <admad.coder@gmail.com>
2019-08-06 19:19:52 +0530

    Update docblock of Connection::__construct().

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -120,7 +120,14 @@ class Connection implements ConnectionInterface
     /**
      * Constructor.
      *
-     * @param array $config configuration for connecting to database
+     * @param array $config Configuration array.
+     * ### Available options:
+     * - `driver` Sort name or FCQN for driver.
+     * - `log` Boolean indicating whether to use query logging.
+     * - `name` Connection name.
+     * - `cacheMetaData` Boolean indicating whether metadata (datasource schemas) should be cached.
+     *    If set to a string it will be used as the name of cache config to use.
+     * - `cacheKeyPrefix` Custom prefix to use when generation cache keys. Defaults to connection name.
      */
     public function __construct(array $config)
     {
