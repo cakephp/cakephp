1a0ee7ec04f336f33eb9b5d2dcb7be70387c5388
mscherer <mark.scherer@spryker.com>
2019-08-14 01:20:52 +0200

    Fix typo

--- src/Database/Type/BinaryType.php
+++ src/Database/Type/BinaryType.php
@@ -107,7 +107,7 @@ class BinaryType extends Type implements TypeInterface
     }
 
     /**
-     * Marshalls flat data into PHP objects.
+     * Marshals flat data into PHP objects.
      *
      * Most useful for converting request data into PHP objects
      * that make sense for the rest of the ORM/Database layers.
--- src/Database/TypeInterface.php
+++ src/Database/TypeInterface.php
@@ -49,7 +49,7 @@ interface TypeInterface
     public function toStatement($value, Driver $driver);
 
     /**
-     * Marshalls flat data into PHP objects.
+     * Marshals flat data into PHP objects.
      *
      * Most useful for converting request data into PHP objects,
      * that make sense for the rest of the ORM/Database layers.
