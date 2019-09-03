676c771c04706744dbf1782cc0652e246f61d751
ADmad <admad.coder@gmail.com>
2019-09-03 13:40:11 +0530

    Fix psalm errors for Routing exceptions

--- src/Routing/Exception/MissingControllerException.php
+++ src/Routing/Exception/MissingControllerException.php
@@ -29,7 +29,12 @@ class MissingControllerException extends Exception
     protected $_messageTemplate = 'Controller class %s could not be found.';
 
     /**
-     * @inheritDoc
+     * Constructor.
+     *
+     * @param string|array $message Either the string of the error message, or an array of attributes
+     *   that are made available in the view, and sprintf()'d into Exception::$_messageTemplate
+     * @param int|null $code The code of the error, is also the HTTP status code for the error. Defaults to 404.
+     * @param \Throwable|null $previous the previous exception.
      */
     public function __construct($message, ?int $code = 404, ?Throwable $previous = null)
     {
