7c56443dd19b261e7ba8765273090265cca64f22
Jason Horvath <jason.horvath@greaterdevelopment.com>
2019-09-10 20:40:15 -0400

    Unset CURLOPT_POSTFIELDS key if no length

--- src/Http/Client/Adapter/Curl.php
+++ src/Http/Client/Adapter/Curl.php
@@ -108,12 +108,14 @@ class Curl implements AdapterInterface
                 $out[CURLOPT_CUSTOMREQUEST] = $request->getMethod();
                 break;
         }
-
+        
         $body = $request->getBody();
-        if (!empty($body->getContents())) {
-            $body->rewind();
-            $out[CURLOPT_POSTFIELDS] = $body->getContents();
+        $body->rewind();
+        $out[CURLOPT_POSTFIELDS] = $body->getContents();
+        if ($out[CURLOPT_POSTFIELDS] === '') {
+            unset($out[CURLOPT_POSTFIELDS]);
         }
+        
         if (empty($options['ssl_cafile'])) {
             $options['ssl_cafile'] = CaBundle::getBundledCaBundlePath();
         }
