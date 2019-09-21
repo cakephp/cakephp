7542dc2ed815070958b0f6fafd8315b7a9d397a4
ADmad <admad.coder@gmail.com>
2019-09-22 02:00:41 +0530

    Cleanup CSRF protection token too in Router::reverseArray().
    Refs #13661.

--- tests/TestCase/Routing/RouterTest.php
+++ tests/TestCase/Routing/RouterTest.php
@@ -2792,7 +2792,8 @@ class RouterTest extends TestCase
             'bare' => 1,
             'return' => 1,
             'requested' => 1,
-            '_Token' => ['key' => 'sekret']
+            '_Token' => ['key' => 'sekret'],
+            '_csrfToken' => 'foo',
         ];
         $result = Router::reverse($params);
         $this->assertEquals('/posts/view/1', $result);
