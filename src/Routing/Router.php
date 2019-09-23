f6d652235325da5938f38783f0d26e2bd4c7b93c
ADmad <admad.coder@gmail.com>
2019-09-23 18:44:40 +0530

    Fix extra key being set in context.

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -231,14 +231,13 @@ class Router
         static::$_request = $request;
 
         $uri = $request->getUri();
-        $context = [
+        static::$_requestContext = [
             '_scheme' => $uri->getScheme(),
             '_host' => $uri->getHost(),
             '_port' => $uri->getPort(),
             '_base' => $request->getAttribute('base'),
-            '_params' => $request->getAttribute('params', []),
+            'params' => $request->getAttribute('params', []),
         ];
-        static::$_requestContext = $context;
     }
 
     /**
@@ -433,8 +432,8 @@ class Router
             'action' => 'index',
             '_ext' => null,
         ];
-        if (!empty($context['_params'])) {
-            $params = $context['_params'];
+        if (!empty($context['params'])) {
+            $params = $context['params'];
         }
 
         $frag = '';
