fce5c40366bde66bc53dcac988082cf453fce64d
ADmad <admad.coder@gmail.com>
2019-10-03 13:48:14 +0530

    Add HttpsEnforcerMiddleware.
    This will allow deprecating / removing the "requireSecure" feature of SecurityComponent.

---
+++ tests/TestCase/Http/Middleware/HttpsEnforcerMiddlewareTest.php
@@ -0,0 +1,94 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         4.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Test\TestCase\Http\Middleware;
+
+use Cake\Controller\Exception\SecurityException;
+use Cake\Http\Middleware\HttpsEnforcerMiddleware;
+use Cake\Http\Response;
+use Cake\Http\ServerRequestFactory;
+use Cake\TestSuite\TestCase;
+use TestApp\Http\TestRequestHandler;
+use Zend\Diactoros\Response\RedirectResponse;
+
+/**
+ * Test for HttpsEnforcerMiddleware
+ */
+class HttpsEnforcerMiddlewareTest extends TestCase
+{
+    public function testForRequestWithHttp()
+    {
+        $request = ServerRequestFactory::fromGlobals([
+            'REQUEST_URI' => '/',
+            'HTTPS' => '1',
+        ]);
+        $handler = new TestRequestHandler(function ($req) {
+            return new Response();
+        });
+
+        $middleware = new HttpsEnforcerMiddleware();
+
+        $result = $middleware->process($request, $handler);
+        $this->assertInstanceOf(Response::class, $result);
+    }
+
+    public function testRedirect()
+    {
+        $request = ServerRequestFactory::fromGlobals([
+            'REQUEST_URI' => '/foo',
+        ]);
+        $handler = new TestRequestHandler(function ($req) {
+            return new Response();
+        });
+
+        $middleware = new HttpsEnforcerMiddleware();
+
+        $result = $middleware->process($request, $handler);
+        $this->assertInstanceOf(RedirectResponse::class, $result);
+        $this->assertSame(301, $result->getStatusCode());
+        $this->assertEquals(['location' => ['https://localhost/foo']], $result->getHeaders());
+
+        $middleware = new HttpsEnforcerMiddleware([
+            'statusCode' => 302,
+            'headers' => ['X-Foo' => 'bar'],
+        ]);
+        $result = $middleware->process($request, $handler);
+        $this->assertInstanceOf(RedirectResponse::class, $result);
+        $this->assertSame(302, $result->getStatusCode());
+        $this->assertEquals(
+            [
+                'location' => ['https://localhost/foo'],
+                'X-Foo' => ['bar'],
+            ],
+            $result->getHeaders()
+        );
+    }
+
+    public function testSecurityException()
+    {
+        $this->expectException(SecurityException::class);
+
+        $request = ServerRequestFactory::fromGlobals([
+            'REQUEST_URI' => '/',
+        ]);
+        $handler = new TestRequestHandler(function ($req) {
+            return new Response();
+        });
+
+        $middleware = new HttpsEnforcerMiddleware(['redirect' => false]);
+        $middleware->process($request, $handler);
+    }
+}
