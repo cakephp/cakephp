8b18990970997ebe85d78d184e18fd4e3ba3414c
Corey Taylor <corey.taylor.fl@gmail.com>
2019-10-15 20:46:41 -0500

    Updated Valiation::time() to accept microseconds in the form HH:MM:SS.FFFFFF

--- src/Validation/Validation.php
+++ src/Validation/Validation.php
@@ -584,8 +584,10 @@ class Validation
 
     /**
      * Time validation, determines if the string passed is a valid time.
-     * Validates time as 24hr (HH:MM) or am/pm ([H]H:MM[a|p]m)
-     * Does not allow/validate seconds.
+     * Validates time as 24hr (HH:MM[:SS][.FFFFFF]) or am/pm ([H]H:MM[a|p]m)
+     *
+     * Seconds and fractional seconds (microseconds) are allowed but optional
+     * in 24hr format.
      *
      * @param mixed $check a valid time string/object
      * @return bool Success
@@ -603,10 +605,9 @@ class Validation
             return false;
         }
 
-        return static::_check(
-            $check,
-            '%^((0?[1-9]|1[012])(:[0-5]\d){0,2} ?([AP]M|[ap]m))$|^([01]\d|2[0-3])(:[0-5]\d){0,2}$%'
-        );
+        $meridianClockRegex = '^((0?[1-9]|1[012])(:[0-5]\d){0,2} ?([AP]M|[ap]m))$';
+        $standardClockRegex = '^([01]\d|2[0-3])((:[0-5]\d){0,2}|(:[0-5]\d){2}\.\d{0,6})$';
+        return static::_check($check, '%' . $meridianClockRegex . '|' . $standardClockRegex . '%');
     }
 
     /**
@@ -1688,7 +1689,13 @@ class Validation
                 is_numeric($value['second']) &&
                 is_numeric($value['microsecond'])
             ) {
-                $formatted .= sprintf('%02d:%02d:%02d.%06d', $value['hour'], $value['minute'], $value['second'], $value['microsecond']);
+                $formatted .= sprintf(
+                    '%02d:%02d:%02d.%06d',
+                    $value['hour'],
+                    $value['minute'],
+                    $value['second'],
+                    $value['microsecond']
+                );
             }
         }
 
