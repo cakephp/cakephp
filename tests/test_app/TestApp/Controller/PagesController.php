396556c499ef22ad2adeb0e686c87c515cbb7da6
Corey Taylor <corey.taylor.fl@gmail.com>
2019-10-28 08:34:49 -0500

    Removed old links to 2.0 book

--- tests/test_app/TestApp/Controller/PagesController.php
+++ tests/test_app/TestApp/Controller/PagesController.php
@@ -26,9 +26,6 @@ use Cake\View\Exception\MissingTemplateException;
 /**
  * Static content controller
  *
- * Override this controller by placing a copy in controllers directory of an application
- *
- * @link https://book.cakephp.org/2.0/en/controllers/pages-controller.html
  */
 class PagesController extends AppController
 {
