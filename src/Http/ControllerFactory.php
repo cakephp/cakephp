9c72b1e02b9861874f394e169bdbb354555d3f8c
ADmad <admad.coder@gmail.com>
2019-11-02 20:11:12 +0530

    Add deprecated class stub

---
+++ src/Http/ControllerFactory.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+class_alias(
+    'Cake\Controller\ControllerFactory',
+    'Cake\Http\ControllerFactory',
+);
+deprecationWarning(
+    'Use Cake\Controller\ControllerFactory instead of Cake\Http\ControllerFactory.'
+);
