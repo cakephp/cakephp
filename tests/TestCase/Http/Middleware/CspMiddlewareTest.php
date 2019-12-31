52b9c2fa16e1efa40acaba888ad03fdfe6b3b20d
Tiborhajos <tiborhajos@gmail.com>
2019-12-31 19:01:56 +0100

    Update tests/TestCase/Http/Middleware/CspMiddlewareTest.php
    Co-Authored-By: Edgaras Janušauskas <edgaras.janusauskas@gmail.com>

--- tests/TestCase/Http/Middleware/CspMiddlewareTest.php
+++ tests/TestCase/Http/Middleware/CspMiddlewareTest.php
@@ -10,7 +10,7 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         4.0.0
+ * @since         3.9.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
