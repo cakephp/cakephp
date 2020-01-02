570dda610219cbb4d0ac1301cf2dde7c5f099d15
ADmad <admad.coder@gmail.com>
2020-01-03 01:21:40 +0530

    Fix docblocks and add typehints

--- src/Log/LogTrait.php
+++ src/Log/LogTrait.php
@@ -27,12 +27,12 @@ trait LogTrait
      * Convenience method to write a message to Log. See Log::write()
      * for more information on writing to logs.
      *
-     * @param mixed $msg Log message.
+     * @param string $msg Log message.
      * @param int|string $level Error level.
      * @param string|array $context Additional log data relevant to this message.
      * @return bool Success of log write.
      */
-    public function log($msg, $level = LogLevel::ERROR, $context = []): bool
+    public function log(string $msg, $level = LogLevel::ERROR, $context = []): bool
     {
         return Log::write($level, $msg, $context);
     }
