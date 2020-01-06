12e480593727dd4d0aee7a05c15415c3546295ed
Mark Story <mark@mark-story.com>
2020-01-06 14:39:00 -0500

    Don't map() when we don't need to.
    If query hydration is disabled we don't need to map + clean the results.
    
    Refs #14133

--- src/ORM/Association.php
+++ src/ORM/Association.php
@@ -1264,7 +1264,7 @@ abstract class Association
 
         $property = $options['propertyPath'];
         $propertyPath = explode('.', $property);
-        $query->formatResults(function ($results) use ($formatters, $property, $propertyPath) {
+        $query->formatResults(function ($results) use ($formatters, $property, $propertyPath, $query) {
             $extracted = [];
             foreach ($results as $result) {
                 foreach ($propertyPath as $propertyPathItem) {
@@ -1282,15 +1282,16 @@ abstract class Association
             }
 
             /** @var \Cake\Collection\CollectionInterface $results */
-            return $results
-                ->insert($property, $extracted)
-                ->map(function ($result) {
-                    if ($result instanceof EntityInterface) {
-                        $result->clean();
-                    }
+            $results = $results->insert($property, $extracted);
+            if ($query->isHydrationEnabled()) {
+                $results = $results->map(function ($result) {
+                    $result->clean();
 
                     return $result;
                 });
+            }
+
+            return $results;
         }, Query::PREPEND);
     }
 
