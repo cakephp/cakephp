3a7c7027e006e79c2cbc75fc5c4cc0983e424056
Corey Taylor <corey.taylor.fl@gmail.com>
2020-01-08 05:10:32 -0600

    Fixed contain() BelongsToMany association restricting fields with 'fields' option
    Backport changes from #14141 to 3.x

--- tests/TestCase/ORM/QueryRegressionTest.php
+++ tests/TestCase/ORM/QueryRegressionTest.php
@@ -154,12 +154,14 @@ class QueryRegressionTest extends TestCase
      */
     public function testEagerLoadingBelongsToManyList()
     {
-        $this->expectException(\InvalidArgumentException::class);
         $this->loadFixtures('Articles', 'Tags', 'ArticlesTags');
         $table = $this->getTableLocator()->get('Articles');
         $table->belongsToMany('Tags', [
             'finder' => 'list',
         ]);
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('"_joinData" is missing from the belongsToMany results');
         $table->find()->contain('Tags')->toArray();
     }
 
