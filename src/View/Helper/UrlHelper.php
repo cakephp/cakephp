192b89e0e921ad1ba5be2f2f53cb66b094546615
Marc Würth <ravage@bluewin.ch>
2020-01-16 22:09:46 +0100

    Fix Notice Error: Trying to access array offset on value of type null
    On PHP 7.4 when providing null for the $path

--- src/View/Helper/UrlHelper.php
+++ src/View/Helper/UrlHelper.php
@@ -174,7 +174,7 @@ class UrlHelper extends Helper
         if (!array_key_exists('plugin', $options) || $options['plugin'] !== false) {
             list($plugin, $path) = $this->_View->pluginSplit($path, false);
         }
-        if (!empty($options['pathPrefix']) && $path[0] !== '/') {
+        if (!empty($options['pathPrefix']) && ($path === null || $path[0] !== '/')) {
             $path = $options['pathPrefix'] . $path;
         }
         if (
