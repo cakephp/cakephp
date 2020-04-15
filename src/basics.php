0a94b9f540b2be58d20ac2076a071ae2e0d880ac
Marcos Dantas <marcos.adantas@hotmail.com>
2020-04-15 12:34:26 -0300

    Update basics.php
    This improve a lot when you simple die debug on api.

--- src/basics.php
+++ src/basics.php
@@ -142,7 +142,13 @@ if (!function_exists('dd')) {
             'line' => $trace[0]['line'],
             'file' => $trace[0]['file'],
         ];
-
+        
+        $origin = substr($_SERVER['HTTP_REFERER'], 0, (strlen($_SERVER['HTTP_REFERER']) - 1));
+        $headers = ['authorization', 'content-type', 'api-token'];
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Headers: '.implode($headers, ' '));
+        header('Access-Control-Allow-Origin:'.str_replace('\/', '', $origin));
+        
         Debugger::printVar($var, $location, $showHtml);
         die(1);
     }
