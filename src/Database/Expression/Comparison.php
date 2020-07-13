388c81d58e5c6a1705f2b2c4bad35ff8545c2834
Corey Taylor <corey.taylor.fl@gmail.com>
2020-07-13 10:05:09 -0500

    Ensure Comparison alias is created

--- src/Database/Expression/Comparison.php
+++ src/Database/Expression/Comparison.php
@@ -1,5 +1,4 @@
 <?php
 declare(strict_types=1);
 
-// @deprecated 4.1.0 Add backwards compatible alias.
-class_alias('Cake\Database\Expression\ComparisonExpression', 'Cake\Database\Expression\Comparison');
+class_exists('Cake\Database\Expression\ComparisonExpression');
