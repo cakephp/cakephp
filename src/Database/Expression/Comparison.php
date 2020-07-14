86ac4ef353cd01c83c1a2468eda492cf38b6be24
Corey Taylor <corey.taylor.fl@gmail.com>
2020-07-13 19:50:14 -0500

    Add deprecation warning to Comparison

--- src/Database/Expression/Comparison.php
+++ src/Database/Expression/Comparison.php
@@ -2,3 +2,5 @@
 declare(strict_types=1);
 
 class_exists('Cake\Database\Expression\ComparisonExpression');
+
+deprecationWarning('`Comparison` deprecated in 4.1.0, use `ComparisonExpression` instead.');
