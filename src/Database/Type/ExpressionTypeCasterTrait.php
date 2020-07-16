85e1e53b35f746152f44b54f7b5bdc3a95bcc760
Corey Taylor <corey.taylor.fl@gmail.com>
2020-07-16 14:40:36 -0500

    Updated psalm to 3.12.3

--- src/Database/Type/ExpressionTypeCasterTrait.php
+++ src/Database/Type/ExpressionTypeCasterTrait.php
@@ -49,7 +49,7 @@ trait ExpressionTypeCasterTrait
         $multi = $type !== $baseType;
 
         if ($multi) {
-            /** @psalm-suppress InvalidArgument */
+            /** @var \Cake\Database\Type\ExpressionTypeInterface $converter */
             return array_map([$converter, 'toExpression'], $value);
         }
 
