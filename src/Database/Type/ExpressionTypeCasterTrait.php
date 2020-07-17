a84ff95833f74f521803f84520e7c6ca6d3ed460
Corey Taylor <corey.taylor.fl@gmail.com>
2020-07-17 12:29:56 -0500

    Change @var to @psalm-var to avoid IDE issues

--- src/Database/Type/ExpressionTypeCasterTrait.php
+++ src/Database/Type/ExpressionTypeCasterTrait.php
@@ -49,7 +49,7 @@ trait ExpressionTypeCasterTrait
         $multi = $type !== $baseType;
 
         if ($multi) {
-            /** @var \Cake\Database\Type\ExpressionTypeInterface $converter */
+            /** @psalm-var \Cake\Database\Type\ExpressionTypeInterface $converter */
             return array_map([$converter, 'toExpression'], $value);
         }
 
