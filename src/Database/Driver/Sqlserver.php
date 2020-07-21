e3e34d9fba2fb902ccaf88876bfe42b774d6e0a8
Marcos Mariani <contact@mariani.dev>
2020-07-20 22:20:51 -0300

    Allow PDO::ATTR_CURSOR to be overridden

--- src/Database/Driver/Sqlserver.php
+++ src/Database/Driver/Sqlserver.php
@@ -193,7 +193,9 @@ class Sqlserver extends Driver
     public function prepare($query, array $options = []): StatementInterface
     {
         $this->connect();
-        $options = [PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL] + $options;
+        if (!isset($options[PDO::ATTR_CURSOR]) {
+            $options = [PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL] + $options;
+        }
         $isObject = $query instanceof Query;
         /** @psalm-suppress PossiblyInvalidMethodCall */
         if ($isObject && $query->isBufferedResultsEnabled() === false) {
