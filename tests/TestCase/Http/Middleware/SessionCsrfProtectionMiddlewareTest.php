a120ac6e0d83896b47e624986a9e0d0e8d96e6c6
ADmad <admad.coder@gmail.com>
2020-07-23 12:12:29 +0530

    Fix "@since" tag

--- tests/TestCase/Http/Middleware/SessionCsrfProtectionMiddlewareTest.php
+++ tests/TestCase/Http/Middleware/SessionCsrfProtectionMiddlewareTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         3.5.0
+ * @since         4.2.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Cake\Test\TestCase\Http\Middleware;
