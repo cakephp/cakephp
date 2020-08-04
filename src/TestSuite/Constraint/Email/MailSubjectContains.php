2917fe629f10c9c751309e640dd4a58868134e15
Mario Rothauer <office@rothauer-it.com>
2020-08-04 12:03:53 +0200

    changed since to 4.2.0

--- src/TestSuite/Constraint/Email/MailSubjectContains.php
+++ src/TestSuite/Constraint/Email/MailSubjectContains.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @link          https://cakephp.org CakePHP(tm) Project
- * @since         4.1.2
+ * @since         4.2.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace Cake\TestSuite\Constraint\Email;
