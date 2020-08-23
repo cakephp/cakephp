56c9bec302715b2e4aa6d32214fa74c85e6d1069
tanden <sumita.takaki@gmail.com>
2020-08-23 23:33:24 +0900

    [modify] only pass HH:MM form reject HH

--- tests/TestCase/Validation/ValidationTest.php
+++ tests/TestCase/Validation/ValidationTest.php
@@ -1650,6 +1650,11 @@ class ValidationTest extends TestCase
         $this->assertTrue(Validation::time('1:00pm'));
         $this->assertFalse(Validation::time('13:00pm'));
         $this->assertFalse(Validation::time('9:00'));
+        $this->assertFalse(Validation::time('1'));
+        $this->assertFalse(Validation::time('12'));
+        $this->assertFalse(Validation::time('12:0'));
+        $this->assertFalse(Validation::time('12:000'));
+        $this->assertFalse(Validation::time('24'));
     }
 
     /**
