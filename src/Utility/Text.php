c8dbf4638df119b26f4d30a1dcfca0427f701fdf
Anton Volokha <anton.v@seed4.me>
2020-10-04 16:44:31 +0300

    Use typecasting to solve issue

--- src/Utility/Text.php
+++ src/Utility/Text.php
@@ -236,7 +236,7 @@ class Text
         /** @var array<string, mixed> $dataReplacements */
         $dataReplacements = array_combine($hashKeys, array_values($data));
         foreach ($dataReplacements as $tmpHash => $tmpValue) {
-            $tmpValue = is_string($tmpValue) ? $tmpValue  : '';
+            $tmpValue = is_array($tmpValue) ? '' : (string) $tmpValue;
             $str = str_replace($tmpHash, $tmpValue, $str);
         }
 
