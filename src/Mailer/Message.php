fdfb3c03fdd7c1e65fbf77633138096990747a3b
othercorey <corey.taylor.fl@gmail.com>
2020-10-05 09:42:19 -0500

    Clarify rfc link explains Sender field

--- src/Mailer/Message.php
+++ src/Mailer/Message.php
@@ -347,7 +347,7 @@ class Message implements JsonSerializable, Serializable
     }
 
     /**
-     * Sets the "sender" address (acting "on behalf of")
+     * Sets the "sender" address. See rfc link below for full explanation.
      *
      * @param string|array $email String with email,
      *   Array with email as key, name as value or email as value (without name)
@@ -362,7 +362,7 @@ class Message implements JsonSerializable, Serializable
     }
 
     /**
-     * Gets the "sender" address (acting "on behalf of")
+     * Gets the "sender" address. See rfc link below for full explanation.
      *
      * @return array
      * @link https://tools.ietf.org/html/rfc2822.html#section-3.6.2
