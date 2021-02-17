1bab22cc29ef938c17e762f0a4e67152dbe9c988
Waldemar Bartikowski <waldemar.bartikowski@prowise.com>
2021-02-17 16:10:05 +0100

    Backport sprintf formatter fix (Issue #15288).
    See also https://github.com/cakephp/cakephp/pull/15289

--- src/I18n/Formatter/IcuFormatter.php
+++ src/I18n/Formatter/IcuFormatter.php
@@ -37,25 +37,6 @@ class IcuFormatter implements FormatterInterface
      */
     public function format($locale, $message, array $vars)
     {
-        unset($vars['_singular'], $vars['_count']);
-
-        return $this->_formatMessage($locale, $message, $vars);
-    }
-
-    /**
-     * Does the actual formatting using the MessageFormatter class
-     *
-     * @param string $locale The locale in which the message is presented.
-     * @param string|array $message The message to be translated
-     * @param array $vars The list of values to interpolate in the message
-     * @return string The formatted message
-     * @throws \Aura\Intl\Exception\CannotInstantiateFormatter if any error occurred
-     * while parsing the message
-     * @throws \Aura\Intl\Exception\CannotFormat If any error related to the passed
-     * variables is found
-     */
-    protected function _formatMessage($locale, $message, $vars)
-    {
         if ($message === '') {
             return $message;
         }
