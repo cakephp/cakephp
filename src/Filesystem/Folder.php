f9b065fea0b0995063a865d5bf51eaadd8c9023d
ndm2 <ndm2@users.noreply.github.com>
2021-05-02 15:52:07 +0200

    Fix `Text file busy` errors when deleting directories.
    Unsetting iterator references helps releasing possible locks.

--- src/Filesystem/Folder.php
+++ src/Filesystem/Folder.php
@@ -558,6 +558,8 @@ class Folder
             );
             $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::SELF_FIRST);
         } catch (Exception $e) {
+            unset($directory, $iterator);
+
             if ($type === null) {
                 return [[], []];
             }
@@ -573,12 +575,14 @@ class Folder
             if ($skipHidden) {
                 $subPathName = $fsIterator->getSubPathname();
                 if ($subPathName[0] === '.' || strpos($subPathName, DIRECTORY_SEPARATOR . '.') !== false) {
+                    unset($fsIterator);
                     continue;
                 }
             }
             /** @var \FilesystemIterator $item */
             $item = $fsIterator->current();
             if (!empty($exceptions) && isset($exceptions[$item->getFilename()])) {
+                unset($fsIterator, $item);
                 continue;
             }
 
@@ -587,7 +591,12 @@ class Folder
             } elseif ($item->isDir() && !$item->isDot()) {
                 $directories[] = $itemPath;
             }
+
+            unset($fsIterator, $item);
         }
+
+        unset($directory, $iterator);
+
         if ($type === null) {
             return [$directories, $files];
         }
@@ -707,6 +716,8 @@ class Folder
                 $directory = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::CURRENT_AS_SELF);
                 $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::CHILD_FIRST);
             } catch (Exception $e) {
+                unset($directory, $iterator);
+
                 return false;
             }
 
@@ -728,10 +739,14 @@ class Folder
                     } else {
                         $this->_errors[] = sprintf('%s NOT removed', $filePath);
 
+                        unset($directory, $iterator, $item);
+
                         return false;
                     }
                 }
             }
+
+            unset($directory, $iterator);
 
             $path = rtrim($path, DIRECTORY_SEPARATOR);
             // phpcs:disable
