e5c4f45c97c52b529e0248612c11c7d60dc7c5c7
Corey Taylor <corey.taylor.fl@gmail.com>
2021-07-02 02:54:02 -0500

    Remove deprecated ComparisonExpression alias

--- src/Database/Expression/Comparison.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-class_exists('Cake\Database\Expression\ComparisonExpression');
-
-deprecationWarning('`Comparison` deprecated in 4.1.0, use `ComparisonExpression` instead.');
