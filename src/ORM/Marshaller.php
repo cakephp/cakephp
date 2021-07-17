d6ecb8f77afe37a03c777148580f8c378f7b55eb
othercorey <corey.taylor.fl@gmail.com>
2021-07-16 23:15:42 -0500

    Fix line length

--- src/ORM/Marshaller.php
+++ src/ORM/Marshaller.php
@@ -262,9 +262,10 @@ class Marshaller
         }
 
         if ($validator === null) {
-            throw new RuntimeException(
-                sprintf('validate must be a boolean, a string or an object. Got %s.', get_debug_type($options['validate']))
-            );
+            throw new RuntimeException(sprintf(
+                'validate must be a boolean, a string or an object. Got %s.', 
+                get_debug_type($options['validate'])
+            ));
         }
 
         return $validator->validate($data, $isNew);
