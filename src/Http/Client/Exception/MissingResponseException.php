21ffbdf2f403c10b0bbb4b9edefbab2459d22739
Mark Story <mark@mark-story.com>
2021-08-14 21:31:50 -0400

    Raise error when no mocks are found
    When the HttpClient cannot find a mocked response it should raise an
    error. This makes it simpler to detect mistakes in mock setup code, and
    make tests more explicit.

---
+++ src/Http/Client/Exception/MissingResponseException.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         3.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cake\Http\Client\Exception;
+
+use RuntimeException;
+
+/**
+ * Used to indicate that a request did not have a matching mock response.
+ */
+class MissingResponseException extends RuntimeException
+{
+    /**
+     * Constructor
+     *
+     * @param string $method The HTTP method used.
+     * @param string $url The request URL.
+     */
+    public function __construct(string $method, string $url)
+    {
+        $message = "Unable to find a mocked response for {$method} to {$url}.";
+        parent::__construct($message);
+    }
+}
