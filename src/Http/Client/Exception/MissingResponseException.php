1e281442b45c7eb33937836b7ac78c4789a72ecd
Mark Story <mark@mark-story.com>
2021-08-15 10:36:15 -0400

    Update src/Http/Client/Exception/MissingResponseException.php
    Co-authored-by: ADmad <ADmad@users.noreply.github.com>

--- src/Http/Client/Exception/MissingResponseException.php
+++ src/Http/Client/Exception/MissingResponseException.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @link          https://cakephp.org CakePHP(tm) Project
- * @since         3.0.0
+ * @since         4.3.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace Cake\Http\Client\Exception;
