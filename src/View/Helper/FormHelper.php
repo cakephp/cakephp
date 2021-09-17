e9f9b2871f7ea878c575f53d6590d6d58048b657
Mark Scherer <dereuromark@users.noreply.github.com>
2021-09-17 19:43:48 +0200

    Update src/View/Helper/FormHelper.php
    Co-authored-by: ADmad <ADmad@users.noreply.github.com>

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -158,7 +158,7 @@ class FormHelper extends Helper
     /**
      * Default widgets
      *
-     * @var array<string, mixed>
+     * @var array<string, array<string>>
      */
     protected $_defaultWidgets = [
         'button' => ['Button'],
