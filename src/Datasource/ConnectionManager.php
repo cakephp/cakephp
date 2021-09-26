2e2a98c6e579cd3002f538226c5d1174e50a5f55
Mark Scherer <dereuromark@users.noreply.github.com>
2021-09-26 11:40:20 +0200

    Update src/Datasource/ConnectionManager.php
    Co-authored-by: Mark Story <mark@mark-story.com>

--- src/Datasource/ConnectionManager.php
+++ src/Datasource/ConnectionManager.php
@@ -73,7 +73,7 @@ class ConnectionManager
      * The connection will not be constructed until it is first used.
      *
      * @param array|string $key The name of the connection config, or an array of multiple configs.
-     * @param array<string,mixed>|null $config An array of name => config data for adapter.
+     * @param array<string, mixed>|null $config An array of name => config data for adapter.
      * @return void
      * @throws \Cake\Core\Exception\CakeException When trying to modify an existing config.
      * @see \Cake\Core\StaticConfigTrait::config()
