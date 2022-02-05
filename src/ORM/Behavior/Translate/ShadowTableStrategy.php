4b2cad52d166554755048ff1d9f5eec910e470b6
mscherer <mark.scherer@spryker.com>
2022-02-05 01:04:47 +0100

    Fix PHPStan found issues around mixed and object usage.

--- src/ORM/Behavior/Translate/ShadowTableStrategy.php
+++ src/ORM/Behavior/Translate/ShadowTableStrategy.php
@@ -567,6 +567,7 @@ class ShadowTableStrategy implements TranslateStrategyInterface
      */
     protected function bundleTranslatedFields(EntityInterface $entity): void
     {
+        /** @var array<string, \Cake\Datasource\EntityInterface> $translations */
         $translations = (array)$entity->get('_translations');
 
         if (empty($translations) && !$entity->isDirty('_translations')) {
