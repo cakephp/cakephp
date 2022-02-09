5830f07b899ab17609d46eca88f8724a6e8ea2c6
Val Bancer <valerij.bancer@gmail.com>
2022-02-09 17:05:30 +0100

    Fix memory leak in TranslatorRegistry

--- src/I18n/TranslatorRegistry.php
+++ src/I18n/TranslatorRegistry.php
@@ -145,6 +145,7 @@ class TranslatorRegistry extends TranslatorLocator
 
         $key = "translations.$name.$locale";
         $translator = $this->_cacher->read($key);
+        gc_collect_cycles();
         if (!$translator || !$translator->getPackage()) {
             $translator = $this->_getTranslator($name, $locale);
             $this->_cacher->write($key, $translator);
