eacec3404a9dada6bfda13e4caf1a8c1d1d09e00
Mark Story <mark@mark-story.com>
2022-02-16 21:42:50 -0500

    Improve doc block

--- src/Controller/Component/CheckHttpCacheComponent.php
+++ src/Controller/Component/CheckHttpCacheComponent.php
@@ -23,9 +23,9 @@ use Cake\Event\EventInterface;
  * Use HTTP caching headers to see if rendering can be skipped.
  *
  * Checks if the response can be considered different according to the request
- * headers, and the caching response headers. If it was not modified, then the
- * controller and view render process is skipped. And the client will get a
- * empty response with a "304 Not Modified" header.
+ * headers, and caching headers in the response. If the response was not modified,
+ * then the controller and view render process is skipped. And the client will get a
+ * response with an empty body and a "304 Not Modified" header.
  *
  * To use this component your controller actions must set either the `Last-Modified`
  * or `Etag` header. Without one of these headers being set this component
