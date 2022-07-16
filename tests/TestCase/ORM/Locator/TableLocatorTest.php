17f6e0d18c8cdf637289c335893bfa529f47ae58
ADmad <admad.coder@gmail.com>
2022-07-16 19:10:30 +0530

    Ensure TableLocator returns the same instance when using a table's registry alias.
    Refs #16419

--- tests/TestCase/ORM/Locator/TableLocatorTest.php
+++ tests/TestCase/ORM/Locator/TableLocatorTest.php
@@ -178,6 +178,10 @@ class TableLocatorTest extends TestCase
 
         $result = $this->_locator->get(ArticlesTable::class);
         $this->assertSame('Articles', $result->getAlias());
+        $this->assertSame(ArticlesTable::class, $result->getRegistryAlias());
+
+        $result2 = $this->_locator->get($result->getRegistryAlias());
+        $this->assertSame($result, $result2);
     }
 
     /**
