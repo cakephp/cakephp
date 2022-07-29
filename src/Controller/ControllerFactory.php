0c32784d6d6ed44847c8437102488cd39d8afe63
Corey Taylor <corey.taylor.fl@gmail.com>
2022-07-29 17:18:02 -0500

    Fix string to int action parameter coercion failing for '0'

--- src/Controller/ControllerFactory.php
+++ src/Controller/ControllerFactory.php
@@ -268,7 +268,7 @@ class ControllerFactory implements ControllerFactoryInterface, RequestHandlerInt
             case 'float':
                 return is_numeric($argument) ? (float)$argument : null;
             case 'int':
-                return filter_var($argument, FILTER_VALIDATE_INT) ? (int)$argument : null;
+                return filter_var($argument, FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
             case 'bool':
                 return $argument === '0' ? false : ($argument === '1' ? true : null);
             case 'array':
