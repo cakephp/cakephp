e931296bd3ef2e0d3f7310cf8dc4d59262f34ce4
Corey Taylor <corey.taylor.fl@gmail.com>
2022-09-28 00:04:16 -0500

    Use source table connection when discovering target table

--- src/ORM/Association.php
+++ src/ORM/Association.php
@@ -398,7 +398,7 @@ abstract class Association
             $config = [];
             $exists = $tableLocator->exists($registryAlias);
             if (!$exists) {
-                $config = ['className' => $this->_className];
+                $config = ['className' => $this->_className, 'connection' => $this->_sourceTable->getConnection()];
             }
             $this->_targetTable = $tableLocator->get($registryAlias, $config);
 
