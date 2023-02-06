1267000f25a8e485d52ac4d3e09989b9b4ecd22c
Mark Story <mark@mark-story.com>
2023-02-05 21:49:26 -0500

    Add deprecation for NumberHelper engine option.

--- src/View/Helper/NumberHelper.php
+++ src/View/Helper/NumberHelper.php
@@ -71,6 +71,9 @@ class NumberHelper extends Helper
         if ($engineClass === null) {
             throw new CakeException(sprintf('Class for %s could not be found', $config['engine']));
         }
+        if ($engineClass !== Number::class) {
+            deprecationWarning('4.5.0 - The `engine` option for NumberHelper will be removed in 5.0');
+        }
 
         $this->_engine = new $engineClass($config);
     }
