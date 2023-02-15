5f34c8e54dff9e09b81f1623edf0eb0b199b52e2
Nicos Panayides <nicosp@gmail.com>
2023-02-15 16:46:17 +0200

    phpcs fixes

--- tests/test_app/TestApp/Error/Exception/MyPDOStringException.php
+++ tests/test_app/TestApp/Error/Exception/MyPDOStringException.php
@@ -18,10 +18,10 @@ class MyPDOStringException extends PDOException
 
     /**
      * @inheritDoc
-    */
-    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+     */
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null)
     {
-       parent::__construct($message, $code, $previous);
-       $this->code = 'DB' . strval($code);
+        parent::__construct($message, $code, $previous);
+        $this->code = 'DB' . strval($code);
     }
 }
