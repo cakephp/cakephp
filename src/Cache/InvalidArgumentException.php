431d092a2f879e874079385bf1e4e32855f1087d
Brian French <fr3nch13@gmail.com>
2023-02-17 13:13:58 -0500

    Adding stub file.

---
+++ src/Cache/InvalidArgumentException.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @deprecated 4.4.12 Use {@link \Cake\Cache\Exception\InvalidArgumentException} instead.
+ */
+class_exists('Cake\Cache\Exception\InvalidArgumentException');
