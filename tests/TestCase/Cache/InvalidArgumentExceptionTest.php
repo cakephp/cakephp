7c9f6725cd66887f04daa3e773f70e1a99a81b62
Brian French <fr3nch13@gmail.com>
2023-02-23 15:29:47 -0500

    Added test for aliased Exception.
    Added this here to hopefully cover codecov's coverage report.
    I'm not sure if this is the cake way, but I based it off of how Console\Command is tested.

---
+++ tests/TestCase/Cache/InvalidArgumentExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         4.4.12
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ * @deprecated Backwards compatibility test for alias. Will be removed in 5.0
+ */
+namespace Cake\Test\TestCase\Cache;
+
+use Cake\Cache\InvalidArgumentException;
+use Cake\TestSuite\TestCase;
+
+/**
+ * InvalidArgumentExceptionTest class
+ */
+class InvalidArgumentExceptionTest extends TestCase
+{
+    /**
+     * tests That the old/deprecated class is properly aliased to the new one.
+     */
+    public function testThrowingOldException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        throw new InvalidArgumentException('I should be the old one, aliased.');
+    }
+}
