d705554207b1c20f9a0b6af253bd23e9286589f3
ADmad <admad.coder@gmail.com>
2023-04-11 22:24:05 +0530

    Remove unnecessary use of collection.

--- src/ORM/LazyEagerLoader.php
+++ src/ORM/LazyEagerLoader.php
@@ -16,12 +16,11 @@ declare(strict_types=1);
  */
 namespace Cake\ORM;
 
-use Cake\Collection\Collection;
-use Cake\Collection\CollectionInterface;
 use Cake\Database\Expression\QueryExpression;
 use Cake\Database\Expression\TupleComparison;
 use Cake\Datasource\EntityInterface;
 use Cake\ORM\Query\SelectQuery;
+use Cake\Utility\Hash;
 
 /**
  * Contains methods that are capable of injecting eagerly loaded associations into
@@ -53,7 +52,6 @@ class LazyEagerLoader
             $returnSingle = true;
         }
 
-        $entities = new Collection($entities);
         $query = $this->_getQuery($entities, $contain, $source);
         $associations = array_keys($query->getContain());
 
@@ -67,17 +65,17 @@ class LazyEagerLoader
      * Builds a query for loading the passed list of entity objects along with the
      * associations specified in $contain.
      *
-     * @param \Cake\Collection\CollectionInterface $objects The original entities
+     * @param array $entities The original entities
      * @param array $contain The associations to be loaded
      * @param \Cake\ORM\Table $source The table to use for fetching the top level entities
      * @return \Cake\ORM\Query\SelectQuery
      */
-    protected function _getQuery(CollectionInterface $objects, array $contain, Table $source): SelectQuery
+    protected function _getQuery(array $entities, array $contain, Table $source): SelectQuery
     {
         $primaryKey = $source->getPrimaryKey();
         $method = is_string($primaryKey) ? 'get' : 'extract';
 
-        $keys = $objects->map(fn (EntityInterface $entity) => $entity->{$method}($primaryKey));
+        $keys = Hash::map($entities, '{*}', fn (EntityInterface $entity) => $entity->{$method}($primaryKey));
 
         $query = $source
             ->find()
@@ -88,13 +86,13 @@ class LazyEagerLoader
                 }
 
                 if (is_string($primaryKey)) {
-                    return $exp->in($source->aliasField($primaryKey), $keys->toList());
+                    return $exp->in($source->aliasField($primaryKey), $keys);
                 }
 
                 $types = array_intersect_key($q->getDefaultTypes(), array_flip($primaryKey));
                 $primaryKey = array_map([$source, 'aliasField'], $primaryKey);
 
-                return new TupleComparison($primaryKey, $keys->toList(), $types, 'IN');
+                return new TupleComparison($primaryKey, $keys, $types, 'IN');
             })
             ->enableAutoFields()
             ->contain($contain);
@@ -133,28 +131,28 @@ class LazyEagerLoader
      * Injects the results of the eager loader query into the original list of
      * entities.
      *
-     * @param iterable<\Cake\Datasource\EntityInterface> $objects The original list of entities
-     * @param \Cake\ORM\Query\SelectQuery $results The loaded results
+     * @param array<\Cake\Datasource\EntityInterface> $entities The original list of entities
+     * @param \Cake\ORM\Query\SelectQuery $query The query to load results
      * @param array<string> $associations The top level associations that were loaded
      * @param \Cake\ORM\Table $source The table where the entities came from
      * @return array<\Cake\Datasource\EntityInterface>
      */
     protected function _injectResults(
-        iterable $objects,
-        SelectQuery $results,
+        array $entities,
+        SelectQuery $query,
         array $associations,
         Table $source
     ): array {
         $injected = [];
         $properties = $this->_getPropertyMap($source, $associations);
         $primaryKey = (array)$source->getPrimaryKey();
-        /** @var array<string, \Cake\Datasource\EntityInterface> $results */
-        $results = $results
+        /** @var array<\Cake\Datasource\EntityInterface> $results */
+        $results = $query
             ->all()
             ->indexBy(fn (EntityInterface $e) => implode(';', $e->extract($primaryKey)))
             ->toArray();
 
-        foreach ($objects as $k => $object) {
+        foreach ($entities as $k => $object) {
             $key = implode(';', $object->extract($primaryKey));
             if (!isset($results[$key])) {
                 $injected[$k] = $object;
