b58978b3433c3e1a8954a5d667896a8da2d93365
Mark Story <mark@mark-story.com>
2023-08-22 18:19:15 -0400

    Update framework provided property annotations (#17249)
    * Update framework provided property annotations
    
    Update the components we annotate in `Controller` to no longer include
    deprecated components and include the `CheckHttpCache` component which
    was omitted.
    
    Fixes #17247
    
    * Revert annotation removals.
    
    Because these comoponents still exist and still work fine we don't need
    to remove them.

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -91,6 +91,7 @@ use UnexpectedValueException;
  * @property \Cake\Controller\Component\RequestHandlerComponent $RequestHandler
  * @property \Cake\Controller\Component\SecurityComponent $Security
  * @property \Cake\Controller\Component\AuthComponent $Auth
+ * @property \Cake\Controller\Component\CheckHttpCacheComponent $CheckHttpCache
  * @link https://book.cakephp.org/4/en/controllers.html
  */
 #[\AllowDynamicProperties]
