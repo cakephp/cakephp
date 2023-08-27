33b20370cf96c9a2ddfa2b1a7cf84ad7b87da8f2
Mark Story <mark@mark-story.com>
2023-08-26 23:28:54 -0400

    4.5 - Add opt-in path for EntityTrait::has()
    Give applications a way to gradually opt-in to the breaking changes
    being made to EntityTrait::has() in 5.x. This should help smooth out
    upgrades for large applications a bit.

---
+++ tests/test_app/TestApp/Model/Entity/ForwardsCompatHas.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace TestApp\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Tests entity class used for asserting correct loading
+ */
+class ForwardsCompatHas extends Entity
+{
+    protected $_hasAllowsNull = true;
+}
