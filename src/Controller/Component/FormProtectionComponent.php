958824c548a35771d3a6a2caf96dd09fd9dec544
wowDAS Ing. Markus Ramšak <anreiz-kojen0k@icloud.com>
2023-09-08 14:34:45 +0200

    Update FormProtectionComponent.php

--- src/Controller/Component/FormProtectionComponent.php
+++ src/Controller/Component/FormProtectionComponent.php
@@ -77,7 +77,7 @@ class FormProtectionComponent extends Component
     {
         $session = $this->getController()->getRequest()->getSession();
         $session->start();
-        
+
         return $session->id();
     }
 
