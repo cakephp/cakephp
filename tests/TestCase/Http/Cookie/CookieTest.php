8c848aff54cdd7244e48c94be0189282d7e46e6d
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-09-23 11:35:34 +0200

    remove test which should have been removed by the merge

--- tests/TestCase/Http/Cookie/CookieTest.php
+++ tests/TestCase/Http/Cookie/CookieTest.php
@@ -490,15 +490,6 @@ class CookieTest extends TestCase
         $this->assertNull($cookie->getExpiry());
     }
 
-    public function testInvalidExpiresForDefaults(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Invalid type `array` for expire');
-
-        Cookie::setDefaults(['expires' => ['ompalompa']]);
-        new Cookie('cakephp', 'cakephp-rocks');
-    }
-
     public function testInvalidSameSiteForDefaults(): void
     {
         $this->expectException(ValueError::class);
