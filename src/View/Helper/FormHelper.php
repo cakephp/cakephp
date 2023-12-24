9eebaac8026b337f3e836f49cbb90f6a8106b8e7
mscherer <dereuromark@web.de>
2023-12-24 18:30:21 +0100

    Fix up enum name display for fallback.

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -1364,7 +1364,8 @@ class FormHelper extends Helper
         /** @var \BackedEnum $case */
         foreach ($enumClass::cases() as $case) {
             $hasLabel = $case instanceof EnumLabelInterface || method_exists($case, 'label');
-            $values[$case->value] = $hasLabel ? $case->label() : $case->name;
+            $values[$case->value] = $hasLabel ? $case->label()
+                : Inflector::humanize(mb_strtolower($case->name));
         }
 
         return $values;
