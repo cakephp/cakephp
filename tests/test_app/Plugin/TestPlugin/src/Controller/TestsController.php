4d9fa302728d208c3150f86e90460556a8eea683
Adam Halfar <adam.halfar@seznam.cz>
2024-08-05 11:46:03 +0200

    Fix coding standard

--- tests/test_app/Plugin/TestPlugin/src/Controller/TestsController.php
+++ tests/test_app/Plugin/TestPlugin/src/Controller/TestsController.php
@@ -20,6 +20,8 @@ declare(strict_types=1);
  */
 namespace TestPlugin\Controller;
 
+use Cake\Http\Response;
+
 class TestsController extends TestPluginAppController
 {
     public function index(): void
@@ -30,7 +32,7 @@ class TestsController extends TestPluginAppController
     /**
      * @return \Cake\Http\Response
      */
-    public function some_method(): \Cake\Http\Response
+    public function some_method(): Response
     {
         $this->response->body(25);
 
