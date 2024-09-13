35e6a98bd6b5af47a05d4bc418a3327aa4edf095
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-09-13 12:31:14 +0700

    [Rector] Start Add Rector to CI start with php5.3 set

---
+++ rector.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/contrib',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSkip([
+        __DIR__ . '/tests/test_app/templates',
+        __DIR__ . '/tests/test_app/Plugin/TestPlugin/templates',
+    ])
+    ->withPhpSets(php53: true);
