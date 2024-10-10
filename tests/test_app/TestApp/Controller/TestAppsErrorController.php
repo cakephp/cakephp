aee75edfd4338834a91a14784e17499f6ea39662
ADmad <admad.coder@gmail.com>
2024-10-10 17:14:24 +0530

    Allow customzing error response using methods of ErrorController.
    Closes #17958

--- tests/test_app/TestApp/Controller/TestAppsErrorController.php
+++ tests/test_app/TestApp/Controller/TestAppsErrorController.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 namespace TestApp\Controller;
 
 use Cake\Controller\ErrorController;
+use Cake\Http\Response;
 
 class TestAppsErrorController extends ErrorController
 {
+    protected function missingWidgetThing()
+    {
+        $this->viewBuilder()->setLayout('default');
+    }
+
+    protected function xml()
+    {
+        return new Response(['body' => '<xml>rendered xml exception</xml>']);
+    }
 }
