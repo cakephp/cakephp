f925d8701728ff61c47421af95727392d681860a
Mark Story <mark@mark-story.com>
2025-04-14 23:18:29 -0400

    Only automatically promote id columns to autoIncrement.

--- src/Database/Schema/SqlserverSchemaDialect.php
+++ src/Database/Schema/SqlserverSchemaDialect.php
@@ -596,7 +596,8 @@ class SqlserverSchemaDialect extends SchemaDialect
         $primaryKey = $schema->getPrimaryKey();
         if (
             in_array($data['type'], $autoIncrementTypes, true) &&
-            $primaryKey === [$name]
+            $primaryKey === [$name] &&
+            $name === 'id'
         ) {
             $data['autoIncrement'] = true;
         }
