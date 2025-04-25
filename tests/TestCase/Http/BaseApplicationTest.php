089770ecf614add151323988ee5da243ad85deca
mscherer <dereuromark@web.de>
2025-04-25 03:29:12 +0200

    ConsoleIo to interface.

--- tests/TestCase/Http/BaseApplicationTest.php
+++ tests/TestCase/Http/BaseApplicationTest.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 namespace Cake\Test\TestCase\Http;
 
 use Cake\Console\CommandRunner;
+use Cake\Console\ConsoleIo;
 use Cake\Console\TestSuite\StubConsoleOutput;
 use Cake\Core\BasePlugin;
 use Cake\Core\Configure;
