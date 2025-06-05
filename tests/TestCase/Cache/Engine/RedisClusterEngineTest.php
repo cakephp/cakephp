c980618ed0623c4566f2becf670a79a90ad73a8b
Nicos Panayides <nicosp@gmail.com>
2025-06-05 10:29:05 +0300

    Improve the tests

--- tests/TestCase/Cache/Engine/RedisClusterEngineTest.php
+++ tests/TestCase/Cache/Engine/RedisClusterEngineTest.php
@@ -47,72 +47,144 @@ class RedisClusterEngineTest extends TestCase
         unset($this->engine);
     }
 
-    public function testClearEmpty(): void
+    protected function mockCluster(array $data): RedisCluster
     {
-        $cluster = new class() extends RedisCluster {
-            private array $data = [
-                'node-1' => [
-                    'key_1',
-                ],
-                'node-2' => [
-                    'key_2',
-                ],
-                'node-3' => [
-                    'key_3',
-                ],
-            ];
-
-            public function __construct()
+        return new class ($data) extends RedisCluster {
+            public function __construct(private array $data)
             {
             }
 
+            /**
+             * @phpcs:disable CakePHP.NamingConventions.ValidFunctionName.PublicWithUnderscore
+             */
             public function _masters(): array
             {
+                // @phpcs:enable CakePHP.NamingConventions.ValidFunctionName.PublicWithUnderscore
+
                 return array_keys($this->data);
             }
 
+            public function get($key)
+            {
+                foreach ($this->data as $nodeData) {
+                    if (array_key_exists($key, $nodeData)) {
+                        return serialize($nodeData[$key]);
+                    }
+                }
+
+                return false;
+            }
+
+            /**
+             * Emulates scan and returns one key at a time, to ensure we properly handle
+             * the iteration.
+             */
             public function scan(&$iterator, $str_node, $pattern = null, $count = 0): bool|array
             {
                 if (!isset($this->data[$str_node])) {
                     return false;
                 }
 
-                $keys = $this->data[$str_node];
+                $keys = array_keys($this->data[$str_node]);
 
-                if ($iterator === null) {
-                    $iterator = 0;
+                if ($iterator === null || $iterator['node'] !== $str_node) {
+                    $iterator = [
+                        'node' => $str_node,
+                        'count' => 0,
+                    ];
                 } else {
-                    $iterator++;
+                    $iterator['count'] = $iterator['count'] + 1;
                 }
 
-                if (!isset($keys[$iterator])) {
+                $index = $iterator['count'];
+
+                if (!isset($keys[$index])) {
                     return false;
                 }
 
-                return [ $keys[$iterator] ];
+                return [ $keys[$index] ];
             }
 
             public function unlink($key, ...$other_keys): int
             {
                 $count = 0;
                 foreach ($this->data as &$nodeData) {
-                    $index = array_search($key, $nodeData, true);
-
-                    if (is_int($index)) {
+                    if (array_key_exists($key, $nodeData)) {
                         $count++;
-                        unset($nodeData[$index]);
+                        unset($nodeData[$key]);
                     }
                 }
 
                 return $count;
             }
+
+            public function del($key, ...$other_keys): int
+            {
+                return $this->unlink($key, ...$other_keys);
+            }
+
+            public function flushdb($key_or_address, $async = false)
+            {
+                $this->data = [];
+
+                return true;
+            }
         };
+    }
+
+    public function testClearEmpty(): void
+    {
+        $cluster = $this->mockCluster([
+            'node-1' => [
+                'cake_key_1' => 'value_1',
+            ],
+            'node-2' => [
+                'cake_key_2' => 'value_2',
+            ],
+            'node-3' => [
+                'cake_key_3' => 'value_3',
+            ],
+        ]);
 
         $this->engine->expects($this->atLeastOnce())
             ->method('connect')
             ->willReturn($cluster);
 
         $this->assertTrue($this->engine->init());
+        $this->assertSame('value_1', $this->engine->get('key_1'));
+
         $this->assertTrue($this->engine->clear());
+        $this->assertNull($this->engine->get('key_1'));
+        $this->assertNull($this->engine->get('key_2'));
+        $this->assertNull($this->engine->get('key_3'));
+    }
+
+    public function testClearEmptyFlushDb(): void
+    {
+        $cluster = $this->mockCluster([
+            'node-1' => [
+                'cake_key_1' => 'value_1',
+            ],
+            'node-2' => [
+                'cake_key_2' => 'value_2',
+            ],
+            'node-3' => [
+                'cake_key_3' => 'value_3',
+            ],
+        ]);
+
+        $this->engine->expects($this->atLeastOnce())
+            ->method('connect')
+            ->willReturn($cluster);
+
+        $this->assertTrue($this->engine->init());
+        $this->assertSame('value_1', $this->engine->get('key_1'));
+
+        // Test flushDB
+        $this->engine->setConfig('clearUsesFlushDb', true);
+        $this->assertTrue($this->engine->clear());
+        $this->assertNull($this->engine->get('key_1'));
+        $this->assertNull($this->engine->get('key_2'));
+        $this->assertNull($this->engine->get('key_3'));
     }
 }
