65a7e1bfc9e65f64138fbc0966a06851f0eff69c
mscherer <dereuromark@web.de>
2025-08-10 12:53:24 +0200

    Split out toQuarterRange() from TimeHelper::toQuarter()

--- src/I18n/DateTime.php
+++ src/I18n/DateTime.php
@@ -597,6 +597,38 @@ class DateTime extends Chronos implements JsonSerializable, Stringable
     }
 
     /**
+     * Returns the quarter range
+     *
+     * @return array<string> Array with start and end date of quarter in Y-m-d format
+     */
+    public function toQuarterRange(): array
+    {
+        $quarter = $this->quarter;
+        $year = $this->year;
+
+        switch ($quarter) {
+            case 1:
+                $start = $year . '-01-01';
+                $end = $year . '-03-31';
+                break;
+            case 2:
+                $start = $year . '-04-01';
+                $end = $year . '-06-30';
+                break;
+            case 3:
+                $start = $year . '-07-01';
+                $end = $year . '-09-30';
+                break;
+            case 4:
+                $start = $year . '-10-01';
+                $end = $year . '-12-31';
+                break;
+        }
+
+        return [$start, $end];
+    }
+
+    /**
      * @inheritDoc
      */
     public function __toString(): string
