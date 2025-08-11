52fc69916c0c404177f03a06ea5ee0f1da848789
Marcelo Rocha <contato@omarcelo.com.br>
2025-08-11 08:44:14 -0300

    Remove phpdoc causing error on IDE

--- src/Collection/Collection.php
+++ src/Collection/Collection.php
@@ -27,7 +27,6 @@ use IteratorIterator;
  * @template TKey
  * @template-covariant TValue
  * @template-implemements \Cake\Collection\CollectionInterface<TKey, TValue>
- * @extends IteratorIterator<TKey, TValue, \Traversable<TKey, TValue>>
  */
 class Collection extends IteratorIterator implements CollectionInterface
 {
