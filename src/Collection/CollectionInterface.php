3259b74db1796aabcd520254b81e02919f54c026
Marcelo Rocha <contato@omarcelo.com.br>
2025-08-16 15:31:46 -0300

    Fix phpstan issues

--- src/Collection/CollectionInterface.php
+++ src/Collection/CollectionInterface.php
@@ -812,7 +812,7 @@ interface CollectionInterface extends Iterator, JsonSerializable, Countable
      * Returns an numerically-indexed array representation of the results.
      * This is equivalent to calling `toArray(false)`
      *
-     * @return list<TValue>|array
+     * @return list<TValue>
      */
     public function toList(): array;
 
