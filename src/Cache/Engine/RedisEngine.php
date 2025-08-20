ee400ba32ea079d650bea72104d40389866c58c2
Nicos Panayides <nicosp@gmail.com>
2025-08-20 16:16:05 +0300

    Restore node as empty array because null is not accepted even if the documentation says otherwise.

--- src/Cache/Engine/RedisEngine.php
+++ src/Cache/Engine/RedisEngine.php
@@ -89,7 +89,7 @@ class RedisEngine extends CacheEngine
         'unix_socket' => false,
         'scanCount' => 10,
         'readTimeout' => 0,
-        'nodes' => null,
+        'nodes' => [],
         'failover' => null,
         'clearUsesFlushDb' => false,
     ];
