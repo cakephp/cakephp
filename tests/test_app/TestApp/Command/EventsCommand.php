881b03c9e4003cff02b96f651025102d6926f1be
Jamison Bryant <jamison@bryant.ai>
2025-09-09 13:48:14 -0400

    Extend Command instead of BaseCommand

--- tests/test_app/TestApp/Command/EventsCommand.php
+++ tests/test_app/TestApp/Command/EventsCommand.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 namespace TestApp\Command;
 
+use Cake\Command\Command;
 use Cake\Console\Arguments;
-use Cake\Console\BaseCommand;
 use Cake\Console\ConsoleIo;
 use Cake\Event\EventInterface;
 
-class EventsCommand extends BaseCommand
+class EventsCommand extends Command
 {
     public static function getDescription(): string
     {
