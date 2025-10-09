3e3eb60b62f88eaff635ec2407c0304ccabab55d
Christian <christianhowlen@gmail.com>
2025-10-09 12:48:45 +0200

    Use static getGroup() method for commands to allow custom CLI grouping

--- src/Console/Command/HelpCommand.php
+++ src/Console/Command/HelpCommand.php
@@ -103,8 +103,8 @@ class HelpCommand extends BaseCommand implements CommandCollectionAwareInterface
             $prefix = 'app';
             if ($namespace === 'Cake') {
                 $prefix = 'cakephp';
-            } elseif (defined("$class::GROUP")) {
-                $prefix = constant("$class::GROUP");
+            } elseif (method_exists($class, 'getGroup')) {
+                $prefix = $class::getGroup();
             } elseif (in_array($namespace, $plugins, true)) {
                 $prefix = Inflector::underscore($namespace);
             }
