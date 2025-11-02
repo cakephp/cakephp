4bc004cb65be352fb839433a238d46fbd41dd2af
Mark Scherer <dereuromark@users.noreply.github.com>
2025-11-03 00:04:33 +0100

    Deprecate unused options parameter in constructor
    Mark the options parameter as deprecated for removal in 6.0.

--- src/Http/Client/Auth/Digest.php
+++ src/Http/Client/Auth/Digest.php
@@ -87,6 +87,8 @@ class Digest
     /**
      * Constructor
      *
+     * Deprecated: $options list is unused and will be removed in 6.0.     
+     *
      * @param \Cake\Http\Client $client Http client object.
      * @param array|null $options Options list.
      */
