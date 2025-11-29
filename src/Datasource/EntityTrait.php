fd78d3cc7c63dc67a91e92f9de26b5459e3a0581
ADmad <admad.coder@gmail.com>
2025-11-29 20:04:27 +0530

    Fix post 5.next merge error

--- src/Datasource/EntityTrait.php
+++ src/Datasource/EntityTrait.php
@@ -1336,7 +1336,7 @@ trait EntityTrait
     public function setPatchable(array|string $field, bool $set): static
     {
         if ($field === '*') {
-            $this->patchable = array_map(fn() => $set, $this->_accessible);
+            $this->patchable = array_map(fn() => $set, $this->patchable);
             $this->patchable['*'] = $set;
 
             return $this;
