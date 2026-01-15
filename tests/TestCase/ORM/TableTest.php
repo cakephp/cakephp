9c233ae24100359d3fe4d6068425b63c6f73f4d5
Jasper Smet <josbeir@gmail.com>
2026-01-15 19:44:23 +0100

    fix: 14 notices in TableTest

--- tests/TestCase/ORM/TableTest.php
+++ tests/TestCase/ORM/TableTest.php
@@ -63,6 +63,7 @@ use Exception;
 use InvalidArgumentException;
 use Mockery;
 use PDOException;
+use PHPUnit\Framework\Attributes\AllowMockObjectsWithoutExpectations;
 use PHPUnit\Framework\Attributes\DataProvider;
 use RuntimeException;
 use TestApp\Model\Entity\Article;
@@ -78,6 +79,7 @@ use TestPlugin\Model\Table\CommentsTable;
 /**
  * Tests Table class
  */
+#[AllowMockObjectsWithoutExpectations]
 class TableTest extends TestCase
 {
     /**
