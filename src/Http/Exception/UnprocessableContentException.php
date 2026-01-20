effd44f194f7f83dd5f47da8f5663969d4c458b8
ADmad <admad.coder@gmail.com>
2026-01-20 18:50:13 +0530

    Fix property name

--- src/Http/Exception/UnprocessableContentException.php
+++ src/Http/Exception/UnprocessableContentException.php
@@ -24,7 +24,7 @@ class UnprocessableContentException extends HttpException
     /**
      * @inheritDoc
      */
-    protected int $_defaultCode = 422;
+    protected int $defaultCode = 422;
 
     /**
      * Constructor
