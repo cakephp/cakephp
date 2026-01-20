2ae55a1c38f9c8cf02704bf1f85dcbc28d70d8d2
Jasper Smet <josbeir@gmail.com>
2026-01-20 21:52:30 +0100

    chore: bump since keyword in docblock to 6.0.0

--- src/Attribute/Resolver.php
+++ src/Attribute/Resolver.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @link          https://cakephp.org CakePHP(tm) Project
- * @since         5.4.0
+ * @since         6.0.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace Cake\Attribute;
