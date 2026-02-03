41724dd942fefc8b7fe4952b3728af43b16be36f
Joachim Rey <joachim.rey@etu.univ-lyon1.fr>
2026-02-03 21:48:30 +0100

    remove spaces

--- src/I18n/Number.php
+++ src/I18n/Number.php
@@ -396,7 +396,7 @@ class Number
     {
         static::$_formatters[$locale][$type] = static::_setAttributes(
             new NumberFormatter($locale, $type),
-                                                                      $options,
+            $options,
         );
     }
 
