adbab641ee77ad846ee3c005825e459a0435f7df
Nicos Panayides <nicosp@gmail.com>
2026-03-04 12:26:52 +0200

    Replace another supression with a type hint

--- src/ORM/Marshaller.php
+++ src/ORM/Marshaller.php
@@ -524,8 +524,10 @@ class Marshaller
             $filter = [$primaryKey[0] . ' IN' => $ids];
         }
 
-        // @phpstan-ignore return.type
-        return $target->find()->where($filter)->toArray();
+        /** @var \Cake\ORM\Query\SelectQuery<\Cake\Datasource\EntityInterface> $query */
+        $query = $target->find()->where($filter);
+
+        return $query->toArray();
     }
 
     /**
