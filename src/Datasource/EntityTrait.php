1e2576b184a4cb59c1c8307304ff350da1f70894
ADmad <admad.coder@gmail.com>
2026-03-05 19:19:19 +0530

    Drop use of Entity class

--- src/Datasource/EntityTrait.php
+++ src/Datasource/EntityTrait.php
@@ -18,7 +18,6 @@ namespace Cake\Datasource;
 
 use Cake\Collection\Collection;
 use Cake\Datasource\Exception\MissingPropertyException;
-use Cake\ORM\Entity;
 use Cake\Utility\Hash;
 use Cake\Utility\Inflector;
 use Error;
@@ -816,7 +815,7 @@ trait EntityTrait
             return static::$accessors[$class][$type][$property] = '';
         }
 
-        if (static::class === Entity::class) {
+        if (static::class === self::class) {
             return '';
         }
 
