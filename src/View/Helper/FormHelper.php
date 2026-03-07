74f8ba0d2d21341efee930056503ac23dad81115
mscherer <dereuromark@web.de>
2026-03-07 18:28:19 +0100

    Add {{name}} template variable to inputContainer and error templates
    This allows using the HTML field name in custom templates for these elements,
    enabling use cases like creating field-specific error container IDs for AJAX.
    
    Fixes #18955

--- src/View/Helper/FormHelper.php
+++ src/View/Helper/FormHelper.php
@@ -816,6 +816,7 @@ class FormHelper extends Helper
         return $this->formatTemplate('error', [
             'content' => $error,
             'id' => $this->_domId($field) . '-error',
+            'name' => $this->_fieldName($field),
         ]);
     }
 
@@ -1188,6 +1189,7 @@ class FormHelper extends Helper
             'errorSuffix' => $errorSuffix,
             'label' => $label,
             'options' => $options,
+            'name' => $this->_fieldName($fieldName),
         ]);
 
         if ($newTemplates) {
@@ -1235,6 +1237,7 @@ class FormHelper extends Helper
             'content' => $options['content'],
             'error' => $options['error'],
             'label' => $options['label'] ?? '',
+            'name' => $options['name'] ?? '',
             'required' => $options['options']['required'] ? ' ' . $this->templater()->get('requiredClass') : '',
             'type' => $options['options']['type'],
             'containerClass' => $this->templater()->get('containerClass'),
@@ -2442,14 +2445,7 @@ class FormHelper extends Helper
         }
 
         if (!isset($options['name'])) {
-            $endsWithBrackets = '';
-            if (str_ends_with($field, '[]')) {
-                $field = substr($field, 0, -2);
-                $endsWithBrackets = '[]';
-            }
-            $parts = explode('.', $field);
-            $first = array_shift($parts);
-            $options['name'] = $first . ($parts !== [] ? '[' . implode('][', $parts) . ']' : '') . $endsWithBrackets;
+            $options['name'] = $this->_fieldName($field);
         }
 
         if (isset($options['value']) && !isset($options['val'])) {
@@ -2491,6 +2487,29 @@ class FormHelper extends Helper
         }
 
         return $options;
+    }
+
+    /**
+     * Generate the HTML name attribute value from a field name.
+     *
+     * Converts dot notation field names to bracket notation used in HTML forms.
+     * For example, "User.email" becomes "User[email]" and "User.address.city"
+     * becomes "User[address][city]".
+     *
+     * @param string $field Field name in dot notation.
+     * @return string HTML name attribute value.
+     */
+    protected function _fieldName(string $field): string
+    {
+        $endsWithBrackets = '';
+        if (str_ends_with($field, '[]')) {
+            $field = substr($field, 0, -2);
+            $endsWithBrackets = '[]';
+        }
+        $parts = explode('.', $field);
+        $first = array_shift($parts);
+
+        return $first . ($parts !== [] ? '[' . implode('][', $parts) . ']' : '') . $endsWithBrackets;
     }
 
     /**
