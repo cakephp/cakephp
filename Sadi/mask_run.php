01155c93d6cde90dcdfffe5423d1db6be2799f0d
Mallik Hassan <sadi.research@gmail.com>
2026-04-14 09:15:35 -0300

    Remove accidentally added personal files

--- Sadi/mask_run.php
+++
@@ -1,9 +0,0 @@
-<?php
-   require '/Users/mallikimtiaz.hassan/Desktop/Sadi/cakephp/vendor/autoload.php';
-                                                                 
-   use Cake\Utility\Text;                               
-                          
-   var_dump(Text::mask('データベースアクセス & ORM', 0, 5));
-   var_dump(Text::mask('データベースアクセス & ORM', -999, 5, '*'));
-   var_dump(Text::mask('4111111111111234', 0, 12, '*'));
-?>
