8e839689e5b4c0b77c11226121bb5d060adbf9a6
Mark Scherer <dereuromark@users.noreply.github.com>
2026-05-25 16:54:26 +0200

    Add green split package boundary rules

--- structarmed.php
+++ structarmed.php
@@ -5,7 +5,21 @@ use Boundwize\StructArmed\Architecture;
 use Boundwize\StructArmed\Rule\Rules\Layer\MayNotDependOnRule;
 
 return Architecture::define()
+    ->layerPattern('Collection', '/^Cake\\\\Collection\\\\.*$/')
+    ->layerPattern('Database', '/^Cake\\\\Database\\\\.*$/')
     ->layerPattern('Datasource', '/^Cake\\\\Datasource\\\\.*$/')
+    ->rule(
+        'collection.must_not_depend_on_database',
+        new MayNotDependOnRule(from: 'Collection', to: 'Database', toPath: 'Cake/Database')
+    )
+    ->rule(
+        'collection.must_not_depend_on_orm',
+        new MayNotDependOnRule(from: 'Collection', to: 'ORM', toPath: 'Cake/ORM')
+    )
+    ->rule(
+        'database.must_not_depend_on_orm',
+        new MayNotDependOnRule(from: 'Database', to: 'ORM', toPath: 'Cake/ORM')
+    )
     ->rule(
         'datasource.must_not_depend_on_orm',
         new MayNotDependOnRule(from: 'Datasource', to: 'ORM', toPath: 'Cake/ORM')
