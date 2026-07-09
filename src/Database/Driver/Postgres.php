9f1ad970a3b72293d4a37e694276645f804e819f
Mark Story <mark@mark-story.com>
2026-07-08 23:06:25 -0400

    Fix weakness in FunctionsBuilder::jsonValue() with postgres (#19530)
    The postgres driver needs to transforms jsonValue function calls to
    rename the function and apply postgres specific cast operations. If the
    current implementation is provided a user-controlled path expression,
    the generated query can be manipulated.
    
    Thanks to Himanshu Anand for reporting this

--- src/Database/Driver/Postgres.php
+++ src/Database/Driver/Postgres.php
@@ -326,9 +326,7 @@ class Postgres extends Driver
                 $expression->setName('JSONB_PATH_QUERY')
                     ->iterateParts(function ($p, $key) {
                         if ($key === 0) {
-                            $p = sprintf('%s::jsonb', $p);
-                        } elseif ($key === 1) {
-                            $p = sprintf("'%s'::jsonpath", $this->quoteIdentifier($p['value']));
+                            return sprintf('%s::jsonb', $p);
                         }
 
                         return $p;
