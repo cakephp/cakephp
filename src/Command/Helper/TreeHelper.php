a91db9f6fb2425422c7d14cb79336529a1775502
ADmad <admad.coder@gmail.com>
2026-09-22 15:27:35 +0530

    Don't hide phpcs warnings (#19632)

--- src/Command/Helper/TreeHelper.php
+++ src/Command/Helper/TreeHelper.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 use Cake\Console\Helper\TreeHelper;
 use function Cake\Core\deprecationWarning;
 
-deprecationWarning('5.4.0', 'Cake\Command\Helper\TreeHelper is deprecated. Use Cake\Console\Helper\TreeHelper instead.');
+deprecationWarning(
+    '5.4.0',
+    'Cake\Command\Helper\TreeHelper is deprecated. Use Cake\Console\Helper\TreeHelper instead.',
+);
 
 class_exists(TreeHelper::class);
